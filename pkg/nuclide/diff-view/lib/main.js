38b797a6f0d5481fb236f3f4d7a2db7fd8dc2b59
Mostafa Eweda <most@fb.com>
2015-05-26 22:00:48 -0700

    Diff Viewer [1/n]: new package

---
+++ pkg/nuclide/diff-view/lib/main.js
@@ -0,0 +1,17 @@
+'use babel';
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+/* flow */
+
+module.exports = {
+
+  activate(state: ?mixed): void {
+    // TODO(most): Add activation code here.
+  },
+
+};
