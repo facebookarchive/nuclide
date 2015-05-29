83e1cb5365785ac9f64b92a554267d72297774b2
Leo Huang <lifeihuang@fb.com>
2015-05-29 16:04:10 -0700

    Add comments for the client-server versioning

--- pkg/nuclide/version/lib/main.js
+++ pkg/nuclide/version/lib/main.js
@@ -15,6 +15,22 @@ var path = require('path');
 const TEST_VERSION = 'test-version';
 var version;
 
+/*
+ * This is the versioning of Nuclide client-server protocol.
+ * It is not a communication protocol per se. It is the sum of communication and
+ * services API.
+ *
+ * First, no commit shall break the protocol in that client and server
+ * from the same master shall always work with each other.
+ * That means, no client new feature shall be enabled before the dependent
+ * server serice is in place, while it is OK to add a new server service before
+ * the client is ready.
+ *
+ * Rule number two. Every commit that breaks the backward compatibility shall
+ * bump the version in version.json. This includes any client changes
+ * (new feature or whatever) that do not work with the older servers.
+ * It also includes server changes that break older clients.
+ */
 function getVersion(): string {
   if (!version) {
     try {
