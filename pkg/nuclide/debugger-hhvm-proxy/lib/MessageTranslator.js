87697b0972e7c218dd6aff981dd0aa9cf56c11a3
Peter Hallam <peterhal@fb.com>
2015-06-23 15:24:39 -0700

    Part7: Message Translater.js

---
+++ pkg/nuclide/debugger-hhvm-proxy/lib/MessageTranslator.js
@@ -0,0 +1,89 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+
+var {log} = require('./utils');
+var ChromeCallback = require('./ChromeCallback');
+var {DbgpSocket} = require('./DbgpSocket');
+var DebuggerHandler = require('./DebuggerHandler');
+var PageHandler = require('./PageHandler');
+var ConsoleHandler = require('./ConsoleHandler');
+var RuntimeHandler = require('./RuntimeHandler');
+
+/**
+ * Translates Chrome dev tools JSON messages to/from dbgp.
+ * TODO: Should we proactively push files to the debugger?
+ * Currently we reactively push files to the debuger when they appear in a stack trace.
+ */
+class MessageTranslator {
+  _socket: ?DbgpSocket;
+  _callback: ChromeCallback;
+  _debuggerHandler: DebuggerHandler;
+  _handlers: Map<string, Handler>;
+
+  constructor(socket: Socket, callback: (message: string) => void) {
+    var dbgpSocket = new DbgpSocket(socket);
+    this._socket = dbgpSocket;
+    this._callback = new ChromeCallback(callback);
+    this._handlers = new Map();
+    this._debuggerHandler = new DebuggerHandler(this._callback, dbgpSocket);
+    this._addHandler(this._debuggerHandler);
+    this._addHandler(new PageHandler(this._callback));
+    this._addHandler(new ConsoleHandler(this._callback));
+    this._addHandler(new RuntimeHandler(this._callback));
+  }
+
+  _addHandler(handler: Handler): void {
+    this._handlers.set(handler.getDomain(), handler);
+  }
+
+  onSessionEnd(callback: () => void): void {
+    log('onSessionEnd');
+    this._debuggerHandler.onSessionEnd(callback);
+  }
+
+  async handleCommand(command: string): Promise {
+    log('handleCommand: ' + command);
+    var {id, method, params} = JSON.parse(command);
+
+    if (!method || typeof method !== 'string') {
+      this._replyWithError(id, 'Missing method: ' + command);
+      return;
+    }
+    var methodParts = method.split('.');
+    if (methodParts.length !== 2) {
+      this._replyWithError(id, 'Badly formatted method: ' + command);
+      return;
+    }
+    var [domain, method] = methodParts;
+
+    if (!this._handlers.has(domain)) {
+      this._replyWithError(id, 'Unknown domain: ' + command);
+      return;
+    }
+
+    await this._handlers.get(domain).handleMethod(id, method, params);
+  }
+
+  _replyWithError(id: number, error: string): void {
+    log(error);
+    this._callback.replyWithError(id, error);
+  }
+
+  dispose(): void {
+    if (this._socket) {
+      this._socket.dispose();
+      this._socket = null;
+    }
+  }
+}
+
+module.exports = MessageTranslator;
---
+++ pkg/nuclide/debugger-hhvm-proxy/spec/MessageTranslator-spec.js
@@ -0,0 +1,45 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+
+var MessageTranslator = require('../lib/MessageTranslator');
+
+describe('debugger-hhvm-proxy DebuggerHandler', () => {
+    var callback;
+    var socket;
+    var translater;
+
+    beforeEach(() => {
+      callback = jasmine.createSpy('callback');
+      socket = jasmine.createSpyObj('socket', ['write', 'end', 'destroy', 'on']);
+      translater = new MessageTranslator(socket, callback);
+    });
+
+    it('handleCommand', () => {
+      waitsForPromise(async () => {
+        await translater.handleCommand('{"id": 1, "method": "Page.enable"}');
+        expect(callback).toHaveBeenCalledWith('{"id":1,"result":{}}');
+      });
+    });
+
+    it('handleCommand - bad domain', () => {
+      waitsForPromise(async () => {
+        await translater.handleCommand('{"id": 1, "method": "foo.enable"}');
+        expect(callback).toHaveBeenCalledWith('{"id":1,"result":{},"error":"Unknown domain: {\\\"id\\\": 1, \\\"method\\\": \\\"foo.enable\\\"}"}');
+      });
+    });
+
+    it('dispose', () => {
+      translater.dispose();
+      expect(socket.end).toHaveBeenCalled();
+      expect(socket.destroy).toHaveBeenCalled();
+    });
+});
