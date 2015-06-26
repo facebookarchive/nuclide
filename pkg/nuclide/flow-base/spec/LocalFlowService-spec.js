308c0792270c3bf89dc1d1861783006bd039cd62
Nat Mote <nmote@fb.com>
2015-06-26 15:22:12 -0700

    Add tests for Flow server creation and disposal

--- pkg/nuclide/flow-base/spec/LocalFlowService-spec.js
+++ pkg/nuclide/flow-base/spec/LocalFlowService-spec.js
@@ -9,7 +9,7 @@
  * the root directory of this source tree.
  */
 
-describe('LocalFlowService::getType', () => {
+describe('LocalFlowService', () => {
   var file = 'test.js';
   var currentContents = '/* @flow */\nvar x = "this_is_a_string"';
   var line = 2;
@@ -17,56 +17,129 @@ describe('LocalFlowService::getType', () => {
 
   var flowService;
 
-  function runWith(outputString) {
-    spyOn(flowService, '_execFlow').andReturn({stdout: outputString});
-    return flowService.getType(file, currentContents, line, column);
+  function newFlowService() {
+    var localFlowService = '../lib/LocalFlowService';
+    // we have to invalidate the require cache in order to mock modules we
+    // depend on
+    delete require.cache[require.resolve(localFlowService)];
+    return new (require(localFlowService))();
   }
 
   beforeEach(() => {
     spyOn(require('../lib/FlowHelpers'), 'getFlowExecOptions').andReturn({});
-    flowService = new (require('../lib/LocalFlowService'))();
+    flowService = newFlowService();
   });
 
   afterEach(() => {
+    flowService = null;
     unspy(require('../lib/FlowHelpers'), 'getFlowExecOptions');
   });
 
-  it('should return the type on success', () => {
-    waitsForPromise(async () => {
-      expect(await runWith('thisIsAType')).toBe('thisIsAType');
+  function mockExec(outputString) {
+    spyOn(flowService, '_execFlow').andReturn({stdout: outputString});
+  }
+
+  describe('flow server creation and teardown', () => {
+    var childSpy;
+
+    beforeEach(() => {
+      var called = false;
+      // we want asyncExecute to throw the first time, to mimic Flow not
+      // runinng. Then, it will spawn a new flow process, and we want that to be
+      // successful
+      spyOn(require('nuclide-commons'), 'asyncExecute').andCallFake(() => {
+        if (called) {
+          return {};
+        } else {
+          called = true;
+          throw {
+            stderr: 'There is no flow server running\n\'/path/to/flow/root\'',
+          };
+        }
+      });
+
+      childSpy = {
+        stdout: { on() {} },
+        stderr: { on() {} },
+        kill() {},
+      };
+      spyOn(childSpy, 'kill');
+      spyOn(require('nuclide-commons'), 'safeSpawn').andReturn(childSpy);
+      // we have to create another flow service here since we've mocked modules
+      // we depend on since the outer beforeEach ran.
+      flowService = newFlowService();
+      waitsForPromise(async () => {
+        await flowService._execFlow([], {});
+      });
+    });
+
+    afterEach(() => {
+      unspy(require('nuclide-commons'), 'asyncExecute');
+      unspy(require('nuclide-commons'), 'safeSpawn');
+    });
+
+    describe('_execFlow', () => {
+      it('should spawn a new Flow server', () => {
+        expect(require('nuclide-commons').safeSpawn).toHaveBeenCalledWith(
+          'flow',
+          ['server', '/path/to/flow/root']
+        );
+      });
+    });
+
+    describe('dispose', () => {
+      it('should kill flow servers', () => {
+        waitsForPromise(async () => {
+          flowService.dispose();
+          expect(childSpy.kill).toHaveBeenCalledWith('SIGKILL');
+        });
+      });
     });
   });
 
-  it('should return null if the type is unknown', () => {
-    waitsForPromise(async () => {
-      expect(await runWith('(unknown)')).toBe(null);
-    });
-  });
+  describe('getType', () => {
+    function runWith(outputString) {
+      mockExec(outputString);
+      return flowService.getType(file, currentContents, line, column);
+    }
 
-  it('should return null if the type is empty', () => {
-    waitsForPromise(async () => {
-      expect(await runWith('')).toBe(null);
+    it('should return the type on success', () => {
+      waitsForPromise(async () => {
+        expect(await runWith('thisIsAType')).toBe('thisIsAType');
+      });
     });
-  });
 
-  it('should return null on failure', () => {
-    waitsForPromise(async () => {
-      expect(await runWith('something\nFailure uh oh')).toBe(null);
+    it('should return null if the type is unknown', () => {
+      waitsForPromise(async () => {
+        expect(await runWith('(unknown)')).toBe(null);
+      });
     });
-  });
 
-  it('should return a type containing the string Failure', () => {
-    waitsForPromise(async () => {
-      expect(await runWith('Failure')).toBe('Failure');
+    it('should return null if the type is empty', () => {
+      waitsForPromise(async () => {
+        expect(await runWith('')).toBe(null);
+      });
     });
-  });
 
-  it('should return null if the flow process fails', () => {
-    waitsForPromise(async () => {
-      spyOn(flowService, '_execFlow').andThrow('error');
-      // this causes some errors to get logged, but I don't think it's a big
-      // deal and I don't know how to mock a module
-      expect(await flowService.getType(file, currentContents, line, column)).toBe(null);
+    it('should return null on failure', () => {
+      waitsForPromise(async () => {
+        expect(await runWith('something\nFailure uh oh')).toBe(null);
+      });
+    });
+
+    it('should return a type containing the string Failure', () => {
+      waitsForPromise(async () => {
+        expect(await runWith('Failure')).toBe('Failure');
+      });
+    });
+
+    it('should return null if the flow process fails', () => {
+      waitsForPromise(async () => {
+        spyOn(flowService, '_execFlow').andThrow('error');
+        // this causes some errors to get logged, but I don't think it's a big
+        // deal and I don't know how to mock a module
+        expect(await flowService.getType(file, currentContents, line, column)).toBe(null);
+      });
     });
   });
 });
