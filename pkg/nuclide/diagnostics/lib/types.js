efc860ce59f4c19dfc99b3efcd1eae3c9a2156ba
Nat Mote <nmote@fb.com>
2015-07-21 09:46:12 -0700

    Minor changes to diagnostic type declarations

--- pkg/nuclide/diagnostics/lib/types.js
+++ pkg/nuclide/diagnostics/lib/types.js
@@ -9,6 +9,8 @@
  * the root directory of this source tree.
  */
 
+import type {NuclideUri} from 'nuclide-remote-uri';
+
 export type InvalidationMessage = {
   scope: 'file';
   filePaths: Array<NuclideUri>;
@@ -21,11 +23,21 @@ export type InvalidationMessage = {
 export type MessageUpdateCallback = (update: DiagnosticProviderUpdate) => mixed;
 export type MessageInvalidationCallback = (message: InvalidationMessage) => mixed;
 
+// TODO figure out how to allow the diagnostic consumer to poll (for example, if
+// it was just activated and wants diagnostic messages without having to wait
+// for an event to occur)
 export type DiagnosticProvider = {
   onMessageUpdate: (callback: MessageUpdateCallback) => atom$Disposable;
   onMessageInvalidation: (callback: MessageInvalidationCallback) => atom$Disposable;
 };
 
+// Implicit invalidation semantics:
+//
+// - Previous 'file' scope messages are invalidated if and only if
+// filePathToMessages contains their key as a path.
+//
+// - All previous 'project' scope messages are invalidated whenever
+// projectMessages is populated.
 export type DiagnosticProviderUpdate = {
   filePathToMessages?: Map<NuclideUri, Array<FileDiagnosticMessage>>;
   projectMessages?: Array<ProjectDiagnosticMessage>;
@@ -44,7 +56,8 @@ export type Trace = {
 export type FileDiagnosticMessage = {
   scope: 'file';
   providerName: string;
-  type: string;
+  type: MessageType;
+  filePath: NuclideUri;
   text?: string;
   html?: string;
   range?: atom$Range;
@@ -54,7 +67,7 @@ export type FileDiagnosticMessage = {
 export type ProjectDiagnosticMessage = {
   scope: 'project';
   providerName: string;
-  type: string;
+  type: MessageType;
   text?: string;
   html?: string;
   range?: atom$Range;
