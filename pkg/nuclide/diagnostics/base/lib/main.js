b799b1a8c1c78933f09ed03b4211ac93b865d73b
Michael Bolin <mbolin@fb.com>
2015-07-22 12:52:47 -0700

    Add missing `./` in require() call.

--- pkg/nuclide/diagnostics/base/lib/main.js
+++ pkg/nuclide/diagnostics/base/lib/main.js
@@ -8,7 +8,7 @@
  * This source code is licensed under the license found in the LICENSE file in
  * the root directory of this source tree.
  */
-var DiagnosticStore = require('DiagnosticStore');
+var DiagnosticStore = require('./DiagnosticStore');
 
 module.exports = {
   DiagnosticStore,
