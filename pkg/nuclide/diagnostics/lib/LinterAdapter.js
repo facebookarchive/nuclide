1366154b6d2b68f4f3493d845b81bc54fe240c1f
Nat Mote <nmote@fb.com>
2015-07-22 10:52:26 -0700

    Beginnings of LinterAdapter

---
+++ pkg/nuclide/diagnostics/lib/LinterAdapter.js
@@ -0,0 +1,55 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+import type {
+  MessageUpdateCallback,
+  MessageInvalidationCallback,
+} from './types';
+
+import type {NuclideUri} from 'nuclide-remote-uri';
+
+type LinterMessage = {
+  type: 'Error' | 'Warning',
+  text: string,
+  filePath: NuclideUri,
+  range: atom$Range,
+};
+
+export type LinterProvider = {
+  // providerName is an extension to the current linter api
+  providerName?: string;
+  grammarScopes: Array<string>;
+  scope: 'file' | 'project';
+  lintOnFly: bool;
+  lint: (textEditor: TextEditor) => Promise<Array<LinterMessage>>;
+};
+
+var {Disposable} = require('atom');
+
+// TODO implement
+class LinterAdapter {
+  constructor(provider: LinterProvider) {
+  }
+
+  onMessageUpdate(callback: MessageUpdateCallback): atom$Disposable {
+    return new Disposable(() => {});
+  }
+
+  onMessageInvalidation(callback: MessageInvalidationCallback): atom$Disposable {
+    // no-op; we don't publish invalidations
+    return new Disposable(() => {});
+  }
+
+  dispose() {
+  }
+}
+
+module.exports = LinterAdapter;
--- pkg/nuclide/diagnostics/lib/main.js
+++ pkg/nuclide/diagnostics/lib/main.js
@@ -9,10 +9,57 @@
  * the root directory of this source tree.
  */
 
+import type {LinterProvider} from './LinterAdapter';
+import type {DiagnosticProvider} from './types';
+
+var {Disposable, CompositeDisposable} = require('atom');
+
+var disposables = null;
+
+function addDisposable(disposable: atom$Disposable) {
+  if (disposables) {
+    disposables.add(disposable);
+  } else {
+    var logger = require('nuclide-logging').getLogger();
+    logger.error('disposables is null');
+  }
+}
+
 module.exports = {
 
   activate(state: ?Object): void {
-    // TODO(jessicalin): Add activation code here.
+    if (!disposables) {
+      disposables = new CompositeDisposable();
+    }
   },
 
+  consumeLinterProvider(provider: LinterProvider): atom$Disposable {
+    var LinterAdapter = require('./LinterAdapter');
+    var adapter = new LinterAdapter(provider);
+    var diagnosticDisposable = this.consumeDiagnosticProvider(adapter);
+    var adapterDisposable = new Disposable(() => {
+      diagnosticDisposable.dispose();
+      adapter.dispose();
+    });
+    addDisposable(adapter);
+    return adapterDisposable;
+  },
+
+  consumeDiagnosticProvider(provider: DiagnosticProvider): atom$Disposable {
+    // TODO consume the provider
+    var disposable = new Disposable(() => {
+      // TODO deregister for any other events we subscribe to
+
+      // TODO remove the provider from anywhere we've stored it
+    });
+    addDisposable(disposable);
+    return disposable;
+  },
+
+  deactivate() {
+    if (disposables) {
+      disposables.dispose();
+      disposables = null;
+    }
+  }
 };
