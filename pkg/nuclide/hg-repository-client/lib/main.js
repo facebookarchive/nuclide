3f7be9d6e72ed0eae85cf36bcf59aeff56fcb18f
Michael Bolin <mbolin@fb.com>
2015-07-22 23:46:34 -0700

    Export HgRepositoryClient in a way that Flow understands.

--- pkg/nuclide/hg-repository-client/lib/main.js
+++ pkg/nuclide/hg-repository-client/lib/main.js
@@ -8,9 +8,8 @@
  * This source code is licensed under the license found in the LICENSE file in
  * the root directory of this source tree.
  */
+var HgRepositoryClient = require('./HgRepositoryClient');
 
 module.exports = {
-  get HgRepositoryClient() {
-    return require('./HgRepositoryClient');
-  },
+  HgRepositoryClient,
 };
