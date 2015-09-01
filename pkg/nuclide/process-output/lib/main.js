7b9ff0303055588aaac58da702bb4caf5f9dff1f
Jess Lin <jessicalin@fb.com>
2015-09-01 10:03:07 -0700

    Allow consumers of the process-output service to specify an outputHandler [11]
    Reviewed By: @bolinfest
    
    Differential Revision: D2399432

--- pkg/nuclide/process-output/lib/main.js
+++ pkg/nuclide/process-output/lib/main.js
@@ -11,11 +11,18 @@
 
 import type BufferedProcessError from 'nuclide-atom-helpers/ScriptBufferedProcessStore';
 import type {ScriptBufferedProcessStore} from 'nuclide-atom-helpers';
+import type ProcessOutputHandler from './types';
 
 import {CompositeDisposable} from 'atom';
 import invariant from 'assert';
 
 var NUCLIDE_PROCESS_OUTPUT_VIEW_URI = 'atom://nuclide/process-output/';
+var PROCESS_OUTPUT_HANDLER_KEY = 'nuclide-processOutputHandler';
+type CreateProcessOutputViewOptions = {
+  // Using the PROCESS_OUTPUT_HANDLER_KEY constant here results in a syntax error.
+  'nuclide-processOutputHandler': ?ProcessOutputHandler;
+};
+
 var subscriptions: ?CompositeDisposable;
 var processToDisposables: ?Map<ScriptBufferedProcessStore, CompositeDisposable>;
 var logger;
@@ -34,17 +41,21 @@ function getLogger() {
  *   - args
  *   - options
  * where each field corresponds to the arguments to Node's `spawn`.
+ * @param options The same as the `options` passed to the atom.workspace.open()
+ *   call that triggered this function. In this case, it should contain special
+ *   Nuclide arguments (see `runCommandInNewPane`).
  */
-function createProcessOutputView(uri: string): HTMLElement {
+function createProcessOutputView(uri: string, openOptions: CreateProcessOutputViewOptions): HTMLElement {
   var ProcessOutputWrapper = require('./ProcessOutputWrapper');
   var {ScriptBufferedProcessStore} = require('nuclide-atom-helpers');
 
   var commandInfo = uri.slice(NUCLIDE_PROCESS_OUTPUT_VIEW_URI.length);
   var {command, args, options} = JSON.parse(commandInfo);
+  var processOutputHandler = openOptions[PROCESS_OUTPUT_HANDLER_KEY];
 
   var processOutputStore = new ScriptBufferedProcessStore(command, args, options);
   var hostElement = new ProcessOutputWrapper();
-  hostElement.initialize(processOutputStore, {title: command});
+  hostElement.initialize(processOutputStore, {title: command, processOutputHandler});
 
   var processSubscriptions = new CompositeDisposable();
   invariant(processToDisposables);
@@ -69,16 +80,30 @@ function createProcessOutputView(uri: string): HTMLElement {
   return hostElement;
 }
 
+/**
+ * @param command Same as the `command` argument to Node's ChildProcess.spawn().
+ * @param args Same as the `args` argument to Node's ChildProcess.spawn().
+ * @param commandOptions Same as the `options` argument to Node's ChildProcess.spawn().
+ * @param processOutputHandler An optional ProcessOutputHandler that is appropriate
+ *   for the expected output. See the constructor of ProcessOutputView for more information.
+ */
 function runCommandInNewPane(
   command: string,
   args: Array<string>,
-  options?: Object = {}) {
-    var commandInfo = JSON.stringify({
-      command,
-      args,
-      options,
-    });
-    atom.workspace.open(NUCLIDE_PROCESS_OUTPUT_VIEW_URI + commandInfo);
+  options?: Object = {},
+  processOutputHandler?: ProcessOutputHandler,
+): Promise<atom$TextEditor> {
+  var commandInfo = JSON.stringify({
+    command,
+    args,
+    options,
+  });
+  var openOptions = {
+    [PROCESS_OUTPUT_HANDLER_KEY]: processOutputHandler,
+  };
+  // Not documented: the 'options' passed to atom.workspace.open() are passed to the opener.
+  // There's no other great way for a consumer of this service to specify a ProcessOutputHandler.
+  atom.workspace.open(NUCLIDE_PROCESS_OUTPUT_VIEW_URI + commandInfo, openOptions);
 }
 
 module.exports = {
@@ -86,9 +111,9 @@ module.exports = {
   activate(state: ?Object): void {
     if (!subscriptions) {
       subscriptions = new CompositeDisposable();
-      subscriptions.add(atom.workspace.addOpener(uri => {
+      subscriptions.add(atom.workspace.addOpener((uri, options) => {
         if (uri.startsWith(NUCLIDE_PROCESS_OUTPUT_VIEW_URI)) {
-          return createProcessOutputView(uri);
+          return createProcessOutputView(uri, options);
         }
       }));
       processToDisposables = new Map();
