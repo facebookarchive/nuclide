ac7a8adee9d0260a09bd4f672ebff14834261f5f
Jonas Gebhardt <jxg@fb.com>
2015-09-04 22:54:20 -0700

    Add quick-open-interfaces package
    Reviewed By: @bolinfest
    
    Differential Revision: D2413697

---
+++ pkg/nuclide/quick-open-interfaces/lib/types.js
@@ -0,0 +1,53 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+export type Store = {
+  toggleProvider(service: Provider): void;
+}
+
+export type ProviderType = 'DIRECTORY' | 'GLOBAL';
+
+export type Provider = {
+  executeQuery(query: string, directory?: atom$Directory): Promise<Array<FileResult>>;
+  getPromptText(): string;
+  getProviderType(): ProviderType;
+  getTabTitle(): string;
+  getName(): string;
+
+  getAction?: () => string;
+  getDebounceDelay?: () => number;
+  isEligibleForDirectory?: (directory: atom$Directory) => boolean;
+  getComponentForItem?: (item: FileResult) => ReactElement;
+};
+
+export type FileResult = {
+  path: string;
+  matchIndexes?: Array<number>;
+  score?: number;
+  // The original query that prompted this result, e.g. to highlight it in the UI.
+  query?: string;
+  context?: string;
+};
+
+export type ProviderResult = {
+  error: ?Object;
+  loading: boolean;
+  result: Array<FileResult>;
+};
+
+export type DirectoryName = string;
+export type ServiceName = string;
+
+export type GroupedResult = {
+  [key: DirectoryName]: {
+    [key: ServiceName]: ProviderResult
+  }
+};
