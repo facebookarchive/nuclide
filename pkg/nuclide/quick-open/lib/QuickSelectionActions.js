07728065f3eede32e1c3d1c6801b9da4e6925b79
Jonas Gebhardt <jxg@fb.com>
2015-09-04 17:07:39 -0700

    Add Dispatcher, Actions for quick-open fluxification
    Reviewed By: @ssorallen
    
    Differential Revision: D2263392

---
+++ pkg/nuclide/quick-open/lib/QuickSelectionActions.js
@@ -0,0 +1,33 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+var QuickSelectionDispatcher = require('./QuickSelectionDispatcher');
+var {ActionTypes} = QuickSelectionDispatcher;
+
+var QuickSelectionActions = {
+
+  query(query: string): void {
+    QuickSelectionDispatcher.getInstance().dispatch({
+      actionType: ActionTypes.QUERY,
+      query,
+    });
+  },
+
+  changeActiveProvider(providerName: string): void {
+    QuickSelectionDispatcher.getInstance().dispatch({
+      actionType: ActionTypes.ACTIVE_PROVIDER_CHANGED,
+      providerName,
+    });
+  },
+
+};
+
+module.exports = QuickSelectionActions;
---
+++ pkg/nuclide/quick-open/lib/QuickSelectionDispatcher.js
@@ -0,0 +1,31 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+var {Dispatcher} = require('flux');
+
+var quickopenDispatcher;
+
+class QuickSelectionDispatcher {
+
+  static getInstance() {
+    if (!quickopenDispatcher) {
+      quickopenDispatcher = new Dispatcher();
+    }
+    return quickopenDispatcher;
+  }
+}
+
+QuickSelectionDispatcher.ActionTypes = {
+  ACTIVE_PROVIDER_CHANGED: 'ACTIVE_PROVIDER_CHANGED',
+  QUERY: 'QUERY',
+};
+
+module.exports = QuickSelectionDispatcher;
