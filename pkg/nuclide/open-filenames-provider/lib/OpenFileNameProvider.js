1751c0d37a583be6fbb47e6b2d8f3fc83c70b76a
Jonas Gebhardt <jxg@fb.com>
2015-09-04 22:54:24 -0700

    Adjust OpenFileNames provider to adhere to quick-open-interfaces types
    Reviewed By: @bolinfest
    
    Differential Revision: D2413884

--- pkg/nuclide/open-filenames-provider/lib/OpenFileNameProvider.js
+++ pkg/nuclide/open-filenames-provider/lib/OpenFileNameProvider.js
@@ -9,42 +9,52 @@
  * the root directory of this source tree.
  */
 
-class OpenFileListProvider {
+import type {
+  FileResult,
+  Provider,
+  ProviderType,
+} from 'nuclide-quick-open-interfaces';
 
-  // Returns the currently opened tabs, ordered from most recently opened to least recently opened.
-  static getOpenTabsMatching(query: string): Array<Object> {
-    var queryRegExp = new RegExp(query, 'i');
-    return atom.workspace.getTextEditors()
-     .sort((a, b) => b.lastOpened - a.lastOpened)
-     .map(editor => editor.getPath())
-     .filter(path => !query.length || queryRegExp.test(path))
-     .map(file => ({path: file, matchIndexes: []}));
-  }
+// Returns the currently opened tabs, ordered from most recently opened to least recently opened.
+function getOpenTabsMatching(query: string): Array<FileResult> {
+  var queryRegExp = new RegExp(query, 'i');
+  return atom.workspace.getTextEditors()
+   .sort((a, b) => b.lastOpened - a.lastOpened)
+   .map(editor => editor.getPath())
+   .filter(path => !query.length || queryRegExp.test(path))
+   .map(file => ({path: file, matchIndexes: []}));
+}
 
-  getProviderType(): string {
+var OpenFileListProvider: Provider = {
+
+  getName(): string {
+    return 'OpenFileListProvider';
+  },
+
+  getProviderType(): ProviderType {
     return 'GLOBAL';
-  }
+  },
 
   getDebounceDelay(): number {
     return 0;
-  }
+  },
 
   getAction(): string {
     return 'nuclide-open-filenames-provider:toggle-provider';
-  }
+  },
 
   getPromptText(): string {
     return 'Search names of open files';
-  }
+  },
 
   getTabTitle(): string {
     return 'Open Files';
-  }
+  },
 
-  executeQuery(query: string): Promise<Array<Object>> {
-    return Promise.resolve(OpenFileListProvider.getOpenTabsMatching(query));
-  }
+  executeQuery(query: string): Promise<Array<FileResult>> {
+    return Promise.resolve(getOpenTabsMatching(query));
+  },
 
-}
+};
 
-module.exports = new OpenFileListProvider();
+module.exports = OpenFileListProvider;
