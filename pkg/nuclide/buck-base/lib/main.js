78a2d3eb30963c5d66373bd950589e98de23c43b
Michael Bolin <mbolin@fb.com>
2015-09-09 13:46:06 -0700

    Move isBuckFile function into nuclide-buck-base package.
    Reviewed By: @jssln
    
    Differential Revision: D2424040

--- pkg/nuclide/buck-base/lib/main.js
+++ pkg/nuclide/buck-base/lib/main.js
@@ -13,4 +13,11 @@ module.exports = {
   get BuckProject() {
     return require('./BuckProject');
   },
+
+  isBuckFile: function(filePath: string): boolean {
+    // TODO(mbolin): Buck does have an option where the user can customize the
+    // name of the build file: https://github.com/facebook/buck/issues/238.
+    // This function will not work for those who use that option.
+    return require('path').basename(filePath) === 'BUCK';
+  },
 };
