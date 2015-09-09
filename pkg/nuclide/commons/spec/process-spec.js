d8b46daad66f394ada39f6b74a0156a32bd00543
Jess Lin <jessicalin@fb.com>
2015-09-09 12:10:45 -0700

    Refactor process-spec.js
    Reviewed By: @natthu
    
    Differential Revision: D2424806

--- pkg/nuclide/commons/spec/process-spec.js
+++ pkg/nuclide/commons/spec/process-spec.js
@@ -16,7 +16,7 @@ var {
   DARWIN_PATH_HELPER_REGEXP,
 } = processLib.__test__;
 
-describe('process.asyncExecute', () => {
+describe('nuclide-commons/process', () => {
 
   var origPlatform;
 
@@ -57,38 +57,16 @@ describe('process.asyncExecute', () => {
     });
   });
 
-  if (origPlatform !== 'win32') {
-    it('returns stdout of the running process', () => {
-      waitsForPromise(async () => {
-        var val = await processLib.asyncExecute('echo', ['-n', 'foo'], {env: process.env});
-        expect(val.stdout).toEqual('foo');
+  describe('asyncExecute', () => {
+    if (origPlatform !== 'win32') {
+      it('returns stdout of the running process', () => {
+        waitsForPromise(async () => {
+          var val = await processLib.asyncExecute('echo', ['-n', 'foo'], {env: process.env});
+          expect(val.stdout).toEqual('foo');
+        });
       });
-    });
-
-    it('checkOutput throws an error if the process cannot be started', () => {
-      waitsForPromise(async () => {
-          try {
-            await processLib.checkOutput('non_existing_command', /* args */ []);
-          } catch(error) {
-            // `exit` with a non-zero error code should reject the Promise and return the generic
-            // ENOENT (End Of ENTity) exit code.
-            expect(error.exitCode).toBe('ENOENT');
-            return;
-          }
-          // Force failure if the error was not thrown.
-          expect('should have exited because of error code > 0').toEqual(null);
-        });
-    });
-
-    it('checkOutput does not throw an error if the exit code !== 0', () => {
-      waitsForPromise(async () => {
-          var {exitCode} = await processLib.checkOutput(process.execPath, ['-e', 'process.exit(1)']);
-          expect(exitCode).toBe(1);
-        });
-    });
-
-    it('asyncExecute throw an error if the exit code !== 0', () => {
-      waitsForPromise(async () => {
+      it('throws an error if the exit code !== 0', () => {
+        waitsForPromise(async () => {
           try {
             await processLib.asyncExecute(process.execPath, ['-e', 'process.exit(1)']);
           } catch(error) {
@@ -100,46 +78,70 @@ describe('process.asyncExecute', () => {
           // Force failure if the error was not thrown.
           expect('should have exited because of error code > 0').toEqual(null);
         });
-    });
-
-    it('pipes stdout to stdin of `pipedCommand`', () => {
-      waitsForPromise(async () => {
-        var val = await processLib.asyncExecute(
-            'seq',
-            ['1', '100'],
-            {env: process.env, pipedCommand: 'head', pipedArgs: ['-10']});
-        expect(val.stdout).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10].join('\n') + '\n');
       });
-    });
+      it('pipes stdout to stdin of `pipedCommand`', () => {
+        waitsForPromise(async () => {
+          var val = await processLib.asyncExecute(
+              'seq',
+              ['1', '100'],
+              {env: process.env, pipedCommand: 'head', pipedArgs: ['-10']});
+          expect(val.stdout).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10].join('\n') + '\n');
+        });
+      });
+      describe('when passed a pipedCommand', () => {
+        it('captures an error message if the first command exits', () => {
+          waitsForPromise(async () => {
+            try {
+              await processLib.asyncExecute(
+                'exit',
+                ['5'],
+                {env: process.env, pipedCommand: 'head', pipedArgs: ['-10']});
+            } catch(error) {
+              // `exit` with a non-zero error code should reject the Promise and return the generic
+              // ENOENT (End Of ENTity) exit code.
+              expect(error.exitCode).toEqual('ENOENT');
+              return;
+            }
+            // Force failure if the error was not thrown.
+            expect('should have exited because of error code > 0').toEqual(null);
+          });
+        });
+      });
+    }
+  });
 
-    describe('pipedCommand', () => {
-      it('captures an error message if the first command exits', () => {
+  describe('checkOutput', () => {
+    if (origPlatform !== 'win32') {
+      it('checkOutput throws an error if the process cannot be started', () => {
         waitsForPromise(async () => {
           try {
-            await processLib.asyncExecute(
-              'exit',
-              ['5'],
-              {env: process.env, pipedCommand: 'head', pipedArgs: ['-10']});
+            await processLib.checkOutput('non_existing_command', /* args */ []);
           } catch(error) {
             // `exit` with a non-zero error code should reject the Promise and return the generic
             // ENOENT (End Of ENTity) exit code.
-            expect(error.exitCode).toEqual('ENOENT');
+            expect(error.exitCode).toBe('ENOENT');
             return;
           }
           // Force failure if the error was not thrown.
           expect('should have exited because of error code > 0').toEqual(null);
         });
       });
-    });
-  }
-});
+      it('checkOutput does not throw an error if the exit code !== 0', () => {
+        waitsForPromise(async () => {
+          var {exitCode} = await processLib.checkOutput(process.execPath, ['-e', 'process.exit(1)']);
+          expect(exitCode).toBe(1);
+        });
+      });
+    }
+  });
 
-describe('process.safeSpawn', () => {
-  it('should not crash the process on an error', () => {
-    waitsForPromise(async () => {
-      var child = await processLib.safeSpawn('fakeCommand');
-      expect(child).not.toBe(null);
-      expect(child.listeners('error').length).toBeGreaterThan(0);
+  describe('process.safeSpawn', () => {
+    it('should not crash the process on an error', () => {
+      waitsForPromise(async () => {
+        var child = await processLib.safeSpawn('fakeCommand');
+        expect(child).not.toBe(null);
+        expect(child.listeners('error').length).toBeGreaterThan(0);
+      });
     });
   });
 });
