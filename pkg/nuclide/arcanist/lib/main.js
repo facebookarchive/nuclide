48e1f31c8e8e425459e1e8845ea364908e4ae3c0
Michael Bolin <mbolin@fb.com>
2015-09-14 07:30:29 -0700

    Properly syntax highlight an .arcconfig file.
    Reviewed By: @nmote
    
    Differential Revision: D2436589

--- pkg/nuclide/arcanist/lib/main.js
+++ pkg/nuclide/arcanist/lib/main.js
@@ -8,10 +8,28 @@
  * This source code is licensed under the license found in the LICENSE file in
  * the root directory of this source tree.
  */
+import {CompositeDisposable} from 'atom';
+
+var subscriptions: ?CompositeDisposable = null;
 
 module.exports = {
 
-  activate: () => {},
+  activate(): void {
+    if (subscriptions) {
+      return;
+    }
+
+    var {registerGrammarForFileExtension} = require('nuclide-atom-helpers');
+    subscriptions = new CompositeDisposable();
+    subscriptions.add(registerGrammarForFileExtension('source.json', '.arcconfig'));
+  },
+
+  dactivate(): void {
+    if (subscriptions) {
+      subscriptions.dispose();
+      subscriptions = null;
+    }
+  },
 
   provideLinter() {
     return require('./ArcanistLinter');
