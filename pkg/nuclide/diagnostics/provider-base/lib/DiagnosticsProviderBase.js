acc75ea95ddfab7bfbc82c2b7b34b09db2632776
Nat Mote <nmote@fb.com>
2015-09-21 17:03:34 -0700

    README for nuclide-diagnostics-provider-base
    Reviewed By: @bolinfest
    
    Differential Revision: D2441294

--- pkg/nuclide/diagnostics/provider-base/lib/DiagnosticsProviderBase.js
+++ pkg/nuclide/diagnostics/provider-base/lib/DiagnosticsProviderBase.js
@@ -12,16 +12,30 @@
 import type {TextEventDispatcher} from 'nuclide-text-event-dispatcher';
 
 type ProviderBaseOptions = {
+  /** The callback by which a provider is notified of text events, such as a file save. */
   onTextEditorEvent?: (editor: TextEditor) => mixed;
+  /**
+   * The callback by which a provider is notified that a new consumer has subscribed to diagnostic
+   * updates.
+   */
   onNewUpdateSubscriber?: (callback: MessageUpdateCallback) => mixed;
+  /**
+   * The callback by which a provider is notified that a new consumer has subscribed to diagnostic
+   * invalidations.
+   */
   onNewInvalidateSubscriber?: (callback: MessageInvalidationCallback) => mixed;
-  // If true, this will cause onTextEditorEvent to get called more often --
-  // approximately whenever the user stops typing. If false, it will get called
-  // only when the user saves.
+  /**
+   * If true, this will cause onTextEditorEvent to get called more often -- approximately whenever
+   * the user stops typing. If false, it will get called only when the user saves.
+   */
   shouldRunOnTheFly?: boolean;
-  // If grammarScopes is not specified, enableForAllGrammars must be true. If
-  // grammarScopes is specified and enableForAllGrammars is true,
-  // enableForAllGrammars takes precedence.
+  /**
+   * The following two options specify which grammars the provider is interested in. Most providers
+   * will include a set of grammarScopes, and will therefore get notifications only about
+   * TextEditors that are associated with those grammarScopes. Instead, a provider may set
+   * enableForAllGrammars to true, and then it will get notified of changes in all TextEditors. If
+   * enableForAllGrammars is true, it overrides the grammars in grammarScopes.
+   */
   grammarScopes?: Set<string>;
   enableForAllGrammars?: boolean;
 }
