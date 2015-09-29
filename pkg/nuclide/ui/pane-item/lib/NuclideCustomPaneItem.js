a7fd0d2dacd19ece540c82a66af8ae41e26368f2
Natthu Bharambe <natthu@fb.com>
2015-09-28 20:32:37 -0700

    Create a new package 'nuclide-ui-pane' to avoid boilerplate creating new tabs.
    Reviewed By: @bolinfest
    
    Differential Revision: D2482255

---
+++ pkg/nuclide/ui/pane-item/lib/NuclideCustomPaneItem.js
@@ -0,0 +1,67 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+import invariant from 'assert';
+import React from 'react-for-atom';
+
+import type {NuclideCustomPaneItemOptions} from './types';
+
+class NuclideCustomPaneItem extends HTMLElement {
+
+  _title: ?string;
+  _iconName: ?string;
+  _uri: ?string;
+  _allowSplit: boolean;
+
+  initialize(options: NuclideCustomPaneItemOptions) {
+    this._title = options.title;
+    this._iconName = options.iconName;
+    this._uri = options.uri;
+    this._allowSplit = !!options.allowSplit;
+
+    var component = this.__renderPaneItem(options);
+    React.render(component, this);
+  }
+
+  /**
+   * Subclasses should override this method to render the pane using options passed from above.
+   * This method is invoked as part of initialize(), and so, it should be safe to invoke any of the
+   * getter methods on this class in this method.
+   *
+   * @return A React component that this element call React.render() on.
+   */
+  __renderPaneItem(options: NuclideCustomPaneItemOptions): ReactElement {
+    throw new Error('Subclass should implement this method.');
+  }
+
+  getTitle(): string {
+    invariant(this._title);
+    return this._title;
+  }
+
+  getIconName(): ?string {
+    return this._iconName;
+  }
+
+  getURI(): ?string {
+    return this._uri;
+  }
+
+  copy(): boolean {
+    return this._allowSplit;
+  }
+
+  detachedCallback() {
+    React.unmountComponentAtNode(this);
+  }
+}
+
+module.exports = NuclideCustomPaneItem;
---
+++ pkg/nuclide/ui/pane-item/lib/types.js
@@ -0,0 +1,18 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+export type NuclideCustomPaneItemOptions = {
+  title: string; // Title for the custom pane item being created.
+  iconName?: string; // Optional string representing the octicon that is rendered next to the title.
+  uri?: string;
+  allowSplit?: bool; // Whether splits are allowed on the pane item. Defaults to false.
+  initialProps: Object; // The pane item specific properties.
+};
