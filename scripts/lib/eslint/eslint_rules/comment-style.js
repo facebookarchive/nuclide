ccc58a25b983408ec2150d1c68accadbe5d3bc9b
Nat Mote <nmote@fb.com>
2015-10-05 15:00:11 -0700

    Add license header to custom ESLint rule
    Reviewed By: @natthu
    
    Differential Revision: D2509266
    
    fb-gh-sync-id: b393d9269db8488f422097928568c621b66ee360

--- scripts/lib/eslint/eslint_rules/comment-style.js
+++ scripts/lib/eslint/eslint_rules/comment-style.js
@@ -1,3 +1,14 @@
+'use babel';
+/* @noflow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
 var whitelistedComments = [
   /@flow/,
   /^\s*eslint-/,
