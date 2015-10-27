81d29730a63bc7173fa80676e4fdc97eb36720b5
Nat Mote <nmote@fb.com>
2015-10-26 17:31:59 -0700

    nuclide-busy-signal Message Store
    Reviewed By: mostafaeweda
    
    Differential Revision: D2578129
    
    fb-gh-sync-id: 0e4df522888de334f06922789be38106c8a823af

---
+++ pkg/nuclide/busy-signal/lib/MessageStore.js
@@ -0,0 +1,64 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+import {Observable, BehaviorSubject} from 'rx';
+import {Disposable} from 'atom';
+import invariant from 'assert';
+
+export class MessageStore {
+  // provider to id to messages.
+  _currentMessages: Map<BusySignalProvider, Map<number, BusySignalMessage>>;
+  _messageStream: BehaviorSubject<Array<BusySignalMessage>>;
+
+  constructor() {
+    this._currentMessages = new Map();
+    this._messageStream = new BehaviorSubject([]);
+  }
+
+  consumeProvider(provider: BusySignalProvider): atom$Disposable {
+    const subscription =
+      provider.messages.subscribe(message => this._processUpdate(provider, message));
+    return new Disposable(() => {
+      subscription.dispose();
+      this._currentMessages.delete(provider);
+      this._publishMessages();
+    });
+  }
+
+  getMessageStream(): Observable<Array<BusySignalMessage>> {
+    return this._messageStream;
+  }
+
+  _processUpdate(provider: BusySignalProvider, message: BusySignalMessage): void {
+    let idMap = this._currentMessages.get(provider);
+    if (idMap == null) {
+      idMap = new Map();
+      this._currentMessages.set(provider, idMap);
+    }
+    if (message.status === 'busy') {
+      idMap.set(message.id, message);
+    } else {
+      invariant(message.status === 'done');
+      idMap.delete(message.id);
+    }
+    this._publishMessages();
+  }
+
+  _publishMessages(): void {
+    const messages = [];
+    for (const idMap of this._currentMessages.values()) {
+      for (const message of idMap.values()) {
+        messages.push(message);
+      }
+    }
+    this._messageStream.onNext(messages);
+  }
+}
---
+++ pkg/nuclide/busy-signal/spec/MessageStore-spec.js
@@ -0,0 +1,80 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+import {Subject} from 'rx';
+
+import {MessageStore} from '../lib/MessageStore';
+
+describe('MessageStore', () => {
+  let messageStore: MessageStore = (null: any);
+  let messagePublisher: Subject<BusySignalMessage> = (null: any);
+  let messageStreamResults: Array<Array<BusySignalMessage>> = (null: any);
+  let sampleMessage: BusySignalMessage = (null: any);
+
+  function getLastResult() {
+    return messageStreamResults[messageStreamResults.length - 1];
+  }
+
+  beforeEach(() => {
+    messageStore = new MessageStore();
+    messagePublisher = new Subject();
+    messageStreamResults = [];
+    sampleMessage = {
+      status: 'busy',
+      id: 0,
+      message: 'foobar',
+    };
+    const provider = {
+      messages: messagePublisher,
+    };
+    messageStore.consumeProvider(provider);
+    messageStore.getMessageStream().subscribe(messages => messageStreamResults.push(messages));
+  });
+
+  it('should publish the current state as soon as it receives a subscriber', () => {
+    expect(messageStreamResults).toEqual([[]]);
+  });
+
+  it('should publish messages it receives', () => {
+    messagePublisher.onNext(sampleMessage);
+    expect(getLastResult()).toEqual([sampleMessage]);
+  });
+
+  it('should remove invalidated messages', () => {
+    messagePublisher.onNext(sampleMessage);
+    messagePublisher.onNext({...sampleMessage, status: 'done'});
+    expect(getLastResult()).toEqual([]);
+  });
+
+  it('should allow multiple messages from a single provider', () => {
+    const firstMessage = sampleMessage;
+    const secondMessage = {...sampleMessage, id: 1};
+    messagePublisher.onNext(firstMessage);
+    messagePublisher.onNext(secondMessage);
+    // Message order within a provider is respected.
+    expect(getLastResult()).toEqual([firstMessage, secondMessage]);
+    messagePublisher.onNext({...sampleMessage, status: 'done'});
+    messagePublisher.onNext({id: 1, status: 'done'});
+    expect(getLastResult()).toEqual([]);
+  });
+
+  it('should handle messages from different providers', () => {
+    const otherPublisher = new Subject();
+    messageStore.consumeProvider({
+      messages: otherPublisher,
+    });
+    messagePublisher.onNext(sampleMessage);
+    otherPublisher.onNext(sampleMessage);
+    expect(getLastResult()).toEqual([sampleMessage, sampleMessage]);
+    messagePublisher.onNext({...sampleMessage, status: 'done'});
+    expect(getLastResult()).toEqual([sampleMessage]);
+  });
+});
