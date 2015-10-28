af3cec622e8708796a49c141cb9adef68468c819
Nat Mote <nmote@fb.com>
2015-10-28 14:47:39 -0700

    Integrate StatusBarTile and MessageStore to complete the nuclide-busy-signal package
    Reviewed By: mostafaeweda
    
    Differential Revision: D2578249
    
    fb-gh-sync-id: de3d0d6652fe73cc0b07e4374ebf132e02cb3be2

--- pkg/nuclide/busy-signal/lib/main.js
+++ pkg/nuclide/busy-signal/lib/main.js
@@ -9,10 +9,68 @@
  * the root directory of this source tree.
  */
 
-module.exports = {
+import type {StatusBarTile as StatusBarTileType} from './StatusBarTile';
 
-  activate(state: ?Object): void {
-    // TODO(nmote): Add activation code here.
-  },
+import {Disposable, CompositeDisposable} from 'atom';
+import invariant from 'assert';
+import {MessageStore} from './MessageStore';
 
-};
+class Activation {
+  _statusBarTile: ?StatusBarTileType;
+  _disposables: CompositeDisposable;
+  _messageStore: MessageStore;
+
+  constructor() {
+    this._disposables = new CompositeDisposable();
+    this._messageStore = new MessageStore();
+  }
+
+  dispose() {
+    this._disposables.dispose();
+  }
+
+  consumeStatusBar(statusBar: atom$StatusBar): atom$Disposable {
+    const {StatusBarTile} = require('./StatusBarTile');
+    const statusBarTile = this._statusBarTile = new StatusBarTile();
+    statusBarTile.consumeMessageStream(this._messageStore.getMessageStream());
+    const disposable = new Disposable(() => {
+      if (this._statusBarTile) {
+        this._statusBarTile.dispose();
+        this._statusBarTile = null;
+      }
+    });
+    statusBarTile.consumeStatusBar(statusBar);
+    this._disposables.add(disposable);
+    return disposable;
+  }
+
+  consumeBusySignalProvider(provider: BusySignalProvider): atom$Disposable {
+    const disposable = this._messageStore.consumeProvider(provider);
+    this._disposables.add(disposable);
+    return disposable;
+  }
+}
+
+let activation: ?Activation = null;
+
+export function activate(state: ?Object): void {
+  deactivate();
+  activation = new Activation();
+}
+
+export function consumeStatusBar(statusBar: atom$StatusBar): atom$Disposable {
+  invariant(activation);
+  return activation.consumeStatusBar(statusBar);
+}
+
+export function consumeBusySignalProvider(provider: BusySignalProvider): atom$Disposable {
+  invariant(activation);
+  return activation.consumeBusySignalProvider(provider);
+}
+
+export function deactivate(): void {
+  if (activation) {
+    activation.dispose();
+    activation = null;
+  }
+}
