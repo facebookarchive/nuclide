2ba644fb4cc12499b693cb6981e473924dce54ae
Max Sherman <jonaldislarry@fb.com>
2015-11-30 18:10:51 -0800

    Add simple test for adding/removing project folder locally
    Reviewed By: zertosh
    
    Differential Revision: D2701326
    
    fb-gh-sync-id: 3d59a65549757d7429db866bf82c3e16db649466

---
+++ pkg/nuclide/hg-repository/spec/hg-repository-integration-spec.js
@@ -0,0 +1,40 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+import {activateAllPackages, fixtures} from 'nuclide-test-helpers';
+import path from 'path';
+
+describe('Mercurial Repository Integration Tests', () => {
+  beforeEach(() => {
+    waitsForPromise({timeout: 240000}, async () => {
+      await activateAllPackages();
+    });
+  });
+
+  it('adds opens and removes project without errors', () => {
+    waitsForPromise(async () => {
+      spyOn(console, 'error');
+      // Copy mercurial project to temporary directory.
+      const repoPath = await fixtures.copyGlobalMercurialFixture('hg_repo_1');
+      // Add this directory as a new project in atom.
+      atom.project.setPaths([repoPath]);
+      // Open a file within this project.
+      await atom.workspace.open(path.join(repoPath, 'test.txt'));
+      // Remove project
+      atom.project.removePath(repoPath);
+      expect(console.error).not.toHaveBeenCalled(); // eslint-disable-line no-console
+    });
+  });
+
+  afterEach(() => {
+    atom.packages.deactivatePackages();
+    atom.packages.unloadPackages();
+  });
+});
