3f9402a555e759fc204bb895f6e57d838578f7ea
Jess Lin <jessicalin@fb.com>
2015-12-01 15:16:12 -0800

    Re-render the ConnectionDialog when the saved connection profiles change. [6/P]
    Reviewed By: scv119
    
    Differential Revision: D2576131
    
    fb-gh-sync-id: e0fdc5e486a2f188aabb1d4e1298047f30785b59

--- pkg/nuclide/ssh-dialog/lib/open-connection.js
+++ pkg/nuclide/ssh-dialog/lib/open-connection.js
@@ -12,9 +12,11 @@
 import {
   getDefaultConnectionProfile,
   getOfficialRemoteServerCommand,
+  onSavedConnectionProfilesDidChange,
   saveConnectionConfig,
 } from './connection-profile-utils';
 
+import type {Disposable} from 'atom';
 import type {RemoteConnection} from 'nuclide-remote-connection';
 import type {NuclideRemoteConnectionProfile} from './connection-types';
 
@@ -32,10 +34,32 @@ export function openConnectionDialog(props): Promise<?RemoteConnection> {
   }
 
   return dialogPromiseQueue.submit((resolve, reject) => {
+    const React = require('react-for-atom');
+    const ConnectionDialog = require('./ConnectionDialog');
+    const workspaceEl = atom.views.getView(atom.workspace);
+    const hostEl = document.createElement('div');
+    workspaceEl.appendChild(hostEl);
+
+    // During the lifetime of this 'openConnectionDialog' flow, the 'default'
+    // connection profile should not change (even if it is reset by the user
+    // connecting to a remote project from another Atom window).
     const defaultConnectionProfile: NuclideRemoteConnectionProfile = getDefaultConnectionProfile();
 
-    const dialogProps = extend.immutableExtend({
-      connectionProfiles: [defaultConnectionProfile],
+    // We want to observe changes in the saved connection profiles during the
+    // lifetime of this connection dialog, because the user can add/delete
+    // a profile from a connection dialog.
+    let connectionProfilesSubscription: ?Disposable = null;
+    function cleanupSubscriptionFunc(): void {
+      if (connectionProfilesSubscription) {
+        connectionProfilesSubscription.dispose();
+      }
+    }
+
+    // The connection profiles could change, but the rest of the props passed
+    // to the ConnectionDialog will not.
+    // Note: the `cleanupSubscriptionFunc` is called when the dialog closes:
+    // `onConnect`, `onError`, or `onCancel`.
+    const baseDialogProps = extend.immutableExtend({
       // Select the default connection profile, which should always be index 0.
       indexOfInitiallySelectedConnectionProfile: 0,
       // TODO jessicalin This will be filled in and used later in this stack.
@@ -45,31 +69,42 @@ export function openConnectionDialog(props): Promise<?RemoteConnection> {
       onConnect: async (connection, config) => {
         resolve(connection);
         saveConnectionConfig(config, getOfficialRemoteServerCommand());
+        cleanupSubscriptionFunc();
       },
       onError: (err, config) => {
         resolve(/*connection*/ null);
         saveConnectionConfig(config, getOfficialRemoteServerCommand());
+        cleanupSubscriptionFunc();
       },
-      onCancel: () => resolve(/*connection*/ null),
+      onCancel: () => {
+        resolve(/*connection*/ null);
+        cleanupSubscriptionFunc();
+      },
+      onClosed: () => {
+        // Unmount the ConnectionDialog and clean up the host element.
+        if (hostEl) {
+          React.unmountComponentAtNode(hostEl);
+          if (hostEl.parentNode) {
+            hostEl.parentNode.removeChild(hostEl);
+          }
+        }
+      },
     }, props);
 
-    const React = require('react-for-atom');
-    const ConnectionDialog = require('./ConnectionDialog');
-
-    const workspaceEl = atom.views.getView(atom.workspace);
-    const hostEl = document.createElement('div');
-    workspaceEl.appendChild(hostEl);
-
-    dialogProps.onClosed = () => {
-      // Unmount the ConnectionDialog and clean up the host element.
-      if (hostEl) {
-        React.unmountComponentAtNode(hostEl);
-        if (hostEl.parentNode) {
-          hostEl.parentNode.removeChild(hostEl);
-        }
+    // If/when the saved connection profiles change, we want to re-render the dialog
+    // with the new set of connection profiles.
+    connectionProfilesSubscription = onSavedConnectionProfilesDidChange(
+      (newProfiles: ?Array<NuclideRemoteConnectionProfile>) => {
+        const connectionProfiles = newProfiles ? [defaultConnectionProfile].concat(newProfiles) :
+            [defaultConnectionProfile];
+        const newDialogProps = extend.immutableExtend(baseDialogProps, {connectionProfiles});
+        React.render(<ConnectionDialog {...newDialogProps} />, hostEl);
       }
-    };
+    );
 
-    React.render(<ConnectionDialog {...dialogProps} />, hostEl);
+    const initialDialogProps = extend.immutableExtend(baseDialogProps, {
+      connectionProfiles: [defaultConnectionProfile],
+    });
+    React.render(<ConnectionDialog {...initialDialogProps} />, hostEl);
   });
 }
