b7aad0dc3f4a58c73b6e843c977a0ff6be283bad
Dan Abramov <gaearon@fb.com>
2015-12-10 09:14:09 -0800

    Remove support for format-js.json configuration file
    Reviewed By: kyldvs
    
    Differential Revision: D2739227
    
    fb-gh-sync-id: fb2891af3ad014d46b8b84cd30f900124b674880

--- pkg/nuclide/format-js/lib/options.js
+++ pkg/nuclide/format-js/lib/options.js
@@ -12,7 +12,6 @@
 import type ModuleMap from '../../format-js-base/lib/state/ModuleMap';
 import type {SourceOptions} from '../../format-js-base/lib/options/SourceOptions';
 
-const {Directory, File} = require('atom');
 const NuclideCommons = require('../../commons');
 
 const featureConfig = require('../../feature-config');
@@ -34,140 +33,11 @@ const defaultBuiltIns = arrayFrom(formatJSBase.defaultBuiltIns);
 const defaultBuiltInTypes = arrayFrom(formatJSBase.defaultBuiltInTypes);
 
 /**
- * Single entry point to get the options for a source file. Deals with finding
- * the configuration file and caching.
+ * Single entry point to get the options for a source file.
+ * Currently, it only looks at the Nuclide settings, but we may
+ * extend it to look at the project-specific configuration file later.
  */
 async function getOptions(sourcePath: string): SourceOptions {
-  const jsonFile = getJSON(sourcePath);
-  if (!jsonFile) {
-    return getNuclideOptions(sourcePath);
-  }
-
-  // Parse the json.
-  const json = JSON.parse(await jsonFile.read());
-
-  // Construct the blacklist.
-  const blacklist = getNuclideBlacklist();
-  if (json.blacklist) {
-    json.blacklist.forEach(transform => blacklist.add(transform));
-  }
-
-  // Construct the module map.
-  const rootDir = jsonFile.getParent();
-  const nuclideSettings = getNuclideSettings();
-  const jsonSettings = getJSONSettings(json);
-
-  const aliases = new Map([
-    ...defaultAliases,
-    ...nuclideSettings.aliases,
-    ...jsonSettings.aliases,
-  ]);
-  const builtIns = new Set([
-    ...defaultBuiltIns,
-    ...nuclideSettings.builtIns,
-    ...jsonSettings.builtIns,
-  ]);
-  const builtInTypes = new Set([
-    ...defaultBuiltInTypes,
-    ...nuclideSettings.builtInTypes,
-    ...jsonSettings.builtInTypes,
-  ]);
-
-  const moduleMapOptions = {
-    paths: [],
-    pathsToRelativize: [],
-    aliases,
-    aliasesToRelativize: new Map(),
-    builtIns,
-    builtInTypes,
-  };
-
-  // Add the paths to the module map options. Make sure to take into account
-  // relativization.
-  const paths = getPaths(rootDir, json);
-
-  if (json.relativize) {
-    moduleMapOptions.pathsToRelativize = paths;
-  } else {
-    moduleMapOptions.paths = paths;
-  }
-
-  const moduleMap = createModuleMap(moduleMapOptions);
-
-  // Put everything together in the source options object.
-  return {
-    blacklist,
-    moduleMap,
-    sourcePath,
-  };
-}
-
-/**
- * Gets the JSON file that has configuration information for the given source
- * file.
- *
- * TODO: Cache things.
- */
-function getJSON(sourcePath: string): ?File {
-  const startFile = new File(sourcePath);
-  let current = startFile.getParent();
-  while (true) {
-    const jsonFile = current.getFile('formatjs.json');
-    if (jsonFile.existsSync()) {
-      return jsonFile;
-    }
-    // Make sure to perform the root check after testing for the file.
-    if (current.isRoot()) {
-      return undefined;
-    }
-    current = current.getParent();
-  }
-}
-
-function getJSONSettings(json: Object): Settings {
-  return {
-    aliases: fixAliases(json.aliases),
-    builtIns: json.builtIns || [],
-    builtInTypes: json.builtInTypes || [],
-  };
-}
-
-function getPaths(directory: Directory, json: Object): Array<string> {
-  const pathGroups: Array<Array<string>> = [];
-  const entries = directory.getEntriesSync();
-  for (const entry of entries) {
-    const path = entry.getRealPathSync();
-
-    // Check if we should ignore the path.
-    if (
-      json.ignorePatterns &&
-      json.ignorePatterns.length > 0 &&
-      json.ignorePatterns.some(pattern => path.indexOf(pattern) >= 0)
-    ) {
-      continue;
-    }
-
-    // Add appropriate paths to pathGroups.
-    if (entry.isFile()) {
-      // TODO: Make this configurable.
-      if (path.endsWith('.js')) {
-        pathGroups.push([path]);
-      }
-    } else if (entry.isDirectory()) {
-      pathGroups.push(getPaths(entry, json));
-    }
-  }
-
-  return Array.prototype.concat.apply([], pathGroups);
-}
-
-// Functions for getting default options based purely on Nuclide's settings.
-
-/**
- * Returns default options based on Nuclide's settings when tehre isn't a
- * configuration file present.
- */
-function getNuclideOptions(sourcePath: string): SourceOptions {
   return {
     blacklist: getNuclideBlacklist(),
     moduleMap: getNuclideModuleMap(),
