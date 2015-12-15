ce9c6d112834f7199ba89a3e8a0bda4a655cc25b
Ross Allen <rossallen@fb.com>
2015-12-15 11:05:20 -0800

    Find workspace state in different Atom versions
    Reviewed By: zertosh
    
    Differential Revision: D2758419
    
    fb-gh-sync-id: 54daad840f26e3022c49efcc97843b2a524b10d0

--- pkg/nuclide/atom-helpers/lib/workspace.js
+++ pkg/nuclide/atom-helpers/lib/workspace.js
@@ -14,13 +14,20 @@ import path from 'path';
 /**
  * @return The path to the JSON file on disk where the workspace state is stored.
  */
-export function getPathToWorkspaceState(): string {
+export function getPathToWorkspaceState(): ?string {
+  // Atom <1.2 this function exists on `atom.constructor`. Atom >=1.2 it exists on the global `atom`
+  // object. Find the appropriate location, and return `null` if both fail unexpectedly.
+  const getStateKey = atom.getStateKey || atom.constructor.getStateKey;
+  if (typeof getStateKey !== 'function') {
+    return null;
+  }
+
   // As you can imagine, the way that we are getting this path is not documented and is therefore
   // unstable.
   // TODO(t8750960): Work with the Atom core team to get a stable API for this.
   return path.join(
     atom.getConfigDirPath(),
     'storage',
-    atom.constructor.getStateKey(atom.project.getPaths(), 'editor'),
+    getStateKey(atom.project.getPaths(), 'editor'),
   );
 }
