1bfcd377f1c5e314a0b666a1d74f16a5dfc87896
Matthew Dapena-Tretter <matthewwithanm@fb.com>
2016-01-14 14:54:17 -0800

    Wrap webview tag in React component
    Reviewed By: ssorallen
    
    Differential Revision: D2777629
    
    fb-gh-sync-id: 3fa57a3a5aa09e031f71785ca8f15b74a4e4c937

---
+++ pkg/nuclide/ui/webview/lib/main.js
@@ -0,0 +1,90 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+/*eslint-disable react/prop-types */
+
+import {CompositeDisposable, Disposable} from 'atom';
+import React from 'react-for-atom';
+
+type Props = {
+  className: ?string;
+  nodeintegration: ?boolean;
+  onDidFinishLoad: (event: Event) => mixed;
+  src: string;
+  style: ?Object;
+};
+
+export default class Webview extends React.Component<void, Props> {
+
+  constructor(props: Object) {
+    super(props);
+    this._handleDidFinishLoad = this._handleDidFinishLoad.bind(this);
+    this._disposables = new CompositeDisposable();
+  }
+
+  componentDidMount() {
+    const element = React.findDOMNode(this);
+
+    // Add event listeners. This has the drawbacks of 1) adding an event listener even when we don't
+    // have a callback for it and 2) needing to add explicit support for each event type we want to
+    // support. However, those costs aren't great enough to justify a new abstraction for managing
+    // it at this time.
+    element.addEventListener('did-finish-load', this._handleDidFinishLoad);
+    this._disposables.add(
+      new Disposable(
+        () => element.removeEventListener('did-finish-load', this._handleDidFinishLoad),
+      ),
+    );
+
+    this.updateAttributes({});
+  }
+
+  componentDidUpdate(prevProps: Props): void {
+    this.updateAttributes(prevProps);
+  }
+
+  componentWillUnmount() {
+    this._disposables.dispose();
+  }
+
+  render(): ?ReactElement {
+    return (
+      <webview className={this.props.className} style={this.props.style} />
+    );
+  }
+
+  /**
+   * Update the attributes on the current element. Custom attributes won't be added by React because
+   * "webview" isn't a valid custom element name (custom elements need a dash), so we set the
+   * attributes ourselves. But not "className" or "style" because React has special rules for those.
+   * *sigh*
+   */
+  updateAttributes(prevProps: Object): void {
+    const element = React.findDOMNode(this);
+    const specialProps = ['className', 'style', 'onDidFinishLoad'];
+    const normalProps = Object.keys(this.props).filter(prop => specialProps.indexOf(prop) === -1);
+    normalProps.forEach(prop => {
+      const value = this.props[prop];
+      const prevValue = prevProps[prop];
+      const valueChanged = value !== prevValue;
+      if (valueChanged) {
+        element[prop] = value;
+      }
+    });
+  }
+
+  _handleDidFinishLoad(event: Event): void {
+    if (this.props.onDidFinishLoad) {
+      this.props.onDidFinishLoad(event);
+    }
+  }
+
+}
