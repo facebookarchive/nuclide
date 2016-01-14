e181653e64b2fc171b54477932d698131a61ea61
Matthew Dapena-Tretter <matthewwithanm@fb.com>
2016-01-14 14:54:20 -0800

    Use Webview component in Inspector
    Reviewed By: ssorallen
    
    Differential Revision: D2777620
    
    fb-gh-sync-id: 2176e113d1ec1c7541a1165f2d52e57ec6b41aa7

--- pkg/nuclide/react-native-inspector/lib/ui/Inspector.js
+++ pkg/nuclide/react-native-inspector/lib/ui/Inspector.js
@@ -12,37 +12,39 @@
 import type {Gadget} from '../../../gadgets-interfaces';
 import React from 'react-for-atom';
 import path from 'path';
+import Webview from '../../../ui/webview';
 import {toJsString} from '../../../commons';
 
 class Inspector extends React.Component {
+
+  constructor(props: mixed) {
+    super(props);
+    this._handleDidFinishLoad = this._handleDidFinishLoad.bind(this);
+  }
 
   getTitle(): string {
     return 'React Native Inspector';
   }
 
   render(): ?ReactElement {
-    return <div style={{width: '100%', height: '100%'}} />;
+    return (
+      <Webview
+        style={{width: '100%', height: '100%'}}
+        nodeintegration={true}
+        className="native-key-bindings"
+        onDidFinishLoad={this._handleDidFinishLoad}
+        src="atom://nuclide/pkg/nuclide/react-native-inspector/lib/ui/inspector.html"
+      />
+    );
   }
 
-  componentDidMount() {
-    // TODO: We create the webview element by hand here because React 0.13 doesn't support custom
-    //       attributes (and we need the `nodeintegration` attribute). When we upgrade to 0.14,
-    //       change this.
-    const el = React.findDOMNode(this);
-    const webview = ((document.createElement('webview'): any): WebviewElement);
-    webview.style.width = '100%';
-    webview.style.height = '100%';
-    webview.nodeintegration = true;
-    webview.className = 'native-key-bindings';
-    webview.addEventListener('did-finish-load', () => {
-      const packageDirectory = path.resolve(__dirname, '../../');
-      const requirePaths = require.cache[__filename].paths;
-      webview.executeJavaScript(
-        `initializeElementInspector(${toJsString(packageDirectory)}, ${toJsString(requirePaths)});`
-      );
-    });
-    webview.src = 'atom://nuclide/pkg/nuclide/react-native-inspector/lib/ui/inspector.html';
-    el.appendChild(webview);
+  _handleDidFinishLoad(event) {
+    const element = event.target;
+    const packageDirectory = path.resolve(__dirname, '../../');
+    const requirePaths = require.cache[__filename].paths;
+    element.executeJavaScript(
+      `initializeElementInspector(${toJsString(packageDirectory)}, ${toJsString(requirePaths)});`
+    );
   }
 
 }
