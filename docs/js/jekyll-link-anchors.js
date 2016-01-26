19113b5557cea425576e4b627934330a5cfe3f91
Joel Marcey <joelm@fb.com>
2016-01-26 15:20:15 -0800

    Support uniquely named anchors even with duplicate text
    Reviewed By: ssorallen
    
    Differential Revision: D2865567
    
    fb-gh-sync-id: ce1aeed6e2a6125bb65d6e86ef221da94520db08

--- docs/js/jekyll-link-anchors.js
+++ docs/js/jekyll-link-anchors.js
@@ -14,13 +14,17 @@
   // Requires that your headings already have an `id` attribute set (because that's what jekyll
   // does). For every heading in your page, this adds a little anchor link `#` that you can click
   // to get a permalink to the heading. Ignores `h1`, because you should only have one per page.
-  // The text content of the tag is used to generate the link, so it will fail "gracefully-ish" if
-  //you have duplicate heading text.
+
+  // This also allows us to have uniquely named links even with headings of the same name.
+  // e.g.,
+  //   h2: Mac  (#mac)
+  //     h3: prerequisites (#mac__prerequisites)
+  //   h2: Linux (#linux)
+  //     h3: prerequisites (#linux__prerequisites)
 
   var headingNodes = [];
   var results;
-  var link;
-  var tags = ['h2', 'h3', 'h4', 'h5', 'h6'];
+  var tags = ['h2', 'h3']; // Avoid having 4+ levels of headings, linkable at least
 
   tags.forEach(function(tag) {
     results = document.getElementsByTagName(tag);
@@ -28,10 +32,29 @@
   });
 
   headingNodes.forEach(function(node) {
+    var link;
+    var id;
+    var psib;
     link = document.createElement('a');
     link.className = 'header-link';
     link.textContent = '#';
-    link.href = '#' + node.getAttribute('id');
+    id = '';
+    // Avoid duplicate anchor links
+    // If we are at an h3, go through the previous element siblings of this node, and find its
+    // h2 parent and append it to the href text.
+    if (node.localName === 'h3') {
+      psib = node.previousElementSibling;
+      while (psib) {
+        // this should be an h2 99.9% of the time, but if not, we can still create the link.
+        if (headingNodes.indexOf(psib) !== -1) {
+          id += psib.getAttribute('id') + '__';
+          break;
+        }
+        psib = psib.previousElementSibling;
+      }
+    }
+    link.id = id + node.getAttribute('id');
+    link.href = "#" + link.id;
     node.appendChild(link);
   });
 
