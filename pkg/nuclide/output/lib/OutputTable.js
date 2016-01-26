66c5632187459cc62940f17bbc1d19c232cbc498
Matthew Dapena-Tretter <matthewwithanm@fb.com>
2016-01-26 10:12:48 -0800

    Automatically scroll output when records are added
    Summary:
    public
    
    When records are added, scroll to the bottom of the view (unless the
    user has actively scrolled elsewhere).
    
    Reviewed By: johnislarry
    
    Differential Revision: D2864073
    
    fb-gh-sync-id: 5ed74d245ec8b1b63d1bd2bae9e2a235f238b807

--- pkg/nuclide/output/lib/OutputTable.js
+++ pkg/nuclide/output/lib/OutputTable.js
@@ -13,6 +13,7 @@
 
 import type {Record} from './types';
 
+import {debounce} from '../../commons';
 import {React} from 'react-for-atom';
 import RecordView from './RecordView';
 
@@ -23,10 +24,26 @@ type Props = {
 
 export default class OutputTable extends React.Component<void, Props, void> {
 
+  _isScrolledToBottom: boolean;
+  _tableWrapper: ?HTMLElement;
+  _userIsScrolling: boolean;
+
   constructor(props: Props) {
     super(props);
+    this._isScrolledToBottom = true;
+    this._userIsScrolling = false;
     (this: any)._handleClearButtonClick = this._handleClearButtonClick.bind(this);
-    (this: any)._renderRow = this._renderRow.bind(this);
+    (this: any)._handleTableWrapper = this._handleTableWrapper.bind(this);
+    (this: any)._handleScroll = this._handleScroll.bind(this);
+    (this: any)._handleScrollEnd = debounce(this._handleScrollEnd, 100);
+  }
+
+  componentDidUpdate(prevProps: Props): void {
+    // If records are added while we're scrolled to the bottom (or very very close, at least),
+    // automatically scroll.
+    if (this.props.records.length !== prevProps.records.length) {
+      this._autoscroll();
+    }
   }
 
   render(): ?ReactElement {
@@ -40,18 +57,54 @@ export default class OutputTable extends React.Component<void, Props, void> {
             Clear
           </button>
         </div>
-        <div className="nuclide-output-table-wrapper">
-          {this.props.records.map(this._renderRow)}
+        <div
+          className="nuclide-output-table-wrapper"
+          ref={this._handleTableWrapper}
+          onScroll={this._handleScroll}
+        >
+          {this.props.records.map(this._renderRow, this)}
         </div>
       </div>
     );
+  }
+
+  _handleScroll(event: SyntheticMouseEvent): void {
+    this._userIsScrolling = true;
+    this._handleScrollEnd();
+  }
+
+  _handleScrollEnd(): void {
+    this._userIsScrolling = false;
+
+    if (!this._tableWrapper) {
+      return;
+    }
+
+    const {scrollTop, scrollHeight, offsetHeight} = this._tableWrapper;
+    this._isScrolledToBottom = scrollHeight - (offsetHeight + scrollTop) < 5;
+  }
+
+  _handleTableWrapper(el: HTMLElement): void {
+    this._tableWrapper = el;
+    this._autoscroll();
   }
 
   _renderRow(record: Record, index: number): ReactElement {
     return <RecordView key={index} record={record} />;
   }
 
+  /**
+   * Scroll to the bottom of the list if autoscroll is active.
+   */
+  _autoscroll(): void {
+    if (!this._tableWrapper || this._userIsScrolling || !this._isScrolledToBottom) {
+      return;
+    }
+    this._tableWrapper.scrollTop = this._tableWrapper.scrollHeight;
+  }
+
   _handleClearButtonClick(event: SyntheticMouseEvent): void {
+    this._isScrolledToBottom = true;
     this.props.clearRecords();
   }
 
