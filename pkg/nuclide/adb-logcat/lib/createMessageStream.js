7e0fd1aafb624bdf4d2e2e57bdc50afd49ddb08d
Matthew Dapena-Tretter <matthewwithanm@fb.com>
2016-01-26 10:05:35 -0800

    Group logcat lines into entries and format them [adb logcat 4/N]
    Reviewed By: hansonw
    
    Differential Revision: D2833276
    
    fb-gh-sync-id: ae5c84b6f1bc4daf3766a931e9b885317cc9e9a1

--- pkg/nuclide/adb-logcat/lib/createMessageStream.js
+++ pkg/nuclide/adb-logcat/lib/createMessageStream.js
@@ -10,14 +10,83 @@
  */
 
 import type {Message} from '../../output/lib/types';
-import type Rx from 'rx';
+
+import createMessage from './createMessage';
+import parseLogcatMetadata from './parseLogcatMetadata';
+import {CompositeDisposable} from 'atom';
+import Rx from 'rx';
 
 export default function createMessageStream(
   line$: Rx.Observable<string>,
 ): Rx.Observable<Message> {
-  return line$
-    .map(line => ({
-      text: line,
-      level: 'info',
-    }));
+
+  // Separate the lines into groups, beginning with metadata lines.
+  return Rx.Observable.create(observer => {
+    let buffer = [];
+    let prevMetadata = null;
+    const prevLineIsBlank = () => buffer[buffer.length - 1] === '';
+
+    const flush = () => {
+      if (buffer.length === 0) {
+        return;
+      }
+
+      // Remove the empty line, which is a message separator.
+      if (prevLineIsBlank()) {
+        buffer.pop();
+      }
+
+      observer.onNext({
+        metadata: prevMetadata,
+        message: buffer.join('\n'),
+      });
+      buffer = [];
+      prevMetadata = null;
+    };
+
+    return new CompositeDisposable(
+
+      // Buffer incoming lines.
+      line$.subscribe(
+        // onNext
+        line => {
+          let metadata;
+          const hasPreviousLines = buffer.length > 0;
+
+          if (!hasPreviousLines || prevLineIsBlank()) {
+            metadata = parseLogcatMetadata(line);
+          }
+
+          if (metadata) {
+            // We've reached a new message so the other one must be done.
+            flush();
+            prevMetadata = metadata;
+          } else {
+            buffer.push(line);
+          }
+        },
+
+        // onError
+        error => {
+          flush();
+          observer.onError(error);
+        },
+
+        // onCompleted
+        () => {
+          flush();
+          observer.onCompleted();
+        },
+      ),
+
+      // We know *for certain* that we have a complete entry once we see the metadata for the next
+      // one. But what if the next one takes a long time to happen? After a certain point, we need
+      // to just assume we have the complete entry and move on.
+      line$.debounce(200).subscribe(flush),
+
+    );
+
+  })
+  .map(createMessage)
+  .share();
 }
---
+++ pkg/nuclide/adb-logcat/spec/createMessageStream-spec.js
@@ -0,0 +1,40 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+import createMessageStream from '../lib/createMessageStream';
+import Rx from 'rx';
+
+describe('createMessageStream', () => {
+
+  it('splits the output by message', () => {
+    waitsForPromise(async () => {
+      const output$ = Rx.Observable.from([
+        '[ 01-14 17:15:01.003   640:  654 I/ProcessStatsService ]',
+        'Prepared write state in 0ms',
+        '',
+        '[ 01-14 17:15:01.003   640:  654 I/ProcessStatsService ]',
+        'Prepared write state in 0ms',
+        '',
+      ]);
+
+      const message$ =  createMessageStream(output$)
+        .map(message => message.text)
+        .toArray();
+
+      const messages = await message$.toPromise();
+      expect(messages.length).toBe(2);
+      messages.forEach(message => {
+        expect(message).toBe('Prepared write state in 0ms');
+      });
+    });
+  });
+
+});
