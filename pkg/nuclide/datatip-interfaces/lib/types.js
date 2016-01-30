99398651ef3e6daf6fa26c745e64be9b16c81258
Jonas Gebhardt <jxg@fb.com>
2016-01-29 16:34:08 -0800

    Add type definitions
    Reviewed By: johnislarry
    
    Differential Revision: D2875386
    
    fb-gh-sync-id: 5ff9b264b687a12dbdcf2b856c037086d6e511a2

---
+++ pkg/nuclide/datatip-interfaces/lib/types.js
@@ -0,0 +1,25 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+export type Datatip = {
+  component: ReactElement,
+  range: atom$Range,
+  pinnable?: boolean,
+}
+
+export type DatatipProvider = {
+  datatip(editor: atom$TextEditor, bufferPosition: atom$Point): Promise<?Datatip>;
+  inclusionPriority: number;
+  validForScope(scopeName: string): boolean;
+  // A unique name for the provider to be used for analytics.
+  // It is recommended that it be the name of the provider's package.
+  providerName: string;
+};
