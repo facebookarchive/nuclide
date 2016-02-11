74b441f357b8b8eea18cd920f87013e918e6c338
Matthew Dapena-Tretter <matthewwithanm@fb.com>
2016-02-10 17:06:13 -0800

    Add commands for starting and stopping the debugger server [debugger 2/N]
    Reviewed By: ssorallen
    
    Differential Revision: D2909199
    
    fb-gh-sync-id: 4852cfc80613a93364d101dc535ca2d40d1a9d48
    shipit-source-id: 4852cfc80613a93364d101dc535ca2d40d1a9d48

--- pkg/nuclide/react-native-debugger/lib/Activation.js
+++ pkg/nuclide/react-native-debugger/lib/Activation.js
@@ -9,19 +9,57 @@
  * the root directory of this source tree.
  */
 
-import {CompositeDisposable} from 'atom';
+import {event as eventLib} from '../../commons';
+import ExecutorServer from '../../react-native-node-executor/lib/ExecutorServer';
+import serviceHub from '../../service-hub-plus';
+import {CompositeDisposable, Disposable} from 'atom';
+import Rx from 'rx';
+
+const {observableFromSubscribeFunction} = eventLib;
 
 export class Activation {
+  _connectionDisposables: ?CompositeDisposable;
   _disposables: CompositeDisposable;
 
-  constructor(state: ?Object): void {
-    // TODO(matthewwithanm): Assign all fields here so they are
-    // non-nullable for the lifetime of Activation.
-    this._disposables = new CompositeDisposable();
+  constructor(state: ?Object) {
+    this._disposables = new CompositeDisposable(
+      atom.commands.add('atom-workspace', {
+        'nuclide-react-native-debugger:connect': this._connect.bind(this),
+        'nuclide-react-native-debugger:disconnect': this._disconnect.bind(this),
+      }),
+      new Disposable(() => this._disconnect()),
+    );
+  }
+
+  _connect(): void {
+    this._disconnect();
+    const server = new ExecutorServer(8090);
+    this._connectionDisposables = new CompositeDisposable(
+      new Disposable(() => server.close()),
+      // $FlowIgnore: Not sure how to annotate combineLatest
+      Rx.Observable.combineLatest(
+        observableFromSubscribeFunction(server.onDidEvalApplicationScript.bind(server)),
+        Rx.Observable.fromPromise(serviceHub.consumeFirstProvider('nuclide-debugger.remote')),
+      )
+        .subscribe(([pid, debuggerService]) => {
+          debuggerService.debugNode(pid);
+        }),
+    );
+  }
+
+  _disconnect(): void {
+    if (this._connectionDisposables == null) {
+      return;
+    }
+    this._connectionDisposables.dispose();
+    this._connectionDisposables = null;
   }
 
   dispose(): void {
     this._disposables.dispose();
+    if (this._connectionDisposables != null) {
+      this._connectionDisposables.dispose();
+    }
   }
 
 }
