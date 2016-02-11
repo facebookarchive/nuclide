e7d2ceeddf47c1f7d8a96e3d84302355c642ceb0
Matthew Dapena-Tretter <matthewwithanm@fb.com>
2016-02-10 17:06:10 -0800

    Add empty package [debugger 1/N]
    Reviewed By: ssorallen
    
    Differential Revision: D2909195
    
    fb-gh-sync-id: b2ecf24a84ace4f0d4e755e75c7a89c029f5037b
    shipit-source-id: b2ecf24a84ace4f0d4e755e75c7a89c029f5037b

---
+++ pkg/nuclide/react-native-debugger/lib/main.js
@@ -0,0 +1,28 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+import type {Activation as ActivationType} from './Activation';
+
+import invariant from 'assert';
+
+let activation: ?ActivationType = null;
+
+export function activate(state: ?Object): void {
+  invariant(activation == null);
+  const {Activation} = require('./Activation');
+  activation = new Activation(state);
+}
+
+export function deactivate(): void {
+  invariant(activation != null);
+  activation.dispose();
+  activation = null;
+}
