b3681bb9d918b9ea605de0d4737315430a0af3c6
Matthew Dapena-Tretter <matthewwithanm@fb.com>
2016-02-18 18:30:23 -0800

    Get udid from xcrun [ios-simulator-logs 4/N]
    Reviewed By: johnislarry
    
    Differential Revision: D2947210
    
    fb-gh-sync-id: 6e611047a6974e77b505cc1f99312f8ab5331aa4
    shipit-source-id: 6e611047a6974e77b505cc1f99312f8ab5331aa4

--- pkg/nuclide/ios-simulator-logs/lib/createProcessStream.js
+++ pkg/nuclide/ios-simulator-logs/lib/createProcessStream.js
@@ -16,18 +16,63 @@ import path from 'path';
 import Rx from 'rx';
 
 export function createProcessStream(): Rx.Observable<string> {
-  return observeProcess(spawnTailProcess)
+  // Get a list of devices and their states from `xcrun simctl`.
+  const simctlOutput$ = observeProcess(spawnSimctlList)
     .filter(event => event.kind === 'stdout')
-    .map(event => (invariant(event.data != null), event.data));
+    .map(event => (invariant(event.data != null), event.data))
+    .reduce((acc, next) => acc + next, '')
+    .map(rawJson => JSON.parse(rawJson));
+
+  const udid$ = simctlOutput$
+    .map(json => {
+      const {devices} = json;
+      const device = _findAvailableDevice(devices);
+      if (device == null) {
+        throw new Error('No active iOS simulator found');
+      }
+      return device.udid;
+    })
+    // Retry every second until we find an active device.
+    .retryWhen(error$ => error$.delay(1000));
+
+  return udid$
+    .first()
+    .flatMap(udid => (
+      observeProcess(() => tailDeviceLogs(udid))
+        .filter(event => event.kind === 'stdout')
+        .map(event => (invariant(event.data != null), event.data))
+    ));
 }
 
-function spawnTailProcess(): Promise<child_process$ChildProcess> {
+/**
+ * Finds the first booted available device in a list of devices (formatted in the output style of
+ * `simctl`.). Exported for testing only.
+ */
+export function _findAvailableDevice(devices: Object): ?Object {
+  for (const key of Object.keys(devices)) {
+    for (const device of devices[key]) {
+      if (device.availability === '(available)' && device.state === 'Booted') {
+        return device;
+      }
+    }
+  }
+}
+
+function spawnSimctlList(): Promise<child_process$ChildProcess> {
+  return safeSpawn('xcrun', [
+    'simctl',
+    'list',
+    '--json',
+  ]);
+}
+
+function tailDeviceLogs(udid: string): Promise<child_process$ChildProcess> {
   const logDir = path.join(
     os.homedir(),
     'Library',
     'Logs',
     'CoreSimulator',
-    '721337CC-8D03-4BBC-8119-2DC710B05D14', // TODO: Don't hardcode this udid
+    udid,
     'asl',
   );
   return safeSpawn('syslog', [
