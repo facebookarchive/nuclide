d109d7b82740d2f3ae3f0564fe30c00979c1d6f3
Jeffrey Tan <jeffreytan@fb.com>
2016-02-22 10:34:24 -0800

    Nuclide lldb debugger launch (3/N)
    Reviewed By: johnislarry
    
    Differential Revision: D2888085
    
    fb-gh-sync-id: 68bf49201db7f7c310c45897f05c3414b6a6129c
    shipit-source-id: 68bf49201db7f7c310c45897f05c3414b6a6129c

---
+++ pkg/nuclide/debugger/lldb-server/scripts/debugger_store.py
@@ -0,0 +1,53 @@
+# Copyright (c) 2015-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the LICENSE file in
+# the root directory of this source tree.
+
+from file_manager import FileManager
+import serialize
+from remote_objects import RemoteObjectManager
+from modules import ModuleSourcePathUpdater
+
+
+class DebuggerStore:
+    '''Provides a central place for all debugger related managers.
+    '''
+    def __init__(self, channel, debugger, basepath='.'):
+        '''
+        channel: channel to send client chrome notification message.
+        debugger: lldb SBDebugger object.
+        '''
+
+        self._channel = channel
+        self._debugger = debugger
+        self._file_manager = FileManager(channel)
+        self._remote_object_manager = RemoteObjectManager()
+        self._location_serializer = serialize.LocationSerializer(
+            self._file_manager, basepath)
+        self._module_source_path_updater = ModuleSourcePathUpdater(
+            self._debugger.GetSelectedTarget(), self._file_manager, basepath)
+
+    @property
+    def channel(self):
+        return self._channel
+
+    @property
+    def debugger(self):
+        return self._debugger;
+
+    @property
+    def file_manager(self):
+        return self._file_manager
+
+    @property
+    def remote_object_manager(self):
+        return self._remote_object_manager
+
+    @property
+    def location_serializer(self):
+        return self._location_serializer
+
+    @property
+    def module_source_path_updater(self):
+        return self._module_source_path_updater
