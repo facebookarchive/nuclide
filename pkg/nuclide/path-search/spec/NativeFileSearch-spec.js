0ac4de9daca253f0b3b3b8380ae8b2b6565812af
Hanson Wang <hansonw@fb.com>
2016-02-29 10:17:35 -0800

    Sync fuzzy-native to v0.2.2
    Reviewed By: zertosh
    
    Differential Revision: D2988355
    
    fb-gh-sync-id: 1001b9adfcfeb93860c0a52de70db0c705148089
    shipit-source-id: 1001b9adfcfeb93860c0a52de70db0c705148089

--- pkg/nuclide/path-search/spec/NativeFileSearch-spec.js
+++ pkg/nuclide/path-search/spec/NativeFileSearch-spec.js
@@ -95,7 +95,7 @@ describe('NativeFileSearch', () => {
           path.join(dirPath, 'deeper/deeper'),
         ]);
         expect(indexes(results)).toEqual([
-          [0, 1, 2, 3, 4, 5],
+          [7, 8, 9, 10, 11, 12],
         ]);
       });
     });
