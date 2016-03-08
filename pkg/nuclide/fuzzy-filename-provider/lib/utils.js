14e2d142fe4bc67b0ca1ee37a65f4eab945d73b4
Jonas Gebhardt <jxg@fb.com>
2016-03-07 17:59:50 -0800

    always use fuzzy-file-search-service
    Reviewed By: hansonw
    
    Differential Revision: D3020762
    
    fb-gh-sync-id: 46fe9b270b785f2d366e408b0730e819e6c009b2
    shipit-source-id: 46fe9b270b785f2d366e408b0730e819e6c009b2

--- pkg/nuclide/fuzzy-filename-provider/lib/utils.js
+++ pkg/nuclide/fuzzy-filename-provider/lib/utils.js
@@ -10,7 +10,6 @@
  */
 
 import typeof * as FuzzyFileSearchService from '../../fuzzy-file-search-service';
-import featureConfig from '../../feature-config';
 
 import {getServiceByNuclideUri} from '../../client';
 
@@ -21,10 +20,9 @@ export async function getFuzzyFileSearchService(
   directory: atom$Directory,
 ): Promise<?FuzzyFileSearchService> {
   const directoryPath = directory.getPath();
-  const serviceName = featureConfig.get('nuclide-fuzzy-filename-provider.useRxMode')
-    ? 'FuzzyFileSearchRxService'
-    : 'FuzzyFileSearchService';
   const service: ?FuzzyFileSearchService = getServiceByNuclideUri(
-    serviceName, directoryPath);
+    'FuzzyFileSearchService',
+    directoryPath,
+  );
   return service;
 }
