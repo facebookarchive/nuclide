7b86f9e3e1cf5c30ee579bc04c52fdccd7c7de0d
Peter Hallam <peterhal@fb.com>
2016-03-09 10:42:23 -0800

    Add example test for mocking imported functions.
    Reviewed By: nmote
    
    Differential Revision: D3021470
    
    fb-gh-sync-id: fc906e791fcc2f1b06428fc56a91191bbedebbe7
    shipit-source-id: fc906e791fcc2f1b06428fc56a91191bbedebbe7

---
+++ pkg/nuclide/test-helpers/spec/toBeTested.js
@@ -0,0 +1,16 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+import {importedFunction} from './toBeMocked';
+
+export function functionToTest(): any {
+  return importedFunction(42);
+}
