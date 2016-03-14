91893fce83727a8929668c7f5cc28af5b1f6aae3
Andres Suarez <asuarez@fb.com>
2016-03-14 11:07:12 -0700

    Add nuclide-debugger-common package.json
    Reviewed By: johnislarry
    
    Differential Revision: D3046651
    
    fb-gh-sync-id: 92caab7861ab442f71271ffd3380e56599e30631
    shipit-source-id: 92caab7861ab442f71271ffd3380e56599e30631

---
+++ pkg/nuclide/debugger/common/lib/main.js
@@ -0,0 +1,12 @@
+'use babel';
+/* @noflow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+throw new Error("This module's main is not meant to be required.");
