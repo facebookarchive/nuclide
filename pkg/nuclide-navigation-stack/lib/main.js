d81c15b440583283048cd1de44e4bde9c8bb69c6
Peter Hallam <peterhal@fb.com>
2016-03-18 10:40:09 -0700

    Add empty nav-stack package.
    Reviewed By: nmote
    
    Differential Revision: D3063560
    
    fb-gh-sync-id: 5db1f8f8ec91048a5f9dc75e383350ee7b1b33e9
    shipit-source-id: 5db1f8f8ec91048a5f9dc75e383350ee7b1b33e9

---
+++ pkg/nuclide-navigation-stack/lib/main.js
@@ -0,0 +1,44 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+import {CompositeDisposable} from 'atom';
+
+class Activation {
+  _disposables: CompositeDisposable;
+
+  constructor(state: ?Object) {
+    this._disposables = new CompositeDisposable();
+  }
+
+  activate() {
+
+  }
+
+  dispose() {
+    this._disposables.dispose();
+  }
+}
+
+let activation: ?Activation = null;
+
+export function activate(state: ?Object) {
+  if (activation == null) {
+    activation = new Activation(state);
+    activation.activate();
+  }
+}
+
+export function deactivate() {
+  if (activation != null) {
+    activation.dispose();
+    activation = null;
+  }
+}
