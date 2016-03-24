32c4f039ed856eecb91ae7a2e4f7179ffca33a4c
Jonas Gebhardt <jxg@fb.com>
2016-03-23 20:43:43 -0700

    Add ButtonGroup component
    Reviewed By: zertosh
    
    Differential Revision: D3069207
    
    fb-gh-sync-id: a74f1949e52289b4250e544ea4eaa4f842f00bdc
    shipit-source-id: a74f1949e52289b4250e544ea4eaa4f842f00bdc

---
+++ pkg/nuclide-ui/lib/ButtonGroup.js
@@ -0,0 +1,60 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+import classnames from 'classnames';
+import {React} from 'react-for-atom';
+
+type ButtonGroupSize = 'EXTRA_SMALL' | 'SMALL' | 'LARGE';
+
+type Props = {
+  /** The size of the buttons within the group. Overrides any `size` props on child buttons. */
+  size?: ButtonGroupSize;
+  /** The contents of the ButtonGroup; Generally, an instance of `Button`. */
+  children: ReactElement;
+  className?: string;
+}
+
+export const ButtonGroupSizes = Object.freeze({
+  EXTRA_SMALL: 'EXTRA_SMALL',
+  SMALL: 'SMALL',
+  LARGE: 'LARGE',
+});
+
+const ButtonGroupSizeClassnames = Object.freeze({
+  EXTRA_SMALL: 'btn-group-xs',
+  SMALL: 'btn-group-sm',
+  LARGE: 'btn-group-lg',
+});
+
+/**
+ * Visually groups Buttons passed in as children.
+ */
+/* eslint-disable react/prop-types */
+export const ButtonGroup = (props: Props) => {
+  const {
+    size,
+    children,
+    className,
+  } = props;
+  const sizeClassName = size == null ? '' : ButtonGroupSizeClassnames[size] || '';
+  const newClassName = classnames(
+    className,
+    'btn-group',
+    {
+      [sizeClassName]: size != null,
+    },
+  );
+  return (
+    <div className={newClassName}>
+      {children}
+    </div>
+  );
+};
