56be7d08bb52de9f01b7ad9de2c8e9550546d66c
Jonas Gebhardt <jxg@fb.com>
2016-03-23 20:43:39 -0700

    Add Block component
    Reviewed By: ssorallen
    
    Differential Revision: D3069204
    
    fb-gh-sync-id: 7cb69057e0ebe6e6abbb17c2583fbf1fc2e7ffa1
    shipit-source-id: 7cb69057e0ebe6e6abbb17c2583fbf1fc2e7ffa1

---
+++ pkg/nuclide-ui/lib/Block.js
@@ -0,0 +1,22 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+import {React} from 'react-for-atom';
+
+type Props = {
+  children: ReactElement
+}
+
+/** A Block. */
+/* eslint-disable react/prop-types */
+export const Block = (props: Props) => (
+  <div className="block">{props.children}</div>
+);
