b5015e31a272080dce2bfd4d6de6a4f632f85653
Jonas Gebhardt <jxg@fb.com>
2016-03-23 20:43:46 -0700

    Add ProgressBar component
    Reviewed By: zertosh
    
    Differential Revision: D3069209
    
    fb-gh-sync-id: f2da295b5ccbe5263177f8506fcafb4867567829
    shipit-source-id: f2da295b5ccbe5263177f8506fcafb4867567829

---
+++ pkg/nuclide-ui/lib/ProgressBar.js
@@ -0,0 +1,29 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+import {React} from 'react-for-atom';
+
+type Props = {
+  /**
+   * The progress value. If none is provided, the Progressbar will render in `indefinite` mode.
+   * Use `indefinite mode` to indicate an initializing period,
+   * Prefer using the `LoadingSpinner` component for surfacing non-deterministic progress.
+   */
+  value?: number;
+  /** Determines the scaling of `value`. `min` is implicitly set to `0`. */
+  max?: number;
+}
+
+/** A Progressbar for showing deterministic progress. */
+/* eslint-disable react/prop-types */
+export const ProgressBar = (props: Props) => (
+  <progress value={props.value} max={props.max} {...props} />
+);
