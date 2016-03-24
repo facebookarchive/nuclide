9fb359f81edd6c9e1905d749e7808c1fb044a469
Jonas Gebhardt <jxg@fb.com>
2016-03-23 20:43:51 -0700

    Render example modules
    Reviewed By: zertosh
    
    Differential Revision: D3069213
    
    fb-gh-sync-id: 539e86b4cb5707762367a0a52200e88512b333a4
    shipit-source-id: 539e86b4cb5707762367a0a52200e88512b333a4

--- pkg/sample-ui-playground/lib/Playground.js
+++ pkg/sample-ui-playground/lib/Playground.js
@@ -11,6 +11,23 @@
 
 import {React} from 'react-for-atom';
 
+import {ButtonExamples} from '../../nuclide-ui/lib/Button.example';
+import {ProgressIndicatorExamples} from '../../nuclide-ui/lib/ProgressIndicators.example';
+
+const playgroundComponents = [
+  ButtonExamples,
+  ProgressIndicatorExamples,
+];
+
+type ComponentSpec = {
+  sectionName: string;
+  description: string;
+  examples: Array<{
+    title: string;
+    component: ReactComponent | () => ReactElement;
+  }>;
+}
+
 export class Playground extends React.Component {
   static gadgetId = 'sample-ui-playground-gadget';
   static defaultLocation = 'right';
@@ -23,9 +40,42 @@ export class Playground extends React.Component {
     return 'puzzle';
   }
 
+  renderExampleForComponent(
+    spec: ComponentSpec,
+    index: number,
+  ): ReactElement {
+    const {
+      sectionName,
+      description,
+      examples,
+    } = spec;
+    const flattenedExample = [].concat(...examples.map((example, i) => {
+      const {
+        title,
+        // $FlowIssue
+        component: Component,
+      } = example;
+      return [
+        <h2 key={`${index}:${i}t`}>{title}</h2>,
+        <div key={`${index}:${i}c`} className="nuclide-ui-playground-example">
+          <Component />
+        </div>,
+      ];
+    }));
+    return (
+      <section className="nuclide-ui-playground-bordered">
+        <h1>{sectionName}</h1>
+        <p>{description}</p>
+        {flattenedExample}
+      </section>
+    );
+  }
+
   render(): ReactElement {
+    const renderedExamples = playgroundComponents.map(this.renderExampleForComponent);
     return (
       <div className="nuclide-ui-playground">
+        {renderedExamples}
       </div>
     );
   }
