ccfddfd30f2f907eb39ed87f3580119048067a8f
Nat Mote <nmote@fb.com>
2016-03-31 13:12:48 -0700

    Main logic for tunnel vision
    Reviewed By: cbosuna
    
    Differential Revision: D3115165
    
    fb-gh-sync-id: dc46df8b6079730a5413dbf22ad4b037408f9482
    fbshipit-source-id: dc46df8b6079730a5413dbf22ad4b037408f9482

--- pkg/nuclide-tunnel-vision/lib/main.js
+++ pkg/nuclide-tunnel-vision/lib/main.js
@@ -13,15 +13,28 @@ import {CompositeDisposable, Disposable} from 'atom';
 import invariant from 'assert';
 
 export type TunnelVisionProvider = {
+  isVisible: () => boolean;
+  toggle: () => void;
 };
 
 class Activation {
   _disposables: CompositeDisposable;
+
   _providers: Set<TunnelVisionProvider>;
+  // non-null iff we are in tunnel vision mode. The set of providers to show when we exit tunnel
+  // vision mode.
+  _restoreState: ?Set<TunnelVisionProvider>;
 
   constructor(state: ?Object) {
     this._disposables = new CompositeDisposable();
     this._providers = new Set();
+    this._restoreState = null;
+
+    atom.commands.add(
+      atom.views.getView(atom.workspace),
+      'nuclide-tunnel-vision:toggle',
+      this._toggleTunnelVision.bind(this),
+    );
   }
 
   dispose() {
@@ -33,6 +46,40 @@ class Activation {
     return new Disposable(() => {
       this._providers.delete(provider);
     });
+  }
+
+  _toggleTunnelVision(): void {
+    if (this._isInTunnelVision()) {
+      this._exitTunnelVision();
+    } else {
+      this._enterTunnelVision();
+    }
+  }
+
+  _isInTunnelVision() {
+    return this._restoreState != null;
+  }
+
+  _enterTunnelVision(): void {
+    const restoreState = new Set();
+    for (const provider of this._providers) {
+      if (provider.isVisible()) {
+        provider.toggle();
+        restoreState.add(provider);
+      }
+    }
+    this._restoreState = restoreState;
+  }
+
+  _exitTunnelVision(): void {
+    const restoreState = this._restoreState;
+    invariant(restoreState != null);
+    for (const provider of restoreState) {
+      if (!provider.isVisible()) {
+        provider.toggle();
+      }
+    }
+    this._restoreState = null;
   }
 }
 
