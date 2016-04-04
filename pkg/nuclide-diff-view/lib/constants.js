4975b3cd5b6b9acb20f563bdbcc47a2ab52e2125
Mostafa Eweda <most@fb.com>
2016-04-04 11:38:31 -0700

    Diff View: Refactor constants to ES6 export
    Reviewed By: zertosh
    
    Differential Revision: D3134284
    
    fb-gh-sync-id: 7a2a167d78039747945c649fb578f279bb13c9f0
    fbshipit-source-id: 7a2a167d78039747945c649fb578f279bb13c9f0

--- pkg/nuclide-diff-view/lib/constants.js
+++ pkg/nuclide-diff-view/lib/constants.js
@@ -9,13 +9,10 @@
  * the root directory of this source tree.
  */
 
-const {
-  StatusCodeNumber: HgStatusCodeNumber,
-} = require('../../nuclide-hg-repository-base').hgConstants;
-
 import type {
   StatusCodeNumberValue,
 } from '../../nuclide-hg-repository-base/lib/HgService';
+
 import type {
   CommitModeType,
   CommitModeStateType,
@@ -26,10 +23,16 @@ import type {
   DiffOptionType,
 } from './types';
 
-const GK_DIFF_VIEW_PUBLISH: string = 'nuclide_diff_view_publish';
-const TOOLBAR_VISIBLE_SETTING: string = 'nuclide-diff-view.toolbarVisible';
+import {
+  hgConstants,
+} from '../../nuclide-hg-repository-base';
 
-const FileChangeStatus = Object.freeze({
+const {StatusCodeNumber: HgStatusCodeNumber} = hgConstants;
+
+export const GK_DIFF_VIEW_PUBLISH = 'nuclide_diff_view_publish';
+export const TOOLBAR_VISIBLE_SETTING = 'nuclide-diff-view.toolbarVisible';
+
+export const FileChangeStatus = Object.freeze({
   ADDED: 1,
   MODIFIED: 2,
   MISSING: 3,
@@ -39,7 +42,7 @@ const FileChangeStatus = Object.freeze({
 
 (FileChangeStatus: { [key: string]: FileChangeStatusValue });
 
-const DiffMode = Object.freeze({
+export const DiffMode = Object.freeze({
   BROWSE_MODE: 'Browse',
   COMMIT_MODE: 'Commit',
   PUBLISH_MODE: 'Publish',
@@ -48,7 +51,7 @@ const DiffMode = Object.freeze({
 // This is to work around flow's missing support of enums.
 (DiffMode: { [key: string]: DiffModeType });
 
-const DiffOption = Object.freeze({
+export const DiffOption = Object.freeze({
   DIRTY: 'Dirty',
   LAST_COMMIT: 'Last Commit',
   COMPARE_COMMIT: 'Compare Commit',
@@ -57,7 +60,7 @@ const DiffOption = Object.freeze({
 // This is to work around flow's missing support of enums.
 (DiffOption: { [key: string]: DiffOptionType });
 
-const CommitMode = Object.freeze({
+export const CommitMode = Object.freeze({
   COMMIT: 'Commit',
   AMEND: 'Amend',
 });
@@ -65,7 +68,7 @@ const CommitMode = Object.freeze({
 // This is to work around flow's missing support of enums.
 (CommitMode: { [key: string]: CommitModeType });
 
-const CommitModeState = Object.freeze({
+export const CommitModeState = Object.freeze({
   READY: 'Ready',
   LOADING_COMMIT_MESSAGE: 'Loading Commit Message',
   AWAITING_COMMIT: 'Awaiting Commit',
@@ -74,7 +77,7 @@ const CommitModeState = Object.freeze({
 // This is to work around flow's missing support of enums.
 (CommitModeState: { [key: string]: CommitModeStateType });
 
-const PublishMode = Object.freeze({
+export const PublishMode = Object.freeze({
   CREATE: 'Create',
   UPDATE: 'Update',
 });
@@ -82,7 +85,7 @@ const PublishMode = Object.freeze({
 // This is to work around flow's missing support of enums.
 (PublishMode: { [key: string]: PublishModeType });
 
-const PublishModeState = Object.freeze({
+export const PublishModeState = Object.freeze({
   READY: 'Ready',
   LOADING_PUBLISH_MESSAGE: 'Loading Publish Message',
   AWAITING_PUBLISH: 'Awaiting Publish',
@@ -91,33 +94,20 @@ const PublishModeState = Object.freeze({
 // This is to work around flow's missing support of enums.
 (PublishModeState: { [key: string]: PublishModeStateType });
 
-const HgStatusToFileChangeStatus : {[key: StatusCodeNumberValue]: FileChangeStatusValue} = {
-  [HgStatusCodeNumber.ADDED]: FileChangeStatus.ADDED,
-  [HgStatusCodeNumber.MODIFIED]: FileChangeStatus.MODIFIED,
-  [HgStatusCodeNumber.MISSING]: FileChangeStatus.MISSING,
-  [HgStatusCodeNumber.REMOVED]: FileChangeStatus.REMOVED,
-  [HgStatusCodeNumber.UNTRACKED]: FileChangeStatus.UNTRACKED,
-};
+export const HgStatusToFileChangeStatus
+  : {[key: StatusCodeNumberValue]: FileChangeStatusValue} = Object.freeze({
+    [HgStatusCodeNumber.ADDED]: FileChangeStatus.ADDED,
+    [HgStatusCodeNumber.MODIFIED]: FileChangeStatus.MODIFIED,
+    [HgStatusCodeNumber.MISSING]: FileChangeStatus.MISSING,
+    [HgStatusCodeNumber.REMOVED]: FileChangeStatus.REMOVED,
+    [HgStatusCodeNumber.UNTRACKED]: FileChangeStatus.UNTRACKED,
+  }
+);
 
-const FileChangeStatusToPrefix: {[key: FileChangeStatusValue]: string} = {
+export const FileChangeStatusToPrefix: {[key: FileChangeStatusValue]: string} = Object.freeze({
   [FileChangeStatus.ADDED]: '[A] ',
   [FileChangeStatus.MODIFIED]: '[M] ',
   [FileChangeStatus.MISSING]: '[!] ',
   [FileChangeStatus.REMOVED]: '[D] ',
   [FileChangeStatus.UNTRACKED]: '[?] ',
-};
-
-module.exports = {
-  DiffMode,
-  DiffOption,
-  CommitMode,
-  CommitModeState,
-  PublishMode,
-  PublishModeState,
-  FileChangeStatus,
-  HgStatusToFileChangeStatus,
-  FileChangeStatusToPrefix,
-  HgStatusCodeNumber,
-  GK_DIFF_VIEW_PUBLISH,
-  TOOLBAR_VISIBLE_SETTING,
-};
+});
