77140a03e05ba3af11e9393fa63ef6047ae057cf
Nat Mote <nmote@fb.com>
2016-04-04 10:35:34 -0700

    tunnel-vision integration test
    Summary:As requested in D3115222
    
    Depends on D3115222
    
    Reviewed By: johnislarry
    
    Differential Revision: D3126944
    
    fb-gh-sync-id: 98ec268ac3bae9998f476f18036b7c96f5f2188f
    fbshipit-source-id: 98ec268ac3bae9998f476f18036b7c96f5f2188f

---
+++ spec/tunnel-vision-integration-spec.js
@@ -0,0 +1,45 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+import {
+  activateAllPackages,
+  jasmineIntegrationTestSetup,
+  deactivateAllPackages,
+} from '../pkg/nuclide-integration-test-helpers';
+
+describe('nuclide-tunnel-vision', () => {
+  it('hides and shows things as expected', () => {
+    waitsForPromise({timeout: 60000}, async () => {
+      // Configure some jasmine specific things for integration testing.
+      jasmineIntegrationTestSetup();
+      // Activate nuclide packages.
+      await activateAllPackages();
+
+      const commandTarget = atom.views.getView(atom.workspace);
+
+      atom.commands.dispatch(commandTarget, 'nuclide-outline-view:show');
+      expect(isOutlineViewVisible()).toBeTruthy();
+
+      atom.commands.dispatch(commandTarget, 'nuclide-tunnel-vision:toggle');
+      expect(isOutlineViewVisible()).toBeFalsy();
+
+      atom.commands.dispatch(commandTarget, 'nuclide-tunnel-vision:toggle');
+      expect(isOutlineViewVisible()).toBeTruthy();
+
+      // Deactivate nuclide packages.
+      deactivateAllPackages();
+    });
+  });
+});
+
+function isOutlineViewVisible(): boolean {
+  return document.querySelector('.nuclide-outline-view') != null;
+}
