7e674275257158f8e6f519fa7157d0790133d637
Nat Mote <nmote@fb.com>
2016-04-05 15:12:46 -0700

    Restructure tunnel vision tests
    Summary: Will make it easier to test deserialization
    
    Reviewed By: peterhal
    
    Differential Revision: D3136876
    
    fb-gh-sync-id: c9144b150ff6b64bf4cece73adfd4aae7cc49b63
    fbshipit-source-id: c9144b150ff6b64bf4cece73adfd4aae7cc49b63

--- pkg/nuclide-tunnel-vision/spec/TunnelVision-spec.js
+++ pkg/nuclide-tunnel-vision/spec/TunnelVision-spec.js
@@ -20,62 +20,66 @@ describe('TunnelVision', () => {
     tunnelVision = new TunnelVision();
     provider1 = new FakeProvider('provider1');
     provider2 = new FakeProvider('provider2');
-
-    tunnelVision.consumeTunnelVisionProvider(provider1);
-    tunnelVision.consumeTunnelVisionProvider(provider2);
   });
 
-  it('should hide the providers', () => {
-    tunnelVision.toggleTunnelVision();
-    expect(provider1.isVisible()).toBeFalsy();
-    expect(provider2.isVisible()).toBeFalsy();
-  });
+  describe('during a Nuclide session', () => {
+    beforeEach(() => {
+      tunnelVision.consumeTunnelVisionProvider(provider1);
+      tunnelVision.consumeTunnelVisionProvider(provider2);
+    });
 
-  it('should show the providers again', () => {
-    tunnelVision.toggleTunnelVision();
-    tunnelVision.toggleTunnelVision();
-    expect(provider1.isVisible()).toBeTruthy();
-    expect(provider2.isVisible()).toBeTruthy();
-  });
+    it('should hide the providers', () => {
+      tunnelVision.toggleTunnelVision();
+      expect(provider1.isVisible()).toBeFalsy();
+      expect(provider2.isVisible()).toBeFalsy();
+    });
 
-  it('should only restore the providers that were visible', () => {
-    provider1.toggle();
-    expect(provider1.isVisible()).toBeFalsy();
-    expect(provider2.isVisible()).toBeTruthy();
+    it('should show the providers again', () => {
+      tunnelVision.toggleTunnelVision();
+      tunnelVision.toggleTunnelVision();
+      expect(provider1.isVisible()).toBeTruthy();
+      expect(provider2.isVisible()).toBeTruthy();
+    });
 
-    tunnelVision.toggleTunnelVision();
+    it('should only restore the providers that were visible', () => {
+      provider1.toggle();
+      expect(provider1.isVisible()).toBeFalsy();
+      expect(provider2.isVisible()).toBeTruthy();
 
-    expect(provider1.isVisible()).toBeFalsy();
-    expect(provider2.isVisible()).toBeFalsy();
+      tunnelVision.toggleTunnelVision();
 
-    tunnelVision.toggleTunnelVision();
-    expect(provider1.isVisible()).toBeFalsy();
-    expect(provider2.isVisible()).toBeTruthy();
-  });
+      expect(provider1.isVisible()).toBeFalsy();
+      expect(provider2.isVisible()).toBeFalsy();
 
-  it('should re-enter tunnel vision if a provider is manually opened', () => {
-    provider1.toggle();
-    expect(provider1.isVisible()).toBeFalsy();
-    expect(provider2.isVisible()).toBeTruthy();
+      tunnelVision.toggleTunnelVision();
+      expect(provider1.isVisible()).toBeFalsy();
+      expect(provider2.isVisible()).toBeTruthy();
+    });
 
-    // Enter tunnel vision
-    tunnelVision.toggleTunnelVision();
+    it('should re-enter tunnel vision if a provider is manually opened', () => {
+      provider1.toggle();
+      expect(provider1.isVisible()).toBeFalsy();
+      expect(provider2.isVisible()).toBeTruthy();
 
-    // User manually opens something
-    provider1.toggle();
+      // Enter tunnel vision
+      tunnelVision.toggleTunnelVision();
 
-    // Since something is open, the intent is probably to get back into the tunnel vision state. So,
-    // the provider should be hidden.
-    tunnelVision.toggleTunnelVision();
-    expect(provider1.isVisible()).toBeFalsy();
-    expect(provider2.isVisible()).toBeFalsy();
+      // User manually opens something
+      provider1.toggle();
 
-    // Now they are leaving tunnel vision. We should restore all the providers that we have
-    // previously hidden. So we shoudl restore provider2 (hidden on first entry) and provider1
-    // (hidden on second entry)
-    tunnelVision.toggleTunnelVision();
-    expect(provider1.isVisible()).toBeTruthy();
-    expect(provider2.isVisible()).toBeTruthy();
+      // Since something is open, the intent is probably to get back into the tunnel vision state.
+      // So, the provider should be hidden.
+      tunnelVision.toggleTunnelVision();
+      expect(provider1.isVisible()).toBeFalsy();
+      expect(provider2.isVisible()).toBeFalsy();
+
+      // Now they are leaving tunnel vision. We should restore all the providers that we have
+      // previously hidden. So we shoudl restore provider2 (hidden on first entry) and provider1
+      // (hidden on second entry)
+      tunnelVision.toggleTunnelVision();
+      expect(provider1.isVisible()).toBeTruthy();
+      expect(provider2.isVisible()).toBeTruthy();
+    });
   });
 });
 
