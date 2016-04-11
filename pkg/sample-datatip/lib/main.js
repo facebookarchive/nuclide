6492c7375fc3348faa9ebe762466bbba95324122
Jonas Gebhardt <jxg@fb.com>
2016-04-11 14:51:40 -0700

    Unsubscribe datatip provider on package deactivation
    Summary: In D3130517, I inverted the consumer/provider relationship of the datatip service. As a result of that, toggling the datatip package on & off works correctly. However, each provider also needs to unregister itself when its package deactivates. This diff is part of a stack that updates all datatip providers accordingly.
    
    Reviewed By: ssorallen
    
    Differential Revision: D3135282
    
    fb-gh-sync-id: ad6aa2ae05c45779ff301305287a7b65ddbdeba3
    fbshipit-source-id: ad6aa2ae05c45779ff301305287a7b65ddbdeba3

--- pkg/sample-datatip/lib/main.js
+++ pkg/sample-datatip/lib/main.js
@@ -14,23 +14,61 @@ import type {
   DatatipService,
 } from '../../nuclide-datatip-interfaces';
 
-import {Disposable} from 'atom';
+import {
+  CompositeDisposable,
+  Disposable,
+} from 'atom';
+import invariant from 'assert';
 import {datatip} from './SampleDatatip';
 
 const PACKAGE_NAME = 'sample-datatip';
 
-function createDatatipProvider(): DatatipProvider {
-  return {
-    // show the sample datatip for every type of file
-    validForScope: (scope: string) => true,
-    providerName: PACKAGE_NAME,
-    inclusionPriority: 1,
-    datatip,
-  };
+const datatipProvider: DatatipProvider = {
+  // show the sample datatip for every type of file
+  validForScope: (scope: string) => true,
+  providerName: PACKAGE_NAME,
+  inclusionPriority: 1,
+  datatip,
+};
+
+class Activation {
+  _disposables: CompositeDisposable;
+  datatipService: ?DatatipService;
+
+  constructor(state: ?mixed) {
+    this._disposables = new CompositeDisposable();
+  }
+
+  consumeDatatipService(service: DatatipService): IDisposable {
+    service.addProvider(datatipProvider);
+    this.datatipService = service;
+    const disposable = new Disposable(() => {
+      service.removeProvider(datatipProvider);
+      this.datatipService = null;
+    });
+    this._disposables.add(disposable);
+    return disposable;
+  }
+
+  dispose(): void {
+    this._disposables.dispose();
+  }
+}
+
+let activation: ?Activation = null;
+
+export function activate(state: ?Object): void {
+  activation = new Activation(state);
 }
 
 export function consumeDatatipService(service: DatatipService): IDisposable {
-  const provider = createDatatipProvider();
-  service.addProvider(provider);
-  return new Disposable(() => service.removeProvider(provider));
+  invariant(activation);
+  return activation.consumeDatatipService(service);
+}
+
+export function deactivate(): void {
+  if (activation != null) {
+    activation.dispose();
+    activation = null;
+  }
 }
