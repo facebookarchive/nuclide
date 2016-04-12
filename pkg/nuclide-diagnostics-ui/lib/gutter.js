7fe6620dfbe6d41c0c628b72ffd2310c3e25a8ec
Jonas Gebhardt <jxg@fb.com>
2016-04-11 18:56:27 -0700

    Factor out DiagnosticsMessage component
    Summary:We want to be able to reuse the diagnostics components for gutter messages e.g. in datatips.
    
    This is a split diff. To see the entire refactoring, check out D3152702.
    
    Reviewed By: zertosh
    
    Differential Revision: D3152998
    
    fb-gh-sync-id: f268df3587df3b5af6b552d0a7f9c02d692c0d54
    fbshipit-source-id: f268df3587df3b5af6b552d0a7f9c02d692c0d54

--- pkg/nuclide-diagnostics-ui/lib/gutter.js
+++ pkg/nuclide-diagnostics-ui/lib/gutter.js
@@ -12,16 +12,12 @@
 import type {
   FileMessageUpdate,
   FileDiagnosticMessage,
-  Trace,
 } from '../../nuclide-diagnostics-base';
 
 import type {NuclideUri} from '../../nuclide-remote-uri';
 import {goToLocation as atomGoToLocation} from '../../nuclide-atom-helpers';
 
-import invariant from 'assert';
-
-import {DiagnosticsMessageText} from '../../nuclide-ui/lib/DiagnosticsMessageText';
-import {DiagnosticsTraceItem} from '../../nuclide-ui/lib/DiagnosticsTraceItem';
+import {DiagnosticsMessage} from '../../nuclide-ui/lib/DiagnosticsMessage';
 const {track} = require('../../nuclide-analytics');
 const {
   React,
@@ -218,7 +214,6 @@ function showPopupFor(
     fixer: (message: FileDiagnosticMessage) => void,
     ): HTMLElement {
   const children = messages.map((message, index) => {
-    const contents = createElementForMessage(message, goToLocation, fixer);
     const diagnosticTypeClass = message.type === 'Error'
       ? 'nuclide-diagnostics-gutter-ui-popup-error'
       : 'nuclide-diagnostics-gutter-ui-popup-warning';
@@ -227,7 +222,12 @@ function showPopupFor(
       `native-key-bindings nuclide-diagnostics-gutter-ui-popup-diagnostic ${diagnosticTypeClass}`;
     return (
       <div className={classes} key={index} tabIndex={-1}>
-        {contents}
+        <DiagnosticsMessage
+          fixer={fixer}
+          goToLocation={goToLocation}
+          key={index}
+          message={message}
+        />
       </div>
     );
   });
@@ -277,82 +277,6 @@ function showPopupFor(
       });
     });
   }
-}
-
-function createElementForMessage(
-  message: FileDiagnosticMessage,
-  goToLocation: (path: string, line: number) => mixed,
-  fixer: (message: FileDiagnosticMessage) => void,
-): ReactElement {
-  const providerClassName = message.type === 'Error'
-    ? 'highlight-error'
-    : 'highlight-warning';
-  const copy = () => {
-    const text = plainTextForDiagnostic(message);
-    atom.clipboard.write(text);
-  };
-  let fixButton = null;
-  if (message.fix != null) {
-    const applyFix = () => {
-      fixer(message);
-      track('diagnostics-gutter-autofix');
-    };
-    fixButton = (
-      <button className="btn btn-xs" onClick={applyFix}>Fix</button>
-    );
-  }
-  const header = (
-    <div className="nuclide-diagnostics-gutter-ui-popup-header">
-      {fixButton}
-      <button className="btn btn-xs" onClick={copy}>Copy</button>
-      <span className={`pull-right ${providerClassName}`}>{message.providerName}</span>
-    </div>
-  );
-  const traceElements = message.trace
-    ? message.trace.map((traceItem, i) =>
-      <DiagnosticsTraceItem
-        key={i}
-        trace={traceItem}
-        goToLocation={goToLocation}
-      />
-    )
-    : null;
-  return (
-    <div>
-      {header}
-      <div>
-        <DiagnosticsMessageText message={message} />
-      </div>
-      {traceElements}
-    </div>
-  );
-}
-
-function plainTextForDiagnostic(message: FileDiagnosticMessage): string {
-  const {getPath} = require('../../nuclide-remote-uri');
-  function plainTextForItem(item: FileDiagnosticMessage | Trace): string {
-    let mainComponent = undefined;
-    if (item.html != null) {
-      // Quick and dirty way to get an approximation for the plain text from HTML. This will work in
-      // simple cases, anyway.
-      mainComponent = item.html.replace('<br/>', '\n').replace(/<[^>]*>/g, '');
-    } else {
-      invariant(item.text != null);
-      mainComponent = item.text;
-    }
-
-    let pathComponent = undefined;
-    if (item.filePath == null) {
-      pathComponent = '';
-    } else {
-      const lineComponent = item.range != null ? `:${item.range.start.row + 1}` : '';
-      pathComponent = ': ' + getPath(item.filePath) + lineComponent;
-    }
-
-    return mainComponent + pathComponent;
-  }
-  const trace = message.trace != null ? message.trace : [];
-  return [message, ...trace].map(plainTextForItem).join('\n');
 }
 
 class DiagnosticsPopup extends React.Component {
---
+++ pkg/nuclide-ui/lib/DiagnosticsMessage.js
@@ -0,0 +1,107 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+import type {
+  FileDiagnosticMessage,
+  Trace,
+} from '../../nuclide-diagnostics-base';
+
+import invariant from 'assert';
+import {React} from 'react-for-atom';
+import {Button} from './Button';
+import {DiagnosticsMessageText} from './DiagnosticsMessageText';
+import {DiagnosticsTraceItem} from './DiagnosticsTraceItem';
+
+type DiagnosticsMessageProps = {
+  message: FileDiagnosticMessage;
+  goToLocation: (path: string, line: number) => mixed;
+  fixer: (message: FileDiagnosticMessage) => void;
+};
+
+function plainTextForItem(item: FileDiagnosticMessage | Trace): string {
+  const {getPath} = require('../../nuclide-remote-uri');
+  let mainComponent = undefined;
+  if (item.html != null) {
+    // Quick and dirty way to get an approximation for the plain text from HTML.
+    // This will work in simple cases, anyway.
+    mainComponent = item.html.replace('<br/>', '\n').replace(/<[^>]*>/g, '');
+  } else {
+    invariant(item.text != null);
+    mainComponent = item.text;
+  }
+
+  let pathComponent;
+  if (item.filePath == null) {
+    pathComponent = '';
+  } else {
+    const lineComponent = item.range != null ? `:${item.range.start.row + 1}` : '';
+    pathComponent = ': ' + getPath(item.filePath) + lineComponent;
+  }
+  return mainComponent + pathComponent;
+}
+
+function plainTextForDiagnostic(message: FileDiagnosticMessage): string {
+  const trace = message.trace != null ? message.trace : [];
+  return [message, ...trace].map(plainTextForItem).join('\n');
+}
+
+/**
+ * Visually groups Buttons passed in as children.
+ */
+/* eslint-disable react/prop-types */
+export const DiagnosticsMessage = (props: DiagnosticsMessageProps) => {
+  const {
+      message,
+      goToLocation,
+      fixer,
+  } = props;
+  const providerClassName = message.type === 'Error'
+    ? 'highlight-error'
+    : 'highlight-warning';
+  const copy = () => {
+    const text = plainTextForDiagnostic(message);
+    atom.clipboard.write(text);
+  };
+  let fixButton = null;
+  if (message.fix != null) {
+    const applyFix = () => {
+      fixer(message);
+    };
+    fixButton = (
+      <Button size="EXTRA_SMALL" onClick={applyFix}>Fix</Button>
+    );
+  }
+  const header = (
+    <div className="nuclide-diagnostics-gutter-ui-popup-header">
+      {fixButton}
+      <Button size="EXTRA_SMALL" onClick={copy}>Copy</Button>
+      <span className={`pull-right ${providerClassName}`}>{message.providerName}</span>
+    </div>
+  );
+  const traceElements = message.trace
+    ? message.trace.map((traceItem, i) =>
+      <DiagnosticsTraceItem
+        key={i}
+        trace={traceItem}
+        goToLocation={goToLocation}
+      />
+    )
+    : null;
+  return (
+    <div>
+      {header}
+      <div>
+        <DiagnosticsMessageText message={message} />
+      </div>
+      {traceElements}
+    </div>
+  );
+};
