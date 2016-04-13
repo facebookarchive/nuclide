ba8545035036b9dbd25bd135b3a3fcb29741da1f
Peter Hallam <peterhal@fb.com>
2016-04-12 19:03:06 -0700

    Remove LocalHackLanguage.js
    Summary: This file name doesn't make sense anymore. Move its contents to the same file as its clients.
    
    Reviewed By: mostafaeweda
    
    Differential Revision: D3170884
    
    fb-gh-sync-id: edb0382420e4eab5dbc23f6e3f5d972edf64246b
    fbshipit-source-id: edb0382420e4eab5dbc23f6e3f5d972edf64246b

--- pkg/nuclide-hack/lib/ServerHackLanguage.js
+++ pkg/nuclide-hack/lib/ServerHackLanguage.js
@@ -12,6 +12,8 @@
 import type {NuclideUri} from '../../nuclide-remote-uri';
 import type {CompletionResult} from './HackLanguage';
 import type {
+  HackCompletion,
+  HackDefinitionResult,
   HackDiagnostic,
   HackSearchPosition,
   HackRange,
@@ -19,18 +21,13 @@ import type {
   HackOutline,
 } from '../../nuclide-hack-base/lib/HackService';
 import typeof * as HackService from '../../nuclide-hack-base/lib/HackService';
+import type {SymbolTypeValue} from '../../nuclide-hack-common';
 import {TypeCoverageRegion} from './TypedRegions';
 
 import {Range} from 'atom';
 import {getLogger} from '../../nuclide-logging';
 import {convertTypedRegionsToCoverageRegions} from './TypedRegions';
-import {
-  markFileForCompletion,
-  processCompletions,
-  processDefinitionsForXhp,
-  getSymbolType,
-  SYMBOL_TYPES_WITH_REFERENCES,
-} from './LocalHackLanguage';
+import {SymbolType} from '../../nuclide-hack-common';
 
 /**
  * Serves language requests from HackService.
@@ -201,4 +198,92 @@ function hackRangeToAtomRange(position: HackRange): atom$Range {
         [position.line - 1, position.char_start - 1],
         [position.line - 1, position.char_end],
       );
+}
+
+// The xhp char regex include : and - to match xhp tags like <ui:button-group>.
+const xhpCharRegex = /[\w:-]/;
+
+const stringToSymbolType = {
+  'class': SymbolType.CLASS,
+  'function': SymbolType.FUNCTION,
+  'method': SymbolType.METHOD,
+  'local': SymbolType.LOCAL,
+};
+
+// Symbol types we can get references for.
+const SYMBOL_TYPES_WITH_REFERENCES = new Set([
+  SymbolType.CLASS,
+  SymbolType.FUNCTION,
+  SymbolType.METHOD,
+]);
+
+function getSymbolType(input: string): SymbolTypeValue {
+  let symbolType = stringToSymbolType[input];
+  if (typeof symbolType === 'undefined') {
+    symbolType = SymbolType.METHOD;
+  }
+  return symbolType;
+}
+
+function processCompletions(completionsResponse: Array<HackCompletion>):
+    Array<CompletionResult> {
+  return completionsResponse.map(completion => {
+    const {name, func_details: functionDetails} = completion;
+    let {type} = completion;
+    if (type && type.indexOf('(') === 0 && type.lastIndexOf(')') === type.length - 1) {
+      type = type.substring(1, type.length - 1);
+    }
+    let matchSnippet = name;
+    if (functionDetails) {
+      const {params} = functionDetails;
+      // Construct the snippet: e.g. myFunction(${1:$arg1}, ${2:$arg2});
+      const paramsString = params.map(
+        (param, index) => '${' + (index + 1) + ':' + param.name + '}').join(', ');
+      matchSnippet = name + '(' + paramsString + ')';
+    }
+    return {
+      matchSnippet,
+      matchText: name,
+      matchType: type,
+    };
+  });
+}
+
+// Calculate the offset of the cursor from the beginning of the file.
+// Then insert AUTO332 in at this offset. (Hack uses this as a marker.)
+function markFileForCompletion(contents: string, offset: number): string {
+  return contents.substring(0, offset) +
+      'AUTO332' + contents.substring(offset, contents.length);
+}
+
+function processDefinitionsForXhp(
+  definitionResult: ?HackDefinitionResult,
+  column: number,
+  lineText: string,
+): Array<HackSearchPosition> {
+  if (!definitionResult) {
+    return [];
+  }
+  const {definitions} = definitionResult;
+  return definitions.map(definition => {
+    let {name} = definition;
+    if (name.startsWith(':')) {
+      // XHP class name, usages omit the leading ':'.
+      name = name.substring(1);
+    }
+    const definitionIndex = lineText.indexOf(name);
+    if (
+      definitionIndex === -1 ||
+      definitionIndex >= column ||
+      !xhpCharRegex.test(lineText.substring(definitionIndex, column))
+    ) {
+      return definition;
+    } else {
+      return {
+        ...definition,
+        searchStartColumn: definitionIndex,
+        searchEndColumn: definitionIndex + definition.name.length,
+      };
+    }
+  });
 }
