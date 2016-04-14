4325aa3163e688855dce7b28786f2faf5fad632d
Jonas Gebhardt <jxg@fb.com>
2016-04-14 16:50:47 -0700

    Add Badge component
    Summary: Mirroring the Atom styleguide.
    
    Reviewed By: ssorallen
    
    Differential Revision: D3175028
    
    fb-gh-sync-id: 76b7d0f356af023a2c571c5716fc33837a8972c8
    fbshipit-source-id: 76b7d0f356af023a2c571c5716fc33837a8972c8

---
+++ pkg/nuclide-ui/lib/Badge.js
@@ -0,0 +1,80 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+import type {Octicon} from './Octicons';
+
+import classnames from 'classnames';
+import {React} from 'react-for-atom';
+
+type BadgeSize = 'medium' | 'small' | 'large';
+type BadgeColor = 'info' | 'success' | 'warning' | 'error';
+
+type Props = {
+  className?: string;
+  color?: BadgeColor;
+  /** Octicon icon name, without the `icon-` prefix. E.g. `'arrow-up'` */
+  icon?: Octicon;
+  size?: BadgeSize;
+  /** The value displayed inside the badge. */
+  value: number;
+};
+
+export const BadgeSizes = Object.freeze({
+  medium: 'medium',
+  small: 'small',
+  large: 'large',
+});
+
+export const BadgeColors = Object.freeze({
+  info: 'info',
+  success: 'success',
+  warning: 'warning',
+  error: 'error',
+});
+
+const BadgeSizeClassNames = Object.freeze({
+  small: 'badge-small',
+  medium: 'badge-medium',
+  large: 'badge-large',
+});
+
+const BadgeColorClassNames = Object.freeze({
+  info: 'badge-info',
+  success: 'badge-success',
+  warning: 'badge-warning',
+  error: 'badge-error',
+});
+
+export const Badge = (props: Props) => {
+  const {
+    className,
+    color,
+    icon,
+    size,
+    value,
+  } = props;
+  const sizeClassName = size == null ? '' : BadgeSizeClassNames[size] || '';
+  const colorClassName = color == null ? '' : BadgeColorClassNames[color] || '';
+  const newClassName = classnames(
+    className,
+    'badge',
+    {
+      [sizeClassName]: size != null,
+      [colorClassName]: color != null,
+      [`icon icon-${icon}`]: icon != null,
+    },
+  );
+  return (
+    <span className={newClassName}>
+      {value}
+    </span>
+  );
+};
