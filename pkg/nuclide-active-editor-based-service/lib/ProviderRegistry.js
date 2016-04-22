6b4c59894d1326f67f6e1384b99e5e7698367811
Nat Mote <nmote@fb.com>
2016-04-22 15:22:32 -0700

    ActiveEditorBasedService
    Summary:Adds an abstraction that allows people to easily create packages that fit the
    following model:
    
    - They render results computed from the contents of a text editor
    - These results need to get recomputed whenever the currently-active text editor
      changes
    - The actual thing that computes results depends on the grammar of the current
      file (e.g. different providers for Flow, Hack, etc.)
    
    With this abstraction, such packages only need to subscribe to a result stream
    and figure out how to render results.
    
    In the future I would like to add options to allow clients to customize the
    editor events they subscribe to. For example, some may want to recompute results
    only on save rather than whenever the text is edited. We could even allow
    clients to completely customize events by supplying a function from (TextEditor,
    provider) to Observable, which would allow even something like the LinterAdapter
    to be converted from text-event-dispatcher to this framework.
    
    It may also be useful to add a facility to cache results for all of the open
    tabs, rather than recomputing them on every tab switch. The
    text-event-dispatcher was designed with that in mind, so for this to completely
    subsume the text-event-dispatcher we will need similar functionality.
    
    Reviewed By: peterhal
    
    Differential Revision: D3179743
    
    fb-gh-sync-id: 8c79f4989264eeca14cb0b403e4c748e7759f02f
    fbshipit-source-id: 8c79f4989264eeca14cb0b403e4c748e7759f02f

---
+++ pkg/nuclide-active-editor-based-service/lib/ProviderRegistry.js
@@ -0,0 +1,42 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+import type {Provider} from '..';
+
+export class ProviderRegistry<T: Provider> {
+  _providers: Set<T>;
+
+  constructor() {
+    this._providers = new Set();
+  }
+
+  addProvider(provider: T): void {
+    this._providers.add(provider);
+  }
+
+  removeProvider(provider: T): void {
+    this._providers.delete(provider);
+  }
+
+  findProvider(grammar: string): ?T {
+    let bestProvider = null;
+    let bestPriority = Number.NEGATIVE_INFINITY;
+    for (const provider of this._providers) {
+      if (provider.grammarScopes.indexOf(grammar) !== -1) {
+        if (provider.priority > bestPriority) {
+          bestProvider = provider;
+          bestPriority = provider.priority;
+        }
+      }
+    }
+    return bestProvider;
+  }
+}
---
+++ pkg/nuclide-active-editor-based-service/lib/main.js
@@ -0,0 +1,109 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+import {Disposable} from 'atom';
+import {Observable} from '@reactivex/rxjs';
+
+import {
+  atomEventDebounce,
+} from '../../nuclide-atom-helpers';
+
+import {ProviderRegistry} from './ProviderRegistry';
+
+export type Provider = {
+  priority: number;
+  grammarScopes: Array<string>;
+};
+
+export type Result<V> = {
+  kind: 'not-text-editor';
+} | {
+  kind: 'no-provider';
+  grammar: atom$Grammar;
+} | {
+  // Since providers can be slow, the pane-change and edit events are emitted immediately in case
+  // the UI needs to clear outdated results.
+  kind: 'pane-change';
+} | {
+  kind: 'edit';
+} | {
+  kind: 'result';
+  result: V;
+  // The editor that the result was computed from
+  editor: atom$TextEditor;
+};
+
+type ResultFunction<T, V> = (provider: T, editor: atom$TextEditor) => Promise<V>;
+
+export class ActiveEditorBasedService<T: Provider, V> {
+  _resultFunction: ResultFunction<T, V>;
+  _providerRegistry: ProviderRegistry<T>;
+  _resultsStream: Observable<Result<V>>;
+
+  constructor(resultFunction: ResultFunction<T, V>) {
+    this._resultFunction = resultFunction;
+    this._providerRegistry = new ProviderRegistry();
+    this._resultsStream = this._createResultsStream();
+  }
+
+  consumeProvider(provider: T): IDisposable {
+    this._providerRegistry.addProvider(provider);
+    return new Disposable(() => {
+      this._providerRegistry.removeProvider(provider);
+    });
+  }
+
+  getResultsStream(): Observable<Result<V>> {
+    return this._resultsStream;
+  }
+
+  _createResultsStream(): Observable<Result<V>> {
+    const activeEditors = atomEventDebounce.observeActiveEditorsDebounced();
+    // Emit a pane change event first, so that clients can do something while waiting for a provider
+    // to give a result.
+    return activeEditors.switchMap(editorArg => {
+      // Necessary so the type refinement holds in the callback later
+      const editor = editorArg;
+      if (editor == null) {
+        return Observable.of({ kind: 'not-text-editor' });
+      }
+
+      const editorEvents = atomEventDebounce.observeEditorChangesDebounced(editor);
+
+      return Observable.concat(
+        Observable.of({ kind: 'pane-change' }),
+        editorEvents.flatMap(() => {
+          return Observable.concat(
+            Observable.of({ kind: 'edit' }),
+            Observable.fromPromise(this._getResultForEditor(editor)),
+          );
+        }),
+      );
+    });
+  }
+
+  async _getResultForEditor(editor: atom$TextEditor): Promise<Result<V>> {
+    const grammar = editor.getGrammar().scopeName;
+    const provider = this._providerRegistry.findProvider(grammar);
+    if (provider == null) {
+      return {
+        kind: 'no-provider',
+        grammar: editor.getGrammar(),
+      };
+    }
+    const result = await this._resultFunction(provider, editor);
+    return {
+      kind: 'result',
+      result,
+      editor,
+    };
+  }
+}
