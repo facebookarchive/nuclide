d67f64cf55d1d87bb804f1e7d61c8e1a18b22321
Peter Hallam <peterhal@fb.com>
2016-05-03 13:15:11 -0700

    Diff 7: Extract SocketTransport from SocketClient.
    Summary: This splits the message transport code from the RPC marshalling code.
    
    Reviewed By: mostafaeweda, nmote
    
    Differential Revision: D3237826
    
    fb-gh-sync-id: 78441bbadb1171a7054c26395270fd637bfcda38
    fbshipit-source-id: 78441bbadb1171a7054c26395270fd637bfcda38

--- pkg/nuclide-server/lib/SocketClient.js
+++ pkg/nuclide-server/lib/SocketClient.js
@@ -15,17 +15,11 @@ import {SERVICE_FRAMEWORK3_CHANNEL} from './config';
 import ServiceFramework from './serviceframework/index';
 import invariant from 'assert';
 
-import {getLogger} from '../../nuclide-logging';
-const logger = getLogger();
 import {ObjectRegistry} from './serviceframework/ObjectRegistry';
+import {SocketTransport} from './SocketTransport';
 
-// Server side analog to (parts of) NuclideSocket
-// Handles JSON messaging and reconnect.
-export class SocketClient {
-  id: string;
-  _isDisposed: boolean;
-  _socket: ?WS;
-  _messageQueue: Array<{data: string}>;
+// Per-Client state on the Server for the RPC framework
+export class SocketClient extends SocketTransport {
   _serverComponent: ServiceFramework.ServerComponent;
   _objectRegistry: ObjectRegistry;
 
@@ -33,90 +27,17 @@ export class SocketClient {
       clientId: string,
       serverComponent: ServiceFramework.ServerComponent,
       socket: WS) {
-    this.id = clientId;
-    this._isDisposed = false;
-    this._socket = null;
-    this._messageQueue = [];
+    super(clientId, socket);
     this._objectRegistry = new ObjectRegistry('server');
     this._serverComponent = serverComponent;
-    this._connect(socket);
-  }
-
-  _connect(socket: WS): void {
-    logger.info('Client #%s connecting with a new socket!', this.id);
-    invariant(this._socket == null);
-    this._socket = socket;
-    socket.on('message', message => this._onSocketMessage(message));
-
-    socket.on('close', () => {
-      if (this._socket != null) {
-        // This can occur on a reconnect, where the old socket has been closed
-        // but its close event is sent asynchronously.
-        if (this._socket === socket) {
-          this._socket = null;
-          logger.info('Client #%s socket close recieved on open socket!', this.id);
-        } else {
-          logger.info('Client #%s socket close received on orphaned socket!', this.id);
-        }
-      } else {
-        logger.info('Client #%s recieved socket close on already closed socket!', this.id);
-      }
+    this.onMessage(message => {
+      invariant(message.protocol && message.protocol === SERVICE_FRAMEWORK3_CHANNEL);
+      this._serverComponent.handleMessage(this, message);
     });
-  }
-
-  reconnect(socket: WS): void {
-    this._close();
-    this._connect(socket);
-    const queuedMessages = this._messageQueue;
-    this._messageQueue = [];
-    queuedMessages.forEach(message => { this.sendSocketMessage(message.data); });
-  }
-
-  _close(): void {
-    if (this._socket != null) {
-      this._socket.close();
-      // In certain (Error) conditions socket.close may not emit the on close
-      // event synchronously.
-      this._socket = null;
-    }
-  }
-
-  _onSocketMessage(message: any): void {
-    if (this._isDisposed) {
-      logger.error('Received socket message after connection closed', new Error());
-      return;
-    }
-
-    const parsedMessage: Object = JSON.parse(message);
-    invariant(parsedMessage.protocol && parsedMessage.protocol === SERVICE_FRAMEWORK3_CHANNEL);
-    this._serverComponent.handleMessage(this, parsedMessage);
   }
 
   sendSocketMessage(data: any): void {
-    if (this._isDisposed) {
-      logger.error('Attempt to send socket message after connection closed', new Error());
-      return;
-    }
-
-    // Wrap the data in an object, because if `data` is a primitive data type,
-    // finding it in an array would return the first matching item, not necessarily
-    // the same inserted item.
-    const message = {data};
-    this._messageQueue.push(message);
-    const socket = this._socket;
-    if (socket == null) {
-      return;
-    }
-    socket.send(JSON.stringify(data), err => {
-      if (err) {
-        logger.warn('Failed sending socket message to client:', this.id, data);
-      } else {
-        const messageIndex = this._messageQueue.indexOf(message);
-        if (messageIndex !== -1) {
-          this._messageQueue.splice(messageIndex, 1);
-        }
-      }
-    });
+    this.send(data);
   }
 
   getMarshallingContext(): ObjectRegistry {
@@ -124,8 +45,7 @@ export class SocketClient {
   }
 
   dispose(): void {
-    this._isDisposed = true;
-    this._close();
+    this.close();
     this._objectRegistry.dispose();
   }
 }
