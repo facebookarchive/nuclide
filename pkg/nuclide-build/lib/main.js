3dc4dfb3bb92458fd8bdd983d52c548051bb6cd7
Matthew Dapena-Tretter <matthewwithanm@fb.com>
2016-05-02 22:09:57 -0700

    Add empty package [build 1/N]
    Reviewed By: ssorallen
    
    Differential Revision: D3213238
    
    fb-gh-sync-id: 72612a1adc8cd04dade79f2e4a1c2d356738352d
    fbshipit-source-id: 72612a1adc8cd04dade79f2e4a1c2d356738352d

---
+++ pkg/nuclide-build/lib/main.js
@@ -0,0 +1,26 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+import invariant from 'assert';
+import {CompositeDisposable} from 'atom';
+
+let disposables: ?CompositeDisposable = null;
+
+export function activate(state: ?Object): void {
+  invariant(disposables == null);
+  disposables = new CompositeDisposable();
+}
+
+export function deactivate(): void {
+  invariant(disposables != null);
+  disposables.dispose();
+  disposables = null;
+}
