b29dcb9aadb509da771f9ed0be4781144f21646d
Matthew Dapena-Tretter <matthewwithanm@fb.com>
2016-05-05 11:47:15 -0700

    Add IosSimulator spec
    Summary: This accidentally didn't get pulled over from nuclide-buck-toolbar.
    
    Reviewed By: hansonw
    
    Differential Revision: D3263146
    
    fb-gh-sync-id: d32423f169268bde06126fe3bf6885fe72ca3d36
    fbshipit-source-id: d32423f169268bde06126fe3bf6885fe72ca3d36

---
+++ pkg/nuclide-build-buck/spec/IosSimulator-spec.js
@@ -0,0 +1,111 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+import {
+  parseDevicesFromSimctlOutput,
+  selectDevice,
+} from '../lib/IosSimulator';
+
+describe('IosSimulator', () => {
+
+  it('parses typical output', () => {
+    expect(parseDevicesFromSimctlOutput([
+      '== Devices ==',
+      '-- iOS 8.1 --',
+      '    iPhone 4s (4FE43B33-EF13-49A5-B6A6-658D32F20988) (Booted)',
+      '-- iOS 8.4 --',
+      '    iPhone 4s (EAB622C7-8ADE-4FAE-A911-94C0CA4709BB) (Shutdown)',
+      '    iPhone 5 (AE1CD3D0-A85B-4A73-B320-9CA7BA4FAEB0) (Shutdown)',
+    ].join('\n'))).toEqual([
+      {
+        name: 'iPhone 4s',
+        udid: '4FE43B33-EF13-49A5-B6A6-658D32F20988',
+        os: '8.1',
+        state: 'Booted',
+      },
+      {
+        name: 'iPhone 4s',
+        udid: 'EAB622C7-8ADE-4FAE-A911-94C0CA4709BB',
+        os: '8.4',
+        state: 'Shutdown',
+      },
+      {
+        name: 'iPhone 5',
+        udid: 'AE1CD3D0-A85B-4A73-B320-9CA7BA4FAEB0',
+        os: '8.4',
+        state: 'Shutdown',
+      },
+    ]);
+  });
+
+  it('ignores unavailable simulators', () => {
+    expect(parseDevicesFromSimctlOutput([
+      '== Devices ==',
+      '-- iOS 8.1 --',
+      '    iPhone 4s (4FE43B33-EF13-49A5-B6A6-658D32F20988) (Shutdown)',
+      '-- Unavailable: com.apple.CoreSimulator.SimRuntime.iOS-8-3 --',
+      '    iPhone 5s (EAB622C7-8ADE-4FAE-A911-94C0CA4709BB) (Shutdown)',
+    ].join('\n'))).toEqual([
+      {
+        name: 'iPhone 4s',
+        udid: '4FE43B33-EF13-49A5-B6A6-658D32F20988',
+        os: '8.1',
+        state: 'Shutdown',
+      },
+    ]);
+  });
+
+  it('ignores garbage', () => {
+    expect(parseDevicesFromSimctlOutput('Something went terribly wrong (-42)'))
+      .toEqual([]);
+  });
+
+  it('prefers already running simulator', () => {
+    expect(selectDevice([
+      {
+        name: 'iPhone 4s',
+        udid: '4FE43B33-EF13-49A5-B6A6-658D32F20988',
+        os: '8.1',
+        state: 'Shutdown',
+      },
+      {
+        name: 'iPhone 4s',
+        udid: 'EAB622C7-8ADE-4FAE-A911-94C0CA4709BB',
+        os: '8.4',
+        state: 'Booted',
+      },
+    ])).toBe(1);
+  });
+
+  it('selects iPhone 5s with latest iOS by default', () => {
+    expect(selectDevice([
+      {
+        name: 'iPhone 4s',
+        udid: '4FE43B33-EF13-49A5-B6A6-658D32F20988',
+        os: '8.1',
+        state: 'Shutdown',
+      },
+      {
+        name: 'iPhone 5s',
+        udid: 'EAB622C7-8ADE-4FAE-A911-94C0CA4709BB',
+        os: '8.2',
+        state: 'Shutdown',
+      },
+      {
+        name: 'iPhone 5s',
+        udid: 'EAB622C7-8ADE-4FAE-A911-94C0CA4709BB',
+        os: '8.4',
+        state: 'Shutdown',
+      },
+    ])).toBe(2);
+  });
+
+});
