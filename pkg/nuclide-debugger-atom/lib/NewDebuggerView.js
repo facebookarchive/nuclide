a8c5cf8dd2d19516d141edbaf1ce56421ff71d88
Jonas Gebhardt <jxg@fb.com>
2016-05-05 16:44:27 -0700

    Hook up watch expression component with new debugger UI.
    Reviewed By: johnislarry
    
    Differential Revision: D3216323
    
    fb-gh-sync-id: 1070c3bc69892a1c8dde89905b47c5f1b863e3b0
    fbshipit-source-id: 1070c3bc69892a1c8dde89905b47c5f1b863e3b0

--- pkg/nuclide-debugger-atom/lib/NewDebuggerView.js
+++ pkg/nuclide-debugger-atom/lib/NewDebuggerView.js
@@ -9,15 +9,46 @@
  * the root directory of this source tree.
  */
 
+import type {
+  WatchExpression,
+} from './WatchExpressionComponent';
+
 import {
   React,
 } from 'react-for-atom';
+import {
+  WatchExpressionComponent,
+} from './WatchExpressionComponent';
+
+type Props = {};
 
 export class NewDebuggerView extends React.Component {
+  props: Props;
+  state: {
+    watchExpressions: Array<WatchExpression>;
+  };
+
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      watchExpressions: [],
+    };
+    (this: any).handleUpdateExpressions = this.handleUpdateExpressions.bind(this);
+  }
+
+  handleUpdateExpressions(watchExpressions: Array<WatchExpression>): void {
+    this.setState({
+      watchExpressions,
+    });
+  }
 
   render(): React.Element {
     return (
       <div className="nuclide-debugger-container-new">
+        <WatchExpressionComponent
+          watchExpressions={this.state.watchExpressions}
+          onUpdateExpressions={this.handleUpdateExpressions}
+        />
       </div>
     );
   }
