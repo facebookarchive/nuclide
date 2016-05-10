e3e3c32cd54f7a82f51c3d52753b2be8c5e8f073
James Pearce <jpearce@fb.com>
2016-05-09 18:26:25 -0700

    native notifications 3/4; Native notification support (behind a gatekeeper)
    Summary: The option to send native title & body to the OS with a baby little Nuclide logo. Cute.
    
    Reviewed By: bottledwalter
    
    Differential Revision: D3274782
    
    fbshipit-source-id: 875e8c2236452fa5dcf223b7b86c3223a27854f2

--- pkg/nuclide-notifications/lib/main.js
+++ pkg/nuclide-notifications/lib/main.js
@@ -15,6 +15,7 @@ import featureConfig from '../../nuclide-feature-config';
 let subscriptions: CompositeDisposable = (null: any);
 // eslint-disable-next-line no-unused-vars
 let currentConfig = ((featureConfig.get('nuclide-notifications'): any): {[type: string]: bool});
+let gkEnabled = false;
 
 export function activate(state: ?Object): void {
   subscriptions = new CompositeDisposable(
@@ -24,6 +25,33 @@ export function activate(state: ?Object): void {
       currentConfig = event.newValue;
     }),
 
+  );
+
+  try {
+    // Listen for the gatekeeper to tell us if we can generate native notifications.
+    const gatekeeper = require('../../fb-gatekeeper').gatekeeper;
+    subscriptions.add(
+      gatekeeper.onceInitialized(() => {
+        gkEnabled = gatekeeper.isGkEnabled('nuclide_native_notifications');
+      }),
+    );
+  } catch (e) { }
+
+}
+
+export function raiseNativeNotification(title: string, body: string): void {
+
+  // Check we're in the gatekeeper for native notifications at all.
+  if (!gkEnabled) {
+    return;
+  }
+
+  // eslint-disable-next-line no-new, no-undef
+  new Notification(
+    title, {
+      body: body,
+      icon: 'atom://nuclide/pkg/nuclide-notifications/notification.png',
+    }
   );
 }
 
