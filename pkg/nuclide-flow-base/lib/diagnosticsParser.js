7a2c7f425a54decbbb33de92281fc752b82de562
Nat Mote <nmote@fb.com>
2016-05-10 15:14:32 -0700

    Separate file for Flow output types
    Summary: So they can be centralized and used elsewhere
    
    Reviewed By: ssorallen
    
    Differential Revision: D3282013
    
    fbshipit-source-id: 0fa64b1985967685b5f1ca495d1b6be91cb21f7e

--- pkg/nuclide-flow-base/lib/diagnosticsParser.js
+++ pkg/nuclide-flow-base/lib/diagnosticsParser.js
@@ -16,78 +16,16 @@ import type {
   Range,
 } from '..';
 
-// Types for the old `flow status` output -- v0.22 and below
-
-type OldFlowStatusOutput = {
-  passed: boolean;
-  // This is not actually the Flow version; instead it is a build ID or something.
-  version?: string;
-  errors: Array<OldFlowStatusError>;
-};
-
-type OldFlowStatusError = {
-  kind: string;
-  operation?: OldFlowStatusErrorOperation;
-  message: Array<OldFlowStatusErrorMessageComponent>;
-};
-
-type OldBaseFlowStatusErrorMessageComponent = {
-  // If there is no path component, this is the empty string. We should make it null instead, in
-  // that case (t8644340)
-  path: string;
-  descr: string;
-  line: number;
-  start: number;
-  end: number;
-  endline: number;
-};
-
-type OldFlowStatusErrorMessageComponent = OldBaseFlowStatusErrorMessageComponent & {
-  level: 'error' | 'warning';
-};
-
-// Same as FlowStatusErrorMessageComponent, except without the 'level' field.
-type OldFlowStatusErrorOperation = OldBaseFlowStatusErrorMessageComponent;
-
-// New types for `flow status` v0.23.0 (or possibly v0.24.0, it has yet to be finalized)
-
-type NewFlowStatusOutput = {
-  passed: boolean;
-  flowVersion: string;
-  errors: Array<NewFlowStatusError>;
-};
-
-type NewFlowStatusError = {
-  level: 'error' | 'warning';
-  // e.g. parse, infer, maybe others?
-  kind: string;
-  message: Array<NewFlowStatusErrorMessageComponent>;
-  operation?: NewFlowStatusErrorMessageComponent;
-
-  // There is also an `extra` field where additional details about certain kinds of errors are
-  // provided. For now we will ignore these details.
-};
-
-type NewFlowStatusErrorMessageComponent = {
-  descr: string;
-  loc?: FlowLoc;
-  // The old path, line, etc. fields also currently exist here, but they are deprecated in favor of
-  // `loc`.
-};
-
-type FlowLoc = {
-  // file path
-  source: string;
-  start: FlowPoint;
-  end: FlowPoint;
-};
-
-type FlowPoint = {
-  column: number;
-  line: number;
-  // total character offset
-  offset: number;
-};
+import type {
+  OldFlowStatusOutput,
+  OldFlowStatusError,
+  OldFlowStatusErrorMessageComponent,
+  OldBaseFlowStatusErrorMessageComponent,
+  NewFlowStatusOutput,
+  NewFlowStatusError,
+  NewFlowStatusErrorMessageComponent,
+  FlowLoc,
+} from './flowOutputTypes';
 
 export function flowStatusOutputToDiagnostics(
   root: string,
---
+++ pkg/nuclide-flow-base/lib/flowOutputTypes.js
@@ -0,0 +1,85 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+/* FLOW STATUS */
+
+// Types for the old `flow status` output -- v0.22 and below
+
+export type OldFlowStatusOutput = {
+  passed: boolean;
+  // This is not actually the Flow version; instead it is a build ID or something.
+  version?: string;
+  errors: Array<OldFlowStatusError>;
+};
+
+export type OldFlowStatusError = {
+  kind: string;
+  operation?: OldFlowStatusErrorOperation;
+  message: Array<OldFlowStatusErrorMessageComponent>;
+};
+
+export type OldBaseFlowStatusErrorMessageComponent = {
+  // If there is no path component, this is the empty string. We should make it null instead, in
+  // that case (t8644340)
+  path: string;
+  descr: string;
+  line: number;
+  start: number;
+  end: number;
+  endline: number;
+};
+
+export type OldFlowStatusErrorMessageComponent = OldBaseFlowStatusErrorMessageComponent & {
+  level: 'error' | 'warning';
+};
+
+// Same as FlowStatusErrorMessageComponent, except without the 'level' field.
+export type OldFlowStatusErrorOperation = OldBaseFlowStatusErrorMessageComponent;
+
+// New types for `flow status` v0.23.0 (or possibly v0.24.0, it has yet to be finalized)
+
+export type NewFlowStatusOutput = {
+  passed: boolean;
+  flowVersion: string;
+  errors: Array<NewFlowStatusError>;
+};
+
+export type NewFlowStatusError = {
+  level: 'error' | 'warning';
+  // e.g. parse, infer, maybe others?
+  kind: string;
+  message: Array<NewFlowStatusErrorMessageComponent>;
+  operation?: NewFlowStatusErrorMessageComponent;
+
+  // There is also an `extra` field where additional details about certain kinds of errors are
+  // provided. For now we will ignore these details.
+};
+
+export type NewFlowStatusErrorMessageComponent = {
+  descr: string;
+  loc?: FlowLoc;
+  // The old path, line, etc. fields also currently exist here, but they are deprecated in favor of
+  // `loc`.
+};
+
+export type FlowLoc = {
+  // file path
+  source: string;
+  start: FlowPoint;
+  end: FlowPoint;
+};
+
+export type FlowPoint = {
+  column: number;
+  line: number;
+  // total character offset
+  offset: number;
+};
