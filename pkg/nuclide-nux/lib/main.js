78daf5f4b7168f1e57b8f68d14fc557ee95f30ab
Sameer Chitley <rageandqq@fb.com>
2016-05-18 16:57:41 -0700

    Boilerplate package creation for nuclide-nux framework
    Summary: Created package with script - basic boilerplate
    
    Reviewed By: jgebhardt
    
    Differential Revision: D3315241
    
    fbshipit-source-id: 8c8c95054ece88636ca7126e92414cb86a24a38c

---
+++ pkg/nuclide-nux/lib/main.js
@@ -0,0 +1,19 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+export function activate(state: ?mixed): void {
+}
+
+export function serialize(): mixed {
+}
+
+export function deactivate(): void {
+}
