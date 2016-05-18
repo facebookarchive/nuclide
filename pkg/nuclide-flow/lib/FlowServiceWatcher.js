bd04316db5f69b1786d6354504b92b4e400c70bd
Nat Mote <nmote@fb.com>
2016-05-18 07:54:24 -0700

    Button to disable warnings that Flow is not installed
    Summary:
    People might not be interested in Flow, may just be code browsing, or making
    small changes to existing JS projects that use Flow, and therefore won't want to
    install Flow but will want to browse code in peace. Therefore it's useful to
    have a setting to disable this popup.
    
    Reviewed By: zertosh
    
    Differential Revision: D3311994
    
    fbshipit-source-id: 66b4030f7c6d4d869b67f6fefcf1aa4321c037fc

--- pkg/nuclide-flow/lib/FlowServiceWatcher.js
+++ pkg/nuclide-flow/lib/FlowServiceWatcher.js
@@ -13,7 +13,11 @@ import type {NuclideUri} from '../../nuclide-remote-uri';
 
 import {Subscription} from 'rxjs';
 
+import featureConfig from '../../nuclide-feature-config';
+
 import {getServerStatusUpdates} from './FlowServiceFactory';
+
+const WARN_NOT_INSTALLED_CONFIG = 'nuclide-flow.warnOnNotInstalled';
 
 export class FlowServiceWatcher {
   _subscription: Subscription;
@@ -70,15 +74,26 @@ function handleFailure(pathToRoot: NuclideUri): void {
 }
 
 function handleNotInstalled(pathToRoot: NuclideUri): void {
+  if (!featureConfig.get(WARN_NOT_INSTALLED_CONFIG)) {
+    return;
+  }
   const message = `Flow was not found when attempting to start it in '${pathToRoot}'.<br/><br/>` +
     'You can download it from <a href="http://flowtype.org/">flowtype.org</a>. ' +
     'Make sure it is installed and on your PATH. ' +
     'If this is a remote repository make sure it is available on the remote machine.<br/><br/>' +
     'You will not see this message again until you restart Nuclide';
-  atom.notifications.addError(
+  const notification = atom.notifications.addError(
     message,
     {
       dismissable: true,
+      buttons: [{
+        className: 'icon icon-x',
+        onDidClick() {
+          notification.dismiss();
+          featureConfig.set(WARN_NOT_INSTALLED_CONFIG, false);
+        },
+        text: 'Do not show again (can be reverted in settings)',
+      }],
     }
   );
 }
