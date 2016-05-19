3450b561b05e4b646e8c9fdeaf43dc1a582c3bf9
Sameer Chitley <rageandqq@fb.com>
2016-05-18 17:49:59 -0700

    Define model representations of NuxView and NuxTour.
    Summary: Define data required for serialization/deserialization of NuxView and NuxTour.
    
    Reviewed By: jgebhardt
    
    Differential Revision: D3315527
    
    fbshipit-source-id: 0dd35182770692f653224e79e3a94de135960266

---
+++ pkg/nuclide-nux/lib/NuxModel.js
@@ -0,0 +1,28 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+export type NuxViewModel = {
+  content: string;
+  isCustomContent: boolean;
+  selector: ?string;
+  selectorFunction: ?(() => HTMLElement);
+  position: string;
+  displayPredicate: ?(() => boolean);
+  completionPredicate: ?(() => boolean);
+  completed: boolean;
+};
+
+export type NuxTourModel = {
+  numNuxes: number;
+  completed: boolean;
+  id: string;
+  nuxList: Array<NuxViewModel>;
+};
