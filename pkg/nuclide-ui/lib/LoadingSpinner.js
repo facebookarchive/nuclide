da60dd292096eaee7b0aabc8bdb59316699eb708
Jonas Gebhardt <jxg@fb.com>
2016-05-23 13:29:20 -0700

    Add delay prop to LoadingSpinner
    Summary:
    We often want to show spinners only if we're waiting a substantial amount of time to reduce visual churn.
    
    `<LoadingSpinner delay={200} />` will not render anything until 200ms have passed. I think this is a very common use case for the Loading Spinner, so I chose to build it into this component (rather than an HOC).
    
    Reviewed By: yinghuitan
    
    Differential Revision: D3320967
    
    fbshipit-source-id: ad6dd8d0baee3beb60a2cdaac65a8b5b4a2f0a65

--- pkg/nuclide-ui/lib/LoadingSpinner.js
+++ pkg/nuclide-ui/lib/LoadingSpinner.js
@@ -17,6 +17,11 @@ type Props = {
   className? : string;
   /** The size of the LoadingSpinner. Defaults to MEDIUM. */
   size?: LoadingSpinnerSize;
+  /**
+   * An optional delay (in milliseconds) between mounting the component and actually rendering
+   * the spinner to avoid UI churn.
+   */
+  delay?: number;
 };
 
 export const LoadingSpinnerSizes = Object.freeze({
@@ -36,21 +41,46 @@ const LoadingSpinnerClassnames = Object.freeze({
 /**
  * Shows an indefinite, animated LoadingSpinner.
  */
-export const LoadingSpinner = (props: Props): React.Element => {
-  const {
-    className,
-    size,
-  } = props;
-  const safeSize = size != null && LoadingSpinnerSizes.hasOwnProperty(size)
-    ? size
-    : LoadingSpinnerSizes.MEDIUM;
-  const sizeClassname = LoadingSpinnerClassnames[safeSize];
-  const newClassName = classnames(
-    className,
-    'loading',
-    sizeClassname,
-  );
-  return (
-    <div className={newClassName} />
-  );
-};
+export class LoadingSpinner extends React.Component {
+  props: Props;
+  state: {shouldRender: boolean};
+  _timeout: ?number;
+
+  constructor(props: Props) {
+    super(props);
+    this.state = {shouldRender: false};
+  }
+
+  componentDidMount(): void {
+    const delay = this.props.delay == null ? 0 : this.props.delay;
+    this._timeout = setTimeout(() => this.setState({shouldRender: true}), delay);
+  }
+
+  componentWillUnmount(): void {
+    if (this._timeout != null) {
+      clearTimeout(this._timeout);
+    }
+  }
+
+  render(): ?React.Element {
+    const {
+      className,
+      size,
+    } = this.props;
+    if (!this.state.shouldRender) {
+      return null;
+    }
+    const safeSize = size != null && LoadingSpinnerSizes.hasOwnProperty(size)
+      ? size
+      : LoadingSpinnerSizes.MEDIUM;
+    const sizeClassname = LoadingSpinnerClassnames[safeSize];
+    const newClassName = classnames(
+      className,
+      'loading',
+      sizeClassname,
+    );
+    return (
+      <div className={newClassName} />
+    );
+  }
+}
