7b5756170f34488491e0287d9b0a31adfc862fda
Peter Hallam <peterhal@fb.com>
2016-05-23 18:25:01 -0700

    Diff 8: First cut at Preview Definition Package.
    Summary: Adds the activation and UI for the definition preview package.
    
    Reviewed By: hansonw
    
    Differential Revision: D3327340
    
    fbshipit-source-id: 3406d296be492ec54c4d09228c85934aa778d00f

--- pkg/nuclide-definition-preview/lib/main.js
+++ pkg/nuclide-definition-preview/lib/main.js
@@ -9,32 +9,86 @@
  * the root directory of this source tree.
  */
 
-import {CompositeDisposable} from 'atom';
+import type {DistractionFreeModeProvider} from '../../nuclide-distraction-free-mode';
+import type {DefinitionService} from '../../nuclide-definition-service';
+
+import {CompositeDisposable, Disposable} from 'atom';
+import {CodePreviewState} from './CodePreviewState';
+import invariant from 'assert';
+
+let currentService: ?DefinitionService = null;
+
+type CodePreviewConfig = {
+  width: number;
+  visible: boolean;
+};
+
+const DEFAULT_WIDTH = 300; // px
+const DEFAULT_CONFIG: CodePreviewConfig = {
+  width: DEFAULT_WIDTH,
+  visible: false,
+};
 
 class Activation {
   _disposables: CompositeDisposable;
+  _state: CodePreviewState;
 
-  constructor(state: ?Object) {
-    // TODO(peterhal): Assign all fields here so they are
-    // non-nullable for the lifetime of Activation.
+  constructor(config?: CodePreviewConfig = DEFAULT_CONFIG) {
     this._disposables = new CompositeDisposable();
+    this._state = new CodePreviewState(config.width, config.visible);
+    this.updateService();
+    this._disposables.add(
+      atom.commands.add(
+        'atom-workspace',
+        'nuclide-definition-preview:toggle',
+        () => this._state.toggle()
+      )
+    );
+    this._disposables.add(
+      atom.commands.add(
+        'atom-workspace',
+        'nuclide-definition-preview:show',
+        () => this._state.show()
+      )
+    );
+    this._disposables.add(
+      atom.commands.add(
+        'atom-workspace',
+        'nuclide-definition-preview:hide',
+        () => this._state.hide()
+      )
+    );
   }
 
-  activate() {
-    // TODO(peterhal): Add activation code here.
+  updateService(): void {
+    this._state.setDefinitionService(currentService);
   }
 
   dispose() {
     this._disposables.dispose();
   }
+
+  serialize(): CodePreviewConfig {
+    return {
+      width: this._state.getWidth(),
+      visible: this._state.isVisible(),
+    };
+  }
+
+  getDistractionFreeModeProvider(): DistractionFreeModeProvider {
+    return {
+      name: 'nuclide-definition-preview',
+      isVisible: () => this._state.isVisible(),
+      toggle: () => this._state.toggle(),
+    };
+  }
 }
 
 let activation: ?Activation = null;
 
-export function activate(state: ?Object) {
+export function activate(state: Object | void) {
   if (activation == null) {
     activation = new Activation(state);
-    activation.activate();
   }
 }
 
@@ -43,4 +97,32 @@ export function deactivate() {
     activation.dispose();
     activation = null;
   }
+}
+
+function updateService(): void {
+  if (activation != null) {
+    activation.updateService();
+  }
+}
+
+export function consumeDefinitionService(service: DefinitionService): IDisposable {
+  invariant(currentService == null);
+  currentService = service;
+  updateService();
+  return new Disposable(() => {
+    invariant(currentService === service);
+    currentService = null;
+    updateService();
+  });
+}
+
+export function serialize(): ?CodePreviewConfig {
+  if (activation != null) {
+    return activation.serialize();
+  }
+}
+
+export function getDistractionFreeModeProvider(): DistractionFreeModeProvider {
+  invariant(activation != null);
+  return activation.getDistractionFreeModeProvider();
 }
