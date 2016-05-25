3b8f476b8f7246cc61107264bca56c289c7f4bd6
Peter Hallam <peterhal@fb.com>
2016-05-25 16:44:02 -0700

    Diff 2: Add locationsEqual
    Reviewed By: nmote
    
    Differential Revision: D3343992
    
    fbshipit-source-id: b7babd5252dafb6eb66a479a2418da0aa1721d4c

--- pkg/nuclide-rpc/lib/location.js
+++ pkg/nuclide-rpc/lib/location.js
@@ -11,12 +11,29 @@
 
 import type {Location} from './types';
 
+import invariant from 'assert';
+
 export function locationToString(location: Location): string {
   switch (location.type) {
     case 'source':
       return `${location.fileName}(${location.line})`;
     case 'builtin':
       return '<builtin>';
+    default:
+      throw new Error('Bad location type');
+  }
+}
+
+export function locationsEqual(first: Location, second: Location): boolean {
+  if (first.type !== second.type) {
+    return false;
+  }
+  switch (first.type) {
+    case 'source':
+      invariant(second.type === 'source');
+      return first.fileName === second.fileName && first.line === second.line;
+    case 'builtin':
+      return true;
     default:
       throw new Error('Bad location type');
   }
