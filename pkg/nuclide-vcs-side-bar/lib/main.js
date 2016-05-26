f27dd421eb03f9f8e10c7c21de1c05194272c29e
Ross Allen <rossallen@fb.com>
2016-05-26 12:30:29 -0700

    Create new vcs-side-bar package
    Summary: Create the start of the vcs-side-bar.
    
    Reviewed By: hansonw
    
    Differential Revision: D3325700
    
    fbshipit-source-id: 299c6a465f26f90e99217ec4da2d2a9ed4844d3f

---
+++ pkg/nuclide-vcs-side-bar/lib/main.js
@@ -0,0 +1,21 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+import type {NuclideSideBarService} from '../../nuclide-side-bar';
+
+export function activate(rawState: Object): void {
+}
+
+export function consumeNuclideSideBar(sideBar: NuclideSideBarService): void {
+}
+
+export function deactivate(): void {
+}
