8870d3b5d20220389720e926fa917158c1422cdf
Sameer Chitley <rageandqq@fb.com>
2016-06-02 13:56:10 -0700

    Editing and Saving within Diff View integration test
    Summary:
    The added test checks that:
    
      - The diff view opens
      - Two editor panes are created
      - **KEY: The right pane can be used to edit the file**
      - **KEY: The changes are not reflected on the file system until the file is saved**
    
    Note: I followed the `beforeEach`, `afterEach`, `it` pattern since the other diff-view integration tests do the same, and this keeps things consistent, readable, and understandable.
    
    Reviewed By: johnislarry
    
    Differential Revision: D3374487
    
    fbshipit-source-id: 67becec75516ad64585323ac2698ee16bc1ea809

---
+++ spec/diff-view-right-editor-spec.js
@@ -0,0 +1,107 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+import {
+  activateAllPackages,
+  copyMercurialFixture,
+  deactivateAllPackages,
+  jasmineIntegrationTestSetup,
+  setLocalProject,
+} from '../pkg/nuclide-integration-test-helpers';
+
+import invariant from 'assert';
+import fs from 'fs';
+import path from 'path';
+import {ReactDOM} from 'react-for-atom';
+
+function getResourceTextSync(filePath: string): string {
+  return fs.readFileSync(
+    filePath,
+    {encoding: 'utf-8'},
+  );
+}
+
+describe('Diff View Right Editor Save and Edit Test', () => {
+
+  const TEST_FILE_NAME = 'test.txt';
+  let repoPath: string = (null: any);
+
+  beforeEach(() => {
+    waitsForPromise(async () => {
+      // Configure some jasmine specific things for integration testing.
+      jasmineIntegrationTestSetup();
+      // Activate nuclide packages.
+      await activateAllPackages();
+      // Copy mercurial project to temporary directory.
+      repoPath = await copyMercurialFixture('hg_repo_2', __dirname);
+      // Add this directory as a new project in atom.
+      setLocalProject(repoPath);
+      // Open the test.txt file in the repo.
+      await atom.workspace.open(path.join(repoPath, TEST_FILE_NAME));
+    });
+  });
+
+  afterEach(() => {
+    // Deactivate nuclide packages.
+    deactivateAllPackages();
+  });
+
+  it('edits and saves a file within the diff view\'s right editor', () => {
+    const textEditor = atom.workspace.getActiveTextEditor();
+    invariant(textEditor, 'no active text editor!');
+    // Open diff view
+    atom.commands.dispatch(atom.views.getView(textEditor), 'nuclide-diff-view:open');
+
+    let diffViewElement: ?HTMLElement = (null : any);
+
+    waitsFor('diff view to load', 10000, () => {
+      diffViewElement = atom.workspace.getActivePaneItem();
+      return diffViewElement != null && diffViewElement.tagName === 'NUCLIDE-DIFF-VIEW';
+    });
+
+    runs(() => {
+      const diffViewPackage = atom.packages.getActivePackage('nuclide-diff-view');
+      invariant(diffViewPackage, 'The "nuclide-diff-view" package is not active!');
+      // Get active diff view
+      const activeDiffView = (diffViewPackage.mainModule: any).__testDiffView;
+      invariant(activeDiffView, 'No active diff view!');
+
+      const diffViewContainer = ReactDOM.findDOMNode(activeDiffView.component);
+      expect(diffViewContainer.parentNode).toBe(diffViewElement);
+      invariant(diffViewElement != null);
+      // Ensure both panels TextEditors are showing in diff view
+      const textEditorElements = diffViewElement.querySelectorAll('atom-text-editor');
+      expect(textEditorElements.length).toBe(2);
+
+      // Get right TextEditor from diff view
+      // Downcast to `HTMLElement` to child `TextEditorElement` class
+      const rightEditorElement = ((textEditorElements[1]: any): atom$TextEditorElement);
+      const rightEditor = rightEditorElement.getModel();
+
+      // Test that inserting text does not affect the file until the TextEditor saves
+      rightEditor.insertText('Integration test sample text');
+
+      const TEST_FILE_PATH = path.join(repoPath, TEST_FILE_NAME);
+      expect(getResourceTextSync(TEST_FILE_PATH)).not.toEqual(rightEditor.getText());
+      rightEditor.saveAs(TEST_FILE_PATH);
+      expect(getResourceTextSync(TEST_FILE_PATH)).toEqual(rightEditor.getText());
+
+
+      // Test that deleting text does not affect the file until the TextEditor saves
+      rightEditor.selectAll();
+      rightEditor.delete();
+
+      expect(getResourceTextSync(TEST_FILE_PATH)).not.toEqual(rightEditor.getBuffer());
+      rightEditor.saveAs(TEST_FILE_PATH);
+      expect(getResourceTextSync(TEST_FILE_PATH)).toEqual(rightEditor.getText());
+    });
+  });
+});
