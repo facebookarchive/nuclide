3a72301a6cff97007eb98e2e1b092eb53bf71c85
Peter Hallam <peterhal@fb.com>
2016-06-01 21:14:45 -0700

    Use describeRemotableTest for related-files test.
    Reviewed By: hansonw
    
    Differential Revision: D3373890
    
    fbshipit-source-id: 89f2b9438ddae932caf5c9aab5aff72736dbe594

--- spec/related-files-integration-spec.js
+++ spec/related-files-integration-spec.js
@@ -9,10 +9,61 @@
  * the root directory of this source tree.
  */
 
-import {runTest} from './utils/related-files-common';
+import {describeRemotableTest} from './utils/remotable-tests';
+import invariant from 'assert';
+import {
+  dispatchKeyboardEvent,
+  waitsForFile,
+} from '../pkg/nuclide-integration-test-helpers';
 
-describe('Related Files Integration Test', () => {
+import fsPromise from '../pkg/commons-node/fsPromise';
+import {join} from '../pkg/nuclide-remote-uri';
+import {tempdir} from '../pkg/nuclide-test-helpers';
+
+describeRemotableTest('Related Files Integration Test', context => {
+  const TEST_FILES = ['test.cpp', 'test.h', 'testInternal.h', 'test-inl.h'].sort();
+  const BAD_FILE = 'bad.txt'; // should not switch to this
+
   it('is able to switch between related files', () => {
-    runTest(/*remote*/ false);
+    waitsForPromise({timeout: 60000}, async () => {
+
+      // Create a temporary directory and some test files.
+      const testDir = await fsPromise.realpath(await tempdir.mkdir('related-files'));
+      await Promise.all(TEST_FILES.concat([BAD_FILE]).map(
+        file => fsPromise.writeFile(join(testDir, file), ''),
+      ));
+
+      await context.setProject(testDir);
+      await atom.workspace.open(context.getProjectRelativePath(TEST_FILES[0]));
+    });
+
+    // Should go over the list in reverse order alphabetically.
+    for (let i = 0; i < TEST_FILES.length; i++) {
+      runs(() => {
+        const textEditor = atom.workspace.getActiveTextEditor();
+        invariant(textEditor);
+        const textEditorView = atom.views.getView(textEditor);
+        dispatchKeyboardEvent('n', textEditorView, {cmd: true, alt: true});
+      });
+      waitsForFile(TEST_FILES[TEST_FILES.length - i - 1]);
+    }
+
+    // Reverse direction.
+    for (let i = 0; i < TEST_FILES.length; i++) {
+      runs(() => {
+        const textEditor = atom.workspace.getActiveTextEditor();
+        invariant(textEditor);
+        const textEditorView = atom.views.getView(textEditor);
+        // No keyboard shortcut for this.
+        atom.commands.dispatch(
+          textEditorView,
+          'nuclide-related-files:jump-to-previous-related-file',
+        );
+      });
+      waitsForFile(TEST_FILES[(i + 1) % TEST_FILES.length]);
+    }
+
+    // Add one dummy expect just so we can verify that the test completed.
+    expect(true).toBe(true);
   });
 });
