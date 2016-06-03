7f369c5a4e0a196a5b7f324fe8e892e445d87dd9
Alexander Dvinsky <advinsky@fb.com>
2016-06-03 10:44:52 -0700

    Add an integration test for editing and saving of a remote file
    Summary:
    Opens a remote file
    Edits it
    Verifies that its status is "Modified"
    Issues a save command
    Waits until the status is no longer modified
    Verifies that the contents have been written to the FS
    Edits again.
    Verifies that its status is "Modified" again
    
    Reviewed By: johnislarry
    
    Differential Revision: D3380027
    
    fbshipit-source-id: 570b426ccc6e99291b543fc2060aca21f22296b3

---
+++ spec/edit-remote-file-integration-spec.js
@@ -0,0 +1,70 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+import {
+  activateAllPackages,
+  jasmineIntegrationTestSetup,
+  deactivateAllPackages,
+  copyMercurialFixture,
+  startNuclideServer,
+  addRemoteProject,
+  stopNuclideServer,
+  pollFor,
+} from '../pkg/nuclide-integration-test-helpers';
+
+import path from 'path';
+import fs from 'fs';
+
+import invariant from 'assert';
+
+describe('Edit remote file Integration Test', () => {
+  it('supports editing, saving and the modified status detection of remote files', () => {
+    waitsForPromise({timeout: 60000}, async () => {
+      // Configure some jasmine specific things for integration testing.
+      jasmineIntegrationTestSetup();
+      // Activate nuclide packages.
+      await activateAllPackages();
+
+      const repoPath = await copyMercurialFixture('hg_repo_1');
+      startNuclideServer();
+      const connection = await addRemoteProject(repoPath);
+      invariant(connection, 'Failed to make connection to a remote server');
+
+      const remotePath = path.join(repoPath, 'test.txt');
+      const textEditor = await atom.workspace.open(connection.getUriOfRemotePath(remotePath));
+      invariant(textEditor);
+      const textEditorView = atom.views.getView(textEditor);
+
+      textEditor.insertText('abcdef');
+
+      expect(textEditor.isModified()).toBe(true);
+
+      atom.commands.dispatch(textEditorView, 'core:save');
+
+      await pollFor(
+        () => textEditor.isModified() === false,
+        'Text editor did not lose its modified status',
+        5000,
+      );
+
+      const fsContents = fs.readFileSync(remotePath).toString();
+      expect(fsContents).toEqual(textEditor.getText());
+
+      textEditor.insertText('ghijkl');
+
+      expect(textEditor.isModified()).toBe(true);
+
+      await stopNuclideServer(connection);
+      // Deactivate nuclide packages.
+      deactivateAllPackages();
+    });
+  });
+});
