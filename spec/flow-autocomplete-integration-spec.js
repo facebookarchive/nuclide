82b8127d1bfbc38114301409e77e9473fa24b18c
Hanson Wang <hansonw@fb.com>
2016-06-03 00:17:11 -0700

    Re-merge Flow autocomplete integration tests
    Reviewed By: zertosh
    
    Differential Revision: D3384419
    
    fbshipit-source-id: 373d8306796acc58f26cb49783c530c17a46efea

--- spec/flow-autocomplete-integration-spec.js
+++ spec/flow-autocomplete-integration-spec.js
@@ -9,9 +9,66 @@
  * the root directory of this source tree.
  */
 
-import {describeLocal} from './utils/remotable-tests';
-import {runTest} from './utils/flow-autocomplete-common';
+import {
+  busySignal,
+  copyFixture,
+} from '../pkg/nuclide-integration-test-helpers';
+import {describeRemotableTest} from './utils/remotable-tests';
 
-describeLocal('Flow Autocomplete', context => {
-  runTest(context);
+describeRemotableTest('Flow Autocomplete', context => {
+  it('tests simple autocomplete example', () => {
+    let textEditor: atom$TextEditor = (null : any);
+    let textEditorView: HTMLElement = (null : any);
+
+    waitsForPromise({timeout: 240000}, async () => {
+      // Copy flow project to a temporary location.
+      const flowProjectPath = await copyFixture('flow_project_1');
+
+      // Add this directory as an atom project.
+      await context.setProject(flowProjectPath);
+      // Open a file in the flow project we copied, and get reference to the editor's HTML.
+      textEditor = await atom.workspace.open(context.getProjectRelativePath('main.js'));
+    });
+
+    waitsFor('spinner to start', 10000, () => {
+      return busySignal.isBusy();
+    });
+
+    waitsFor('spinner to stop', 30000, () => {
+      return !busySignal.isBusy();
+    });
+
+    runs(() => {
+      textEditorView = atom.views.getView(textEditor);
+      // Simulate a keypress to trigger the autocomplete menu.
+      textEditor.moveToBottom();
+      textEditor.insertText('n');
+    });
+
+    let autocompleteMenuView: HTMLElement = (null : any);
+    waitsFor('autocomplete suggestions to render', 10000, () => {
+      autocompleteMenuView = textEditorView.querySelector('.autocomplete-plus');
+      if (autocompleteMenuView != null) {
+        return autocompleteMenuView.querySelector('.right-label');
+      }
+      return autocompleteMenuView;
+    });
+
+    runs(() => {
+      // Check autocomplete box renders.
+      expect(autocompleteMenuView).toExist();
+
+      // Check type annotations exist and are correct.
+      expect(autocompleteMenuView.querySelector('.right-label').innerText).toBe('number');
+      const typeHintView = autocompleteMenuView.querySelector('.suggestion-description-content');
+      expect(typeHintView).toExist();
+      expect(typeHintView.innerText).toBe('number');
+
+      // Confirm autocomplete.
+      atom.commands.dispatch(textEditorView, 'autocomplete-plus:confirm');
+      expect(textEditorView.querySelector('.autocomplete-plus')).not.toExist();
+      const lineText = textEditor.lineTextForBufferRow(textEditor.getCursorBufferPosition().row);
+      expect(lineText).toBe('num');
+    });
+  });
 });
