71f5734a57cd3f71eeb2344076066e4d383f7b2b
Alexander Dvinsky <advinsky@fb.com>
2016-06-06 09:25:19 -0700

    Integration test to verify that edited remote files are colored appropriately in the file tree
    Summary: Edits a remote file and verifies that it receives a proper status class in the file-tree
    
    Reviewed By: mostafaeweda
    
    Differential Revision: D3381831
    
    fbshipit-source-id: b9e98c09a2f6fd98189700d4bb9c79fc35b131d0

---
+++ spec/utils/edits-reflect-in-file-tree-common.js
@@ -0,0 +1,51 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+import type {TestContext} from './remotable-tests';
+
+import invariant from 'assert';
+
+import {
+  copyMercurialFixture,
+  fileTreeHasFinishedLoading,
+  getVisibleEntryFromFileTree,
+  pollFor,
+} from '../../pkg/nuclide-integration-test-helpers';
+
+export function runTest(context: TestContext) {
+  it('changes the color of the file in the file tree after an edit', () => {
+    waitsForPromise({timeout: 60000}, async () => {
+      const projectPath = await copyMercurialFixture('hg_repo_1');
+
+      // Add this directory as an atom project.
+      await context.setProject(projectPath);
+
+      const textEditor = await atom.workspace.open(context.getProjectRelativePath('test.txt'));
+      invariant(textEditor);
+      const textEditorView = atom.views.getView(textEditor);
+
+      textEditor.insertText('abcdef');
+
+      atom.commands.dispatch(textEditorView, 'core:save');
+
+      await fileTreeHasFinishedLoading();
+
+      await pollFor(
+        () => {
+          const fileElement = getVisibleEntryFromFileTree('test.txt');
+          return fileElement != null && fileElement.classList.contains('status-modified');
+        },
+        'File did not change its appearance in the file-tree',
+        30000,
+      );
+    });
+  });
+}
