d7edc1a043ab4186ff523af35d5c71d2fd768c43
Sameer Chitley <rageandqq@fb.com>
2016-06-07 11:29:18 -0700

    Add a queue to the NuxManager, allowing NUXes to wait for a trigger before being shown.
    Summary:
    Split the internal list of NUXes into:
    
      - `_pendingNuxList` which stores the list of registered NUXes that have yet to be triggered
      - `_readyToDisplayNuxList` which stores the list of NUXes that have been triggered and are ready to display. This is a list since multiple NUXes may be triggered at the same time (or one might be triggered when another is active).
    
    Reviewed By: jgebhardt
    
    Differential Revision: D3345043
    
    fbshipit-source-id: 3450cf145130797184d7ea3e3b071e78b275d2ae

--- pkg/nuclide-nux/lib/NuxManager.js
+++ pkg/nuclide-nux/lib/NuxManager.js
@@ -9,7 +9,12 @@
  * the root directory of this source tree.
  */
 
-import {CompositeDisposable, Emitter} from 'atom';
+import invariant from 'assert';
+import {
+  CompositeDisposable,
+  Emitter,
+  TextEditor,
+} from 'atom';
 import passesGK from '../../commons-node/passesGK';
 
 import {NuxStore} from './NuxStore';
@@ -25,7 +30,11 @@ export class NuxManager {
   _nuxStore: NuxStore;
   _disposables: CompositeDisposable;
   _emitter: atom$Emitter;
-  _nuxTours: Array<NuxTour>;
+  _isTourActive: boolean;
+  // Registered NUXes that are waiting to be triggered
+  _pendingNuxList: Array<NuxTour>;
+  // Triggered NUXes that are waiting to be displayed
+  _readyToDisplayNuxList: Array<NuxTour>;
 
   constructor(
     nuxStore: NuxStore,
@@ -34,13 +43,25 @@ export class NuxManager {
 
     this._emitter = new Emitter();
     this._disposables = new CompositeDisposable();
-    this._nuxTours = [];
+
+    this._pendingNuxList = [];
+    this._readyToDisplayNuxList = [];
+    this._isTourActive = false;
 
     this._emitter.on('newTour', this._handleNewTour.bind(this));
+    this._emitter.on('nuxTourReady', this._handleReadyTour.bind(this));
+
     this._disposables.add(this._nuxStore.onNewNux(this._handleNewNux.bind(this)));
+    this._disposables.add(
+      atom.workspace.onDidStopChangingActivePaneItem(
+        this._handleActivePaneItemChanged.bind(this)
+      ),
+    );
+
     this._nuxStore.initialize();
   }
 
+  // Handles new NUXes emitted from the store
   _handleNewNux(nuxTourModel: NuxTourModel): void {
     if (nuxTourModel.completed) {
       return;
@@ -58,8 +79,11 @@ export class NuxManager {
       )
     );
 
-    const nuxTour = new NuxTour(nuxTourModel.id, nuxViews);
-    this._nuxTours.push(nuxTour);
+    const nuxTour = new NuxTour(
+      nuxTourModel.id,
+      nuxViews,
+      nuxTourModel.trigger,
+    );
 
     this._emitter.emit(
       'newTour',
@@ -70,6 +94,17 @@ export class NuxManager {
     );
   }
 
+  _handleNuxCompleted(nuxTourModel: NuxTourModel): void {
+    this._isTourActive = false;
+    this._nuxStore.onNuxCompleted(nuxTourModel);
+    if (this._readyToDisplayNuxList.length === 0) {
+      return;
+    }
+    const nextNux = this._readyToDisplayNuxList.shift();
+    this._emitter.emit('nuxTourReady', nextNux);
+  }
+
+  // Handles NUX registry
   _handleNewTour(value: any) {
     const {
       nuxTour,
@@ -77,9 +112,42 @@ export class NuxManager {
     } = value;
     if (nuxTourModel.id === NUX_SAMPLE_OUTLINE_VIEW_TOUR && passesGK(GK_NUX_OUTLINE_VIEW)) {
       nuxTour.setNuxCompleteCallback(
-        this._nuxStore.onNuxCompleted.bind(this._nuxStore, nuxTourModel)
+          this._handleNuxCompleted.bind(this, nuxTourModel)
       );
+      if (nuxTourModel.trigger != null) {
+        this._pendingNuxList.push(nuxTour);
+      } else {
+        this._emitter.emit('nuxTourReady', nuxTour);
+      }
+    }
+  }
+
+  // Handles triggered NUXes that are ready to be displayed
+  _handleReadyTour(nuxTour: NuxTour) {
+    if (!this._isTourActive) {
+      this._isTourActive = true;
       nuxTour.begin();
+    } else {
+      this._readyToDisplayNuxList.push(nuxTour);
+    }
+  }
+
+  _handleActivePaneItemChanged(paneItem: ?mixed): void {
+    // The `paneItem` is not guaranteed to be an instance of `TextEditor` from
+    // Atom's API, but usually is.  We return if the type is not `TextEditor`
+    // since the `NuxTour.isReady` expects a `TextEditor` as its argument.
+    if (paneItem == null || !(paneItem instanceof TextEditor)) {
+      return;
+    }
+    invariant(paneItem instanceof TextEditor);
+    for (let i = 0; i < this._pendingNuxList.length; i++) {
+      const nuxToCheck = this._pendingNuxList[i];
+      if (nuxToCheck.getTriggerType() !== 'editor' ||
+          !nuxToCheck.isReady(paneItem)) {
+        continue;
+      }
+      this._pendingNuxList.splice(i--, 1);
+      this._emitter.emit('nuxTourReady', nuxToCheck);
     }
   }
 
