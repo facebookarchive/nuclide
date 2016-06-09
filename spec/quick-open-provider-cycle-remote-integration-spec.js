c246f8a542928ac528fec17bb9d4f2dbfbdb6f4f
Sameer Chitley <rageandqq@fb.com>
2016-06-09 11:38:00 -0700

    Quick-open cycles through files from all providers integration test
    Summary:
    This test makes sure that the quick open:
    
      - Cycles through all the providers when pressing the arrow key
      - Returns back to the initially selected provider after scrolling enough
    
    In order to make the fuzzy file service actually work during integration tests, I removed the line that was removing it from the list of available features.
    johnislarry said this was initially added because it was causing tests to hang, but they seem fine now.
    
    NOTE: This diff only tests providers that are common across all types of repositories. Specific ones, like BigGrep, Hack Symbols, will be added in another diff.
    
    Reviewed By: johnislarry
    
    Differential Revision: D3376944
    
    fbshipit-source-id: 03e38181811b0bfe4ba2583b799b732697ec36b4

---
+++ spec/quick-open-provider-cycle-remote-integration-spec.js
@@ -0,0 +1,17 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+import {describeRemote} from './utils/remotable-tests';
+import {runTest} from './utils/quick-open-provider-cycle-common';
+
+describeRemote('Quick Open Providers', context => {
+  runTest(context);
+});
