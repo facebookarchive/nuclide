6a1ce34ac1a7eb7ca24e6f66adf0f984f972b6e6
Mitch Rees-Jones <reesjones@fb.com>
2016-06-10 15:56:10 -0700

    Remove services and activation from nuclide-definition-preview
    Summary: Definition preview no longer needs to consume the definition service, so remove it
    
    Reviewed By: peterhal
    
    Differential Revision: D3385703
    
    fbshipit-source-id: 1b359122752df8000bd1054c7519869a38cfe744

--- pkg/nuclide-definition-preview/lib/main.js
+++ pkg/nuclide-definition-preview/lib/main.js
@@ -9,78 +9,13 @@
  * the root directory of this source tree.
  */
 
-import type {DistractionFreeModeProvider} from '../../nuclide-distraction-free-mode';
-import type {DefinitionService} from '../../nuclide-definition-service';
-
-import {CompositeDisposable, Disposable} from 'atom';
-import {CodePreviewState} from './CodePreviewState';
-import invariant from 'assert';
-
-let currentService: ?DefinitionService = null;
-
-type CodePreviewConfig = {
-  width: number;
-  visible: boolean;
-};
-
-const DEFAULT_WIDTH = 300; // px
-const DEFAULT_CONFIG: CodePreviewConfig = {
-  width: DEFAULT_WIDTH,
-  visible: false,
-};
-
 class Activation {
-  _disposables: CompositeDisposable;
-  _state: CodePreviewState;
 
-  constructor(config?: CodePreviewConfig = DEFAULT_CONFIG) {
-    this._disposables = new CompositeDisposable();
-    this._state = new CodePreviewState(config.width, config.visible);
-    this.updateService();
-    this._disposables.add(
-      atom.commands.add(
-        'atom-workspace',
-        'nuclide-definition-preview:toggle',
-        () => this._state.toggle()
-      )
-    );
-    this._disposables.add(
-      atom.commands.add(
-        'atom-workspace',
-        'nuclide-definition-preview:show',
-        () => this._state.show()
-      )
-    );
-    this._disposables.add(
-      atom.commands.add(
-        'atom-workspace',
-        'nuclide-definition-preview:hide',
-        () => this._state.hide()
-      )
-    );
-  }
+  constructor() {
 
-  updateService(): void {
-    this._state.setDefinitionService(currentService);
   }
 
   dispose() {
-    this._disposables.dispose();
-  }
-
-  serialize(): CodePreviewConfig {
-    return {
-      width: this._state.getWidth(),
-      visible: this._state.isVisible(),
-    };
-  }
-
-  getDistractionFreeModeProvider(): DistractionFreeModeProvider {
-    return {
-      name: 'nuclide-definition-preview',
-      isVisible: () => this._state.isVisible(),
-      toggle: () => this._state.toggle(),
-    };
   }
 }
 
@@ -88,7 +23,7 @@ let activation: ?Activation = null;
 
 export function activate(state: Object | void) {
   if (activation == null) {
-    activation = new Activation(state);
+    activation = new Activation();
   }
 }
 
@@ -97,32 +32,4 @@ export function deactivate() {
     activation.dispose();
     activation = null;
   }
-}
-
-function updateService(): void {
-  if (activation != null) {
-    activation.updateService();
-  }
-}
-
-export function consumeDefinitionService(service: DefinitionService): IDisposable {
-  invariant(currentService == null);
-  currentService = service;
-  updateService();
-  return new Disposable(() => {
-    invariant(currentService === service);
-    currentService = null;
-    updateService();
-  });
-}
-
-export function serialize(): ?CodePreviewConfig {
-  if (activation != null) {
-    return activation.serialize();
-  }
-}
-
-export function getDistractionFreeModeProvider(): DistractionFreeModeProvider {
-  invariant(activation != null);
-  return activation.getDistractionFreeModeProvider();
 }
