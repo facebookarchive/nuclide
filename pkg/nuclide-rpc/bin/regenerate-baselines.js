f82d3f4ad3c4a1e6411395d4bc7fd2e97c232f11
Andres Suarez <asuarez@fb.com>
2016-06-14 22:47:28 -0700

    Fix regenerate-baselines's generateProxy call and es6ify
    Summary: Modernized the script and fixed the call to `generateProxy` so that it passes `preserveFunctionNames`.
    
    Reviewed By: peterhal
    
    Differential Revision: D3424041
    
    fbshipit-source-id: 47352264f39e46f8e94f9e8684dd31ea6780e8f6

--- pkg/nuclide-rpc/bin/regenerate-baselines.js
+++ pkg/nuclide-rpc/bin/regenerate-baselines.js
@@ -10,37 +10,45 @@
  * the root directory of this source tree.
  */
 
-/*eslint-disable no-var, prefer-const, no-console*/
-
 // Regenerates the .proxy baseline files in the spec/fixtures directory.
 
 require('../../nuclide-node-transpiler');
 
-var parseServiceDefinition = require('../lib/service-parser').parseServiceDefinition;
-var generateProxy = require('../lib/proxy-generator').generateProxy;
+const parseServiceDefinition = require('../lib/service-parser').parseServiceDefinition;
+const generateProxy = require('../lib/proxy-generator').generateProxy;
 
-var fs = require('fs');
-var path = require('path');
+const fs = require('fs');
+const path = require('path');
 
-var dir = path.join(__dirname, '../spec/fixtures');
-for (var file of fs.readdirSync(dir)) {
+const dir = path.join(__dirname, '../spec/fixtures');
+for (const file of fs.readdirSync(dir)) {
   if (file.endsWith('.def')) {
-    var definitions = parseServiceDefinition(file, fs.readFileSync(path.join(dir, file), 'utf8'));
+    const serviceName = path.basename(file, '.def');
+    const preserveFunctionNames = false;
+    const definitionPath = path.join(dir, file);
 
-    var json = mapDefinitions(definitions);
-    fs.writeFileSync(
-      path.join(dir, file).replace('.def', '.def.json'), JSON.stringify(json, null, 4), 'utf8');
+    const definitionSource = fs.readFileSync(definitionPath, 'utf8');
+    const definitions = parseServiceDefinition(
+      path.basename(file),
+      definitionSource
+    );
 
-    var code = generateProxy(path.basename(file, '.def'), definitions);
+    const json = mapDefinitions(definitions);
     fs.writeFileSync(
-      path.join(dir, file).replace('.def', '.proxy'), code, 'utf8');
+      definitionPath.replace('.def', '.def.json'),
+      JSON.stringify(json, null, 4),
+      'utf8'
+    );
+
+    const code = generateProxy(serviceName, preserveFunctionNames, definitions);
+    fs.writeFileSync(definitionPath.replace('.def', '.proxy'), code, 'utf8');
   }
 }
 
 function mapDefinitions(map) {
-  var obj = {};
-  for (var it of map.values()) {
-    var value;
+  const obj = {};
+  for (const it of map.values()) {
+    let value;
     switch (it.kind) {
       case 'interface':
         value = {
@@ -59,8 +67,8 @@ function mapDefinitions(map) {
 }
 
 function mapToJSON(map) {
-  var result = {};
-  for (var it of map.entries()) {
+  const result = {};
+  for (const it of map.entries()) {
     result[it[0]] = it[1];
   }
   return result;
