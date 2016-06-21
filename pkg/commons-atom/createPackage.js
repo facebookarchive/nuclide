34e26de43c565ab2a0d29878a49082c39b3fdaec
Matthew Dapena-Tretter <matthewwithanm@fb.com>
2016-06-20 21:01:29 -0700

    Add utility for creating package from activation class
    Summary:
    Atom packages are ostensibly singletons, but really the model of instances is a better fit: they're activated (constructed), methods are called on them, and then they're deactivated (destroyed). This commit lets us simplify our code by handling the construction and destruction of a single Activation class.
    
    Example Usage:
    
      // Define an activation class.
      class Activation {
        _disposables: Disposable;
    
        constructor(initialState: Object): void {
          this._disposables = new CompositeDisposable(
            atom.commands.add(...),
            atom.views.addProvider(...),
          );
        }
    
        dispose(): void {
          this._disposables.dispose();
        }
    
        consumeToolbar(toolbar) {
          // whatever
        }
    
      }
    
      // Convert the object to package exports and export them
      export default createPackage(Activation);
    
    Reviewed By: nmote, mostafaeweda
    
    Differential Revision: D2769157
    
    fbshipit-source-id: b117e085adefd0c9d62ce76cfce3708c4306f218

---
+++ pkg/commons-atom/createPackage.js
@@ -0,0 +1,101 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+import invariant from 'assert';
+
+/**
+ * Create an Atom package from an Activation constructor.
+ *
+ * Atom packages are obstensibly singletons, however they contain `activate()` and `deactivate()`
+ * lifecycle methods which can be called multiple times. There's no use-case (so far as we know) for
+ * invoking any other package methods while a package is not activated. Therefore, it makes more
+ * sense to build packages as instances, constructed when a package is activated and destroyed when
+ * the package is deactivated.
+ */
+export default function createPackage(Activation: Class<any>): Object {
+  let activation = null;
+  const pkg = {};
+
+  // Proxy method calls on the package to the activation object.
+  for (const property of getPropertyList(Activation.prototype)) {
+    if (typeof Activation.prototype[property] !== 'function') {
+      continue;
+    }
+    if (property === 'constructor') {
+      continue;
+    }
+    if (property === 'activate') {
+      throw new Error(
+        'Your activation class contains an "activate" method, but that work should be done in the'
+        + ' constructor.'
+      );
+    }
+    if (property === 'deactivate') {
+      throw new Error(
+        'Your activation class contains an "deactivate" method. Please use "dispose" instead.'
+      );
+    }
+
+    pkg[property] = function(...args) {
+      invariant(activation != null, 'Package not activated');
+      return activation[property](...args);
+    };
+  }
+
+  return {
+    ...pkg,
+
+    /**
+     * Calling `activate()` creates a new instance.
+     */
+    activate(initialState: ?Object): void {
+      invariant(activation == null, 'Package already activated');
+      activation = new Activation(initialState);
+    },
+
+    /**
+     * The `deactivate()` method is special-cased to null our activation instance reference.
+     */
+    deactivate(): void {
+      invariant(activation != null, 'Package not activated');
+      if (typeof activation.dispose === 'function') {
+        activation.dispose();
+      }
+      activation = null;
+    },
+  };
+}
+
+function getPrototypeChain(prototype: Class<any>): Array<Class<any>> {
+  const prototypes = [];
+  while (prototype != null) {
+    prototypes.push(prototype);
+    prototype = Object.getPrototypeOf(prototype);
+  }
+  return prototypes;
+}
+
+/**
+ * List the properties (including inherited ones) of the provided prototype, excluding the ones
+ * inherited from `Object`.
+ */
+function getPropertyList(prototype: Class<any>): Array<string> {
+  const properties = [];
+  for (const proto of getPrototypeChain(prototype)) {
+    if (proto === (Object: any).prototype) {
+      break;
+    }
+    for (const property of Object.getOwnPropertyNames(proto)) {
+      properties.push(property);
+    }
+  }
+  return properties;
+}
