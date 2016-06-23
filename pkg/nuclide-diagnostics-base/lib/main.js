55a504aad53aac0c2388bf0339ce81a4c3f3abbb
Nat Mote <nmote@fb.com>
2016-06-23 11:14:10 -0700

    Add type for ObservableDiagnosticUpdater [7/n]
    Summary:
    Largely mimics the previous API, but uses Observables rather than callbacks.
    
    fileMessageUpdates is still a function. The alternative would be to have it be
    an Observable that includes all file changes, and allow the client to filter
    themselves. But, to satisfy the contract that initial values are provided on
    subscribe, we would have to compute and send over all file messages, which could
    be expensive. This isn't just academic because we currently subscribe to file
    messages every time a new editor is opened.
    
    Reviewed By: matthewwithanm
    
    Differential Revision: D3472370
    
    fbshipit-source-id: 690cd0996a7c8bac2b1bff33bbd889b6f307902f

--- pkg/nuclide-diagnostics-base/lib/main.js
+++ pkg/nuclide-diagnostics-base/lib/main.js
@@ -103,6 +103,20 @@ export type DiagnosticUpdater = {
   applyFixesForFile: (file: NuclideUri) => void;
 };
 
+export type ObservableDiagnosticUpdater = {
+  // All observables here will issue an initial value on subscribe.
+
+  // Sent only when the messages for a given file change. Consumers may use this to avoid
+  // unnecessary work if the file(s) they are interested in are not changed.
+  getFileMessageUpdates: (filePath: NuclideUri) => Observable<FileMessageUpdate>;
+  // Sent whenever any project message changes.
+  projectMessageUpdates: Observable<Array<ProjectDiagnosticMessage>>;
+  // Sent whenever any message changes, and includes all messages.
+  allMessageUpdates: Observable<Array<DiagnosticMessage>>;
+  applyFix: (message: FileDiagnosticMessage) => void;
+  applyFixesForFile: (file: NuclideUri) => void;
+};
+
 import DiagnosticStore from './DiagnosticStore';
 
 /**
