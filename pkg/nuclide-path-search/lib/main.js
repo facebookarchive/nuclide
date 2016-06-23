e12bd69b203dff39ecd3b4ee5b56c082098cbe99
Hanson Wang <hansonw@fb.com>
2016-06-23 14:31:31 -0700

    Path search: autoupdate indexer on ignoredNames changes
    Summary: Addresses the previous TODO and stores the last value of ignoredNames, performing a full re-index when it changes.
    
    Reviewed By: zertosh
    
    Differential Revision: D3474576
    
    fbshipit-source-id: 4284511405a831179d048f65333147734e7821d1

--- pkg/nuclide-path-search/lib/main.js
+++ pkg/nuclide-path-search/lib/main.js
@@ -13,6 +13,7 @@ import type {Task} from '../../nuclide-task';
 import type {FileSearchResult as FileSearchResultType} from './FileSearch';
 
 import {getLogger} from '../../nuclide-logging';
+import {arrayEqual} from '../../commons-node/collection';
 import fsPromise from '../../commons-node/fsPromise';
 
 export type FileSearchResult = FileSearchResultType;
@@ -32,14 +33,16 @@ const logger = getLogger();
 class MainProcessFileSearch {
   _task: ?Task;
   _directoryUri: DirectoryUri;
+  _ignoredNames: Array<string>;
 
-  constructor(task: Task, directoryUri: DirectoryUri) {
+  constructor(task: Task, directoryUri: DirectoryUri, ignoredNames: Array<string>) {
     this._task = task;
     this._task.onError(buffer => {
       logger.error('File search process crashed with message:', buffer.toString());
       this.dispose();
     });
     this._directoryUri = directoryUri;
+    this._ignoredNames = ignoredNames;
   }
 
   async query(query: string): Promise<Array<FileSearchResult>> {
@@ -52,6 +55,10 @@ class MainProcessFileSearch {
       method: 'doSearch',
       args: [this._directoryUri, query],
     });
+  }
+
+  getIgnoredNames() {
+    return this._ignoredNames;
   }
 
   dispose() {
@@ -69,6 +76,16 @@ async function newFileSearch(
   directoryUri: string,
   ignoredNames: Array<string>,
 ): Promise<MainProcessFileSearch> {
+  const exists = await fsPromise.exists(directoryUri);
+  if (!exists) {
+    throw new Error('Could not find directory to search : ' + directoryUri);
+  }
+
+  const stat = await fsPromise.stat(directoryUri);
+  if (!stat.isDirectory()) {
+    throw new Error('Provided path is not a directory : ' + directoryUri);
+  }
+
   const {createTask} = require('../../nuclide-task');
   const task = createTask();
   await task.invokeRemoteMethod({
@@ -76,7 +93,7 @@ async function newFileSearch(
     method: 'initFileSearchForDirectory',
     args: [directoryUri, ignoredNames],
   });
-  return new MainProcessFileSearch(task, directoryUri);
+  return new MainProcessFileSearch(task, directoryUri, ignoredNames);
 }
 
 /**
@@ -89,19 +106,14 @@ export async function fileSearchForDirectory(
   directoryUri: string,
   ignoredNames: Array<string>,
 ): Promise<FileSearch> {
-  if (directoryUri in fileSearchForDirectoryUri) {
-    // TODO(hansonw): check if ignoredNames has changed and create a new task
-    return fileSearchForDirectoryUri[directoryUri];
-  }
-
-  const exists = await fsPromise.exists(directoryUri);
-  if (!exists) {
-    throw new Error('Could not find directory to search : ' + directoryUri);
-  }
-
-  const stat = await fsPromise.stat(directoryUri);
-  if (!stat.isDirectory()) {
-    throw new Error('Provided path is not a directory : ' + directoryUri);
+  const cached = fileSearchForDirectoryUri[directoryUri];
+  if (cached != null) {
+    const fileSearch = await cached;
+    if (arrayEqual(fileSearch.getIgnoredNames(), ignoredNames)) {
+      return fileSearch;
+    }
+    // In case of a mismatch, dispose and recreate the searcher task.
+    fileSearch.dispose();
   }
 
   const promise = newFileSearch(directoryUri, ignoredNames);
