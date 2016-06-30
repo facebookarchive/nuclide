3f52f4ea4ceec32fb6a9392db325f14a613a5c41
Jonas Gebhardt <jxg@fb.com>
2016-06-30 10:12:41 -0700

    Use LazyNestedValueComponent to render locals
    Summary: This makes locals look & behave exactly like watch expressions, including interactive expansion etc.
    
    Reviewed By: yinghuitan
    
    Differential Revision: D3487585
    
    fbshipit-source-id: 7d20a12b934f5c05939c1b113ffe5f2cd87f31cc

--- pkg/nuclide-debugger-atom/lib/LocalsComponent.js
+++ pkg/nuclide-debugger-atom/lib/LocalsComponent.js
@@ -9,14 +9,22 @@
  * the root directory of this source tree.
  */
 
-import type {Locals} from './LocalsStore';
+import type {
+  ExpansionResult,
+} from './Bridge';
+import {WatchExpressionStore} from './WatchExpressionStore';
+import type {Local, Locals} from './LocalsStore';
+import type {Observable} from 'rxjs';
 
 import {
   React,
 } from 'react-for-atom';
+import {LazyNestedValueComponent} from '../../nuclide-ui/lib/LazyNestedValueComponent';
+import SimpleValueComponent from '../../nuclide-ui/lib/SimpleValueComponent';
 
 type LocalsComponentProps = {
   locals: Locals;
+  watchExpressionStore: WatchExpressionStore;
 };
 
 export class LocalsComponent extends React.Component {
@@ -24,16 +32,45 @@ export class LocalsComponent extends React.Component {
 
   constructor(props: LocalsComponentProps) {
     super(props);
+    (this: any)._renderExpression = this._renderExpression.bind(this);
+  }
+
+  _renderExpression(
+    fetchChildren: (objectId: string) => Observable<?ExpansionResult>,
+    local: Local,
+    index: number,
+  ): React.Element<any> {
+    const {
+      name,
+      value,
+    } = local;
+    return (
+      <div
+        className="nuclide-debugger-atom-watch-expression-row"
+        key={index}>
+        <div
+          className="nuclide-debugger-atom-watch-expression-row-content">
+          <LazyNestedValueComponent
+            expression={name}
+            evaluationResult={value}
+            fetchChildren={fetchChildren}
+            simpleValueComponent={SimpleValueComponent}
+          />
+        </div>
+      </div>
+    );
   }
 
   render(): ?React.Element<any> {
     const {
+      watchExpressionStore,
       locals,
     } = this.props;
-    const expressions = locals.map(local => {
-      const {name} = local;
-      return <div key={name}>{name}</div>;
-    });
+    if (locals == null || locals.length === 0) {
+      return <span>(no variables)</span>;
+    }
+    const fetchChildren = watchExpressionStore.getProperties.bind(watchExpressionStore);
+    const expressions = locals.map(this._renderExpression.bind(this, fetchChildren));
     return (
       <div>
         {expressions}
