416bafb68232372a759069b41f1c44a6d45da481
Thomas Liu <tyangliu@fb.com>
2016-07-05 22:27:55 -0700

    Add trimRange to commons-atom
    Summary: Moves the start and end points of range to the first and last non-whitespace character positions. Used in next diff for python diagnostics underlining (to underline only the non-leading/trailing whitespace portion of a buffer line).
    
    Reviewed By: zertosh
    
    Differential Revision: D3497514
    
    fbshipit-source-id: 1472d6b56a28db7eb0859eda086d7ab133caf195

--- pkg/commons-atom/range.js
+++ pkg/commons-atom/range.js
@@ -9,6 +9,8 @@
  * the root directory of this source tree.
  */
 
+import {Range} from 'atom';
+
 export function wordAtPosition(
   editor: atom$TextEditor,
   position: atom$Point,
@@ -40,4 +42,33 @@ export function wordAtPosition(
   } else {
     return null;
   }
+}
+
+/**
+ * Gets the trimmed range from a given range, i.e. moves the start and end points
+ * to the first and last non-whitespace characters (or specified regex)
+ * within the range respectively.
+ *
+ * @param editor       the editor containing the range
+ * @param rangeToTrim  the range to trim
+ * @param stopRegex    stop trimming when the first match is found for this regex,
+ *   defaults to first non-whitespace character
+ * @return atom$Range  the trimmed range
+ */
+export function trimRange(
+  editor: atom$TextEditor,
+  rangeToTrim: atom$Range,
+  stopRegex: RegExp = /\S/,
+): atom$Range {
+  const buffer = editor.getBuffer();
+  let {start, end} = rangeToTrim;
+  buffer.scanInRange(stopRegex, rangeToTrim, ({range, stop}) => {
+    start = range.start;
+    stop();
+  });
+  buffer.backwardsScanInRange(stopRegex, rangeToTrim, ({range, stop}) => {
+    end = range.end;
+    stop();
+  });
+  return new Range(start, end);
 }
