c6833a73935245bea4a73f5fcddd9e9167196962
Matthew Dapena-Tretter <matthewwithanm@fb.com>
2016-07-12 10:08:42 -0700

    Add console provider for Atom messages
    Summary:
    Does the bare minimum, but still useful! Also works pretty well as an example for output providers (:
    
    Changelog: Atom messages now show up in the console
    
    Reviewed By: johnislarry
    
    Differential Revision: D3547888
    
    fbshipit-source-id: ab1d84d2be343a39b5af09780c1d9884cd5f5937

---
+++ pkg/nuclide-atom-notifications/lib/main.js
@@ -0,0 +1,61 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+import type {Level, OutputService} from '../../nuclide-console/lib/types';
+
+import {CompositeDisposable} from 'atom';
+import createPackage from '../../commons-atom/createPackage';
+import {observableFromSubscribeFunction} from '../../commons-node/event';
+
+class Activation {
+  _disposables: CompositeDisposable;
+
+  constructor() {
+    this._disposables = new CompositeDisposable();
+  }
+
+  consumeOutputService(api: OutputService): void {
+    const messages = observableFromSubscribeFunction(
+      atom.notifications.onDidAddNotification.bind(atom.notifications)
+    )
+      .map(notification => ({
+        // TODO (matthewwithanm): Add timestamp once nuclide-console supports it.
+        // TODO (matthewwithanm): Show notification description/details.
+        text: notification.getMessage(),
+        level: getLevel(notification.getType()),
+      }));
+
+    this._disposables.add(api.registerOutputProvider({id: 'Atom', messages}));
+  }
+
+  dispose() {
+    this._disposables.dispose();
+  }
+}
+
+function getLevel(atomNotificationType: string): Level {
+  switch (atomNotificationType) {
+    case 'error':
+    case 'fatal':
+      return 'error';
+    case 'info':
+      return 'info';
+    case 'warning':
+      return 'warning';
+    case 'success':
+      // TODO (matthewwithanm): Add a "success" level to nuclide-console?
+      return 'log';
+    default:
+      return 'log';
+  }
+}
+
+export default createPackage(Activation);
