55412cb25332625f8cf42b7010fe19f9435d6777
Matthew Dapena-Tretter <matthewwithanm@fb.com>
2016-07-15 18:28:48 -0700

    Use createPackage
    Summary: Just moves the Activation class into main and uses createPackage instead of manually proxying.
    
    Reviewed By: zertosh
    
    Differential Revision: D3568762
    
    fbshipit-source-id: b83b1a7c493aab90f803cf33116efa81218a32fa

--- pkg/nuclide-console/lib/main.js
+++ pkg/nuclide-console/lib/main.js
@@ -9,39 +9,163 @@
  * the root directory of this source tree.
  */
 
-import type ActivationType from './Activation';
-import type {OutputService, RegisterExecutorFunction} from './types';
-import type {GadgetsService} from '../../nuclide-gadgets/lib/types';
+import type {GadgetsService, Gadget} from '../../nuclide-gadgets/lib/types';
+import type {AppState, OutputProvider, OutputService, RegisterExecutorFunction} from './types';
 
+import createPackage from '../../commons-atom/createPackage';
+import {DisposableSubscription} from '../../commons-node/stream';
+import {CompositeDisposable, Disposable} from 'atom';
+import * as ActionTypes from './ActionTypes';
+import Commands from './Commands';
+import createConsoleGadget from './ui/createConsoleGadget';
+import createStateStream from './createStateStream';
+import featureConfig from '../../nuclide-feature-config';
 import invariant from 'assert';
+import Rx from 'rxjs';
 
-let activation: ?ActivationType = null;
+class Activation {
+  _commands: Commands;
+  _disposables: CompositeDisposable;
+  _outputService: ?OutputService;
+  _registerExecutorFunction: ?RegisterExecutorFunction;
+  _state$: Rx.BehaviorSubject<AppState>;
 
-export function activate(state: ?Object) {
-  if (activation == null) {
-    const Activation = require('./Activation');
-    activation = new Activation(state);
+  constructor(rawState: ?Object) {
+    const action$ = new Rx.Subject();
+    const initialState = deserializeAppState(rawState);
+    this._state$ = new Rx.BehaviorSubject(initialState);
+    createStateStream(
+      action$.asObservable(),
+      initialState,
+    )
+      .sampleTime(100)
+      .subscribe(this._state$);
+    this._commands = new Commands(
+      action$,
+      () => this._state$.getValue(),
+    );
+    this._disposables = new CompositeDisposable(
+      atom.contextMenu.add({
+        '.nuclide-console-record': [
+          {
+            label: 'Copy Message',
+            command: 'nuclide-console:copy-message',
+          },
+        ],
+      }),
+      atom.commands.add(
+        '.nuclide-console-record',
+        'nuclide-console:copy-message',
+        event => {
+          const el = event.target;
+          if (el == null || el.innerText == null) {
+            return;
+          }
+          atom.clipboard.write(el.innerText);
+        },
+      ),
+      atom.commands.add(
+        'atom-workspace',
+        'nuclide-console:clear',
+        () => this._commands.clearRecords(),
+      ),
+      featureConfig.observe(
+        'nuclide-console.maximumMessageCount',
+        maxMessageCount => this._commands.setMaxMessageCount(maxMessageCount),
+      ),
+
+      // Action side-effects
+      new DisposableSubscription(
+        action$.subscribe(action => {
+          if (action.type !== ActionTypes.EXECUTE) {
+            return;
+          }
+          const {executorId, code} = action.payload;
+          const executors = this._state$.getValue().executors;
+          const executor = executors.get(executorId);
+          invariant(executor);
+          executor.send(code);
+        })
+      ),
+    );
   }
-}
 
-export function deactivate() {
-  if (activation != null) {
-    activation.dispose();
-    activation = null;
+  dispose() {
+    this._disposables.dispose();
   }
+
+  consumeGadgetsService(gadgetsApi: GadgetsService): void {
+    const OutputGadget = createConsoleGadget(this._state$.asObservable(), this._commands);
+    this._disposables.add(gadgetsApi.registerGadget(((OutputGadget: any): Gadget)));
+  }
+
+  provideOutputService(): OutputService {
+    if (this._outputService == null) {
+      // Create a local, nullable reference so that the service consumers don't keep the `Commands`
+      // instance in memory.
+      let commands = this._commands;
+      this._disposables.add(new Disposable(() => { commands = null; }));
+
+      this._outputService = {
+        registerOutputProvider(outputProvider: OutputProvider): IDisposable {
+          if (commands != null) {
+            commands.registerOutputProvider(outputProvider);
+          }
+          return new Disposable(() => {
+            if (commands != null) {
+              commands.removeSource(outputProvider.id);
+            }
+          });
+        },
+      };
+    }
+    return this._outputService;
+  }
+
+  provideRegisterExecutor(): RegisterExecutorFunction {
+    if (this._registerExecutorFunction == null) {
+      // Create a local, nullable reference so that the service consumers don't keep the `Commands`
+      // instance in memory.
+      let commands = this._commands;
+      this._disposables.add(new Disposable(() => { commands = null; }));
+
+      this._registerExecutorFunction = executor => {
+        if (commands != null) {
+          commands.registerExecutor(executor);
+        }
+        return new Disposable(() => {
+          if (commands != null) {
+            commands.unregisterExecutor(executor);
+          }
+        });
+      };
+    }
+    return this._registerExecutorFunction;
+  }
+
+  serialize(): Object {
+    const state = this._state$.getValue();
+    return {
+      records: state.records,
+    };
+  }
+
 }
 
-export function consumeGadgetsService(gadgetsApi: GadgetsService): void {
-  invariant(activation);
-  activation.consumeGadgetsService(gadgetsApi);
+function deserializeAppState(rawState: ?Object): AppState {
+  rawState = rawState || {};
+  return {
+    executors: new Map(),
+    currentExecutorId: null,
+    records: rawState.records || [],
+    providers: new Map(),
+    providerStatuses: new Map(),
+    providerSubscriptions: new Map(),
+
+    // This value will be replaced with the value form the config. We just use `POSITIVE_INFINITY`
+    // here to conform to the AppState type defintion.
+    maxMessageCount: Number.POSITIVE_INFINITY,
+  };
 }
 
-export function provideOutputService(): OutputService {
-  invariant(activation);
-  return activation.provideOutputService();
-}
-
-export function provideRegisterExecutor(): RegisterExecutorFunction {
-  invariant(activation);
-  return activation.provideRegisterExecutor();
-}
+export default createPackage(Activation);
