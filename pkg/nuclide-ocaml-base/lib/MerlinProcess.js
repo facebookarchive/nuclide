11ed25473274a5448fcaa307cae94c3418afa5bc
Peter Hallam <peterhal@fb.com>
2016-07-19 10:40:43 -0700

    Add v2 protocol
    Summary: This diff makes nuclide play well with merlin 2.5
    
    Reviewed By: nmote
    
    Differential Revision: D3557413
    
    fbshipit-source-id: 438108c5242c94a0aa7751747b56cbe57adc7764

--- pkg/nuclide-ocaml-base/lib/MerlinProcess.js
+++ pkg/nuclide-ocaml-base/lib/MerlinProcess.js
@@ -231,6 +231,124 @@ export class MerlinProcessV2_3_1 extends MerlinProcessBase {
   }
 }
 
+/**
+ * Wraps an ocamlmerlin process which talks v2 protocol; provides api access to
+ * ocamlmerlin's json-over-stdin/stdout protocol.
+ *
+ * This is based on the protocol description at:
+ *   https://github.com/the-lambda-church/merlin/blob/master/doc/dev/PROTOCOL.md
+ *   https://github.com/the-lambda-church/merlin/tree/master/src/frontend
+ */
+export class MerlinProcessV2_5 extends MerlinProcessBase {
+
+  constructor(proc: child_process$ChildProcess) {
+    super(proc);
+  }
+
+  async pushDotMerlinPath(file: NuclideUri): Promise<mixed> {
+    return await this._promiseQueue.submit(async (resolve, reject) => {
+      const result = await this.runSingleCommand([
+        'reset',
+        'dot_merlin',
+        [file],
+        'auto',
+      ]);
+      resolve(result);
+    });
+  }
+
+  /**
+   * Set the buffer content to query against. Merlin uses an internal
+   * buffer (name + content) that is independent from file content on
+   * disk.
+   *
+   * @return on success: a cursor position pointed at the end of the buffer
+   */
+  async pushNewBuffer(name: NuclideUri, content: string): Promise<mixed> {
+    return await this._promiseQueue.submit(async (resolve, reject) => {
+      const result = await this.runSingleCommand([
+        'tell',
+        'start',
+        'end',
+        content,
+      ]);
+      resolve(result);
+    });
+  }
+
+  /**
+   * Find definition
+   *
+   * `kind` is one of 'ml' or 'mli'
+   *
+   * Note: ocamlmerlin line numbers are 1-based.
+   * @return null if nothing was found; a position of the form
+   *   {"file": "somepath", "pos": {"line": 41, "col": 5}}.
+   */
+  async locate(
+    file: NuclideUri,
+    line: number,
+    col: number,
+    kind: string,
+  ): Promise<?{file: string, pos: {line: number, col: number}}> {
+    return await this._promiseQueue.submit(async (resolve, reject) => {
+      const location = await this.runSingleCommand([
+        'locate',
+        /* identifier name */ '',
+        kind,
+        'at',
+        {line: line + 1, col},
+      ]);
+
+
+      if (typeof location === 'string') {
+        return reject(Error(location));
+      }
+
+      // Ocamlmerlin doesn't include a `file` field at all if the destination is
+      // in the same file.
+      if (!location.file) {
+        location.file = file;
+      }
+
+      resolve(location);
+    });
+  }
+
+  async enclosingType(
+    file: NuclideUri,
+    line: number,
+    col: number,
+  ): Promise<Array<MerlinType>> {
+    return await this._promiseQueue.submit((resolve, reject) => {
+      this.runSingleCommand(['type', 'enclosing', 'at', {line: line + 1, col}])
+        .then(resolve)
+        .catch(reject);
+    });
+  }
+
+  async complete(file: NuclideUri, line: number, col: number, prefix: string): Promise<mixed> {
+    return await this._promiseQueue.submit((resolve, reject) => {
+      this.runSingleCommand([
+        'complete',
+        'prefix',
+        prefix,
+        'at',
+        {line: line + 1, col: col + 1},
+      ]).then(resolve)
+        .catch(reject);
+    });
+  }
+
+  async errors(): Promise<Array<MerlinError>> {
+    return await this._promiseQueue.submit((resolve, reject) => {
+      this.runSingleCommand(['errors'])
+        .then(resolve)
+        .catch(reject);
+    });
+  }
+}
+
 let merlinProcessInstance: ?MerlinProcess;
 
 export async function getInstance(file: NuclideUri): Promise<?MerlinProcess> {
@@ -253,7 +371,18 @@ export async function getInstance(file: NuclideUri): Promise<?MerlinProcess> {
 
   logger.info('Spawning new ocamlmerlin process');
   const process = await safeSpawn(merlinPath, flags, options);
-  merlinProcessInstance = new MerlinProcessV2_3_1(process);
+  const version = await getVersion(process);
+  switch (version) {
+    case '2.5.0':
+      merlinProcessInstance = new MerlinProcessV2_5(process);
+      break;
+    case '2.3.1':
+      merlinProcessInstance = new MerlinProcessV2_3_1(process);
+      break;
+    default:
+      logger.error(`Unsupported merlin version: ${version}`);
+      return null;
+  }
 
   if (dotMerlinPath) {
     // TODO(pieter) add support for multiple .dotmerlin files
@@ -262,6 +391,24 @@ export async function getInstance(file: NuclideUri): Promise<?MerlinProcess> {
   }
 
   return merlinProcessInstance;
+}
+
+async function getVersion(proc: child_process$ChildProcess): Promise<string> {
+  let version;
+  try {
+    // TODO: Support version 3
+    const result = await runSingleCommand(proc, [
+      'protocol',
+      'version',
+      2, // default to version 2
+    ]);
+    const match = result.merlin.match(/^The Merlin toolkit version (\d+(\.\d)*),/);
+    return match != null && match[1] != null ? match[1] : '2.3.1';
+  } catch (e) {
+    // version 2.3.1 doesn't have a 'protocol' command and will throw
+    version = '2.3.1';
+  }
+  return version;
 }
 
 /**
