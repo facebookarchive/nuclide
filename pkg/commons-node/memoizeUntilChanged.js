e8a3a1bda55eda76cbf4888f2b5d5b63f44314b2
Matthew Dapena-Tretter <matthewwithanm@fb.com>
2016-07-21 14:32:16 -0700

    Add memoizeUntilChanged util
    Summary:
    This is something I've wanted for a while to make our components easy to optimize without having to deal with `this.state`.
    
    Originally, I had it just accept a single argument so that we could Flow type it, but having to wrap multiple arguments in an object/array and then define a type for it just didn't seem worth it.
    
    Reviewed By: jgebhardt
    
    Differential Revision: D3585363
    
    fbshipit-source-id: f7fffa6b75160763a576b256c92cdba76a8c5c03

---
+++ pkg/commons-node/memoizeUntilChanged.js
@@ -0,0 +1,59 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+import {arrayEqual} from './collection';
+import Hasher from './Hasher';
+
+type CompareFunc = (a: Array<any>, b: Array<any>) => boolean;
+type KeySelector<T, U> = (x: T) => U;
+
+const NOTHING = Symbol();
+
+/**
+ * Create a memoized version of the provided function that caches only the latest result. This is
+ * especially useful for optimizing React component methods without having to worry about
+ * maintaining state explicitly. For example:
+ *
+ *     class MyComponent extends React.Component {
+ *       constructor(props) {
+ *         super(props);
+ *         this._computeSomethingExpensive = memoizeUntilChanged(this._computeSomethingExpensive);
+ *       }
+ *       _computeSomethingExpensive(x) { ... }
+ *       render() {
+ *         const thingToRender = this._computeSomethingExpensive(this.props.value);
+ *         return <div>{thingToRender}</div>;
+ *       }
+ *     }
+ */
+export default function memoizeUntilChanged<T: Function>(
+  func: T,
+  keySelector_?: KeySelector<T, any>,
+  compareKeys: CompareFunc = arrayEqual,
+): T {
+  let prevArgKeys;
+  let prevResult = NOTHING;
+  const keySelector: KeySelector<T, any> = keySelector_ || createKeySelector();
+  // $FlowIssue: Flow can't express that we want the args to be the same type as the input func's.
+  return function(...args) {
+    const argKeys = args.map(keySelector);
+    if (prevResult === NOTHING || !compareKeys(argKeys, prevArgKeys)) {
+      prevArgKeys = argKeys;
+      prevResult = func.apply(this, args);
+    }
+    return prevResult;
+  };
+}
+
+function createKeySelector<T>(): KeySelector<T, any> {
+  const hasher: Hasher<any> = new Hasher();
+  return x => hasher.getHash(x);
+}
