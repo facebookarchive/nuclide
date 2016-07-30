2c084a1f986869d57f167f7e93b344675ac1967b
Matthew Dapena-Tretter <matthewwithanm@fb.com>
2016-07-29 19:22:58 -0700

    Create utility for converting React element into something that can be used with view registry
    Summary:
    Atom's ViewRegistry doesn't understand React elements. D3637244 provides a way to interop, but it requires a separate model for storing state. This utility allows you to keep your state in a React component which is often more convenient. It does this by mounting the React element and adding an `element` property to it which references the container node. (The `element` property is a special one to Atom's ViewRegistry). Because we're returning the mounted component, all instance methods on your component class are available, allowing you to implement Atom protocols like `getTitle()` on your component.
    
    Like the tools in D3637244 and D3637269, this uses the ReactMountRootElement custom HTML element for the container.
    
    See D3637425 for an example usage.
    
    Reviewed By: ssorallen
    
    Differential Revision: D3637409
    
    fbshipit-source-id: fd88400382c903e2cccc913538507180194bbfb0

---
+++ pkg/commons-atom/viewableFromReactElement.js
@@ -0,0 +1,60 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+import {React, ReactDOM} from 'react-for-atom';
+import ReactMountRootElement from '../nuclide-ui/lib/ReactMountRootElement';
+
+/**
+ * Create an object that can be used as an Atom model from a React element. Example:
+ *
+ *     class UsageStats extends React.Component {
+ *
+ *       // Methods that Atom looks for on models (like `getTitle()`, `getIconName()`, etc.) can be
+ *       // added to the component.
+ *       getTitle(): string {
+ *         return 'Usage Stats';
+ *       }
+ *
+ *       render(): React.Element {
+ *         return <div>Stats</div>;
+ *       }
+ *
+ *     }
+ *
+ *    const item = viewableFromReactElement(<UsageStats />);
+ *    atom.workspace.getPanes()[0].addItem(item); // Or anywhere else Atom uses model "items."
+ */
+export function viewableFromReactElement(reactElement: React.Element<any>): Object {
+  const container = new ReactMountRootElement();
+  const item = ReactDOM.render(reactElement, container);
+
+  // Add the a reference to the container to the item. This will allow Atom's view registry to
+  // associate the item with the HTML element.
+  if (item.element != null) {
+    throw new Error(
+      "Component cannot have an `element` property. That's added by viewableFromReactElement",
+    );
+  }
+  (item: any).element = container;
+
+  // Add a destroy method to the item that will unmount the component. There's no need for users to
+  // implement this themselves because they have `componentWillUnmount()`.
+  if (item.destroy != null) {
+    throw new Error(
+      "Component cannot implement `destroy()`. That's added by `viewableFromReactElement`",
+    );
+  }
+  (item: any).destroy = () => {
+    ReactDOM.unmountComponentAtNode(container);
+  };
+
+  return item;
+}
