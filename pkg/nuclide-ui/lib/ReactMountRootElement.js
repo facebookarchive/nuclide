a465cb3941eb1865a1690d1a25c96130c525a242
Matthew Dapena-Tretter <matthewwithanm@fb.com>
2016-07-29 19:22:53 -0700

    Add custom view provider for React integration
    Summary: This adds a view provider that provides an easy integration point for React components.
    
    Reviewed By: zertosh
    
    Differential Revision: D3637244
    
    fbshipit-source-id: 1dbe48158c26e2f9c27d06573cad71a3458823ed

---
+++ pkg/nuclide-ui/lib/ReactMountRootElement.js
@@ -0,0 +1,40 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+import {React, ReactDOM} from 'react-for-atom';
+
+/**
+ * A custom HTMLElement we render React elements into.
+ */
+class ReactMountRootElement extends HTMLElement {
+  _reactElement: ?React.Element<any>;
+
+  setReactElement(reactElement: React.Element<any>): void {
+    this._reactElement = reactElement;
+  }
+
+  // $FlowIssue -- readonly props: t10620219
+  attachedCallback(): mixed {
+    if (this._reactElement == null) { return; }
+    ReactDOM.render(this._reactElement, this);
+  }
+
+  // $FlowIssue -- readonly props: t10620219
+  detachedCallback(): mixed {
+    if (this._reactElement == null) { return; }
+    ReactDOM.unmountComponentAtNode(this);
+  }
+
+}
+
+export default document.registerElement('nuclide-react-mount-root', {
+  prototype: ReactMountRootElement.prototype,
+});
