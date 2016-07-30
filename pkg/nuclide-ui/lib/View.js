6290bc90ec32655fa7b2887af5836c99a0e96bd9
Matthew Dapena-Tretter <matthewwithanm@fb.com>
2016-07-29 19:22:57 -0700

    Create component for rendering items using the view registry
    Summary:
    This component provides an integration point between our React components and Atom's ViewRegistry. It uses the custom HTMLElement added in D3637244 (with the idea that that element would be the only custom element we need).
    
    Usage: `<View item={someObject} />`
    
    Reviewed By: ssorallen
    
    Differential Revision: D3637269
    
    fbshipit-source-id: b23822eab9347a8e6de1bffabb04c9cb91516b20

---
+++ pkg/nuclide-ui/lib/View.js
@@ -0,0 +1,57 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+import {React, ReactDOM} from 'react-for-atom';
+
+type Props = {
+  item: Object,
+};
+
+/**
+ * A React component used for rendering an item associated with a view via Atom's view registry.
+ * Because we're going through Atom's ViewRegistry (which returns DOM nodes), we need to render an
+ * empty element and manually attach the view (DOM element) we get from Atom.
+ */
+export class View extends React.Component {
+  props: Props;
+  _renderedItem: ?Object;
+
+  shouldComponentUpdate(nextProps: Props): boolean {
+    return this.props.item !== nextProps.item;
+  }
+
+  componentDidMount(): void {
+    this._update(this.props.item);
+  }
+
+  componentDidUpdate(): void {
+    this._update(this.props.item);
+  }
+
+  _update(item: ?Object): void {
+    if (item === this._renderedItem) { return; }
+
+    // Remove the current children.
+    const container = ReactDOM.findDOMNode(this);
+    while (container.lastChild != null) {
+      container.removeChild(container.lastChild);
+    }
+
+    this._renderedItem = item;
+    if (item == null) { return; }
+    const el = atom.views.getView(item);
+    container.appendChild(el);
+  }
+
+  render(): React.Element<any> {
+    return <nuclide-react-mount-root />;
+  }
+}
