579a6f911fc3d2c580b584212f456795a35e847f
Mitch Rees-Jones <reesjones@fb.com>
2016-08-01 11:01:50 -0700

    Prevent context providers from re-rendering when receiving same definition prop
    Summary: Context providers should not re-render when they get a new definition prop that represents the same symbol. This adds and uses a check that allows providers to not update or make queries if the new definition is semantically the same.
    
    Reviewed By: peterhal
    
    Differential Revision: D3621733
    
    fbshipit-source-id: 4e726f1e209f0a688288ad1529199710bac8d8e4

---
+++ pkg/nuclide-definition-service-common/lib/utils.js
@@ -0,0 +1,45 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+import type {Definition} from '../../nuclide-definition-service';
+
+/**
+ * Returns true if the 2 definitions are considered equal. They are considered equal if:
+ *    - they are both null
+ *    - they are both undefined
+ *    - they both refer to the same symbol, i.e. they are non-null/undefined and have the same:
+ *      - name
+ *      - path
+ *      - position (row & column)
+ *      - language
+ *      - project root
+ */
+export function definitionsAreEqual(x: ?Definition, y: ?Definition): boolean {
+  if (x == null || y == null) {
+    return x === y;
+  }
+  if (x.name !== y.name) {
+    return false;
+  }
+  if (x.path !== y.path) {
+    return false;
+  }
+  if (x.position.row !== y.position.row || x.position.column !== y.position.column) {
+    return false;
+  }
+  if (x.language !== y.language) {
+    return false;
+  }
+  if (x.projectRoot !== y.projectRoot) {
+    return false;
+  }
+  return true;
+}
