b0a2dea0bd910f0973ae887dcecdce94d2714314
Mitch Rees-Jones <reesjones@fb.com>
2016-08-03 18:23:36 -0700

    Add login button to context providers
    Summary: Adds a login button for context providers to display when user isn't logged in.
    
    Reviewed By: hansonw
    
    Differential Revision: D3665444
    
    fbshipit-source-id: aa8d8a6da71af2ec70ea94dbc2286684086df51e

--- pkg/nuclide-context-view/lib/ContextViewMessage.js
+++ pkg/nuclide-context-view/lib/ContextViewMessage.js
@@ -14,16 +14,30 @@
  */
 
 import {React} from 'react-for-atom';
+import {Button} from '../../nuclide-ui/lib/Button';
 
 export default class ContextViewMessage extends React.Component {
   static NO_DEFINITION = 'No definition selected.';
   static LOADING = 'Loading...';
+  static NOT_LOGGED_IN = (
+    <div>
+      <div>You need to log in to see this data!</div>
+      <Button className="pull-right" onClick={showLoginModal}>Log in</Button>
+    </div>
+  );
 
   props: {
-    message: string,
+    message: string | React.Element<any>,
   };
 
   render(): React.Element<any> {
     return <div>{this.props.message}</div>;
   }
+}
+
+function showLoginModal(): void {
+  atom.commands.dispatch(
+    atom.views.getView(atom.workspace),
+    'fb-login:login-logout',
+  );
 }
