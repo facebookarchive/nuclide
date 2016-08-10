726a9531911989535a3697765c752a3459ea40ad
Nat Mote <nmote@fb.com>
2016-08-10 10:45:32 -0700

    Add which utility
    Reviewed By: zertosh
    
    Differential Revision: D3696835
    
    fbshipit-source-id: 804299d059cb02ab0b1315c0a80f2a917c99279b

---
+++ pkg/commons-node/which.js
@@ -0,0 +1,28 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+import {checkOutput} from './process';
+
+/**
+ * Provides a cross-platform way to check whether a binary is available.
+ *
+ * We ran into problems with the npm `which` package (the nature of which I unfortunately don't
+ * remember) so we can use this for now.
+ */
+export default async function which(command: string): Promise<?string> {
+  const whichCommand = process.platform === 'win32' ? 'where' : 'which';
+  try {
+    const result = await checkOutput(whichCommand, [command]);
+    return result.stdout.trim();
+  } catch (e) {
+    return null;
+  }
+}
