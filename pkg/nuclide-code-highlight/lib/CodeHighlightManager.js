e357d9aba9c578d93b82e57b4641f960a106d5d2
Peter Hallam <peterhal@fb.com>
2016-08-19 14:45:25 -0700

    Fix editing within codehighlight
    Summary:
    If an edit occurred within a code highlight, the highlight would not be removed.
    - Fix the state machine to always destroy highlights on edits.
    - Also destroy highlights immediately after moving outside the current highlight rather than waiting for the results of the next highlight() call.
    
    Changelog:
    
    Fix issue with lingering code highlights when editing.
    
    Reviewed By: zertosh
    
    Differential Revision: D3743495
    
    fbshipit-source-id: f502275316772896cd4f9e4b157fe4d458189205

--- pkg/nuclide-code-highlight/lib/CodeHighlightManager.js
+++ pkg/nuclide-code-highlight/lib/CodeHighlightManager.js
@@ -25,38 +25,38 @@ export default class CodeHighlightManager {
     this._markers = [];
     const subscriptions = this._subscriptions = new CompositeDisposable();
     const debouncedCallback = debounce(
-      this._highlightInEditor.bind(this),
+      this._onCursorMove.bind(this),
       HIGHLIGHT_DELAY_MS,
       false,
     );
-    atom.workspace.observeTextEditors(editor => {
+    subscriptions.add(atom.workspace.observeTextEditors(editor => {
       subscriptions.add(editor.onDidChangeCursorPosition(event => {
         debouncedCallback(editor, event.newBufferPosition);
       }));
-    });
+      subscriptions.add(editor.onDidChange(event => {
+        this._destroyMarkers();
+        debouncedCallback(editor, editor.getCursorBufferPosition());
+      }));
+    }));
   }
 
-  async _highlightInEditor(editor: atom$TextEditor, position: atom$Point): Promise<void> {
+  async _onCursorMove(editor: atom$TextEditor, position: atom$Point): Promise<void> {
     if (this._isPositionInHighlightedRanges(position)) {
       return;
     }
 
-    const {scopeName} = editor.getGrammar();
-    const [provider] = this._getMatchingProvidersForScopeName(scopeName);
-    if (!provider) {
-      return;
-    }
-
-    // Call out to hack to get highlighting ranges.  This can take some time
-    const highlightedRanges = await provider.highlight(editor, position);
-
-    // If the cursor has moved the highlighted ranges we just computed are useless, so abort
-    if (this._hasCursorMoved(editor, position)) {
-      return;
-    }
-
-    // Destroy old highlighted sections and highlight new ones
+    // The cursor is outside the old markers, so they are now stale
     this._destroyMarkers();
+
+    const originalChangeCount = editor.getBuffer().changeCount;
+    const highlightedRanges = await this._getHighlightedRanges(editor, position);
+
+    // If the cursor has moved, or the file was edited
+    // the highlighted ranges we just computed are useless, so abort
+    if (this._hasEditorChanged(editor, position, originalChangeCount)) {
+      return;
+    }
+
     this._markers = highlightedRanges.map(
       range => editor.markBufferRange(range, {}),
     );
@@ -68,8 +68,26 @@ export default class CodeHighlightManager {
     });
   }
 
-  _hasCursorMoved(editor: atom$TextEditor, position: atom$Point): boolean {
-    return !editor.getCursorBufferPosition().isEqual(position);
+  async _getHighlightedRanges(
+    editor: atom$TextEditor,
+    position: atom$Point,
+  ): Promise<Array<atom$Range>> {
+    const {scopeName} = editor.getGrammar();
+    const [provider] = this._getMatchingProvidersForScopeName(scopeName);
+    if (!provider) {
+      return [];
+    }
+
+    return await provider.highlight(editor, position);
+  }
+
+  _hasEditorChanged(
+    editor: atom$TextEditor,
+    position: atom$Point,
+    originalChangeCount: number,
+  ): boolean {
+    return !editor.getCursorBufferPosition().isEqual(position)
+      || editor.getBuffer().changeCount !== originalChangeCount;
   }
 
   _isPositionInHighlightedRanges(position: atom$Point): boolean {
