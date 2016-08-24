78059fc42908d0e3db10061f86bf7e92ed3938e6
Walter Luh <wluh@fb.com>
2016-08-24 11:19:27 -0700

    Nuclide: Suppress specific lint error instead of all
    Summary: Specifically, nuclide-internal/command-menu-items
    
    Reviewed By: zertosh
    
    Differential Revision: D3765111
    
    fbshipit-source-id: feb2b03d17d6367a8c5fffa7e895b34499642c31

--- pkg/nuclide-home/lib/main.js
+++ pkg/nuclide-home/lib/main.js
@@ -33,7 +33,7 @@ export function activate(state: ?Object): void {
   considerDisplayingHome();
   subscriptions = new CompositeDisposable();
   subscriptions.add(
-    // eslint-disable-next-line
+    // eslint-disable-next-line nuclide-internal/command-menu-items
     atom.commands.add('atom-workspace', 'nuclide-docs:open', e => {
       const url = createUtmUrl('http://nuclide.io/docs', 'help');
       shell.openExternal(url);
