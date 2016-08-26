9b887ae4f49ededbf1ba30720441b28a23b26951
Hanson Wang <hansonw@fb.com>
2016-08-26 11:16:14 -0700

    Buck: helper to generate diagnostics from Buck output
    Summary:
    Unfortunately Buck does not provide any sort of detailed compilation error data via the JSON websocket.
    Until it does, we can parse the log output and look for patterns that match errors ourselves.
    This is tailored to Clang/g++ output at the moment, but could be expanded to other backends as well.
    
    Reviewed By: matthewwithanm
    
    Differential Revision: D3768831
    
    fbshipit-source-id: 62e30e51e2bcb05ad8209922a24f0697917d2b0d

---
+++ pkg/nuclide-buck/lib/getDiagnostics.js
@@ -0,0 +1,67 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+import type {FileDiagnosticMessage} from '../../nuclide-diagnostics-common';
+
+import {Range} from 'atom';
+import nuclideUri from '../../commons-node/nuclideUri';
+import {getFileSystemServiceByNuclideUri} from '../../nuclide-remote-connection';
+
+const DIAGNOSTIC_REGEX = /^([^\s:]+):([0-9]+):[0-9]+: (.*)$/gm;
+
+/**
+ * Consumes Buck console output and emits a set of file-level diagnostic messages.
+ * Ideally Buck should do this for us, but let's parse the messages manually for now.
+ * This only (officially) handles Clang/g++ output.
+ */
+export default async function getDiagnostics(
+  message: string,
+  level: string,
+  root: string,
+): Promise<Array<FileDiagnosticMessage>> {
+  // Only fetch the file system service if we need it.
+  let fileSystemService;
+  // Global regexps need to be reset before use.
+  DIAGNOSTIC_REGEX.lastIndex = 0;
+  // Collect promises and check all matches at once.
+  const promises = [];
+  let match;
+  while ((match = DIAGNOSTIC_REGEX.exec(message))) {
+    const [, file, strLine, text] = match;
+    if (fileSystemService == null) {
+      fileSystemService = getFileSystemServiceByNuclideUri(root);
+    }
+    if (fileSystemService != null) {
+      const filePath = nuclideUri.join(root, file);
+      const localPath = nuclideUri.getPath(filePath);
+      promises.push(fileSystemService.exists(localPath).then(
+        exists => {
+          if (!exists) {
+            return null;
+          }
+          const line = parseInt(strLine, 10);
+          return !exists ? null : {
+            scope: 'file',
+            providerName: 'Buck',
+            type: level === 'error' ? 'Error' : 'Warning',
+            filePath,
+            text,
+            range: new Range([line - 1, 0], [line, 0]),
+          };
+        },
+        // Silently ignore files resulting in an error.
+        () => null,
+      ));
+    }
+  }
+  const diagnostics = await Promise.all(promises);
+  return diagnostics.filter(Boolean);
+}
