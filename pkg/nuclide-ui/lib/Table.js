074ceb7b3ba3898d84234652bf290646a56b4120
Jonas Gebhardt <jxg@fb.com>
2016-08-26 13:49:33 -0700

    Make table column width configurable and resizable
    Summary: Size info needs to be stored as % values to keep the table responsive.
    
    Reviewed By: matthewwithanm
    
    Differential Revision: D3735157
    
    fbshipit-source-id: fb4dd022a4153bce85fb8941b653b14cbc488f90

--- pkg/nuclide-ui/lib/Table.js
+++ pkg/nuclide-ui/lib/Table.js
@@ -12,18 +12,24 @@
 import classnames from 'classnames';
 import {
   React,
+  ReactDOM,
 } from 'react-for-atom';
+import {Disposable} from 'atom';
 
 // ColumnKey must be unique within the containing collection.
 type ColumnKey = string;
 type Column = {
   title: string,
   key: ColumnKey,
+  // Percentage. The `width`s of all columns must add up to 1.
+  width?: number,
 };
 type Row = {
   [key: ColumnKey]: ?mixed;
 };
-
+type WidthMap = {
+  [key: ColumnKey]: number,
+};
 type Props = {
   columns: Array<Column>,
   rows: Array<Row>,
@@ -31,11 +37,160 @@ type Props = {
    * Whether to shade even and odd items differently. Default behavior is `true`.
    */
   alternateBackground?: number,
+  /**
+   * Whether column widths can be resized interactively via drag&drop. Default behavior is `true`.
+   */
+  resizeable?: boolean,
   children?: React.Element<any>,
+};
+type State = {
+  columnWidthRatios: WidthMap,
 };
 
 export class Table extends React.Component {
   props: Props;
+  state: State;
+  _globalEventsDisposable: ?Disposable;
+  _resizeStartX: ?number;
+  _tableWidth: ?number;
+  _columnBeingResized: ?ColumnKey;
+
+  constructor(props: Props) {
+    super(props);
+    this._globalEventsDisposable = null;
+    this._resizeStartX = null;
+    this._tableWidth = null;
+    this._columnBeingResized = null;
+    (this: any)._handleResizerGlobalMouseUp = this._handleResizerGlobalMouseUp.bind(this);
+    (this: any)._handleResizerGlobalMouseMove = this._handleResizerGlobalMouseMove.bind(this);
+    this.state = {
+      columnWidthRatios: this._getInitialWidthsForColumns(this.props.columns),
+    };
+  }
+
+  _getInitialWidthsForColumns(columns: Array<Column>): WidthMap {
+    const columnWidthRatios = {};
+    let assignedWidth = 0;
+    const unresolvedColumns = [];
+    columns.forEach(column => {
+      const {
+        key,
+        width,
+      } = column;
+      if (width != null) {
+        columnWidthRatios[key] = width;
+        assignedWidth += width;
+      } else {
+        unresolvedColumns.push(column);
+      }
+    });
+    const residualColumnWidth = (1 - assignedWidth) / unresolvedColumns.length;
+    unresolvedColumns.forEach(column => {
+      columnWidthRatios[column.key] = residualColumnWidth;
+    });
+    return columnWidthRatios;
+  }
+
+  /* Applies sizing constraints, and returns whether the column width actually changed. */
+  _updateWidths(resizedColumn: ColumnKey, newColumnSize: number): boolean {
+    const {columnWidthRatios} = this.state;
+    const {columns} = this.props;
+    const originalColumnSize = columnWidthRatios[resizedColumn];
+    const columnAfterResizedColumn = columns[
+      columns.findIndex(column => column.key === resizedColumn) + 1
+    ].key;
+    const followingColumnSize = columnWidthRatios[columnAfterResizedColumn];
+    const constrainedNewColumnSize =
+      Math.max(0, Math.min(newColumnSize, followingColumnSize + originalColumnSize));
+    if (Math.abs(newColumnSize - constrainedNewColumnSize) > Number.EPSILON) {
+      return false;
+    }
+    const updatedColumnWidths = {};
+    columns.forEach(column => {
+      const {key} = column;
+      let width;
+      if (column.key === resizedColumn) {
+        width = constrainedNewColumnSize;
+      } else if (column.key === columnAfterResizedColumn) {
+        width =
+          columnWidthRatios[resizedColumn] - constrainedNewColumnSize + columnWidthRatios[key];
+      } else {
+        width = columnWidthRatios[key];
+      }
+      updatedColumnWidths[key] = width;
+    });
+    this.setState({
+      columnWidthRatios: updatedColumnWidths,
+    });
+    return true;
+  }
+
+  _handleResizerMouseDown(key: ColumnKey, event: SyntheticMouseEvent): void {
+    if (this._globalEventsDisposable != null) {
+      this._unsubscribeFromGlobalEvents();
+    }
+    // Prevent browser from initiating drag events on accidentally selected elements.
+    const selection = document.getSelection();
+    if (selection != null) {
+      selection.removeAllRanges();
+    }
+    // $FlowIssue https://github.com/facebook/flow/issues/770
+    document.addEventListener('mousemove', this._handleResizerGlobalMouseMove);
+    // $FlowIssue https://github.com/facebook/flow/issues/770
+    document.addEventListener('mouseup', this._handleResizerGlobalMouseUp);
+    this._resizeStartX = event.pageX;
+    this._tableWidth = ReactDOM.findDOMNode(this.refs.table).getBoundingClientRect().width;
+    this._columnBeingResized = key;
+    this._globalEventsDisposable = new Disposable(() => {
+      // $FlowIssue https://github.com/facebook/flow/issues/770
+      document.removeEventListener('mousemove', this._handleResizerGlobalMouseMove);
+      // $FlowIssue https://github.com/facebook/flow/issues/770
+      document.removeEventListener('mouseup', this._handleResizerGlobalMouseUp);
+      this._resizeStartX = null;
+      this._tableWidth = null;
+      this._columnBeingResized = null;
+    });
+  }
+
+  _unsubscribeFromGlobalEvents(): void {
+    if (this._globalEventsDisposable == null) {
+      return;
+    }
+    this._globalEventsDisposable.dispose();
+    this._globalEventsDisposable = null;
+  }
+
+  _handleResizerGlobalMouseUp(event: MouseEvent): void {
+    this._unsubscribeFromGlobalEvents();
+  }
+
+  _handleResizerGlobalMouseMove(event: MouseEvent): void {
+    if (
+      this._resizeStartX == null ||
+      this._tableWidth == null ||
+      this._columnBeingResized == null
+    ) {
+      return;
+    }
+    const {pageX} = ((event: any): MouseEvent);
+    const deltaX = pageX - this._resizeStartX;
+    const currentColumnSize = this.state.columnWidthRatios[this._columnBeingResized];
+    const didUpdate = this._updateWidths(
+      this._columnBeingResized,
+      (this._tableWidth * currentColumnSize + deltaX) / this._tableWidth,
+    );
+    if (didUpdate) {
+      this._resizeStartX = pageX;
+    }
+  }
+
+  _dispose(): void {
+    this._unsubscribeFromGlobalEvents();
+  }
+
+  componentWillUnmount(): void {
+    this._dispose();
+  }
 
   _renderEmptyCellContent(): React.Element<any> {
     return <div />;
@@ -52,12 +207,27 @@ export class Table extends React.Component {
         title,
         key,
       } = column;
+      const resizeHandle = i === columns.length - 1
+        ? null
+        : <div
+            className="nuclide-ui-table-header-resize-handle"
+            onMouseDown={this._handleResizerMouseDown.bind(this, key)}
+          />;
+      const width = this.state.columnWidthRatios[key];
+      const optionalHeaderCellProps = {};
+      if (width != null) {
+        optionalHeaderCellProps.style = {
+          width: width + '%',
+        };
+      }
       return (
         <th
           className="nuclide-ui-table-header-cell"
           title={title}
-          key={key}>
+          key={key}
+          {...optionalHeaderCellProps}>
           {title}
+          {resizeHandle}
         </th>
       );
     });
