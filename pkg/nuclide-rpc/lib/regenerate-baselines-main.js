32595f0681e14f537c5f72ce52bb5fa0820138da
Peter Hallam <peterhal@fb.com>
2016-09-08 14:59:30 -0700

    Move regenerate-baselines to flow typed file
    Summary: These 4 diffs move the bulk of regenerate-baselines and generate-proxy to be flow typed.
    
    Reviewed By: zertosh
    
    Differential Revision: D3834997
    
    fbshipit-source-id: 84798cab531bdc0596f0636d0be7b9bd83bb2cae

---
+++ pkg/nuclide-rpc/lib/regenerate-baselines-main.js
@@ -0,0 +1,75 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+// Regenerates the .proxy baseline files in the spec/fixtures directory.
+
+const parseServiceDefinition = require('./service-parser').parseServiceDefinition;
+const generateProxy = require('./proxy-generator').generateProxy;
+const stripLocationsFileName = require('./location').stripLocationsFileName;
+
+const fs = require('fs');
+const path = require('path');
+
+const dir = path.join(__dirname, '../spec/fixtures');
+for (const file of fs.readdirSync(dir)) {
+  if (file.endsWith('.def')) {
+    const serviceName = path.basename(file, '.def');
+    const preserveFunctionNames = false;
+    const definitionPath = path.join(dir, file);
+
+    const definitionSource = fs.readFileSync(definitionPath, 'utf8');
+    const definitions = parseServiceDefinition(
+      definitionPath,
+      definitionSource,
+    );
+
+    stripLocationsFileName(definitions);
+
+    const json = mapDefinitions(definitions);
+    fs.writeFileSync(
+      definitionPath.replace('.def', '.def.json'),
+      JSON.stringify(json, null, 4),
+      'utf8',
+    );
+
+    const code = generateProxy(serviceName, preserveFunctionNames, definitions);
+    fs.writeFileSync(definitionPath.replace('.def', '.proxy'), code, 'utf8');
+  }
+}
+
+function mapDefinitions(map) {
+  const obj = {};
+  for (const it of map.values()) {
+    let value;
+    switch (it.kind) {
+      case 'interface':
+        value = {
+          constructorArgs: it.constructorArgs,
+          instanceMethods: mapToJSON(it.instanceMethods),
+          staticMethods: mapToJSON(it.staticMethods),
+        };
+        break;
+      default:
+        value = it;
+        break;
+    }
+    obj[it.name] = value;
+  }
+  return obj;
+}
+
+function mapToJSON(map) {
+  const result = {};
+  for (const it of map.entries()) {
+    result[it[0]] = it[1];
+  }
+  return result;
+}
