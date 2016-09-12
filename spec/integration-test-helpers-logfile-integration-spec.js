dd639d5337cc63abcf43220b99debed931b67352
Max Sherman <jonaldislarry@fb.com>
2016-09-12 12:58:20 -0700

    Testing util for grepping logfiles
    Summary:
    The php debugger tests rely on looking at the logfile to know when events have happened, so this
    utility helps us do that.
    
    Reviewed By: peterhal
    
    Differential Revision: D3376438
    
    fbshipit-source-id: a452d9c1255819ba4eae2cfc611e4b011b7529bf

---
+++ spec/integration-test-helpers-logfile-integration-spec.js
@@ -0,0 +1,42 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+import {
+  activateAllPackages,
+  deactivateAllPackages,
+  jasmineIntegrationTestSetup,
+} from './utils/integration-test-helpers';
+import {getNumberOfMatches} from './utils/logfile';
+import {copyFixture} from '../pkg/nuclide-test-helpers';
+import nuclideUri from '../pkg/commons-node/nuclideUri';
+
+const NUM_LOGS = 5;
+const LOG_STRING = 'max is cool';
+
+describe('test the logfile grepper', () => {
+  it('greps logfiles correctly', () => {
+    waitsForPromise(async () => {
+      jasmineIntegrationTestSetup();
+      await activateAllPackages();
+      const dirPath = await copyFixture('logfile_1', __dirname);
+      spyOnLogFileGetter(nuclideUri.join(dirPath, 'log1.txt'));
+      expect(await getNumberOfMatches(LOG_STRING)).toBe(0);
+      jasmine.unspy(require('../pkg/nuclide-logging'), 'getPathToLogFileForToday');
+      spyOnLogFileGetter(nuclideUri.join(dirPath, 'log2.txt'));
+      expect(await getNumberOfMatches(LOG_STRING)).toBe(NUM_LOGS);
+      deactivateAllPackages();
+    });
+  });
+});
+
+function spyOnLogFileGetter(filePath: string): void {
+  spyOn(require('../pkg/nuclide-logging'), 'getPathToLogFileForToday').andReturn(filePath);
+}
---
+++ spec/utils/logfile.js
@@ -0,0 +1,29 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+import {getPathToLogFileForToday} from '../../pkg/nuclide-logging';
+import {asyncExecute} from '../../pkg/commons-node/process';
+
+/**
+ * Greps a logfile for occurences of a search string.
+ *
+ * @returns A promise that resolves to the number of lines that match the search token.
+ */
+export async function getNumberOfMatches(searchToken: string): Promise<number> {
+  const logFilePath = getPathToLogFileForToday();
+  const args = ['--count', searchToken, logFilePath];
+  try {
+    const result = (await asyncExecute('grep', args)).stdout.trim();
+    return Number(result);
+  } catch (e) {
+    return 0;
+  }
+}
