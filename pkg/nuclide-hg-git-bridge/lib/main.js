4e68d15662ba66a8139ceee80d5f139b52f1e7c8
Mostafa Eweda <most@fb.com>
2016-09-15 14:46:13 -0700

    Add utils to get hg repositories and repository stream
    Summary:
    The code is mostly copied from `nuclide-bookshelf/lib/main.js`
    Extracting it in a utility here because it needs to be used by other consumers.
    
    Reviewed By: peterhal
    
    Differential Revision: D3857620
    
    fbshipit-source-id: 042edb2f18b44240ea2a203bbb3e2d5f1b745b31

--- pkg/nuclide-hg-git-bridge/lib/main.js
+++ pkg/nuclide-hg-git-bridge/lib/main.js
@@ -9,5 +9,30 @@
  * the root directory of this source tree.
  */
 
+import type {HgRepositoryClient} from '../../nuclide-hg-repository-client';
+
+import {arrayCompact} from '../../commons-node/collection';
+import {diffSets} from '../../commons-node/observable';
+import {observableFromSubscribeFunction} from '../../commons-node/event';
+import {Observable} from 'rxjs';
+
 export {default as repositoryContainsPath} from './repositoryContainsPath';
 export {default as repositoryForPath} from './repositoryForPath';
+
+export function getHgRepositories(): Set<HgRepositoryClient> {
+  return new Set(
+    arrayCompact(atom.project.getRepositories())
+      .filter(repository => repository.getType() === 'hg'),
+  );
+}
+
+export function getHgRepositoryStream(): Observable<HgRepositoryClient> {
+  const currentRepositories =
+    observableFromSubscribeFunction(atom.project.onDidChangePaths.bind(atom.project))
+    .startWith(null)
+    .map(() => getHgRepositories());
+
+  return diffSets(currentRepositories).flatMap(
+    repoDiff => Observable.from(repoDiff.added),
+  );
+}
