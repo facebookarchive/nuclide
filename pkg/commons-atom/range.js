2ea4fc368f99414baefe8da9cb374ec2fc9ecaed
Peter Hallam <peterhal@fb.com>
2016-09-19 18:11:37 -0700

    Extract wordAtPositionFromBuffer
    Summary: Need this on the server side for hack.
    
    Reviewed By: mostafaeweda
    
    Differential Revision: D3873940
    
    fbshipit-source-id: 800a7255193930b68f197e09a244b33429e6c262

--- pkg/commons-atom/range.js
+++ pkg/commons-atom/range.js
@@ -10,10 +10,11 @@
  */
 
 import {Range} from 'atom';
+import {wordAtPositionFromBuffer} from '../commons-node/range';
 
 export function wordAtPosition(
   editor: atom$TextEditor,
-  position: atom$Point,
+  position: atom$PointObject,
   wordRegex_: ?RegExp,
 ): ?{wordMatch: Array<string>, range: atom$Range} {
   let wordRegex = wordRegex_;
@@ -21,28 +22,7 @@ export function wordAtPosition(
     wordRegex = editor.getLastCursor().wordRegExp();
   }
   const buffer = editor.getBuffer();
-  const {row, column} = position;
-  const rowRange = buffer.rangeForRow(row);
-  let matchData;
-  // Extract the expression from the row text.
-  buffer.scanInRange(wordRegex, rowRange, data => {
-    const {range} = data;
-    if (range.containsPoint(position)) {
-      matchData = data;
-    }
-    // Stop the scan if the scanner has passed our position.
-    if (range.end.column > column) {
-      data.stop();
-    }
-  });
-  if (matchData) {
-    return {
-      wordMatch: matchData.match,
-      range: matchData.range,
-    };
-  } else {
-    return null;
-  }
+  return wordAtPositionFromBuffer(buffer, position, wordRegex);
 }
 
 /**
