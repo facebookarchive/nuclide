09215356b03b9872200f53265cf9f4360360a186
Aaditya Sriram <asriram@fb.com>
2016-09-20 20:52:16 -0700

    Add data processing for the file changes
    Summary: The need to process the data stems from the fact that we need to show the root folders for the files that have changed.
    
    Reviewed By: mostafaeweda
    
    Differential Revision: D3880582
    
    fbshipit-source-id: 5d90243084e15524820e5c340d6501e40a61cbf7

---
+++ pkg/nuclide-hg-git-bridge/lib/utils.js
@@ -0,0 +1,54 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+import type {NuclideUri} from '../../commons-node/nuclideUri';
+import type {FileChangeStatusValue} from './constants';
+
+import {arrayCompact} from '../../commons-node/collection';
+import nuclideUri from '../../commons-node/nuclideUri';
+import {repositoryForPath} from '../../nuclide-hg-git-bridge';
+
+export function getMultiRootFileChanges(
+  fileChanges: Map<NuclideUri, FileChangeStatusValue>,
+  rootPaths?: Array<NuclideUri>,
+): Map<NuclideUri, Map<NuclideUri, FileChangeStatusValue>> {
+  let roots;
+  if (rootPaths == null) {
+    roots = arrayCompact(
+      atom.project.getDirectories().map(directory => {
+        const rootPath = directory.getPath();
+        const repository = repositoryForPath(rootPath);
+        if ((repository == null || repository.getType() !== 'hg')) {
+          return null;
+        }
+        return rootPath;
+      }),
+    );
+  } else {
+    roots = rootPaths;
+  }
+
+  const sortedFilePaths = Array.from(fileChanges.entries())
+    .sort(([filePath1], [filePath2]) =>
+      nuclideUri.basename(filePath1).toLowerCase().localeCompare(
+        nuclideUri.basename(filePath2).toLowerCase(),
+      ),
+    );
+
+  const changedRoots = new Map(roots.map(root => {
+    const rootChanges = new Map(
+      sortedFilePaths.filter(([filePath]) => nuclideUri.contains(root, filePath)),
+    );
+    return [root, rootChanges];
+  }));
+
+  return changedRoots;
+}
