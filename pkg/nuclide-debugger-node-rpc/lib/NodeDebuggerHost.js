f4c811f2e24bba493c2dbbf282be62d309e722b3
Max Sherman <jonaldislarry@fb.com>
2016-09-23 16:13:52 -0700

    Nuclide JS remote debugging (6/N): Add NodeDebuggerHost for nuclide-debugger-node-rpc
    Summary: Most code are copied from nuclide-debugger-node.
    
    Reviewed By: nmote
    
    Differential Revision: D3759523
    
    fbshipit-source-id: f64397ce49258ff317b914f62df095e5337bb581

---
+++ pkg/nuclide-debugger-node-rpc/lib/NodeDebuggerHost.js
@@ -0,0 +1,67 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+import Rx from 'rxjs';
+import {CompositeDisposable} from 'event-kit';
+import {
+  WebSocketServer,
+} from '../../nuclide-debugger-common/lib/WebSocketServer';
+import {Session} from './Session';
+import UniversalDisposable from '../../commons-node/UniversalDisposable';
+import utils from './utils';
+const {log} = utils;
+
+/**
+ * Responsible for bootstrap and host node inspector backend.
+ */
+export class NodeDebuggerHost {
+  _subscriptions: CompositeDisposable;
+  _nodeSocketServer: WebSocketServer;
+  _close$: Rx.Subject<mixed>;
+
+  constructor() {
+    this._subscriptions = new CompositeDisposable();
+    this._nodeSocketServer = new WebSocketServer();
+    this._subscriptions.add(this._nodeSocketServer);
+    this._close$ = new Rx.Subject();
+    this._close$.first().subscribe(() => { this.dispose(); });
+  }
+
+  start(): string {
+    // This is the port that the V8 debugger usually listens on.
+    // TODO(natthu): Provide a way to override this in the UI.
+    const debugPort = 5858;
+    const wsPort = this._generateRandomInteger(2000, 65535);
+    this._nodeSocketServer.start(wsPort).then(websocket => {
+      log(`Websocket server created for port: ${wsPort}`);
+      // TODO: do we need to add webSocket into CompositeDisposable?
+      const config = {
+        debugPort,
+        preload: false, // This makes the node inspector not load all the source files on startup.
+      };
+      const session = new Session(config, debugPort, websocket);
+      Rx.Observable.fromEvent(session, 'close').subscribe(this._close$);
+    });
+    return `ws://127.0.0.1:${wsPort}/`;
+  }
+
+  _generateRandomInteger(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+
+  onSessionEnd(callback: () => mixed): IDisposable {
+    return new UniversalDisposable(this._close$.first().subscribe(callback));
+  }
+
+  dispose(): void {
+    this._subscriptions.dispose();
+  }
+}
