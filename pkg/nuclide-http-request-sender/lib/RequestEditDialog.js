8fe89dcaed0ac96fd4f4b83e965372d79e062ecc
Max Sherman <jonaldislarry@fb.com>
2016-09-23 12:28:00 -0700

    request sender #15 -- build out UI for http request edit dialog modal
    Summary: Adds actual UI elements for customizing what values should be sent in the http request
    
    Reviewed By: jgebhardt
    
    Differential Revision: D3890908
    
    fbshipit-source-id: 1069a396828ed5d18dfd15cc7e7870a6c856e1c6

--- pkg/nuclide-http-request-sender/lib/RequestEditDialog.js
+++ pkg/nuclide-http-request-sender/lib/RequestEditDialog.js
@@ -12,24 +12,154 @@
 import type {BoundActionCreators} from './types';
 
 import {React} from 'react-for-atom';
+import {AtomInput} from '../../nuclide-ui/lib/AtomInput';
+import {Button, ButtonTypes} from '../../nuclide-ui/lib/Button';
+import {ButtonGroup} from '../../nuclide-ui/lib/ButtonGroup';
+import {Dropdown} from '../../nuclide-ui/lib/Dropdown';
+import {AtomTextEditor} from '../../nuclide-ui/lib/AtomTextEditor';
+import invariant from 'assert';
+import shallowequal from 'shallowequal';
+
+type Headers = {[key: string]: string};
 
 type PropsType = {
   actionCreators: BoundActionCreators,
   uri: string,
   method: string,
-  headers: {[key: string]: string},
+  headers: Headers,
   body: ?string,
 };
 
+const METHOD_DROPDOWN_OPTIONS = [
+  {label: 'GET', value: 'GET'},
+  {label: 'POST', value: 'POST'},
+];
 
 export class RequestEditDialog extends React.Component<void, PropsType, void> {
   props: PropsType;
+  _editorComponent: ?AtomTextEditor;
 
   constructor(props: PropsType) {
     super(props);
+    this._editorComponent = null;
+    (this: any)._onCancel = this._onCancel.bind(this);
+    (this: any)._onSendHttpRequest = this._onSendHttpRequest.bind(this);
+  }
+
+  shouldComponentUpdate(nextProps: PropsType): boolean {
+    const {uri, method, headers, body} = this.props;
+    return nextProps.uri !== uri
+      || nextProps.method !== method
+      || nextProps.body !== body
+      || !shallowequal(nextProps.headers, headers);
+  }
+
+  componentDidMount(): void {
+    this._componentDidRender();
+  }
+
+  componentDidUpdate(): void {
+    this._componentDidRender();
+  }
+
+  /**
+   * This method should be called after every render to set the AtomTextEditor text.
+   */
+  _componentDidRender(): void {
+    const editorComponent = this._editorComponent;
+    invariant(editorComponent != null);
+    const editor = editorComponent.getModel();
+    invariant(editor != null);
+    const jsonGrammar = atom.grammars.grammarForScopeName('source.json');
+    invariant(jsonGrammar != null);
+    editor.setGrammar(jsonGrammar);
+    editor.setText(JSON.stringify(this.props.headers, null, 2));
+  }
+
+  _onSendHttpRequest(): void {
+    this.props.actionCreators.sendHttpRequest();
+    this._toggleDialog();
+  }
+
+  _onCancel(): void {
+    this._toggleDialog();
+  }
+
+  _toggleDialog(): void {
+    atom.commands.dispatch(
+      atom.views.getView(atom.workspace),
+      'nuclide-http-request-sender:toggle-http-request-edit-dialog',
+    );
+  }
+
+  _handleTextBufferChange(event: atom$TextEditEvent): void {
+    // TODO: It's better to store changes, even if they are illegal JSON.
+    let headers;
+    try {
+      headers = JSON.parse(event.newText);
+    } catch (_) {
+      return; // Do not store illegal JSON.
+    }
+    this.props.actionCreators.updateState({headers});
   }
 
   render(): React.Element<any> {
-    return <span>Place holder</span>;
+    return (
+      <div className="block">
+        <div className="nuclide-edit-request-dialog">
+          <label>URI: </label>
+          <AtomInput
+            tabIndex="1"
+            placeholderText="https://www.facebook.com"
+            value={this.props.uri}
+            onDidChange={uri => this.props.actionCreators.updateState({uri})}
+          />
+          <label>Method: </label>
+          <Dropdown
+            value={this.props.method}
+            options={METHOD_DROPDOWN_OPTIONS}
+            onChange={method => this.props.actionCreators.updateState({method})}
+          />
+         {
+           this.props.method !== 'POST'
+             ? null
+             : (
+               <div>
+                 <label>Body</label>
+                 <AtomInput
+                   tabIndex="2"
+                   onDidChange={body => this.props.actionCreators.updateState({body})}
+                 />
+               </div>
+             )
+         }
+          <label>Headers: </label>
+          <div className="nuclide-http-request-sender-headers">
+            <AtomTextEditor
+              ref={editorComponent => {
+                this._editorComponent = editorComponent;
+              }}
+              tabIndex="3"
+              autoGrow={false}
+              softWrapped={true}
+              onDidTextBufferChange={this._handleTextBufferChange.bind(this)}
+            />
+          </div>
+          <ButtonGroup className="nuclide-http-request-sender-button-group">
+            <Button
+              buttonType={ButtonTypes.PRIMARY}
+              tabIndex="5"
+              onClick={this._onSendHttpRequest}>
+              Send HTTP Request
+            </Button>
+            <Button
+              tabIndex="4"
+              onClick={this._onCancel}>
+              Cancel
+            </Button>
+          </ButtonGroup>
+        </div>
+      </div>
+    );
   }
 }
