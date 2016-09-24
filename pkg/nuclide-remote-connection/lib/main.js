820e6067368deb4ececf512a991987a33948a3dd
Andres Suarez <asuarez@fb.com>
2016-09-23 19:41:48 -0700

    Sort services config and expose all services via remote-connection
    Summary: These services are always available, so lets just expose them from here with their types. Getting the service via `getServiceByNuclideUri` and asserting that it exists is by the most common use case.
    
    Reviewed By: hansonw
    
    Differential Revision: D3912242
    
    fbshipit-source-id: aa8681569b0f6d04bcef5b0119c84e52187c4935

--- pkg/nuclide-remote-connection/lib/main.js
+++ pkg/nuclide-remote-connection/lib/main.js
@@ -9,10 +9,8 @@
  * the root directory of this source tree.
  */
 
-import type {NuclideUri} from '../../commons-node/nuclideUri';
-import typeof * as ArcanistService from '../../nuclide-arcanist-rpc';
-import typeof * as FileSystemService from '../../nuclide-server/lib/services/FileSystemService';
 import type {Directory as LocalDirectoryType} from 'atom';
+import type {NuclideUri} from '../../commons-node/nuclideUri';
 
 import nullthrows from 'nullthrows';
 
@@ -52,10 +50,104 @@ export {
   getlocalService,
 };
 
+import typeof * as ArcanistService from '../../nuclide-arcanist-rpc';
+import typeof * as BuckService from '../../nuclide-buck-rpc';
+import typeof * as ClangService from '../../nuclide-clang-rpc';
+import typeof * as CtagsService from '../../nuclide-ctags-rpc';
+import typeof * as FileSystemService from '../../nuclide-server/lib/services/FileSystemService';
+import typeof * as FileWatcherService from '../../nuclide-filewatcher-rpc';
+import typeof * as FlowService from '../../nuclide-flow-rpc';
+import typeof * as FuzzyFileSearchService from '../../nuclide-fuzzy-file-search-rpc';
+import typeof * as GrepService from '../../nuclide-grep-rpc';
+import typeof * as HackService from '../../nuclide-hack-rpc';
+import typeof * as HgService from '../../nuclide-hg-rpc/lib/HgService';
+import typeof * as InfoService from '../../nuclide-server/lib/services/InfoService';
+import typeof * as MerlinService from '../../nuclide-ocaml-rpc/lib/MerlinService';
+import typeof * as NativeDebuggerService from '../../nuclide-debugger-native-rpc';
+import typeof * as NodeDebuggerService from '../../nuclide-debugger-node-rpc';
+import typeof * as OpenFilesService from '../../nuclide-open-files-rpc/lib/OpenFilesService';
+import typeof * as PhpDebuggerService from '../../nuclide-debugger-php-rpc';
+import typeof * as PythonService from '../../nuclide-python-rpc';
+import typeof * as RemoteCommandService from '../../nuclide-remote-atom-rpc';
+import typeof * as SourceControlService
+  from '../../nuclide-server/lib/services/SourceControlService';
+
+export function getArcanistServiceByNuclideUri(uri: NuclideUri): ArcanistService {
+  return nullthrows(getServiceByNuclideUri('ArcanistService', uri));
+}
+
+export function getBuckServiceByNuclideUri(uri: NuclideUri): BuckService {
+  return nullthrows(getServiceByNuclideUri('BuckService', uri));
+}
+
+export function getClangServiceByNuclideUri(uri: NuclideUri): ClangService {
+  return nullthrows(getServiceByNuclideUri('ClangService', uri));
+}
+
+export function getCtagsServiceByNuclideUri(uri: NuclideUri): CtagsService {
+  return nullthrows(getServiceByNuclideUri('CtagsService', uri));
+}
+
 export function getFileSystemServiceByNuclideUri(uri: NuclideUri): FileSystemService {
   return nullthrows(getServiceByNuclideUri('FileSystemService', uri));
 }
 
-export function getArcanistServiceByNuclideUri(uri: NuclideUri): ArcanistService {
-  return nullthrows(getServiceByNuclideUri('ArcanistService', uri));
+export function getFileWatcherServiceByNuclideUri(uri: NuclideUri): FileWatcherService {
+  return nullthrows(getServiceByNuclideUri('FileWatcherService', uri));
+}
+
+export function getFlowServiceByNuclideUri(uri: NuclideUri): FlowService {
+  return nullthrows(getServiceByNuclideUri('FlowService', uri));
+}
+
+export function getFuzzyFileSearchServiceByNuclideUri(uri: NuclideUri): FuzzyFileSearchService {
+  return nullthrows(getServiceByNuclideUri('FuzzyFileSearchService', uri));
+}
+
+export function getGrepServiceByNuclideUri(uri: NuclideUri): GrepService {
+  return nullthrows(getServiceByNuclideUri('GrepService', uri));
+}
+
+export function getHackServiceByNuclideUri(uri: NuclideUri): HackService {
+  return nullthrows(getServiceByNuclideUri('HackService', uri));
+}
+
+export function getHgServiceByNuclideUri(uri: NuclideUri): HgService {
+  return nullthrows(getServiceByNuclideUri('HgService', uri));
+}
+
+export function getInfoServiceByNuclideUri(uri: NuclideUri): InfoService {
+  return nullthrows(getServiceByNuclideUri('InfoService', uri));
+}
+
+export function getMerlinServiceByNuclideUri(uri: NuclideUri): MerlinService {
+  return nullthrows(getServiceByNuclideUri('MerlinService', uri));
+}
+
+export function getNativeDebuggerServiceByNuclideUri(uri: NuclideUri): NativeDebuggerService {
+  return nullthrows(getServiceByNuclideUri('NativeDebuggerService', uri));
+}
+
+export function getNodeDebuggerServiceByNuclideUri(uri: NuclideUri): NodeDebuggerService {
+  return nullthrows(getServiceByNuclideUri('NodeDebuggerService', uri));
+}
+
+export function getOpenFilesServiceByNuclideUri(uri: NuclideUri): OpenFilesService {
+  return nullthrows(getServiceByNuclideUri('OpenFilesService', uri));
+}
+
+export function getPhpDebuggerServiceByNuclideUri(uri: NuclideUri): PhpDebuggerService {
+  return nullthrows(getServiceByNuclideUri('PhpDebuggerService', uri));
+}
+
+export function getPythonServiceByNuclideUri(uri: NuclideUri): PythonService {
+  return nullthrows(getServiceByNuclideUri('PythonService', uri));
+}
+
+export function getRemoteCommandServiceByNuclideUri(uri: NuclideUri): RemoteCommandService {
+  return nullthrows(getServiceByNuclideUri('RemoteCommandService', uri));
+}
+
+export function getSourceControlServiceByNuclideUri(uri: NuclideUri): SourceControlService {
+  return nullthrows(getServiceByNuclideUri('SourceControlService', uri));
 }
