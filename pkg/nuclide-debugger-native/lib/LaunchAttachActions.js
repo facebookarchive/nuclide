e8b6518862a2e462118d5e7433f27a8504dcdca8
Andres Suarez <asuarez@fb.com>
2016-09-26 15:22:55 -0700

    Switch nuclide-debugger-native to typed Dispatcher
    Reviewed By: johnislarry
    
    Differential Revision: D3897778
    
    fbshipit-source-id: d9a615781129e3205d7bfb11e688fd1cc8672e2b

--- pkg/nuclide-debugger-native/lib/LaunchAttachActions.js
+++ pkg/nuclide-debugger-native/lib/LaunchAttachActions.js
@@ -9,7 +9,7 @@
  * the root directory of this source tree.
  */
 
-import type {Dispatcher} from 'flux';
+import type LaunchAttachDispatcher from './LaunchAttachDispatcher';
 import type {
   AttachTargetInfo,
   LaunchTargetInfo,
@@ -21,22 +21,22 @@ import typeof * as NativeDebuggerService
 
 import invariant from 'assert';
 import {CompositeDisposable} from 'atom';
-import {LaunchAttachActionCode} from './Constants';
 import {AttachProcessInfo} from './AttachProcessInfo';
 import {LaunchProcessInfo} from './LaunchProcessInfo';
 import {getServiceByNuclideUri} from '../../nuclide-remote-connection';
 import consumeFirstProvider from '../../commons-atom/consumeFirstProvider';
+import {ActionTypes} from './LaunchAttachDispatcher';
 
 const ATTACH_TARGET_LIST_REFRESH_INTERVAL = 2000;
 
 export class LaunchAttachActions {
-  _dispatcher: Dispatcher;
+  _dispatcher: LaunchAttachDispatcher;
   _targetUri: NuclideUri;
   _refreshTimerId: ?number;
   _dialogVisible: boolean;
   _subscriptions: atom$CompositeDisposable;
 
-  constructor(dispatcher: Dispatcher, targetUri: NuclideUri) {
+  constructor(dispatcher: LaunchAttachDispatcher, targetUri: NuclideUri) {
     this._dispatcher = dispatcher;
     this._targetUri = targetUri;
     this._refreshTimerId = null;
@@ -102,13 +102,9 @@ export class LaunchAttachActions {
       = getServiceByNuclideUri('NativeDebuggerService', this._targetUri);
     invariant(rpcService);
     const attachTargetList = await rpcService.getAttachTargetInfoList();
-    this._emitNewAction(LaunchAttachActionCode.UPDATE_ATTACH_TARGET_LIST, attachTargetList);
-  }
-
-  _emitNewAction(actionType: string, data: Array<AttachTargetInfo>): void {
     this._dispatcher.dispatch({
-      actionType,
-      data,
+      actionType: ActionTypes.UPDATE_ATTACH_TARGET_LIST,
+      attachTargetInfos: attachTargetList,
     });
   }
 
