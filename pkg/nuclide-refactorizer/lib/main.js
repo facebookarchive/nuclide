8043615c1db7d36de6e4a136e5aa13b2fe0e1360
Nat Mote <nmote@fb.com>
2016-09-28 14:39:00 -0700

    Add draft refactoring API
    Reviewed By: hansonw
    
    Differential Revision: D3934635
    
    fbshipit-source-id: 65d6fb9979604dd676c3a0d57f370a8bd4b40ed0

--- pkg/nuclide-refactorizer/lib/main.js
+++ pkg/nuclide-refactorizer/lib/main.js
@@ -10,7 +10,58 @@
  */
 
 import {CompositeDisposable} from 'atom';
+/*
+ * WARNING: This package is still experimental and in early development. Use it at your own risk.
+ */
+
+import type {TextEdit} from '../../nuclide-textedit/lib/rpc-types';
+
+import type {NuclideUri} from '../../commons-node/nuclideUri';
+
 import createPackage from '../../commons-atom/createPackage';
+
+export type RenameRefactorKind = 'rename';
+
+// Will be a union type when we add more
+export type RefactorKind = RenameRefactorKind;
+
+export type RenameRefactoring = {
+  kind: RenameRefactorKind,
+  symbolAtPoint: {
+    text: string,
+    range: atom$Range,
+  },
+};
+
+// Will be a union type when we add more
+export type AvailableRefactoring = RenameRefactoring;
+
+export type RenameRequest = {
+  kind: RenameRefactorKind,
+  editor: atom$TextEditor,
+  symbolAtPoint: {
+    text: string,
+    range: atom$Range,
+  },
+  newName: string,
+};
+
+export type RefactorRequest = RenameRequest;
+
+export type RefactorResponse = {
+  edits: Map<NuclideUri, Array<TextEdit>>,
+};
+
+export type RefactorProvider = {
+  priority: number,
+  grammarScopes: Array<string>,
+
+  refactoringsAtPoint(
+    editor: atom$TextEditor,
+    point: atom$Point,
+  ): Promise<Array<AvailableRefactoring>>,
+  refactor(request: RefactorRequest): Promise<?RefactorResponse>,
+};
 
 class Activation {
   _disposables: CompositeDisposable;
