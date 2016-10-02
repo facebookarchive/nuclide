754e2e510008a089745a48107cf1b3b0ade2217c
Hanson Wang <hansonw@fb.com>
2016-10-02 14:12:05 -0700

    Remove HHVM toolbar (1/n)
    Summary:
    This stack moves the HHVM toolbar into a task runner.
    This first removes all integrations into the HhvmToolbar to make things easier.
    
    Reviewed By: matthewwithanm
    
    Differential Revision: D3955386
    
    fbshipit-source-id: 9575f09078488106ef1700b9604b6322337b7b23

--- pkg/nuclide-hhvm-toolbar/lib/main.js
+++ pkg/nuclide-hhvm-toolbar/lib/main.js
@@ -9,49 +9,23 @@
  * the root directory of this source tree.
  */
 
-import type {DistractionFreeModeProvider} from '../../nuclide-distraction-free-mode';
-import type {GetToolBar} from '../../commons-atom/suda-tool-bar';
 import type {TaskRunnerServiceApi} from '../../nuclide-task-runner/lib/types';
 import type {CwdApi} from '../../nuclide-current-working-directory/lib/CwdApi';
 import type {OutputService} from '../../nuclide-console/lib/types';
 
-import PanelRenderer from '../../commons-atom/PanelRenderer';
 import UniversalDisposable from '../../commons-node/UniversalDisposable';
 import {Disposable} from 'atom';
-import {React, ReactDOM} from 'react-for-atom';
 import invariant from 'assert';
-import HhvmIcon from './ui/HhvmIcon';
 import HhvmBuildSystem from './HhvmBuildSystem';
-import NuclideToolbar from './NuclideToolbar';
-import ProjectStore from './ProjectStore';
 
 class Activation {
 
   _disposables: UniversalDisposable;
-  _projectStore: ProjectStore;
-  _state: Object;
   _buildSystem: ?HhvmBuildSystem;
   _cwdApi: ?CwdApi;
-  _panelRenderer: PanelRenderer;
 
   constructor(state: ?Object) {
-    this._state = {
-      panelVisible: state != null && state.panelVisible != null ? state.panelVisible : true,
-    };
     this._disposables = new UniversalDisposable();
-    this._projectStore = new ProjectStore();
-    this._addCommands();
-    this._disposables.add(
-      this._panelRenderer = new PanelRenderer({
-        location: 'top',
-        createItem: this._createPanelItem.bind(this),
-        // Increase priority (default is 100) to ensure this toolbar comes after the 'tool-bar'
-        // package's toolbar. Hierarchically the controlling toolbar should be above, and
-        // practically this ensures the popover in this build toolbar stacks on top of other UI.
-        priority: 200,
-      }),
-    );
-    this._renderToolbar();
   }
 
   setCwdApi(cwdApi: ?CwdApi) {
@@ -59,44 +33,6 @@ class Activation {
     if (this._buildSystem != null) {
       this._buildSystem.setCwdApi(cwdApi);
     }
-  }
-
-  _addCommands(): void {
-    this._disposables.add(
-      atom.commands.add(
-        'body',
-        'nuclide-hhvm-toolbar:toggle',
-        () => { this.togglePanel(); },
-      ),
-    );
-  }
-
-  _renderToolbar(): void {
-    this._panelRenderer.render({visible: this._state.panelVisible});
-  }
-
-  consumeToolBar(getToolBar: GetToolBar): IDisposable {
-    const toolBar = getToolBar('nuclide-hhvm-toolbar');
-    const {element} = toolBar.addButton({
-      callback: 'nuclide-hhvm-toolbar:toggle',
-      tooltip: 'Toggle HHVM Toolbar',
-      priority: 500,
-    });
-    toolBar.addSpacer({
-      priority: 501,
-    });
-    ReactDOM.render(
-      <div className="hhvm-toolbar-icon-container">
-        <HhvmIcon width="37%" />
-      </div>,
-      element,
-    );
-    const disposable = new Disposable(() => {
-      ReactDOM.unmountComponentAtNode(element);
-      toolBar.removeItems();
-    });
-    this._disposables.add(disposable);
-    return disposable;
   }
 
   consumeBuildSystemRegistry(registry: TaskRunnerServiceApi): void {
@@ -118,55 +54,16 @@ class Activation {
       if (this._cwdApi != null) {
         buildSystem.setCwdApi(this._cwdApi);
       }
+      this._disposables.add(buildSystem);
       this._buildSystem = buildSystem;
     }
     return this._buildSystem;
   }
 
-
-  getDistractionFreeModeProvider(): DistractionFreeModeProvider {
-    return {
-      name: 'nuclide-hhvm-toolbar',
-      isVisible: () => this._state.panelVisible,
-      toggle: () => this.togglePanel(),
-    };
-  }
-
-  _createPanelItem() {
-    const disposables = new UniversalDisposable();
-    let element;
-    return {
-      getElement: () => {
-        if (element == null) {
-          element = document.createElement('div');
-          ReactDOM.render(<NuclideToolbar projectStore={this._projectStore} />, element);
-          disposables.add(() => {
-            ReactDOM.unmountComponentAtNode(element);
-          });
-        }
-        return element;
-      },
-      destroy() {
-        disposables.dispose();
-      },
-    };
-  }
-
-  serialize(): Object {
-    return {
-      panelVisible: this._state.panelVisible,
-    };
-  }
-
   dispose() {
-    this._projectStore.dispose();
     this._disposables.dispose();
   }
 
-  togglePanel(): void {
-    this._state.panelVisible = !this._state.panelVisible;
-    this._renderToolbar();
-  }
 }
 
 let activation: ?Activation = null;
@@ -192,16 +89,6 @@ export function consumeCwdApi(api: CwdApi): IDisposable {
   });
 }
 
-export function consumeToolBar(getToolBar: GetToolBar): IDisposable {
-  invariant(activation);
-  return activation.consumeToolBar(getToolBar);
-}
-
-export function getDistractionFreeModeProvider(): DistractionFreeModeProvider {
-  invariant(activation != null);
-  return activation.getDistractionFreeModeProvider();
-}
-
 export function consumeOutputService(api: OutputService): void {
   invariant(activation != null);
   activation.consumeOutputService(api);
@@ -211,13 +98,5 @@ export function deactivate() {
   if (activation != null) {
     activation.dispose();
     activation = null;
-  }
-}
-
-export function serialize(): Object {
-  if (activation != null) {
-    return activation.serialize();
-  } else {
-    return {};
   }
 }
