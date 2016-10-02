e465930bc1c5f3dcca31ef85c83fdebbcbb6ade1
Hanson Wang <hansonw@fb.com>
2016-10-02 14:12:08 -0700

    Modernize Arcanist package (3/n)
    Summary:
    Convert to the `createPackage` pattern.
    This is in preparation to fold the Arcanist build toolbar in.
    
    Reviewed By: matthewwithanm
    
    Differential Revision: D3955437
    
    fbshipit-source-id: 103aeda3f7f2de2bb3f052260b8f663f56d60b01

--- pkg/nuclide-arcanist/lib/main.js
+++ pkg/nuclide-arcanist/lib/main.js
@@ -13,48 +13,35 @@ import type {BusySignalProvider} from '../../nuclide-busy-signal/lib/types';
 import type {BusySignalProviderBase} from '../../nuclide-busy-signal';
 
 import {CompositeDisposable} from 'atom';
-import invariant from 'assert';
+import createPackage from '../../commons-atom/createPackage';
 import registerGrammar from '../../commons-atom/register-grammar';
 // eslint-disable-next-line nuclide-internal/no-cross-atom-imports
 import {DedupedBusySignalProviderBase} from '../../nuclide-busy-signal';
 import {ArcanistDiagnosticsProvider} from './ArcanistDiagnosticsProvider';
 
-let subscriptions: ?CompositeDisposable = null;
+class Activation {
+  _disposables: CompositeDisposable;
+  _busySignalProvider: BusySignalProviderBase;
 
-let busySignalProvider: ?BusySignalProviderBase = null;
-
-function getBusySignalProvider(): BusySignalProviderBase {
-  if (busySignalProvider == null) {
-    busySignalProvider = new DedupedBusySignalProviderBase();
-  }
-  return busySignalProvider;
-}
-
-export function activate(): void {
-  if (subscriptions) {
-    return;
+  constructor(state: ?Object) {
+    this._disposables = new CompositeDisposable();
+    this._busySignalProvider = new DedupedBusySignalProviderBase();
+    registerGrammar('source.json', '.arcconfig');
   }
 
-  subscriptions = new CompositeDisposable();
-
-  registerGrammar('source.json', '.arcconfig');
-}
-
-export function deactivate(): void {
-  if (subscriptions) {
-    subscriptions.dispose();
-    subscriptions = null;
+  dispose(): void {
+    this._disposables.dispose();
   }
-  busySignalProvider = null;
+
+  provideBusySignal(): BusySignalProvider {
+    return this._busySignalProvider;
+  }
+
+  provideDiagnostics() {
+    const provider = new ArcanistDiagnosticsProvider(this._busySignalProvider);
+    this._disposables.add(provider);
+    return provider;
+  }
 }
 
-export function provideBusySignal(): BusySignalProvider {
-  return getBusySignalProvider();
-}
-
-export function provideDiagnostics() {
-  const provider = new ArcanistDiagnosticsProvider(getBusySignalProvider());
-  invariant(subscriptions != null);
-  subscriptions.add(provider);
-  return provider;
-}
+export default createPackage(Activation);
