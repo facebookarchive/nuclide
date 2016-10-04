67f753bbb9d83f930252feb58b59c1be20e37c72
Andres Suarez <asuarez@fb.com>
2016-10-03 17:41:23 -0700

    Move babel-core to devDeps
    Summary: Now that format-js lives outside of Nuclide, and that Nuclide ships with pre-transpiled code, there's no need to install `babel-core` in production.
    
    Reviewed By: hansonw
    
    Differential Revision: D3956184
    
    fbshipit-source-id: 448c9923edb893a4c9ac2148585ac2dcf06b738e

---
+++ pkg/nuclide-node-transpiler/lib/require-hook.js
@@ -0,0 +1,49 @@
+'use strict';
+/* @noflow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+/* NON-TRANSPILED FILE */
+/* eslint-disable babel/func-params-comma-dangle, prefer-object-spread/prefer-object-spread */
+
+/**
+ * This file installs the logic that modifies Node's built in require()
+ * function to transpile .js files that start with either `'use babel'` or
+ * `"use babel"`.
+ */
+
+const assert = require('assert');
+const fs = require('fs');
+
+const NodeTranspiler = require('./NodeTranspiler');
+const nodeTranspiler = new NodeTranspiler();
+
+// Make sure we can add the require() hook.
+const jsExtension = Object.getOwnPropertyDescriptor(require.extensions, '.js');
+
+// In Atom, this is false - prevent accidental unnecessary use.
+assert(jsExtension.writable);
+
+Object.defineProperty(require.extensions, '.js', {
+  enumerable: true,
+  writable: false,
+  value: function transpiler_require_hook(_module, filename) {
+    // Keep src as a buffer so calculating its digest with crypto is fast.
+    const src = fs.readFileSync(filename);
+
+    let output;
+    if (NodeTranspiler.shouldCompile(src)) {
+      output = nodeTranspiler.transformWithCache(src, filename);
+    } else {
+      output = src.toString();
+    }
+
+    return _module._compile(output, filename);
+  },
+});
