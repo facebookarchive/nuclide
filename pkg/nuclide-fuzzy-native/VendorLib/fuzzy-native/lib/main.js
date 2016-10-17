5c89963b89d5ba4df7c1d77297f0ba214cf8a75c
Hanson Wang <hansonw@fb.com>
2016-10-17 13:11:33 -0700

    Bump fuzzy-native to 0.6.2
    Summary:
    - Add prebuilt binaries for Electron 1.3.6
    - Remove unused ia32 / Node 4 prebuilts
    - Statically link Linux binaries for CentOS compat
    
    Reviewed By: zertosh
    
    Differential Revision: D4028599
    
    fbshipit-source-id: 86670421db8da7201a00b8eb7b074df537ed5d74

--- pkg/nuclide-fuzzy-native/VendorLib/fuzzy-native/lib/main.js
+++ pkg/nuclide-fuzzy-native/VendorLib/fuzzy-native/lib/main.js
@@ -1,7 +1,16 @@
 'use strict';
 
-var binary = require('pre-binding');
 var path = require('path');
-var binding_path = binary.find(path.resolve(path.join(__dirname, '../package.json')));
+var pack = require('../package.json');
+var binding_path = path.join(
+  __dirname,
+  '../build/fuzzy-native/',
+  'v' + pack.version,
+  // ABI v49 is only for Electron. https://github.com/electron/electron/issues/5851
+  process.versions.modules === '49'
+    ? ['electron', 'v1.3', process.platform, process.arch].join('-')
+    : ['node', 'v' + process.versions.modules, process.platform, process.arch].join('-'),
+  'fuzzy-native.node'
+);
 
 module.exports = require(binding_path);
--- pkg/nuclide-fuzzy-native/scripts/sync.sh
+++ pkg/nuclide-fuzzy-native/scripts/sync.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-npm install fuzzy-native
+yarn add fuzzy-native --prod --no-lockfile
 rm -rf node_modules/fuzzy-native/{binding.gyp,src,node_modules}
 rsync -r --delete node_modules/fuzzy-native VendorLib/
 rm -rf node_modules
+hg revert package.json
