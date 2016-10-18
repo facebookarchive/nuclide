c445451e40eb57c90d2db8d93a1e459778518f4b
Andres Suarez <asuarez@fb.com>
2016-10-18 11:04:41 -0700

    Simplify use-minified-libs transform test
    Summary: This change is so that the upcoming Babel 6 upgrade diff is less noisy.
    
    Reviewed By: hansonw
    
    Differential Revision: D4028647
    
    fbshipit-source-id: f2e67d1198ae48252040323efc8d54877c9ee44c

--- pkg/nuclide-node-transpiler/spec/use-minified-libs-spec.js
+++ pkg/nuclide-node-transpiler/spec/use-minified-libs-spec.js
@@ -19,34 +19,86 @@ console.log(__filename);
 
 const assert = require('assert');
 const babel = require('babel-core');
-const fs = require('fs');
-const path = require('path');
 
 const transformer = require('../lib/use-minified-libs-tr');
 
-[
-  'export-all-minified',
-  'export-named-minified',
-  'ignore-other',
-  'import-minified',
-  'import-minified',
-  'no-type',
-  'only-entry',
-  'require-minified',
-].forEach(name => {
-  console.log('use-minified-libs should handle %s', name);
-  const testPath = path.join(__dirname, 'fixtures/use-minified-libs', name + '.test');
-  const errorPath = path.join(__dirname, 'fixtures/use-minified-libs', name + '.error');
-  const expectedPath = path.join(__dirname, 'fixtures/use-minified-libs', name + '.expected');
-  const source = fs.readFileSync(testPath, 'utf8');
-  if (fs.existsSync(errorPath)) {
-    const error = fs.readFileSync(errorPath, 'utf8').trim();
-    assert.throws(() => { assertTransformation(source, ''); }, RegExp(error));
-  } else {
-    const expected = fs.readFileSync(expectedPath, 'utf8');
-    assertTransformation(source, expected);
+// export-all-minified
+assertTransformation(`
+  export * from 'rxjs';
+`, `
+  export * from 'rxjs/bundles/Rx.min.js';
+`);
+
+// export-named-minified
+assertTransformation(`
+  export {Observable} from 'rxjs';
+`, `
+  export { Observable } from 'rxjs/bundles/Rx.min.js';
+`);
+
+// ignore-other
+assertTransformation(`
+  import path from 'path';
+`, `
+  import path from 'path';
+`);
+
+// import-minified
+assertTransformation(`
+  import Rx from 'rxjs';
+  import {Observable} from 'rxjs';
+  assert(Rx.Observable === Observable);
+`, `
+  import Rx from 'rxjs/bundles/Rx.min.js';
+  import { Observable } from 'rxjs/bundles/Rx.min.js';
+  assert(Rx.Observable === Observable);
+`);
+
+// no-type
+assertTransformation(`
+  import type {using} from 'rxjs/observable/using';
+`, `
+`);
+
+// require-minified
+assertTransformation(`
+  const Rx = require('rxjs');
+  const {Observable} = require('rxjs');
+  assert(Rx.Observable === Observable);
+`, `
+  var Rx = require('rxjs/bundles/Rx.min.js');
+  var _require = require('rxjs/bundles/Rx.min.js');
+  var Observable = _require.Observable;
+  assert(Rx.Observable === Observable);
+`);
+
+// with-inline-imports
+assertTransformation(`
+  import Rx from 'rxjs';
+  assert(Rx.Observable === Observable);
+`, `
+  function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+  var _rxjs;
+
+  function _load_rxjs() {
+    return _rxjs = _interopRequireDefault(require('rxjs/bundles/Rx.min.js'));
   }
-});
+
+  assert((_rxjs || _load_rxjs())['default'].Observable === Observable);
+`, [
+  require.resolve('../lib/inline-imports-tr'),
+]);
+
+// only-entry-module
+assert.throws(() => {
+  assertTransformation(`
+    import {using} from 'rxjs/observable/using';
+  `, `
+    import {using} from 'rxjs/observable/using';
+  `);
+}, /SyntaxError: unknown: Line 2: Only importing "rxjs" is supported/);
+
 
 function stripMeta(node) {
   delete node.start;
@@ -62,10 +114,12 @@ function stripMeta(node) {
   return node;
 }
 
-function assertTransformation(source, expected) {
+function assertTransformation(source, expected, plugins) {
   const output = babel.transform(source, {
-    plugins: [transformer],
-    blacklist: ['strict'],
+    plugins: (plugins || []).concat([
+      transformer,
+    ]),
+    blacklist: ['strict', 'es6.modules'],
   }).code;
   try {
     assert.deepEqual(
