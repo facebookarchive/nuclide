15a5682f6c66b010e5d77966688e6322e1ef9731
Hanson Wang <hansonw@fb.com>
2016-10-26 13:24:00 -0700

    Add SharedObservableCache to commons-node
    Summary:
    For file watchers, I need some kind of abstraction where:
    - I subscribe to file updates by filename
    - Only one file watcher should be created for a filename
    - File watchers should be erased when there are no more subscribers.
    
    This applies both remotely and locally; locally we want to re-use the remote watcher instances and remotely we want to re-use the underlying Watchman clients.
    
    `SharedObservableMap` returns observables by key where
    - the first subscription creates and caches the implementing observable, and:
    - the last unsubscription destroys and clears the implementing observable.
    
    Let me know if you think there's a better way of implementing this. The following 2 diffs illustrate the use cases.
    
    Reviewed By: matthewwithanm
    
    Differential Revision: D4071418
    
    fbshipit-source-id: c5db60b847c783927a06efc6883b1e0d60a684a1

---
+++ pkg/commons-node/SharedObservableCache.js
@@ -0,0 +1,57 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+import {Observable} from 'rxjs';
+
+/**
+ * This class creates and caches observables by key.
+ *
+ * The factory is only called once the wrapper observable gets subscribed to,
+ * and multiple subscribers to the same key share the same observable.
+ *
+ * Once all subscribers to a key have unsubscribed, the cached observable is cleared.
+ */
+export default class SharedObservableCache<Tk, To> {
+  _factory: (key: Tk) => Observable<To>;
+  _cache: Map<Tk, {
+    refCount: number,
+    observable: Observable<To>,
+  }>;
+
+  constructor(factory: (key: Tk) => Observable<To>) {
+    this._factory = factory;
+    this._cache = new Map();
+  }
+
+  get(key: Tk): Observable<To> {
+    return Observable.create(observer => {
+      let cached = this._cache.get(key);
+      if (cached == null) {
+        cached = {
+          refCount: 1,
+          observable: this._factory(key),
+        };
+      } else {
+        cached.refCount++;
+      }
+      // Store this in a const to convince Flow this is non-nullable.
+      const data = cached;
+      this._cache.set(key, cached);
+      const subscription = data.observable.subscribe(observer);
+      return () => {
+        if (--data.refCount === 0) {
+          this._cache.delete(key);
+        }
+        subscription.unsubscribe();
+      };
+    });
+  }
+}
