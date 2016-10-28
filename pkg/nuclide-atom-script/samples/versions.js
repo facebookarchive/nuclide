df9218e452acbfe0ba97f1d8c6220a1a186536f2
Andres Suarez <asuarez@fb.com>
2016-10-28 13:51:18 -0700

    Add "versions" atom-script sample
    Summary: An atom-script sample that prints the Atom version and `process.versions` in pretty colors. Used this to verify that I was testing the correct Atom beta with various atom-script features.
    
    Reviewed By: jgebhardt
    
    Differential Revision: D4095712
    
    fbshipit-source-id: a9bf691631a3b1345ffdb61d6df97f3c33dc3828

---
+++ pkg/nuclide-atom-script/samples/versions.js
@@ -0,0 +1,28 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+/* eslint-disable no-console */
+
+import chalk from 'chalk';
+
+import type {ExitCode} from '../lib/types';
+
+export default async function runCommand(): Promise<ExitCode> {
+  const ctx = new chalk.constructor({enabled: true});
+  const out = Object.keys(process.versions)
+    .map(key => ([key, process.versions[key]]))
+    .concat([['atom', atom.getVersion()]])
+    .map(([name, version]) => `${ctx.yellow(name)}=${ctx.green(version)}`)
+    .sort()
+    .join('\n');
+  console.log(out);
+  return 0;
+}
