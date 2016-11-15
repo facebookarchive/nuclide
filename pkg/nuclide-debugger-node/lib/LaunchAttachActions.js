ef6098106fefdf74fa7fa731d2a718720bebdb2b
Jeffrey Tan <jeffreytan@fb.com>
2016-11-15 13:51:16 -0800

    Change Node debugger LaunchAttachActions to use shared base
    Summary: This is not perfect but definitely one step further to reduce boilerplate code.
    
    Reviewed By: johnislarry
    
    Differential Revision: D4178817
    
    fbshipit-source-id: e02f5bc3eeb626b2637b8599430e054d50b95dc3

--- pkg/nuclide-debugger-node/lib/LaunchAttachActions.js
+++ pkg/nuclide-debugger-node/lib/LaunchAttachActions.js
@@ -20,72 +20,19 @@ import {ActionTypes} from './LaunchAttachDispatcher';
 import {NodeAttachProcessInfo} from './NodeAttachProcessInfo';
 import {getNodeDebuggerServiceByNuclideUri} from '../../nuclide-remote-connection';
 import consumeFirstProvider from '../../commons-atom/consumeFirstProvider';
+import {LaunchAttachActionsBase} from '../../nuclide-debugger-base';
 
-const ATTACH_TARGET_LIST_REFRESH_INTERVAL = 2000;
-
-export class LaunchAttachActions {
+export class LaunchAttachActions extends LaunchAttachActionsBase {
   _dispatcher: LaunchAttachDispatcher;
-  _targetUri: NuclideUri;
-  _refreshTimerId: ?number;
-  _parentUIVisible: boolean;
-  _attachUIVisible: boolean;
 
   constructor(dispatcher: LaunchAttachDispatcher, targetUri: NuclideUri) {
+    super(targetUri);
     this._dispatcher = dispatcher;
-    this._targetUri = targetUri;
-    this._refreshTimerId = null;
-    this._parentUIVisible = true;   // Visible by default.
-    this._attachUIVisible = false;
-    (this: any).updateAttachTargetList = this.updateAttachTargetList.bind(this);
-    (this: any).updateParentUIVisibility = this.updateParentUIVisibility.bind(this);
-    (this: any).updateAttachUIVisibility = this.updateAttachUIVisibility.bind(this);
-  }
-
-  updateParentUIVisibility(visible: boolean): void {
-    this._parentUIVisible = visible;
-    this._updateAutoRefresh();
-  }
-
-  updateAttachUIVisibility(visible: boolean): void {
-    this._attachUIVisible = visible;
-    this._updateAutoRefresh();
-  }
-
-  _updateAutoRefresh(): void {
-    if (this._parentUIVisible && this._attachUIVisible) {
-      this._refreshTimerId = setInterval(
-        this.updateAttachTargetList,
-        ATTACH_TARGET_LIST_REFRESH_INTERVAL,
-      );
-    } else {
-      this._killAutoRefreshTimer();
-    }
-  }
-
-  _killAutoRefreshTimer(): void {
-    if (this._refreshTimerId != null) {
-      clearTimeout(this._refreshTimerId);
-      this._refreshTimerId = null;
-    }
   }
 
   attachDebugger(attachTarget: NodeAttachTargetInfo): Promise<void> {
-    const attachInfo = new NodeAttachProcessInfo(this._targetUri, attachTarget);
+    const attachInfo = new NodeAttachProcessInfo(this.getTargetUri(), attachTarget);
     return this._startDebugging(attachInfo);
-  }
-
-  toggleLaunchAttachDialog(): void {
-    atom.commands.dispatch(
-      atom.views.getView(atom.workspace),
-      'nuclide-debugger:toggle-launch-attach',
-    );
-  }
-
-  showDebuggerPanel(): void {
-    atom.commands.dispatch(
-      atom.views.getView(atom.workspace),
-      'nuclide-debugger:show',
-    );
   }
 
   async _startDebugging(processInfo: DebuggerProcessInfo): Promise<void> {
@@ -94,15 +41,11 @@ export class LaunchAttachActions {
   }
 
   async updateAttachTargetList(): Promise<void> {
-    const rpcService = getNodeDebuggerServiceByNuclideUri(this._targetUri);
+    const rpcService = getNodeDebuggerServiceByNuclideUri(this.getTargetUri());
     const attachTargetList = await rpcService.getAttachTargetInfoList();
     this._dispatcher.dispatch({
       actionType: ActionTypes.UPDATE_ATTACH_TARGET_LIST,
       attachTargetInfos: attachTargetList,
     });
-  }
-
-  dispose(): void {
-    this._killAutoRefreshTimer();
   }
 }
