7dfcf01dc63b21a563ea92009187c44e9df113fb
Jeffrey Tan <jeffreytan@fb.com>
2016-11-15 13:51:14 -0800

    Take out the common LaunchAttachActions code into shared base
    Reviewed By: johnislarry
    
    Differential Revision: D4178876
    
    fbshipit-source-id: d0d219ed070f39736fc1c9c428107f7b9e484554

---
+++ pkg/nuclide-debugger-base/lib/LaunchAttachActionsBase.js
@@ -0,0 +1,89 @@
+'use babel';
+/* @flow */
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+import type {NuclideUri} from '../../commons-node/nuclideUri';
+import type {DebuggerProcessInfo} from '../../nuclide-debugger-base';
+
+import consumeFirstProvider from '../../commons-atom/consumeFirstProvider';
+
+const ATTACH_TARGET_LIST_REFRESH_INTERVAL = 2000;
+
+export class LaunchAttachActionsBase {
+  _targetUri: NuclideUri;
+  _refreshTimerId: ?number;
+  _parentUIVisible: boolean;
+  _attachUIVisible: boolean;
+
+  constructor(targetUri: NuclideUri) {
+    this._targetUri = targetUri;
+    this._refreshTimerId = null;
+    this._parentUIVisible = true;   // Visible by default.
+    this._attachUIVisible = false;
+    (this: any).updateAttachTargetList = this.updateAttachTargetList.bind(this);
+    (this: any).updateParentUIVisibility = this.updateParentUIVisibility.bind(this);
+    (this: any).updateAttachUIVisibility = this.updateAttachUIVisibility.bind(this);
+  }
+
+  updateParentUIVisibility(visible: boolean): void {
+    this._parentUIVisible = visible;
+    this._updateAutoRefresh();
+  }
+
+  updateAttachUIVisibility(visible: boolean): void {
+    this._attachUIVisible = visible;
+    this._updateAutoRefresh();
+  }
+
+  _updateAutoRefresh(): void {
+    if (this._parentUIVisible && this._attachUIVisible) {
+      this._refreshTimerId = setInterval(
+        this.updateAttachTargetList,
+        ATTACH_TARGET_LIST_REFRESH_INTERVAL,
+      );
+    } else {
+      this._killAutoRefreshTimer();
+    }
+  }
+
+  updateAttachTargetList(): void {
+    throw Error('Not implemented');
+  }
+
+  _killAutoRefreshTimer(): void {
+    if (this._refreshTimerId != null) {
+      clearTimeout(this._refreshTimerId);
+      this._refreshTimerId = null;
+    }
+  }
+
+  toggleLaunchAttachDialog(): void {
+    atom.commands.dispatch(
+      atom.views.getView(atom.workspace),
+      'nuclide-debugger:toggle-launch-attach',
+    );
+  }
+
+  showDebuggerPanel(): void {
+    atom.commands.dispatch(
+      atom.views.getView(atom.workspace),
+      'nuclide-debugger:show',
+    );
+  }
+
+  async startDebugging(processInfo: DebuggerProcessInfo): Promise<void> {
+    const debuggerService = await consumeFirstProvider('nuclide-debugger.remote');
+    await debuggerService.startDebugging(processInfo);
+  }
+
+  dispose(): void {
+    this._killAutoRefreshTimer();
+  }
+}
