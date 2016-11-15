25d2449bdeeade860031ed18c6584794b6311f7d
Christopher Chedeau <vjeux@fb.com>
2016-11-15 15:52:10 -0800

    Add explanation for the return type
    Summary: See conversation in D4181330#inline-26840244
    
    Reviewed By: nmote
    
    Differential Revision: D4186184
    
    fbshipit-source-id: 034c5cd9a2ea81dc9a759e9536ad178535912b4b

--- pkg/commons-atom/featureConfig.js
+++ pkg/commons-atom/featureConfig.js
@@ -21,6 +21,13 @@ function formatKeyPath(keyPath: string): string {
  * Returns the value of a setting for a Nuclide feature key. Takes and returns the same types as
  * `atom.config.get` exception `keyPath` is not optional. To get the entire config object, use
  * `atom.config.get`.
+ *
+ * Note: This is intentionally typed as mixed, this way each call site has to
+ * first cast it as any and it is obvious that this is an area that is not safe
+ * and flow will not proceed if the callsite doesn't do it.
+ *
+ * Example:
+ *   const config: MyConfigType = (featureConfig.get('config-name'): any);
  */
 function get(
   keyPath: string,
