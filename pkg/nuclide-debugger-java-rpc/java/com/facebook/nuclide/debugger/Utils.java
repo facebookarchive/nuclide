757558d3f9dfc6d4daa803efb78fb22d2f2ec7b8
Jeffrey Tan <jeffreytan@fb.com>
2016-12-14 20:32:32 -0800

    Nuclide Java debugging(10/N): Add JVMConnector for JavaDbg
    Summary:
    Java debugging interface uses connector to connect with JVM. This diff adds launch/attach port/listening three connector API for JavaDbg.
    I will add attach pid in future.
    
    Reviewed By: shushz
    
    Differential Revision: D4252162
    
    fbshipit-source-id: 78eee7d70ae2f6f866657411ea6caeba4352a174

---
+++ pkg/nuclide-debugger-java-rpc/java/com/facebook/nuclide/debugger/Utils.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+package com.facebook.nuclide.debugger;
+
+/**
+ * Static utility class.
+ */
+public final class Utils {
+  private Utils() {
+  }
+
+  public static void logLine(String message) {
+    System.out.println(message);
+  }
+}
