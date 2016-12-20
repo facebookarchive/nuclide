b859983e445c09add920c27b2a6e39b093c67d3b
Andres Suarez <asuarez@fb.com>
2016-12-19 17:20:21 -0800

    Sort services list and expose missing services
    Summary: Will need these soon as I port everything to `getXByNuclideUri`
    
    Reviewed By: mostafaeweda
    
    Differential Revision: D4348492
    
    fbshipit-source-id: 1b2ef42cb448309f92e999297e0b9b0e50492095

--- pkg/nuclide-remote-connection/lib/main.js
+++ pkg/nuclide-remote-connection/lib/main.js
@@ -49,6 +49,7 @@ export {
   getlocalService,
 };
 
+import typeof * as AdbService from '../../nuclide-adb-rpc';
 import typeof * as ArcanistService from '../../nuclide-arcanist-rpc';
 import typeof * as BuckService from '../../nuclide-buck-rpc';
 import typeof * as ClangService from '../../nuclide-clang-rpc';
@@ -61,17 +62,21 @@ import typeof * as GrepService from '../../nuclide-grep-rpc';
 import typeof * as HackService from '../../nuclide-hack-rpc';
 import typeof * as HgService from '../../nuclide-hg-rpc/lib/HgService';
 import typeof * as InfoService from '../../nuclide-server/lib/services/InfoService';
+import typeof * as IwdpDebuggerService from '../../nuclide-debugger-iwdp-rpc';
 import typeof * as MerlinService from '../../nuclide-ocaml-rpc/lib/MerlinService';
-import typeof * as ReasonService from '../../nuclide-ocaml-rpc/lib/ReasonService';
 import typeof * as NativeDebuggerService from '../../nuclide-debugger-native-rpc';
 import typeof * as NodeDebuggerService from '../../nuclide-debugger-node-rpc';
 import typeof * as OpenFilesService from '../../nuclide-open-files-rpc/lib/OpenFilesService';
 import typeof * as PhpDebuggerService from '../../nuclide-debugger-php-rpc';
 import typeof * as PythonService from '../../nuclide-python-rpc';
+import typeof * as ReasonService from '../../nuclide-ocaml-rpc/lib/ReasonService';
 import typeof * as RemoteCommandService from '../../nuclide-remote-atom-rpc';
 import typeof * as SourceControlService
   from '../../nuclide-server/lib/services/SourceControlService';
-import typeof * as AdbService from '../../nuclide-adb-rpc';
+
+export function getAdbServiceByNuclideUri(uri: NuclideUri): AdbService {
+  return nullthrows(getServiceByNuclideUri('AdbService', uri));
+}
 
 export function getArcanistServiceByNuclideUri(uri: NuclideUri): ArcanistService {
   return nullthrows(getServiceByNuclideUri('ArcanistService', uri));
@@ -121,12 +126,12 @@ export function getInfoServiceByNuclideUri(uri: NuclideUri): InfoService {
   return nullthrows(getServiceByNuclideUri('InfoService', uri));
 }
 
-export function getMerlinServiceByNuclideUri(uri: NuclideUri): MerlinService {
-  return nullthrows(getServiceByNuclideUri('MerlinService', uri));
+export function getIwdpDebuggerServiceByNuclideUri(uri: NuclideUri): IwdpDebuggerService {
+  return nullthrows(getServiceByNuclideUri('IwdpDebuggerService', uri));
 }
 
-export function getReasonServiceByNuclideUri(uri: NuclideUri): ReasonService {
-  return nullthrows(getServiceByNuclideUri('ReasonService', uri));
+export function getMerlinServiceByNuclideUri(uri: NuclideUri): MerlinService {
+  return nullthrows(getServiceByNuclideUri('MerlinService', uri));
 }
 
 export function getNativeDebuggerServiceByNuclideUri(uri: NuclideUri): NativeDebuggerService {
@@ -149,14 +154,14 @@ export function getPythonServiceByNuclideUri(uri: NuclideUri): PythonService {
   return nullthrows(getServiceByNuclideUri('PythonService', uri));
 }
 
+export function getReasonServiceByNuclideUri(uri: NuclideUri): ReasonService {
+  return nullthrows(getServiceByNuclideUri('ReasonService', uri));
+}
+
 export function getRemoteCommandServiceByNuclideUri(uri: NuclideUri): RemoteCommandService {
   return nullthrows(getServiceByNuclideUri('RemoteCommandService', uri));
 }
 
 export function getSourceControlServiceByNuclideUri(uri: NuclideUri): SourceControlService {
   return nullthrows(getServiceByNuclideUri('SourceControlService', uri));
-}
-
-export function getAdbServiceByNuclideUri(uri: NuclideUri): AdbService {
-  return nullthrows(getServiceByNuclideUri('AdbService', uri));
 }
