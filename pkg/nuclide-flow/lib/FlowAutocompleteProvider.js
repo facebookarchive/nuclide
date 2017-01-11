deb9edee4f43fc871671144f75dd06a305da583c
Nat Mote <nmote@fb.com>
2017-01-11 15:13:10 -0800

    Consolidate JS identifier regexes
    Reviewed By: zertosh
    
    Differential Revision: D4406502
    
    fbshipit-source-id: 008c010306a181a41b0ed55cec635cf0c8678a05

--- pkg/nuclide-flow/lib/FlowAutocompleteProvider.js
+++ pkg/nuclide-flow/lib/FlowAutocompleteProvider.js
@@ -18,8 +18,7 @@ import AutocompleteCacher from '../../commons-atom/AutocompleteCacher';
 import passesGK from '../../commons-node/passesGK';
 
 import {getFlowServiceByNuclideUri} from './FlowServiceFactory';
-
-const ID_REGEX = /^[a-zA-Z_$][0-9a-zA-Z_$]*$/;
+import {JAVASCRIPT_WHOLE_STRING_IDENTIFIER_REGEX} from './constants';
 
 export default class FlowAutocompleteProvider {
   _cacher: AutocompleteCacher<?Array<atom$AutocompleteSuggestion>>;
@@ -76,7 +75,7 @@ export function shouldFilter(
   lastRequest: atom$AutocompleteRequest,
   currentRequest: atom$AutocompleteRequest,
 ): boolean {
-  const prefixIsIdentifier = ID_REGEX.test(currentRequest.prefix);
+  const prefixIsIdentifier = JAVASCRIPT_WHOLE_STRING_IDENTIFIER_REGEX.test(currentRequest.prefix);
   const previousPrefixIsDot = /^\s*\.\s*$/.test(lastRequest.prefix);
   const currentPrefixIsSingleChar = currentRequest.prefix.length === 1;
   const startsWithPrevious = currentRequest.prefix.length - 1 === lastRequest.prefix.length &&
@@ -138,7 +137,7 @@ function updateResults(
 function getReplacementPrefix(originalPrefix: string): string {
   // Ignore prefix unless it's an identifier (this keeps us from eating leading
   // dots, colons, etc).
-  return ID_REGEX.test(originalPrefix) ? originalPrefix : '';
+  return JAVASCRIPT_WHOLE_STRING_IDENTIFIER_REGEX.test(originalPrefix) ? originalPrefix : '';
 }
 
 /**
