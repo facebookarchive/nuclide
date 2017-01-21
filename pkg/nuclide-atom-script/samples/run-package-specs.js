c31fb2701938dc137b02622bcb140f0de09f1819
Andres Suarez <asuarez@fb.com>
2017-01-20 17:54:30 -0800

    Add run-package-specs atom-script and shell script
    Summary:
    This adds an `atom-script` script that opens a spec file in the spec window without having to load all of Nuclide. Also added a shortcut script as `scripts/run-package-specs.sh`. To use it, just run `scripts/run-package-specs.sh` with a path to a spec file, then it'll open up the spec window with that test loaded. Example:
    
    ```
    ./scripts/run-package-specs.sh spec/quick-open-provider-cycle-integration-spec.js
    
    ./scripts/run-package-specs.sh pkg/nuclide-quick-open/spec/SearchResultManager-spec.js
    ```
    
    Reviewed By: AsyncDBConnMarkedDownDBException
    
    Differential Revision: D4444976
    
    fbshipit-source-id: fab986eb9b6df5751b4844ef8a14f134572f68ae

---
+++ pkg/nuclide-atom-script/samples/run-package-specs.js
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ *
+ * @flow
+ */
+
+/* eslint-disable no-console */
+
+import type {ExitCode} from '../lib/types';
+
+import invariant from 'assert';
+import electron from 'electron';
+// eslint-disable-next-line nuclide-internal/prefer-nuclide-uri
+import path from 'path';
+
+const {ipcRenderer, remote} = electron;
+invariant(ipcRenderer != null && remote != null);
+
+export default async function runCommand(args: Array<string>): Promise<ExitCode> {
+  if (typeof args[0] !== 'string') {
+    console.error(`Usage: atom-script ${__filename} <spec file>`);
+    return 1;
+  }
+
+  const initialWindows = remote.BrowserWindow.getAllWindows();
+
+  const packageSpecPath = path.resolve(args[0]);
+  ipcRenderer.send('run-package-specs', packageSpecPath);
+
+  // Wait for the window to load
+  await new Promise(resolve => setTimeout(resolve, 1000));
+
+  const testWindow = remote.BrowserWindow.getAllWindows().find(browserWindow => {
+    return !initialWindows.includes(browserWindow) &&
+      // $FlowFixMe: Missing def
+      browserWindow.getURL().includes('initialize-test-window.js');
+  });
+
+  if (testWindow == null) {
+    console.error('Could not find spec browser window.');
+    return 1;
+  }
+
+  // If we don't wait for the spec window to close before finishing, we cause
+  // the window to close.
+  await new Promise(resolve => {
+    testWindow.once('close', () => {
+      resolve();
+    });
+  });
+
+  return 0;
+}
