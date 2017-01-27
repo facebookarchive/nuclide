7aa72ff076f0463d0fe1d4591284c435ffcb0c40
Stepan Hruda <stepanhruda@fb.com>
2017-01-27 11:33:14 -0800

    Add availability detection to nuclide-hhvm
    Summary: Add new callback method where the package says whether it's available
    
    Reviewed By: matthewwithanm
    
    Differential Revision: D4474748
    
    fbshipit-source-id: 6b55075829c1caed8c9b469059268f58c1939fad

--- pkg/nuclide-hhvm/lib/HhvmBuildSystem.js
+++ pkg/nuclide-hhvm/lib/HhvmBuildSystem.js
@@ -85,6 +85,10 @@ export default class HhvmBuildSystem {
     ];
   }
 
+  getPriority(): number {
+    return 1; // Take precedence over the Arcanist build toolbar.
+  }
+
   getIcon(): ReactClass<any> {
     return () => <Icon icon="nuclicon-hhvm" className="nuclide-hhvm-task-runner-icon" />;
   }
@@ -101,6 +105,37 @@ export default class HhvmBuildSystem {
   }
 
   setProjectRoot(projectRoot: ?Directory): void {
-    this._projectStore.setProjectRoot(projectRoot == null ? null : projectRoot.getPath());
+    this.setProjectRootNew(projectRoot, (enabled, taskList) => {});
+  }
+
+  setProjectRootNew(
+    projectRoot: ?Directory,
+    callback: (enabled: boolean, taskList: Array<TaskMetadata>) => mixed,
+  ): IDisposable {
+    const path = projectRoot == null ? null : projectRoot.getPath();
+
+    const enabledObservable =
+      observableFromSubscribeFunction(this._projectStore.onChange.bind(this._projectStore))
+        .map(() => this._projectStore)
+        .filter(store => store.getProjectRoot() === path && store.isHHVMProject() !== null)
+        .map(store => store.isHHVMProject() === true)
+        .distinctUntilChanged();
+
+    const tasksObservable = Observable.of([
+      {
+        type: 'debug',
+        label: 'Debug',
+        description: 'Debug an HHVM project',
+        icon: 'nuclicon-debugger',
+        cancelable: false,
+      },
+    ]);
+
+    const subscription = Observable.combineLatest(enabledObservable, tasksObservable)
+      .subscribe(([enabled, tasks]) => callback(enabled, tasks));
+
+    this._projectStore.setProjectRoot(path);
+
+    return new UniversalDisposable(subscription);
   }
 }
