6425e49e20780c5a68e23609566c46550d0ff9e7
TJ Ryan <tjfryan@fb.com>
2017-03-07 15:26:35 -0800

    Adding buttons to interactive commit UI
    Summary: Confirm button, quit button, and manual edit button.
    
    Reviewed By: aadisriram
    
    Differential Revision: D4661647
    
    fbshipit-source-id: 0c319843a6f666e2fcbb6fc1f2db26175e926109

--- pkg/nuclide-patch-editor/lib/main.js
+++ pkg/nuclide-patch-editor/lib/main.js
@@ -12,14 +12,15 @@ import type {CwdApi} from '../../nuclide-current-working-directory/lib/CwdApi';
 import type {HgRepositoryClient} from '../../nuclide-hg-repository-client';
 
 import createPackage from '../../commons-atom/createPackage';
-import InteractiveFileChanges from './ui/InteractiveFileChanges';
-import {isValidTextEditor} from '../../commons-atom/text-editor';
-import {Observable} from 'rxjs';
+import UniversalDisposable from '../../commons-node/UniversalDisposable';
 import {observableFromSubscribeFunction} from '../../commons-node/event';
+import {Observable} from 'rxjs';
+import {isValidTextEditor} from '../../commons-atom/text-editor';
+import {repositoryForPath} from '../../commons-atom/vcs';
+import {Disposable} from 'atom';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {repositoryForPath} from '../../commons-atom/vcs';
-import UniversalDisposable from '../../commons-node/UniversalDisposable';
+import InteractiveFileChanges from './ui/InteractiveFileChanges';
 
 class Activation {
   _subscriptions: UniversalDisposable;
@@ -69,23 +70,31 @@ class Activation {
     }).subscribe(renderOverEditor);
 
     this._subscriptions.add(subscription);
-    return new UniversalDisposable(() => {
+    return new Disposable(() => {
       this._subscriptions.remove(subscription);
     });
   }
 }
 
 function renderOverEditor(editor: atom$TextEditor): void {
-  const element = <InteractiveFileChanges diffContent={editor.getText()} />;
-
   // Clear the editor so that closing the tab without changing anything won't
   // cause the commit to go through by default
+  const diffContent = editor.getText();
   editor.setText('');
   editor.save();
   editor.getGutters().forEach(gutter => gutter.hide());
-  atom.views.getView(editor).style.visibility = 'hidden';
-
+  const editorView = atom.views.getView(editor);
+  editorView.style.visibility = 'hidden';
   const item = document.createElement('div');
+
+  const element = (
+    <InteractiveFileChanges
+      diffContent={diffContent}
+      onConfirm={content => onConfirm(editor, content)}
+      onManualEdit={originalContent => onManualEdit(editor, originalContent, marker, editorView)}
+      onQuit={() => atom.workspace.getActivePane().destroyItem(editor)}
+    />
+  );
   ReactDOM.render(element, item);
   item.style.visibility = 'visible';
 
@@ -97,6 +106,26 @@ function renderOverEditor(editor: atom$TextEditor): void {
     type: 'block',
     item,
   });
+}
+
+function onConfirm(editor: atom$TextEditor, content: string): void {
+  editor.setText(content);
+  editor.save();
+  atom.workspace.getActivePane().destroyItem(editor);
+}
+
+function onManualEdit(
+  editor: atom$TextEditor,
+  originalContent: string,
+  marker: atom$Marker,
+  editorView: atom$TextEditorElement,
+): void {
+  editor.setText(originalContent);
+  editor.save();
+  editor.setGrammar(atom.grammars.grammarForScopeName('source.mercurial.diff'));
+  marker.destroy();
+  editorView.style.visibility = 'visible';
+  editor.getGutters().forEach(gutter => gutter.show());
 }
 
 createPackage(module.exports, Activation);
--- pkg/nuclide-patch-editor/lib/ui/InteractiveFileChanges.js
+++ pkg/nuclide-patch-editor/lib/ui/InteractiveFileChanges.js
@@ -8,26 +8,53 @@
  * @flow
  */
 
+import React from 'react';
 import FileChanges from '../../../nuclide-ui/FileChanges';
 import parse from 'diffparser';
-import React from 'react';
+import {Button} from '../../../nuclide-ui/Button';
 
 type Props = {
   diffContent: string,
+  onConfirm: string => any,
+  onManualEdit: string => any,
+  onQuit: () => any,
 };
 
 export default class InteractiveFileChanges extends React.Component {
   props: Props;
+
+  constructor(props: Props) {
+    super(props);
+
+    (this: any)._onClickConfirm = this._onClickConfirm.bind(this);
+    (this: any)._onClickManual = this._onClickManual.bind(this);
+    (this: any)._onClickQuit = this._onClickQuit.bind(this);
+  }
 
   render(): React.Element<any> {
     const parsedDiffFiles = parse(this.props.diffContent);
 
     return (
       <div>
+        <Button onClick={this._onClickConfirm}>Confirm</Button>
+        <Button onClick={this._onClickQuit}>Quit</Button>
+        <Button onClick={this._onClickManual}>Manual Edit</Button>
         {parsedDiffFiles.map(file =>
           <FileChanges diff={file} key={`${file.from}:${file.to}`} />,
         )}
       </div>
     );
+  }
+
+  _onClickConfirm(): void {
+    this.props.onConfirm(this.props.diffContent);
+  }
+
+  _onClickManual(): void {
+    this.props.onManualEdit(this.props.diffContent);
+  }
+
+  _onClickQuit(): void {
+    this.props.onQuit();
   }
 }
