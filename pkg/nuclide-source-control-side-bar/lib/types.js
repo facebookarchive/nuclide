85a7b418a9e3c2625a0eb62851374421fe6550cb
Andres Suarez <asuarez@fb.com>
2017-03-17 20:36:22 -0700

    Fix source-control-side-bar Action type
    Reviewed By: aadisriram
    
    Differential Revision: D4734654
    
    fbshipit-source-id: 5cdb5ccbc0d1f4ba0c521ca3eeeab9d0f40fe715

--- pkg/nuclide-source-control-side-bar/lib/types.js
+++ pkg/nuclide-source-control-side-bar/lib/types.js
@@ -106,4 +106,5 @@ export type Action =
   | SetProjectDirectoriesAction
   | SetRepositoryBookmarks
   | UnsetBookmarkIsLoading
-  | UpdateToBookmarkAction;
+  | UpdateToBookmarkAction
+  | UpdateUncommittedChanges;
