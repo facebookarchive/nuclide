2c080bcd453ee6bd44cc65c7f9166a250377e7f8
Christopher Chedeau <vjeux@fb.com>
2017-03-23 11:39:17 -0700

    Ugly hack to copy & paste url
    Summary:
    Unfortunately, AtomInput when disabled preventsDefault the copy event. I digged a bit but haven't figured out how to prevent that.
    
    A hacky fix is to make the component not disabled but a controlled input and whenever it changes, then re-update the value to the one we want. Unfortunately, this isn't working. I'm not really sure why.
    
    But, what is working is to manually set the value onDidChange (should be called onChange!). Just need to not trigger it if the value is the same otherwise it stack overflows.
    
    I'm not super happy about this but it solves a big problem people have so I think it's worth putting this small hack in.
    
    Reviewed By: azich
    
    Differential Revision: D4763284
    
    fbshipit-source-id: 79b6cd0ee3a08a6c5b2c5f999fb9ecf663a7c9e1

--- pkg/nuclide-hhvm/lib/HhvmToolbar.js
+++ pkg/nuclide-hhvm/lib/HhvmToolbar.js
@@ -83,6 +83,8 @@ export default class HhvmToolbar extends React.Component {
   render(): React.Element<any> {
     const store = this.props.projectStore;
     const isDebugScript = store.getDebugMode() === 'script';
+    const isDisabled = !isDebugScript;
+    const value = store.getDebugTarget();
     return (
       <div className="hhvm-toolbar">
         <Dropdown
@@ -96,9 +98,17 @@ export default class HhvmToolbar extends React.Component {
         <div className="inline-block" style={{width: '300px'}}>
           <AtomInput
             ref="debugTarget"
-            initialValue={store.getDebugTarget()}
-            disabled={!isDebugScript}
-            onDidChange={this._updateLastScriptCommand}
+            initialValue={value}
+            // Ugly hack: prevent people changing the value without disabling so
+            // that they can copy and paste.
+            onDidChange={isDisabled
+              ? () => {
+                if (this.refs.debugTarget.getText() !== value) {
+                  this.refs.debugTarget.setText(value);
+                }
+              }
+              : this._updateLastScriptCommand
+            }
             size="sm"
           />
         </div>
