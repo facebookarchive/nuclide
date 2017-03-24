04f3acd00092c47467681317e564c586d5a8f766
Jonas Gebhardt <jxg@fb.com>
2017-03-24 16:23:09 -0700

    Remove `copy` button from diagnostics message
    Summary: It's possible to simply copy the contents of the popup via cmd+c, so the button is no longer needed.
    
    Reviewed By: vjeux
    
    Differential Revision: D4774015
    
    fbshipit-source-id: 69586a2f811386bb2aaf50d917c5e95bd22d7247

--- pkg/nuclide-ui/DiagnosticsMessage.js
+++ pkg/nuclide-ui/DiagnosticsMessage.js
@@ -10,48 +10,19 @@
 
 import type {
   FileDiagnosticMessage,
-  Trace,
 } from '../nuclide-diagnostics-common/lib/rpc-types';
 
-import invariant from 'assert';
 import React from 'react';
 import {Button, ButtonTypes} from './Button';
 import {ButtonGroup} from './ButtonGroup';
 import {DiagnosticsMessageText} from './DiagnosticsMessageText';
 import {DiagnosticsTraceItem} from './DiagnosticsTraceItem';
-import nuclideUri from '../commons-node/nuclideUri';
 
 type DiagnosticsMessageProps = {
   message: FileDiagnosticMessage,
   goToLocation: (path: string, line: number) => mixed,
   fixer: (message: FileDiagnosticMessage) => void,
 };
-
-function plainTextForItem(item: FileDiagnosticMessage | Trace): string {
-  let mainComponent = undefined;
-  if (item.html != null) {
-    // Quick and dirty way to get an approximation for the plain text from HTML.
-    // This will work in simple cases, anyway.
-    mainComponent = item.html.replace('<br/>', '\n').replace(/<[^>]*>/g, '');
-  } else {
-    invariant(item.text != null);
-    mainComponent = item.text;
-  }
-
-  let pathComponent;
-  if (item.filePath == null) {
-    pathComponent = '';
-  } else {
-    const lineComponent = item.range != null ? `:${item.range.start.row + 1}` : '';
-    pathComponent = ': ' + nuclideUri.getPath(item.filePath) + lineComponent;
-  }
-  return mainComponent + pathComponent;
-}
-
-function plainTextForDiagnostic(message: FileDiagnosticMessage): string {
-  const trace = message.trace != null ? message.trace : [];
-  return [message, ...trace].map(plainTextForItem).join('\n');
-}
 
 function diagnosticHeader(props: DiagnosticsMessageProps) {
   const {
@@ -61,10 +32,6 @@ function diagnosticHeader(props: DiagnosticsMessageProps) {
   const providerClassName = message.type === 'Error'
     ? 'highlight-error'
     : 'highlight-warning';
-  const copy = () => {
-    const text = plainTextForDiagnostic(message);
-    atom.clipboard.write(text);
-  };
   let fixButton = null;
   if (message.fix != null) {
     const applyFix = () => {
@@ -80,7 +47,6 @@ function diagnosticHeader(props: DiagnosticsMessageProps) {
     <div className="nuclide-diagnostics-gutter-ui-popup-header">
       <ButtonGroup>
         {fixButton}
-        <Button size="EXTRA_SMALL" onClick={copy}>Copy</Button>
       </ButtonGroup>
       <span className={providerClassName}>{message.providerName}</span>
     </div>
