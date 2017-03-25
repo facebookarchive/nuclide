6ab4aab680ae46de30f33d4ba8a9cec8648aca0f
Hanson Wang <hansonw@fb.com>
2017-03-24 18:33:01 -0700

    Find References: modernize the code with createPackage
    Summary: Use a proper package activation class and move the vars inside.
    
    Reviewed By: jgebhardt
    
    Differential Revision: D4774976
    
    fbshipit-source-id: 22449fca043da5772234d03beb00e107f20ccc9f

--- pkg/nuclide-find-references/lib/main.js
+++ pkg/nuclide-find-references/lib/main.js
@@ -13,6 +13,7 @@
 import type {FindReferencesReturn} from './rpc-types';
 
 import crypto from 'crypto';
+import createPackage from '../../commons-atom/createPackage';
 import {observeTextEditors} from '../../commons-atom/text-editor';
 import UniversalDisposable from '../../commons-node/UniversalDisposable';
 import {track} from '../../nuclide-analytics';
@@ -33,42 +34,12 @@ export type FindReferencesProvider = {
 
 const FIND_REFERENCES_URI = 'atom://nuclide/find-references/';
 
-let subscriptions: ?UniversalDisposable = null;
-let providers: Array<FindReferencesProvider> = [];
-const supportedProviders: Map<TextEditor, Array<FindReferencesProvider>> = new Map();
-
-async function getProviderData(): Promise<?FindReferencesReturn> {
-  const editor = atom.workspace.getActiveTextEditor();
-  if (!editor) {
-    return null;
-  }
-  const path = editor.getPath();
-  if (!path) {
-    return null;
-  }
-  const point = editor.getCursorBufferPosition();
-  track('find-references:activate', {
-    path,
-    row: point.row.toString(),
-    column: point.column.toString(),
-  });
-  const supported = supportedProviders.get(editor);
-  if (!supported) {
-    return null;
-  }
-  const providerData = await Promise.all(supported.map(
-    provider => provider.findReferences(editor, point),
-  ));
-  return providerData.filter(x => Boolean(x))[0];
-}
-
 function showWarning(message: string): void {
   atom.notifications.addWarning('nuclide-find-references: ' + message, {dismissable: true});
 }
 
-async function tryCreateView(): Promise<?HTMLElement> {
+async function tryCreateView(data: ?FindReferencesReturn): Promise<?HTMLElement> {
   try {
-    const data = await getProviderData();
     if (data == null) {
       showWarning('Symbol references are not available for this project.');
     } else if (data.type === 'error') {
@@ -109,114 +80,140 @@ function disableForEditor(editor: TextEditor): void {
   elem.classList.remove('enable-nuclide-find-references');
 }
 
-// Returns true if this is this adds the supported provider.
-function addSupportedProvider(editor: TextEditor, provider: FindReferencesProvider) {
-  let supported = supportedProviders.get(editor);
-  if (supported == null) {
-    supported = [];
-    supportedProviders.set(editor, supported);
-  }
-  supported.push(provider);
-  return supported.length === 1;
-}
+class Activation {
+  _subscriptions: UniversalDisposable;
+  _providers: Array<FindReferencesProvider> = [];
+  _supportedProviders: Map<TextEditor, Array<FindReferencesProvider>> = new Map();
 
-export function activate(state: ?any): void {
-  subscriptions = new UniversalDisposable();
-  subscriptions.add(atom.commands.add(
-    'atom-text-editor',
-    'nuclide-find-references:activate',
-    async () => {
-      const view = await tryCreateView();
-      if (view != null) {
-        // Generate a unique identifier.
-        const id = (crypto.randomBytes(8) || '').toString('hex');
-        const uri = FIND_REFERENCES_URI + id;
-        const disposable = atom.workspace.addOpener(newUri => {
-          if (uri === newUri) {
-            return view;
+  constructor(state: ?any): void {
+    this._subscriptions = new UniversalDisposable(
+      atom.commands.add(
+        'atom-text-editor',
+        'nuclide-find-references:activate',
+        async () => {
+          const view = await tryCreateView(await this._getProviderData());
+          if (view != null) {
+            // Generate a unique identifier.
+            const id = (crypto.randomBytes(8) || '').toString('hex');
+            const uri = FIND_REFERENCES_URI + id;
+            const disposable = atom.workspace.addOpener(newUri => {
+              if (uri === newUri) {
+                return view;
+              }
+            });
+            // not a file URI
+            // eslint-disable-next-line nuclide-internal/atom-apis
+            atom.workspace.open(uri);
+            // The new tab opens instantly, so this is no longer needed.
+            disposable.dispose();
           }
+        },
+      ),
+
+      // Mark text editors with a working provider with a special CSS class.
+      // This ensures the context menu option only appears in supported projects.
+      observeTextEditors(async editor => {
+        const path = editor.getPath();
+        if (!path || this._supportedProviders.get(editor)) {
+          return;
+        }
+        this._supportedProviders.set(editor, []);
+        await Promise.all(this._providers.map(
+          async provider => {
+            if (await provider.isEditorSupported(editor)) {
+              if (this._addSupportedProvider(editor, provider)) {
+                enableForEditor(editor);
+              }
+            }
+          },
+        ));
+        const disposable = editor.onDidDestroy(() => {
+          this._supportedProviders.delete(editor);
+          this._subscriptions.remove(disposable);
         });
-        // not a file URI
-        // eslint-disable-next-line nuclide-internal/atom-apis
-        atom.workspace.open(uri);
-        // The new tab opens instantly, so this is no longer needed.
-        disposable.dispose();
-      }
-    },
-  ));
+        this._subscriptions.add(disposable);
+      }),
 
-  // Mark text editors with a working provider with a special CSS class.
-  // This ensures the context menu option only appears in supported projects.
-  subscriptions.add(observeTextEditors(async editor => {
-    const path = editor.getPath();
-    if (!path || supportedProviders.get(editor)) {
-      return;
-    }
-    supportedProviders.set(editor, []);
-    await Promise.all(providers.map(
-      async provider => {
-        if (await provider.isEditorSupported(editor)) {
-          if (addSupportedProvider(editor, provider)) {
-            enableForEditor(editor);
+      // Enable text copy from the symbol reference
+      atom.commands.add(
+        'nuclide-find-references-view',
+        'core:copy',
+        () => {
+          const selection = getSelection();
+          if (selection != null) {
+            const selectedText = selection.toString();
+            atom.clipboard.write(selectedText);
           }
-        }
-      },
-    ));
-    if (subscriptions) {
-      const disposable = editor.onDidDestroy(() => {
-        supportedProviders.delete(editor);
-        if (subscriptions) {
-          subscriptions.remove(disposable);
-        }
-      });
-      subscriptions.add(disposable);
-    }
-  }));
-
-  // Enable text copy from the symbol reference view
-  subscriptions.add(atom.commands.add(
-    'nuclide-find-references-view',
-    'core:copy',
-    () => {
-      const selection = getSelection();
-      if (selection != null) {
-        const selectedText = selection.toString();
-        atom.clipboard.write(selectedText);
-      }
-    },
-  ));
-}
-
-export function deactivate(): void {
-  if (subscriptions) {
-    subscriptions.dispose();
-    subscriptions = null;
+        },
+      ),
+    );
   }
-  providers = [];
-}
 
-export function consumeProvider(provider: FindReferencesProvider): IDisposable {
-  providers.push(provider);
-  // Editors are often open before providers load, so update existing ones too.
-  atom.workspace.getTextEditors().forEach(async editor => {
-    if (await provider.isEditorSupported(editor)) {
-      if (addSupportedProvider(editor, provider)) {
-        enableForEditor(editor);
-      }
-    }
-  });
+  dispose(): void {
+    this._subscriptions.dispose();
+  }
 
-  return new UniversalDisposable(() => {
-    providers = providers.filter(p => p !== provider);
-
-    supportedProviders.forEach((supported, editor) => {
-      const providerIdx = supported.indexOf(provider);
-      if (providerIdx !== -1) {
-        supported.splice(providerIdx, 1);
-        if (supported.length === 0) {
-          disableForEditor(editor);
+  consumeProvider(provider: FindReferencesProvider): IDisposable {
+    this._providers.push(provider);
+    // Editors are often open before providers load, so update existing ones too.
+    atom.workspace.getTextEditors().forEach(async editor => {
+      if (await provider.isEditorSupported(editor)) {
+        if (this._addSupportedProvider(editor, provider)) {
+          enableForEditor(editor);
         }
       }
     });
-  });
+
+    return new UniversalDisposable(() => {
+      this._providers = this._providers.filter(p => p !== provider);
+
+      this._supportedProviders.forEach((supported, editor) => {
+        const providerIdx = supported.indexOf(provider);
+        if (providerIdx !== -1) {
+          supported.splice(providerIdx, 1);
+          if (supported.length === 0) {
+            disableForEditor(editor);
+          }
+        }
+      });
+    });
+  }
+
+  async _getProviderData(): Promise<?FindReferencesReturn> {
+    const editor = atom.workspace.getActiveTextEditor();
+    if (!editor) {
+      return null;
+    }
+    const path = editor.getPath();
+    if (!path) {
+      return null;
+    }
+    const point = editor.getCursorBufferPosition();
+    track('find-references:activate', {
+      path,
+      row: point.row.toString(),
+      column: point.column.toString(),
+    });
+    const supported = this._supportedProviders.get(editor);
+    if (!supported) {
+      return null;
+    }
+    const providerData = await Promise.all(supported.map(
+      provider => provider.findReferences(editor, point),
+    ));
+    return providerData.filter(x => Boolean(x))[0];
+  }
+
+  // Returns true if this adds the first provider for the editor.
+  _addSupportedProvider(editor: TextEditor, provider: FindReferencesProvider) {
+    let supported = this._supportedProviders.get(editor);
+    if (supported == null) {
+      supported = [];
+      this._supportedProviders.set(editor, supported);
+    }
+    supported.push(provider);
+    return supported.length === 1;
+  }
 }
+
+createPackage(module.exports, Activation);
