37d9ce19f45cd6187d71a3f0244088a7eb0ffdf8
Max Sherman <jonaldislarry@fb.com>
2017-04-05 16:57:29 -0700

    Enable the mobile JS debugger to request source via the chrome protocol
    Reviewed By: ebluestein
    
    Differential Revision: D4827166
    
    fbshipit-source-id: e17fe1c81b9310fc4a34f7f605b8bc54233b0206

--- pkg/nuclide-debugger-iwdp-rpc/lib/FileCache.js
+++ pkg/nuclide-debugger-iwdp-rpc/lib/FileCache.js
@@ -19,61 +19,116 @@ const {log} = logger;
 const EMULATOR_LOCALHOST_ADDR: RegExp = /10\.0\.2\.2|10\.0\.3\.2/;
 
 type FileData = {
-  filePath: string, // Path to file on disk.
-  url: string, // Url bundle is served from.
+  nuclidePath: string, // Path that Nuclide can use to access the resource.
+  targetPath: string, // Path that the target can use to access the resource.
   sourceMapUrl?: string, // Url that chrome devtools understands and can decode to get source maps.
 };
-
-type Url = string;
 
 const SOURCE_MAP_REGEX = /\/\/# sourceMappingURL=(.+)$/;
 const SOURCE_MAP_PREFIX = 'data:application/json;base64,';
 
 export class FileCache {
   _disposables: UniversalDisposable;
-  _filePathToFileData: Map<Url, FileData>;
-  _urlToFileData: Map<Url, FileData>;
+  _nuclidePathToFileData: Map<string, FileData>;
+  _targetPathToFileData: Map<string, FileData>;
+  _getScriptSource: (scriptId: string) => Promise<{result: {scriptSource: string}}>;
 
-  constructor() {
-    this._filePathToFileData = new Map();
-    this._urlToFileData = new Map();
+  constructor(
+    getScriptSource: (scriptId: string) => Promise<{result: {scriptSource: string}}>,
+  ) {
+    this._getScriptSource = getScriptSource;
+    this._nuclidePathToFileData = new Map();
+    this._targetPathToFileData = new Map();
     this._disposables = new UniversalDisposable(
-      () => this._filePathToFileData.clear(),
-      () => this._urlToFileData.clear(),
+      () => this._nuclidePathToFileData.clear(),
+      () => this._targetPathToFileData.clear(),
     );
   }
 
-  async scriptParsed(obj: Object): Promise<Object> {
+  scriptParsed(obj: Object): Promise<Object> {
     const {params} = obj;
     if (params == null) {
-      return obj;
+      return Promise.resolve(obj);
     }
     const {url: urlString} = params;
-    if (urlString == null || urlString === '') {
-      return obj;
+    if (urlString == null) {
+      return Promise.resolve(obj);
     }
-    if (!urlString.startsWith('http:')) {
-      return obj;
+    if (urlString.startsWith('http:')) {
+      return this._processScriptParsedWithDownloadableUrl(obj, urlString);
     }
+    const {sourceMapURL} = params;
+    if (sourceMapURL != null && sourceMapURL !== '') {
+      return this._processScriptParsedWithoutDownloadableUrl(obj, urlString);
+    }
+    return Promise.resolve(obj);
+  }
+
+  // Used to process `Debugger.scriptParsed` messages that have reported a `sourceMapURL` without
+  // a corresponding `url`.
+  async _processScriptParsedWithoutDownloadableUrl(
+    obj: Object,
+    urlString: string,
+  ): Promise<Object> {
+    const {params} = obj;
+    const {scriptId} = params;
+    const {result} = await this._getScriptSource(scriptId);
+    const {scriptSource} = result;
+
+    const filePath = await fsPromise.tempfile({suffix: '.js'});
+    await fsPromise.writeFile(filePath, scriptSource);
+    const nuclidePath = `file://${filePath}`;
+
+    const newFileData: FileData = {
+      nuclidePath,
+      targetPath: urlString,
+      sourceMapUrl: await getSourceMapFromDisk(scriptSource),
+    };
+    this._targetPathToFileData.set(newFileData.targetPath, newFileData);
+    this._nuclidePathToFileData.set(newFileData.nuclidePath, newFileData);
+    updateMessageObjWithFileData(obj, newFileData);
+    return obj;
+  }
+
+  // Used to process `Debugger.scriptParsed` messages that have reported a `url` with an http:
+  // prefix, indicating that we need to download our resources.
+  async _processScriptParsedWithDownloadableUrl(obj: Object, urlString: string): Promise<Object> {
     const url = new URL(urlString);
-    const fileData = this._urlToFileData.get(urlString);
+    const fileData = this._targetPathToFileData.get(urlString);
     if (fileData != null) {
       updateMessageObjWithFileData(obj, fileData);
       return obj;
     }
-    const newFileData = await createFileData(url);
-    this._urlToFileData.set(newFileData.url, newFileData);
-    this._filePathToFileData.set(newFileData.filePath, newFileData);
+
+    log(`FileCache got url: ${urlString}`);
+    const localhostedUrl = urlString.replace(EMULATOR_LOCALHOST_ADDR, 'localhost');
+    log(`Converted to: ${localhostedUrl}`);
+    const fileResponse = await xfetch(localhostedUrl, {});
+    const basename = nuclideUri.basename(url.pathname);
+    const [contents, filePath] = await Promise.all([
+      fileResponse.text(),
+      fsPromise.tempfile({prefix: basename, suffix: '.js'}),
+    ]);
+    await fsPromise.writeFile(filePath, contents);
+    const nuclidePath = `file://${filePath}`;
+
+    const newFileData: FileData = {
+      nuclidePath,
+      targetPath: urlString,
+      sourceMapUrl: await getSourceMapFromUrl(url, contents),
+    };
+    this._targetPathToFileData.set(newFileData.targetPath, newFileData);
+    this._nuclidePathToFileData.set(newFileData.nuclidePath, newFileData);
     updateMessageObjWithFileData(obj, newFileData);
     return obj;
   }
 
   getUrlFromFilePath(filePath: string): string {
-    const fileData = this._filePathToFileData.get(filePath);
+    const fileData = this._nuclidePathToFileData.get(filePath);
     if (fileData == null) {
       return filePath;
     }
-    return fileData.url;
+    return fileData.targetPath;
   }
 
   async dispose(): Promise<void> {
@@ -81,26 +136,22 @@ export class FileCache {
   }
 }
 
-async function createFileData(url: URL): Promise<FileData> {
-  // Handle the bundle file.
-  log(`FileCache got url: ${url.toString()}`);
-  const localhostedUrl = url.toString().replace(EMULATOR_LOCALHOST_ADDR, 'localhost');
-  log(`Converted to: ${localhostedUrl}`);
-  const fileResponse = await xfetch(localhostedUrl, {});
-  const basename = nuclideUri.basename(url.pathname);
-  const [fileText, filePath] = await Promise.all([
-    fileResponse.text(),
-    fsPromise.tempfile({prefix: basename, suffix: '.js'}),
-  ]);
-  await fsPromise.writeFile(filePath, fileText);
-  const fileSystemUrl = `file://${filePath}`;
-
-  const matches = SOURCE_MAP_REGEX.exec(fileText);
+async function getSourceMapFromDisk(bundle: string): Promise<void | string> {
+  const matches = SOURCE_MAP_REGEX.exec(bundle);
   if (matches == null) {
-    return {
-      filePath: fileSystemUrl,
-      url: url.toString(),
-    };
+    return undefined;
+  }
+  // Handle source maps for the bundle.
+  const sourceMapPath = matches[1];
+  const sourceMap = await fsPromise.readFile(sourceMapPath);
+  const base64SourceMap = new Buffer(sourceMap).toString('base64');
+  return `${SOURCE_MAP_PREFIX}${base64SourceMap}`;
+}
+
+async function getSourceMapFromUrl(url: URL, bundle: string): Promise<void | string> {
+  const matches = SOURCE_MAP_REGEX.exec(bundle);
+  if (matches == null) {
+    return undefined;
   }
 
   // Handle source maps for the bundle.
@@ -111,17 +162,13 @@ async function createFileData(url: URL): Promise<FileData> {
   );
   const sourceMap = await sourceMapResponse.text();
   const base64SourceMap = new Buffer(sourceMap).toString('base64');
-  return {
-    filePath: fileSystemUrl,
-    url: url.toString(),
-    sourceMapUrl: `${SOURCE_MAP_PREFIX}${base64SourceMap}`,
-  };
+  return `${SOURCE_MAP_PREFIX}${base64SourceMap}`;
 }
 
 function updateMessageObjWithFileData(
   obj: {params: {url: string, sourceMapURL?: string}},
   fileData: FileData,
 ): void {
-  obj.params.url = fileData.filePath;
+  obj.params.url = fileData.nuclidePath;
   obj.params.sourceMapURL = fileData.sourceMapUrl;
 }
