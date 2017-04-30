6b8dd656c64de71cb1c52aaa3cb722befb7a46fb
Christopher Chedeau <vjeux@fb.com>
2017-04-29 18:57:43 -0700

    Introduce <EmptyState> component
    Summary: This takes Interactive Smartlog css and makes it a standalone component
    
    Reviewed By: jgebhardt
    
    Differential Revision: D4974593
    
    fbshipit-source-id: 0310963040e8b83a6e2da67a13ede4157453b541

---
+++ pkg/nuclide-ui/EmptyState.js
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ *
+ * @flow
+ */
+
+import React from 'react';
+
+type Props = {
+  title: string,
+  message: string,
+};
+
+export class EmptyState extends React.Component {
+  props: Props;
+
+  render(): React.Element<any> {
+    return (
+      <div className="nuclide-ui-empty-state-container">
+        <div className="nuclide-ui-empty-state-message">
+          <h1>{this.props.title}</h1>
+          {this.props.message}
+        </div>
+      </div>
+    );
+  }
+}
