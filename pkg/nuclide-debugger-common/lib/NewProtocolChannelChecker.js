8912b37dbd6623daac71c6516a540d3c3d6ad083
Jeffrey Tan <jeffreytan@fb.com>
2017-06-01 09:37:27 -0700

    Remove Chrome dependency(23/N): Add toggle option to easily switch channel
    Summary: GK in Nuclide sucks in OSS. Use this approach instead.
    
    Reviewed By: ebluestein
    
    Differential Revision: D5160039
    
    fbshipit-source-id: 27c4627c6f2eefe1ec75cac200c115741fd98b40

--- pkg/nuclide-debugger-common/lib/NewProtocolChannelChecker.js
+++ pkg/nuclide-debugger-common/lib/NewProtocolChannelChecker.js
@@ -12,5 +12,8 @@
 import passesGK from '../../commons-node/passesGK';
 
 export function isNewProtocolChannelEnabled(): Promise<boolean> {
+  if (atom.config.get('nuclide.nuclide-debugger.forceNewChannel')) {
+    return Promise.resolve(true);
+  }
   return passesGK('nuclide_new_debugger_protocol_channel', 10 * 1000);
 }
