9cc9834b0734f4ad930cbd7b7fe5b1aba3623de5
Hanson Wang <hansonw@fb.com>
2017-06-08 16:07:20 -0700

    Bump modules/ to 0.1.5
    Summary:
    I had to struggle a bit to get this to publish correctly :O But it works now!
    
    - `prepublishOnly` seems bugged - it doesn't actually wait for the script to finish before publishing (but runs it anyway). Just use plain old `prepublish` for now.
    - Switch all modules to use the common prepublish script.
    - Make the prepublish script ignore existing flow files (for `atom-ide-ui/index-entry.js.flow`)
    
    Unfortunately I don't think anyone can really use the published flow types until we also start publishing our `flow-libs`.. so we should do that :)
    
    How to repro the publish:
    
    - `lerna publish --exact --skip-git --skip-npm` -> choose a version
    - commit & import & land
    - `lerna publish --exact --skip-git --repo-version <version>`
    Closes https://github.com/facebook/nuclide/pull/1179
    
    Reviewed By: wbinnssmith
    
    Differential Revision: D5189763
    
    Pulled By: hansonw
    
    fbshipit-source-id: 4081ab91302528a38fd07fec260dcb2709cb4aea

--- scripts/modules-prepublish.sh
+++ scripts/modules-prepublish.sh
@@ -14,8 +14,7 @@ CALLING_DIR="$(pwd -P)"
 echo "Module prepublish: making copies for flow (1 / 2)..."
 find "$CALLING_DIR" \
   -name '*.js' \
-  -not -path '*/spec*' |
-    while read -r filepath; do cp "$filepath" "$filepath.flow"; done
+  -not -path '*/spec*' -exec cp -n {} {}.flow \;
 
 echo "Module prepublish: compiling source (2 / 2) ..."
 "$OWN_DIR/release-transpile.js" --overwrite "$CALLING_DIR"
