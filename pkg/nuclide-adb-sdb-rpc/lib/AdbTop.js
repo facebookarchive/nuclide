b7508d7d5326119c89c75df3ae2d422e9fa5868c
Walter Erquinigo <wallace@fb.com>
2017-06-09 13:16:04 -0700

    Convert promises to observables - part 1
    Summary:
    Getting the android processes uses a lot of promises and some of these promises never resolve and are not automatically cancelled like normal observables, which leaves hanging adb processes.
    Using observables will solve this issue
    
    Reviewed By: sas
    
    Differential Revision: D5189283
    
    fbshipit-source-id: 92f1f9866456827ca635c7e653bb231c081c06ce

--- pkg/nuclide-adb-sdb-rpc/lib/AdbTop.js
+++ pkg/nuclide-adb-sdb-rpc/lib/AdbTop.js
@@ -18,11 +18,13 @@
 
 import type {Process} from './types';
 
-import {sleep} from 'nuclide-commons/promise';
 import {Adb} from './Adb';
 import {arrayCompact} from 'nuclide-commons/collection';
+import {Observable} from 'rxjs';
 
 type CPU_MEM = [number, number];
+
+const VALID_PROCESS_REGEX = new RegExp(/\d+\s()/);
 
 export class AdbTop {
   _adb: Adb;
@@ -35,9 +37,9 @@ export class AdbTop {
 
   async fetch(): Promise<Process[]> {
     const [processes, javaProcesses, cpuAndMemUsage] = await Promise.all([
-      this._getProcessList().catch(() => []),
+      this._getProcessList().toPromise().catch(() => []),
       this._adb.getJavaProcesses(this._device).catch(() => []),
-      this._getProcessAndMemoryUsage().catch(() => new Map()),
+      this._getProcessAndMemoryUsage().toPromise().catch(() => new Map()),
     ]);
     const javaPids = new Set(
       javaProcesses.map(javaProc => Number(javaProc.pid)),
@@ -69,84 +71,85 @@ export class AdbTop {
     );
   }
 
-  async _getProcessList(): Promise<Array<string>> {
-    return (await this._adb
+  _getProcessList(): Observable<Array<string>> {
+    return this._adb
       .runShortCommand(this._device, ['shell', 'ps'])
-      .toPromise()).split(/\n/);
+      .map(stdout => stdout.split(/\n/));
   }
 
-  async _getProcessAndMemoryUsage(): Promise<Map<number, CPU_MEM>> {
-    const [procTimePrev, cpuTimePrev] = await Promise.all([
-      this._getProcessesTime(),
-      this._getGlobalCPUTime(),
-    ]);
-    await sleep(500);
-    const [procTime, cpuTime] = await Promise.all([
-      this._getProcessesTime(),
-      this._getGlobalCPUTime(),
-    ]);
-
-    // pid => cpuUsage, memory usage
-    const cpuAndMemUsage = new Map();
-    const deltaCpu = cpuTime - cpuTimePrev;
-    procTime.forEach((p1, pid) => {
-      if (!procTimePrev.has(pid)) {
-        return;
-      }
-      const p0 = procTimePrev.get(pid);
-      if (p0 != null) {
-        const deltaProc = p1[0] - p0[0];
-        const memUsage = p1[1];
-        cpuAndMemUsage.set(pid, [deltaProc / deltaCpu * 100, memUsage]);
-      }
-    });
-    return cpuAndMemUsage;
+  _getProcessAndMemoryUsage(): Observable<Map<number, CPU_MEM>> {
+    return Observable.interval(500)
+      .startWith(0)
+      .switchMap(() => {
+        return Observable.forkJoin(
+          this._getProcessesTime(),
+          this._getGlobalCPUTime(),
+        );
+      })
+      .take(2)
+      .toArray()
+      .map(times => {
+        const [procTimePrev, cpuTimePrev] = times[0];
+        const [procTime, cpuTime] = times[1];
+        // pid => cpuUsage, memory usage
+        const cpuAndMemUsage = new Map();
+        const deltaCpu = cpuTime - cpuTimePrev;
+        procTime.forEach((p1, pid) => {
+          if (!procTimePrev.has(pid)) {
+            return;
+          }
+          const p0 = procTimePrev.get(pid);
+          if (p0 != null) {
+            const deltaProc = p1[0] - p0[0];
+            const memUsage = p1[1];
+            cpuAndMemUsage.set(pid, [deltaProc / deltaCpu * 100, memUsage]);
+          }
+        });
+        return cpuAndMemUsage;
+      });
   }
 
   /**
    * Returns a map: pid => utime + stime
    */
-  async _getProcessesTime(): Promise<Map<number, CPU_MEM>> {
-    const validProcessRegex = new RegExp(/\d+\s()/);
+  _getProcessesTime(): Observable<Map<number, CPU_MEM>> {
     // We look for the all the /proc/PID/stat files failing silently if the process dies as the
     // command runs.
-    const procTime = (await this._adb
+    return this._adb
       .runShortCommand(this._device, [
         'shell',
         'for file in /proc/[0-9]*/stat; do cat "$file" 2>/dev/null || true; done',
       ])
-      .toPromise())
-      .split(/\n/)
-      .filter(x => validProcessRegex.test(x));
-    return new Map(
-      procTime.map(x => {
-        const info = x.trim().split(/\s/);
-        return [
-          parseInt(info[0], 10),
-          [
-            parseInt(info[12], 10) + parseInt(info[13], 10), // stime + utime
-            parseInt(info[23], 10), // RSS
-          ],
-        ];
-      }),
-    );
+      .map(stdout => {
+        const lines = stdout
+          .split(/\n/)
+          .filter(line => VALID_PROCESS_REGEX.test(line));
+        return new Map(
+          lines.map(line => {
+            const info = line.trim().split(/\s/);
+            return [
+              parseInt(info[0], 10),
+              [
+                parseInt(info[12], 10) + parseInt(info[13], 10), // stime + utime
+                parseInt(info[23], 10), // RSS
+              ],
+            ];
+          }),
+        );
+      });
   }
 
-  async _getGlobalCPUTime(): Promise<number> {
-    return (await this._getGlobalProcessStat())
-      .split(/\s+/)
-      .slice(1, -2)
-      .reduce((acc, current) => {
-        const val = parseInt(current, 10);
-        return acc + val;
+  _getGlobalCPUTime(): Observable<number> {
+    return this._getGlobalProcessStat().map(stdout => {
+      return stdout.split(/\s+/).slice(1, -2).reduce((acc, current) => {
+        return acc + parseInt(current, 10);
       }, 0);
+    });
   }
 
-  async _getGlobalProcessStat(): Promise<string> {
-    return (await this._adb
+  _getGlobalProcessStat(): Observable<string> {
+    return this._adb
       .runShortCommand(this._device, ['shell', 'cat', '/proc/stat'])
-      .toPromise())
-      .split(/\n/)[0]
-      .trim();
+      .map(stdout => stdout.split(/\n/)[0].trim());
   }
 }
