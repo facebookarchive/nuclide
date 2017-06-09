df7365218caac59dc8176512faed45beab63362c
Jonas Gebhardt <jxg@fb.com>
2017-06-09 16:46:04 -0700

    Make SplitButtonDropdown forward className prop
    Summary: Also since I'm at it, cache props and sort them.
    
    Reviewed By: matthewwithanm
    
    Differential Revision: D5207354
    
    fbshipit-source-id: 0265508985c06b74f8a70ec9200ce14b3a29405e

--- pkg/nuclide-ui/SplitButtonDropdown.js
+++ pkg/nuclide-ui/SplitButtonDropdown.js
@@ -16,55 +16,64 @@ import React from 'react';
 import {Button, ButtonSizes} from 'nuclide-commons-ui/Button';
 import {ButtonGroup} from 'nuclide-commons-ui/ButtonGroup';
 import {Dropdown} from './Dropdown';
+import classnames from 'classnames';
 import electron from 'electron';
-
 const {remote} = electron;
 invariant(remote != null);
 
 type ButtonSize = 'EXTRA_SMALL' | 'SMALL' | 'LARGE';
 
 type Props = {
-  value: any,
   buttonComponent?: ReactClass<any>,
-  options: Array<Option>,
+  changeDisabled?: boolean,
+  className?: string,
+  confirmDisabled?: boolean,
   onChange?: (value: any) => mixed,
   onConfirm: (value: any) => mixed,
-  confirmDisabled?: boolean,
-  changeDisabled?: boolean,
+  options: Array<Option>,
   size?: ?ButtonSize,
+  value: any,
 };
 
 export class SplitButtonDropdown extends React.Component {
   props: Props;
 
   render(): React.Element<any> {
-    const selectedOption =
-      this._findSelectedOption(this.props.options) || this.props.options[0];
-
+    const {
+      buttonComponent,
+      changeDisabled,
+      className,
+      confirmDisabled,
+      onChange,
+      onConfirm,
+      options,
+      size,
+      value,
+    } = this.props;
+    const selectedOption = this._findSelectedOption(options) || options[0];
     invariant(selectedOption.type !== 'separator');
-
-    const ButtonComponent = this.props.buttonComponent || Button;
-
-    const dropdownOptions = this.props.options.map(option => ({
+    const ButtonComponent = buttonComponent || Button;
+    const dropdownOptions = options.map(option => ({
       ...option,
       selectedLabel: '',
     }));
 
     return (
-      <ButtonGroup className="nuclide-ui-split-button-dropdown">
+      <ButtonGroup
+        className={classnames(className, 'nuclide-ui-split-button-dropdown')}>
         <ButtonComponent
-          size={this.props.size == null ? undefined : this.props.size}
-          disabled={this.props.confirmDisabled === true}
+          size={size == null ? undefined : size}
+          disabled={confirmDisabled === true}
           icon={selectedOption.icon || undefined}
-          onClick={this.props.onConfirm}>
+          onClick={onConfirm}>
           {selectedOption.selectedLabel || selectedOption.label || ''}
         </ButtonComponent>
         <Dropdown
-          size={this._getDropdownSize(this.props.size)}
-          disabled={this.props.changeDisabled === true}
+          size={this._getDropdownSize(size)}
+          disabled={changeDisabled === true}
           options={dropdownOptions}
-          value={this.props.value}
-          onChange={this.props.onChange}
+          value={value}
+          onChange={onChange}
         />
       </ButtonGroup>
     );
