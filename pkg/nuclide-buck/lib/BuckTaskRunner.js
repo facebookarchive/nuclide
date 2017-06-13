c9c918c838ff02cf590bea1f3d5566f0d197d883
Eric Bluestein <ericblue@fb.com>
2017-06-12 18:52:45 -0700

    Band-aid fix for regression causing cxx debug button to be disabled in task runner
    Summary: enable the debug button for cxx_binary and cxx_test
    
    Reviewed By: hansonw
    
    Differential Revision: D5234594
    
    fbshipit-source-id: 835beb838dba1e849cd0b4bb21dd32cb692d5a70

--- pkg/nuclide-buck/lib/BuckTaskRunner.js
+++ pkg/nuclide-buck/lib/BuckTaskRunner.js
@@ -78,6 +78,9 @@ function shouldEnableTask(taskType: TaskType, ruleType: string): boolean {
       return true;
     case 'run':
       return ruleType.endsWith('binary');
+    case 'debug':
+      // TODO: Ericblue fix this
+      return ruleType.endsWith('binary') || ruleType.endsWith('test');
     default:
       return false;
   }
