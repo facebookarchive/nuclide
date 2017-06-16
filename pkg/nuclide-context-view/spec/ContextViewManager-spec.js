3e15421d895a5f9663a2449bcfad5581ed7a3e6d
Hanson Wang <hansonw@fb.com>
2017-06-15 18:28:30 -0700

    Fix flaky context view test
    Summary:
    `observePaneItemVisibility` appears to be quite unpredictable in tests after the 1.17 upgrade.
    Sometimes it ends up firing after `beforeEach` but before the actual test, invalidating the `show()` call.
    
    Instead, what we'll do is call `show()` after the test starts.
    
    Reviewed By: matthewwithanm
    
    Differential Revision: D5250575
    
    fbshipit-source-id: e12198a26b3f863ae0bb8cb198770a57b6689c4f

--- pkg/nuclide-context-view/spec/ContextViewManager-spec.js
+++ pkg/nuclide-context-view/spec/ContextViewManager-spec.js
@@ -24,8 +24,7 @@ const PROVIDER2_ID = 'context-provider-2';
 const PROVIDER2_TITLE = 'Provider Two';
 
 describe('ContextViewManager', () => {
-  let managerShowing: ContextViewManager; // Initialized as showing
-  let managerHidden: ContextViewManager; // Initialized as hidden
+  let manager: ContextViewManager;
   let disposables: CompositeDisposable;
   let provider1: ContextProvider;
   let provider2: ContextProvider;
@@ -45,9 +44,7 @@ describe('ContextViewManager', () => {
   beforeEach(() => {
     disposables = new CompositeDisposable();
 
-    managerShowing = new ContextViewManager();
-    managerShowing.show();
-    managerHidden = new ContextViewManager();
+    manager = new ContextViewManager();
     provider1 = {
       getElementFactory: elementFactory,
       id: PROVIDER1_ID,
@@ -69,8 +66,7 @@ describe('ContextViewManager', () => {
         return Promise.resolve(null);
       },
     };
-    disposables.add(managerShowing);
-    disposables.add(managerHidden);
+    disposables.add(manager);
   });
 
   afterEach(() => {
@@ -80,45 +76,45 @@ describe('ContextViewManager', () => {
   /** Registration/deregistration API */
 
   it('correctly registers a single context provider and rerenders', () => {
-    spyOn(managerShowing, '_render');
-    const registered = managerShowing.registerProvider(provider1);
+    spyOn(manager, '_render');
+    const registered = manager.registerProvider(provider1);
     expect(registered).toBe(true);
-    expect(managerShowing._contextProviders.length).toBe(1);
-    expect(managerShowing._render).toHaveBeenCalled();
+    expect(manager._contextProviders.length).toBe(1);
+    expect(manager._render).toHaveBeenCalled();
   });
   it('correctly registers multiple context provdiers and rerenders', () => {
-    spyOn(managerShowing, '_render');
-    const registered1 = managerShowing.registerProvider(provider1);
-    const registered2 = managerShowing.registerProvider(provider2);
+    spyOn(manager, '_render');
+    const registered1 = manager.registerProvider(provider1);
+    const registered2 = manager.registerProvider(provider2);
     expect(registered1).toBe(true);
     expect(registered2).toBe(true);
-    expect(managerShowing._contextProviders.length).toBe(2);
-    expect(managerShowing._render).toHaveBeenCalled();
+    expect(manager._contextProviders.length).toBe(2);
+    expect(manager._render).toHaveBeenCalled();
   });
   it('does not register a provider with an already existing ID', () => {
-    const registered1 = managerShowing.registerProvider(provider1);
-    const registeredAgain = managerShowing.registerProvider(provider1);
+    const registered1 = manager.registerProvider(provider1);
+    const registeredAgain = manager.registerProvider(provider1);
     expect(registered1).toBe(true);
     expect(registeredAgain).toBe(false); // Shouldn't re-register provider with same ID
-    expect(managerShowing._contextProviders.length).toBe(1);
+    expect(manager._contextProviders.length).toBe(1);
   });
   it('unregisters a provider and rerenders', () => {
-    spyOn(managerShowing, '_render');
-    managerShowing.registerProvider(provider1);
-    const unregistered = managerShowing.unregisterProvider(PROVIDER1_ID);
+    spyOn(manager, '_render');
+    manager.registerProvider(provider1);
+    const unregistered = manager.unregisterProvider(PROVIDER1_ID);
     expect(unregistered).toBe(true);
-    expect(managerShowing._contextProviders.length).toBe(0);
-    expect(managerShowing._render).toHaveBeenCalled();
+    expect(manager._contextProviders.length).toBe(0);
+    expect(manager._render).toHaveBeenCalled();
   });
   it('does not unregister a provider that has not been registered', () => {
-    spyOn(managerShowing, '_render');
-    const unregistered1 = managerShowing.unregisterProvider(PROVIDER1_ID);
+    spyOn(manager, '_render');
+    const unregistered1 = manager.unregisterProvider(PROVIDER1_ID);
     expect(unregistered1).toBe(false);
-    expect(managerShowing._contextProviders.length).toBe(0);
-    managerShowing.registerProvider(provider1);
-    const unregistered2 = managerShowing.unregisterProvider(PROVIDER2_ID);
+    expect(manager._contextProviders.length).toBe(0);
+    manager.registerProvider(provider1);
+    const unregistered2 = manager.unregisterProvider(PROVIDER2_ID);
     expect(unregistered2).toBe(false);
-    expect(managerShowing._contextProviders.length).toBe(1);
+    expect(manager._contextProviders.length).toBe(1);
   });
   it('orders providers based on priority', () => {
     const provider3 = {
@@ -142,78 +138,85 @@ describe('ContextViewManager', () => {
     featureConfig.set(provider3.id.concat('.priority'), provider3Priority);
     featureConfig.set(provider4.id.concat('.priority'), provider4Priority);
     featureConfig.set(provider5.id.concat('.priority'), provider5Priority);
-    managerHidden.registerProvider(provider2);
-    managerHidden.registerProvider(provider1);
-    expect(managerHidden._contextProviders[0].id).toBe(PROVIDER1_ID);
-    expect(managerHidden._contextProviders[1].id).toBe(PROVIDER2_ID);
+    manager.registerProvider(provider2);
+    manager.registerProvider(provider1);
+    expect(manager._contextProviders[0].id).toBe(PROVIDER1_ID);
+    expect(manager._contextProviders[1].id).toBe(PROVIDER2_ID);
+
+    manager = new ContextViewManager();
     // Insert order: 4, 5, 1, 3, 2
     // Provider list should end up as [1, 2, 3, 4, 5]
-    managerShowing.registerProvider(provider4);
-    managerShowing.registerProvider(provider5);
-    managerShowing.registerProvider(provider1);
-    managerShowing.registerProvider(provider3);
-    managerShowing.registerProvider(provider2);
-    expect(managerShowing._contextProviders[0].id).toBe(PROVIDER1_ID);
-    expect(managerShowing._contextProviders[1].id).toBe(PROVIDER2_ID);
-    expect(managerShowing._contextProviders[2].id).toBe('3');
-    expect(managerShowing._contextProviders[3].id).toBe('4');
-    expect(managerShowing._contextProviders[4].id).toBe('5');
+    manager.registerProvider(provider4);
+    manager.registerProvider(provider5);
+    manager.registerProvider(provider1);
+    manager.registerProvider(provider3);
+    manager.registerProvider(provider2);
+    expect(manager._contextProviders[0].id).toBe(PROVIDER1_ID);
+    expect(manager._contextProviders[1].id).toBe(PROVIDER2_ID);
+    expect(manager._contextProviders[2].id).toBe('3');
+    expect(manager._contextProviders[3].id).toBe('4');
+    expect(manager._contextProviders[4].id).toBe('5');
   });
 
   /** Actions affecting definition service subscription */
   it('consumes the definition service when showing', () => {
-    spyOn(managerShowing, 'updateSubscription').andCallThrough();
-    spyOn(managerShowing, '_render').andCallThrough();
-    spyOn(managerShowing, '_renderProviders');
-    managerShowing.consumeDefinitionProvider(defProvider);
-    expect(managerShowing.updateSubscription).toHaveBeenCalled();
-    expect(managerShowing._defServiceSubscription).toBeTruthy();
-    expect(managerShowing._render).toHaveBeenCalled();
-    expect(managerShowing._renderProviders).toHaveBeenCalled();
+    manager.show();
+    spyOn(manager, 'updateSubscription').andCallThrough();
+    spyOn(manager, '_render').andCallThrough();
+    spyOn(manager, '_renderProviders');
+    manager.consumeDefinitionProvider(defProvider);
+    expect(manager.updateSubscription).toHaveBeenCalled();
+    expect(manager._defServiceSubscription).toBeTruthy();
+    expect(manager._render).toHaveBeenCalled();
+    expect(manager._renderProviders).toHaveBeenCalled();
   });
   it('consumes the definition service when hidden', () => {
-    spyOn(managerHidden, 'updateSubscription').andCallThrough();
-    spyOn(managerHidden, '_render').andCallThrough();
-    spyOn(managerHidden, '_renderProviders');
-    spyOn(managerHidden, '_disposeView');
-    expect(managerHidden._defServiceSubscription).toBeNull();
-    managerHidden.consumeDefinitionProvider(defProvider);
-    expect(managerHidden.updateSubscription).toHaveBeenCalled();
-    expect(managerHidden._defServiceSubscription).toBeNull();
-    expect(managerHidden._render).toHaveBeenCalled();
-    expect(managerHidden._disposeView).toHaveBeenCalled();
-    expect(managerHidden._renderProviders).not.toHaveBeenCalled();
+    spyOn(manager, 'updateSubscription').andCallThrough();
+    spyOn(manager, '_render').andCallThrough();
+    spyOn(manager, '_renderProviders');
+    spyOn(manager, '_disposeView');
+    expect(manager._defServiceSubscription).toBeNull();
+    manager.consumeDefinitionProvider(defProvider);
+    expect(manager.updateSubscription).toHaveBeenCalled();
+    expect(manager._defServiceSubscription).toBeNull();
+    expect(manager._render).toHaveBeenCalled();
+    expect(manager._disposeView).toHaveBeenCalled();
+    expect(manager._renderProviders).not.toHaveBeenCalled();
   });
-  it('shows and hides correctly', () => {
-    managerShowing.consumeDefinitionProvider(defProvider);
-    managerHidden.consumeDefinitionProvider(defProvider);
-    spyOn(managerShowing, '_render').andCallThrough();
-    spyOn(managerShowing, '_disposeView');
-    spyOn(managerShowing, 'updateSubscription').andCallThrough();
-    spyOn(managerHidden, '_render').andCallThrough();
-    spyOn(managerHidden, '_disposeView');
-    spyOn(managerHidden, 'updateSubscription').andCallThrough();
-    managerShowing.hide();
-    expect(managerShowing._isVisible).toBe(false);
-    expect(managerShowing._render).toHaveBeenCalled();
-    expect(managerShowing._disposeView).toHaveBeenCalled();
-    expect(managerShowing.updateSubscription).toHaveBeenCalled();
-    expect(managerShowing._defServiceSubscription).toBeNull();
-    managerHidden.show();
-    expect(managerHidden._isVisible).toBe(true);
-    expect(managerHidden._render).toHaveBeenCalled();
-    expect(managerHidden.updateSubscription).toHaveBeenCalled();
+  it('hides correctly', () => {
+    manager.show();
+    manager.consumeDefinitionProvider(defProvider);
+    spyOn(manager, '_render').andCallThrough();
+    spyOn(manager, '_disposeView');
+    spyOn(manager, 'updateSubscription').andCallThrough();
+    manager.hide();
+    expect(manager._isVisible).toBe(false);
+    expect(manager._render).toHaveBeenCalled();
+    expect(manager._disposeView).toHaveBeenCalled();
+    expect(manager.updateSubscription).toHaveBeenCalled();
+    expect(manager._defServiceSubscription).toBeNull();
+  });
+  it('shows correctly', () => {
+    manager.consumeDefinitionProvider(defProvider);
+    spyOn(manager, '_render').andCallThrough();
+    spyOn(manager, '_disposeView');
+    spyOn(manager, 'updateSubscription').andCallThrough();
+    manager.show();
+    expect(manager._isVisible).toBe(true);
+    expect(manager._render).toHaveBeenCalled();
+    expect(manager.updateSubscription).toHaveBeenCalled();
   });
   it('disposes correctly', () => {
-    managerShowing.consumeDefinitionProvider(defProvider);
+    manager.show();
+    manager.consumeDefinitionProvider(defProvider);
     // i.e. the subscription is unsubscribed if not null
     invariant(
-      managerShowing._defServiceSubscription != null,
+      manager._defServiceSubscription != null,
       'Subscription should exist if panel is visible and def. service consumed',
     );
-    const subscription = managerShowing._defServiceSubscription;
+    const subscription = manager._defServiceSubscription;
     spyOn(subscription, 'unsubscribe');
-    managerShowing.dispose();
+    manager.dispose();
     expect(subscription.unsubscribe).toHaveBeenCalled();
   });
 });
