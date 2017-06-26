3d9d9073320113413bbd1e74d637ae698d21b679
Hanson Wang <hansonw@fb.com>
2017-06-26 14:53:47 -0700

    Add a deprecation mechanism for Atom commands
    Summary:
    Right now we can't really rename commands - what if someone has it in their keymap?
    
    This introduces a small migration package (I called it "nuclide-deprecation-cop" like the Atom one. I'd also like to use this to migrate the atom-ide CSS classnames, in case external packages are using those, and also the atom-ide service names).
    
    Right now this:
    
    - Prompts you to migrate your keymap file automatically. (Should we even prompt?)
    - If any migrated command is manually triggered (old menu, context menu) we'll pop up a warning.
    
    Note that we don't have a CSON parser available from Nuclide, so I implemented the keymap replacer by just searching for quoted strings. I think this should work for JSON/CSON just fine.
    
    Reviewed By: wbinnssmith
    
    Differential Revision: D5314545
    
    fbshipit-source-id: 2774f9875c220350418e656a797ff80b04bad7e5

---
+++ pkg/nuclide-deprecation-cop/lib/main.js
@@ -0,0 +1,89 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ *
+ * @flow
+ * @format
+ */
+
+import {getLogger} from 'log4js';
+import createPackage from 'nuclide-commons-atom/createPackage';
+import UniversalDisposable from 'nuclide-commons/UniversalDisposable';
+import {track} from '../../nuclide-analytics';
+import updateKeymap from './updateKeymap';
+
+/**
+ * Put deprecated commands with their new equivalents here.
+ * This will:
+ * - automatically warn and forward direct dispatches of the old command
+ * - prompt to automatically update the user keybindings
+ */
+const DEPRECATED_COMMANDS = {
+  'nuclide-code-format:format-code': 'code-format:format-code',
+  'nuclide-datatip:copy-to-clipboard': 'datatip:copy-to-clipboard',
+  'nuclide-datatip:toggle': 'datatip:toggle',
+  'nuclide-diagnostics-ui:fix-all-in-current-file': 'diagnostics:fix-all-in-current-file',
+  'nuclide-diagnostics-ui:go-to-first-diagnostic': 'diagnostics:go-to-first-diagnostic',
+  'nuclide-diagnostics-ui:go-to-last-diagnostic': 'diagnostics:go-to-last-diagnostic',
+  'nuclide-diagnostics-ui:go-to-next-diagnostic-trace': 'diagnostics:go-to-next-diagnostic-trace',
+  'nuclide-diagnostics-ui:go-to-next-diagnostic': 'diagnostics:go-to-next-diagnostic',
+  'nuclide-diagnostics-ui:go-to-previous-diagnostic-trace': 'diagnostics:go-to-previous-diagnostic-trace',
+  'nuclide-diagnostics-ui:go-to-previous-diagnostic': 'diagnostics:go-to-previous-diagnostic',
+  'nuclide-diagnostics-ui:open-all-files-with-errors': 'diagnostics:open-all-files-with-errors',
+  'nuclide-diagnostics-ui:toggle-table': 'diagnostics:toggle-table',
+  'nuclide-find-references:activate': 'find-references:activate',
+  'nuclide-outline-view:toggle': 'outline-view:toggle',
+};
+
+class Activation {
+  _disposables: UniversalDisposable;
+  _warnedCommands: Set<string>;
+
+  constructor() {
+    this._warnedCommands = new Set();
+    this._disposables = new UniversalDisposable(this._deprecateCommands());
+
+    // $FlowIgnore: private API
+    const keymapPath = atom.keymaps.getUserKeymapPath();
+    updateKeymap(keymapPath, DEPRECATED_COMMANDS).catch(err => {
+      // Nonexistent keymaps are normal.
+      if (err.code !== 'ENOENT') {
+        getLogger('nuclide-deprecation-cop').error(
+          'Error updating user keymap:',
+          err,
+        );
+      }
+    });
+  }
+
+  _deprecateCommands(): IDisposable {
+    // Catch any direct invocations of the commands (context menu, dispatch).
+    return atom.commands.onWillDispatch((event: any) => {
+      const command = event.type;
+      if (!DEPRECATED_COMMANDS.hasOwnProperty(command)) {
+        return;
+      }
+      const newCommand = DEPRECATED_COMMANDS[command];
+      if (!this._warnedCommands.has(command)) {
+        track('deprecated-command-dispatched', {command});
+        atom.notifications.addWarning('Nuclide: Deprecated Command', {
+          icon: 'nuclicon-nuclide',
+          description: `The command \`${command}\` has been deprecated.\n` +
+            `Please use the new command \`${newCommand}\`.`,
+          dismissable: true,
+        });
+        this._warnedCommands.add(command);
+      }
+      atom.commands.dispatch(event.target, newCommand, event.detail);
+    });
+  }
+
+  dispose(): void {
+    this._disposables.dispose();
+  }
+}
+
+createPackage(module.exports, Activation);
---
+++ pkg/nuclide-deprecation-cop/lib/updateKeymap.js
@@ -0,0 +1,72 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ *
+ * @flow
+ * @format
+ */
+
+import escapeRegExp from 'escape-string-regexp';
+import fsPromise from 'nuclide-commons/fsPromise';
+import {track} from '../../nuclide-analytics';
+
+function quoteRegExp(str: string): string {
+  const boundary = '([\'"])';
+  return `${boundary}(${str})${boundary}`;
+}
+
+export default (async function updateKeymap(
+  keymapPath: string,
+  deprecatedCommands: {[command: string]: string},
+): Promise<void> {
+  const keymapFile = await fsPromise.readFile(keymapPath, 'utf8');
+  // Search for all deprecated commands (whole words, escaped).
+  const mergedRegExp = Object.keys(deprecatedCommands)
+    .map(escapeRegExp)
+    .join('|');
+  const matches = keymapFile.match(new RegExp(quoteRegExp(mergedRegExp), 'g'));
+  if (matches != null) {
+    // Format as a list.
+    const matchesSet = new Set(matches.map(m => m.substr(1, m.length - 2)));
+    const matchesArray = Array.from(matchesSet);
+    const matchesList = matchesArray
+      .map(command => `- \`${command}\``)
+      .join('\n');
+    return new Promise(resolve => {
+      const notification = atom.notifications.addInfo(
+        'Nuclide: Deprecated Commands',
+        {
+          icon: 'nuclicon-nuclide',
+          description: 'Your keymap contains the following deprecated commands:' +
+            `\n\n${matchesList}\n\n` +
+            'Would you like to update your keymap?',
+          dismissable: true,
+          buttons: [
+            {
+              text: 'Update Keymap',
+              className: 'icon icon-keyboard',
+              onDidClick: async () => {
+                track('deprecated-command-replaced', {commands: matchesArray});
+                const replaced = matchesArray.reduce(
+                  (str, match) =>
+                    str.replace(
+                      new RegExp(quoteRegExp(escapeRegExp(match)), 'g'),
+                      `$1${deprecatedCommands[match]}$3`,
+                    ),
+                  keymapFile,
+                );
+                await fsPromise.writeFile(keymapPath, replaced);
+                atom.notifications.addSuccess('Keymap successfully updated!');
+                notification.dismiss();
+                resolve();
+              },
+            },
+          ],
+        },
+      );
+    });
+  }
+});
