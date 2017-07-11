ca630e8c63a65ea0aa1f3c083d85adfe428014c0
Aaditya Sriram <asriram@fb.com>
2017-07-11 09:48:02 -0700

    Prevent success notification if flag is passed to not show it
    Summary: Earlier only failure notifications were being hidden if the flag was passed, this change ensures that success notifications are hidden too.
    
    Reviewed By: jgebhardt
    
    Differential Revision: D5394544
    
    fbshipit-source-id: 2d664d44892267bf40644b942b08e89da97bac69

--- pkg/nuclide-console-base/lib/main.js
+++ pkg/nuclide-console-base/lib/main.js
@@ -51,9 +51,11 @@ export function pipeProcessMessagesToConsole(
           text: `${processName} completed successfully`,
           level: 'success',
         });
-        atom.notifications.addSuccess('Operation completed successfully', {
-          detail: `${processName} finished`,
-        });
+        if (showNotificationOnCompletion) {
+          atom.notifications.addSuccess('Operation completed successfully', {
+            detail: `${processName} finished`,
+          });
+        }
       } else {
         progressUpdates.next({
           text: `${processName} exited with non zero code`,
