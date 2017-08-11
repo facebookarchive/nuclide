e9ae88f70212112cf980e5beda056fec84edc0fb
Jordan Eldredge <jeldredge@fb.com>
2017-08-11 14:33:22 -0700

    Add ClickOutsideBoundary component
    Summary: Add a component which can respond to external clicks.
    
    Reviewed By: matthewwithanm
    
    Differential Revision: D5600903
    
    fbshipit-source-id: 8311838a5d3287e3d17dfb78cae3ef01eb9cbb36

---
+++ modules/nuclide-commons-ui/ClickOutsideBoundary.js
@@ -0,0 +1,75 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @flow
+ * @format
+ */
+
+import React from 'react';
+import {findDOMNode} from 'react-dom';
+
+type Props = {
+  onClickOutside: () => void,
+  children: ?React.Element<any>,
+};
+
+export default class ClickOutsideBoundary extends React.Component {
+  _lastInternalEvent: ?MouseEvent;
+  _node: null | Element | Text;
+
+  constructor(props: Props) {
+    super(props);
+    this._lastInternalEvent = null;
+    this._node = null;
+  }
+
+  componentDidMount() {
+    const node = (this._node = findDOMNode(this));
+    if (node == null) {
+      return;
+    }
+    window.document.addEventListener('click', this._handleDocumentClick);
+    // We use an actual DOM node (via refs) because React does not gaurnetee
+    // any particular event ordering between synthentic events and native
+    // events, and we requrie that the internal event fire before the global event.
+    // https://discuss.reactjs.org/t/ordering-of-native-and-react-events/829/2
+    node.addEventListener('click', this._handleInternalClick);
+  }
+
+  _handleDocumentClick = (e: MouseEvent) => {
+    // A more straight-forward approach would be to use
+    // `this._node.contains(e.target)`, however that fails in the edge case were
+    // some other event handler causes the target to be removed from the DOM
+    // before the event reaches the document root. So instead, we use this
+    // reference comparison approach which works for all cases where an event
+    // passed trough the boundary node, and makes it all the way to the document
+    // root.
+    if (e !== this._lastInternalEvent) {
+      if (this.props.onClickOutside != null) {
+        this.props.onClickOutside();
+      }
+    }
+    this._lastInternalEvent = null;
+  };
+
+  _handleInternalClick = (e: MouseEvent) => {
+    this._lastInternalEvent = e;
+  };
+
+  componentWillUnmount() {
+    window.document.removeEventListener('click', this._handleDocumentClick);
+    if (this._node != null) {
+      this._node.removeEventListener('click', this._handleInternalClick);
+    }
+  }
+
+  render() {
+    const {onClickOutside, ...passThroughProps} = this.props;
+    return <div {...passThroughProps} />;
+  }
+}
---
+++ modules/nuclide-commons-ui/spec/ClickOutsideBoundry-spec.js
@@ -0,0 +1,131 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @flow
+ * @format
+ */
+
+import React from 'react';
+import invariant from 'assert';
+import {render} from 'react-dom';
+import ClickOutsideBoundary from '../ClickOutsideBoundary';
+
+const clickId = (id: string) => {
+  const node = document.getElementById(id);
+  invariant(node != null, `node ${id} should be present in the DOM`);
+  node.click();
+};
+
+// A component which removes itself from the DOM when clicked.
+class ClickAway extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {visible: true};
+  }
+
+  handleClick = () => {
+    this.setState({visible: false});
+  };
+
+  render() {
+    return this.state.visible
+      ? <span {...this.props} onClick={this.handleClick}>
+          Click to dismiss
+        </span>
+      : null;
+  }
+}
+
+describe('ClickOutsideBoundary - onClickOutside handler', () => {
+  let app;
+  let onClickOutside;
+
+  beforeEach(() => {
+    app = document.createElement('div');
+    window.document.body.appendChild(app);
+    onClickOutside = jasmine.createSpy('onClickOutside');
+  });
+
+  afterEach(() => {
+    window.document.body.removeChild(app);
+  });
+
+  describe('_is_ called for an external click', () => {
+    it('on a parent', () => {
+      render(
+        <div id="container">
+          <ClickOutsideBoundary onClickOutside={onClickOutside}>
+            <div id="target" />
+          </ClickOutsideBoundary>
+        </div>,
+        app,
+      );
+      clickId('container');
+      expect(onClickOutside).toHaveBeenCalled();
+    });
+
+    it('on a sibling', () => {
+      render(
+        <div>
+          <ClickOutsideBoundary onClickOutside={onClickOutside}>
+            <div id="target" />
+          </ClickOutsideBoundary>
+          <div id="sibling" />
+        </div>,
+        app,
+      );
+      clickId('sibling');
+      expect(onClickOutside).toHaveBeenCalled();
+    });
+
+    it('on a sibling that dissapears after render', () => {
+      // This would fail if we tool the approach of ignoring all
+      // clicks on elements that were not in the DOM.
+      render(
+        <div>
+          <ClickOutsideBoundary onClickOutside={onClickOutside}>
+            <div id="target" />
+          </ClickOutsideBoundary>
+          <ClickAway id="click-away" />
+        </div>,
+        app,
+      );
+      clickId('click-away');
+      expect(onClickOutside).toHaveBeenCalled();
+    });
+  });
+
+  describe('is _not_ called for an internal click', () => {
+    it('under normal conditions', () => {
+      render(
+        <ClickOutsideBoundary onClickOutside={onClickOutside}>
+          <div id="target" />
+        </ClickOutsideBoundary>,
+        app,
+      );
+      clickId('target');
+      expect(onClickOutside).not.toHaveBeenCalled();
+    });
+
+    it('when the target leaves the DOM before the event reaches window.document', () => {
+      // A simple approach: `this.rootNode.contains(e.target)` would fail this test.
+      const onClickInside = jasmine.createSpy('onClickInside');
+      render(
+        <ClickOutsideBoundary onClickOutside={onClickOutside}>
+          <div onClick={onClickInside}>
+            <ClickAway id="click-away" />
+          </div>
+        </ClickOutsideBoundary>,
+        app,
+      );
+      clickId('click-away');
+      expect(onClickInside).toHaveBeenCalled();
+      expect(onClickOutside).not.toHaveBeenCalled();
+    });
+  });
+});
