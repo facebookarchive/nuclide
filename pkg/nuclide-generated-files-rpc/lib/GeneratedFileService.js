5cdb7a358efbe36f62e5099edffe9617b9852c90
John Armstrong <drmathochist@fb.com>
2017-09-06 12:41:43 -0700

    Adding multiple-file scan
    Summary: Rather than grep each file in a directory individually, scan all unchecked files at once.
    
    Reviewed By: matthewwithanm
    
    Differential Revision: D5756713
    
    fbshipit-source-id: 33f53dffa7e09e09b3020af8b406de6445180246

--- pkg/nuclide-generated-files-rpc/lib/GeneratedFileService.js
+++ pkg/nuclide-generated-files-rpc/lib/GeneratedFileService.js
@@ -15,7 +15,11 @@ import type {LRUCache} from 'lru-cache';
 import nuclideUri from 'nuclide-commons/nuclideUri';
 import LRU from 'lru-cache';
 import {runCommand} from 'nuclide-commons/process';
+import fsPromise from 'nuclide-commons/fsPromise';
 import {config} from './config';
+
+// the first group will be greedy, so best not to use ':' in generated file tags
+const GREP_PARSE_PATTERN = /^(.*):(.*)$/;
 
 export type GeneratedFileType = 'manual' | 'partial' | 'generated';
 
@@ -36,44 +40,94 @@ export async function getGeneratedFileType(
     return 'generated';
   }
 
-  const [generated, partiallyGenerated] = await Promise.all([
-    tagged(filePath, config.generatedTag),
-    tagged(filePath, config.partialGeneratedTag),
-  ]);
+  const dirPath = nuclideUri.dirname(filePath);
+  const filename = nuclideUri.basename(filePath);
+  const fileTags = await findTaggedFiles(dirPath, [filename]);
 
-  if (generated) {
-    cache.set(filePath, 'generated');
-    return 'generated';
+  const tag = fileTags.get(filename);
+  if (tag == null) {
+    cache.set(filePath, 'manual');
+    return 'manual';
   }
 
-  if (partiallyGenerated) {
-    cache.set(filePath, 'partial');
-    return 'partial';
+  cache.set(filePath, tag);
+  return tag;
+}
+
+export async function getGeneratedFileTypes(
+  dirPath: NuclideUri,
+): Promise<Map<NuclideUri, GeneratedFileType>> {
+  const files = await fsPromise.readdir(dirPath);
+  const fileTypes: Map<NuclideUri, GeneratedFileType> = new Map();
+  const uncheckedFiles = [];
+  for (const file of files) {
+    const filePath = nuclideUri.join(dirPath, file);
+    const cachedType = cache.get(filePath);
+    if (cachedType != null) {
+      fileTypes.set(filePath, cachedType);
+    } else {
+      uncheckedFiles.push(file);
+    }
   }
 
-  cache.set(filePath, 'manual');
-  return 'manual';
+  if (uncheckedFiles.length === 0) {
+    return fileTypes;
+  }
+
+  const fileTags = await findTaggedFiles(dirPath, uncheckedFiles);
+
+  for (const file of uncheckedFiles) {
+    const filePath = nuclideUri.join(dirPath, file);
+    const tag = fileTags.get(file);
+    if (tag == null) {
+      cache.set(filePath, 'manual');
+      fileTypes.set(filePath, 'manual');
+    } else {
+      cache.set(filePath, tag);
+      fileTypes.set(filePath, tag);
+    }
+  }
+
+  return fileTypes;
 }
 
 // 1000 entries should allow for a good number of open directories
 const cache: LRUCache<NuclideUri, GeneratedFileType> = new LRU({max: 1000});
 
-function tagged(filePath: NuclideUri, tag: ?string): Promise<boolean> {
-  if (tag == null) {
-    return Promise.resolve(false);
-  }
+function findTaggedFiles(
+  dirPath: NuclideUri,
+  filenames: Array<string>,
+): Promise<Map<string, GeneratedFileType>> {
   const command = 'grep';
-  const args = [tag, nuclideUri.basename(filePath)];
+  const pattern = config.generatedTag + '\\|' + config.partialGeneratedTag;
+  const filesToGrep = filenames.length === 0 ? ['*'] : filenames;
+  const args = ['-HId', 'skip', pattern, ...filesToGrep];
   const options = {
-    cwd: nuclideUri.dirname(filePath),
-    // Grep returns exitCode 1 for empty results,
-    // so we need to say that's not an error
+    cwd: dirPath,
     isExitError: ({exitCode, signal}) => {
       return signal != null && (exitCode == null || exitCode > 1);
     },
   };
   return runCommand(command, args, options)
-    .map(stdout => stdout.length > 0)
+    .map(stdout => {
+      const fileTags: Map<string, GeneratedFileType> = new Map();
+      for (const line of stdout.split('\n')) {
+        const match = line.match(GREP_PARSE_PATTERN);
+        if (match != null && match.length === 3) {
+          const filename = match[1];
+          const tag = match[2];
+          if (tag === config.generatedTag) {
+            fileTags.set(filename, 'generated');
+          } else if (
+            tag === config.partialGeneratedTag &&
+            fileTags.get(filename) !== 'generated'
+          ) {
+            fileTags.set(filename, 'partial');
+          }
+        }
+      }
+      return fileTags;
+    })
     .toPromise();
 }
 
