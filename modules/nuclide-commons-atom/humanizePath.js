d33887258a9325181cb938d8adf51d74d579fd48
Matthew Dapena-Tretter <matthewwithanm@fb.com>
2017-09-12 14:42:18 -0700

    Add `humanizePath()` utility
    Summary: A utility for preparing paths for display.
    
    Reviewed By: hansonw
    
    Differential Revision: D5800119
    
    fbshipit-source-id: 88bfd2319cbc7bd3e515b23e5f07c6925bc0785a

---
+++ modules/nuclide-commons-atom/humanizePath.js
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @flow
+ * @format
+ */
+
+import type {NuclideUri} from 'nuclide-commons/nuclideUri';
+
+import idx from 'idx';
+import nuclideUri from 'nuclide-commons/nuclideUri';
+
+/**
+ * Format a path for display. After the path is humanized, it should no longer be treated like a
+ * parsable, navigable path; it's just for display.
+ *
+ * Note that this (intentionally) provides different results based on the projects currently open in
+ * Atom. If you have multiple directories open, the result will be prefixed with one of their names.
+ * If you only have one, it won't.
+ */
+export default function humanizePath(
+  path: NuclideUri,
+  options: ?{
+    isDirectory?: boolean,
+    rootPaths?: Array<NuclideUri>,
+  },
+): string {
+  const isDirectory = idx(options, _ => _.isDirectory);
+  const rootPaths =
+    idx(options, _ => _.rootPaths) ||
+    atom.project.getDirectories().map(dir => dir.getPath());
+  const normalized = normalizePath(path, isDirectory);
+  let resolved;
+  for (const rootPath of rootPaths) {
+    const normalizedDir = nuclideUri.normalizeDir(rootPath);
+    if (nuclideUri.contains(normalizedDir, normalized)) {
+      resolved = normalized.substr(normalizedDir.length);
+      const rootName = nuclideUri.basename(normalizedDir);
+      // If the path is a root or there's more than one root, include the root's name.
+      if (normalized === normalizedDir) {
+        return nuclideUri.normalizeDir(rootName);
+      }
+      if (rootPaths.length > 1) {
+        return nuclideUri.join(rootName, resolved);
+      }
+      return resolved;
+    }
+  }
+
+  // It's not in one of the project directories so return the full (normalized) path.
+  return normalized;
+}
+
+function normalizePath(path: NuclideUri, isDirectory_: ?boolean): NuclideUri {
+  const isDirectory = isDirectory_ == null ? path.endsWith('/') : isDirectory_;
+  return isDirectory
+    ? nuclideUri.normalizeDir(path)
+    : nuclideUri.normalize(path);
+}
---
+++ modules/nuclide-commons-atom/spec/humanizePath-spec.js
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @flow
+ * @format
+ */
+
+import humanizePath from '../humanizePath';
+
+describe('humanizePath', () => {
+  it("includes the root name if there's more than one", () => {
+    expect(humanizePath('/a/b/c', {rootPaths: ['/a', '/d']})).toBe('a/b/c');
+  });
+
+  it("doesn't care if the rootPaths have trailing slashes", () => {
+    // Atom hasn't always been consistent about this.
+    expect(humanizePath('/a/b/c', {rootPaths: ['/a/', '/d/']})).toBe('a/b/c');
+  });
+
+  it("doesn't include the root name if there's only one", () => {
+    expect(humanizePath('/a/b/c', {rootPaths: ['/a']})).toBe('b/c');
+  });
+
+  it("returns the root name if it's a root path", () => {
+    expect(humanizePath('/a/', {rootPaths: ['/a', '/d']})).toBe('a/');
+  });
+
+  it("returns the absolute path if the file isn't in an open project root", () => {
+    expect(humanizePath('/a/b/c', {rootPaths: []})).toBe('/a/b/c');
+    expect(humanizePath('/a/b/c', {rootPaths: ['/d']})).toBe('/a/b/c');
+  });
+
+  it('doesn\'t use ".." if the file is above the root', () => {
+    expect(humanizePath('/a/b/c', {rootPaths: ['/a/b/c/d']})).toBe('/a/b/c');
+  });
+
+  it("includes a trailing slash if you say it's a directory", () => {
+    expect(humanizePath('/a/b/c', {isDirectory: true, rootPaths: []})).toBe(
+      '/a/b/c/',
+    );
+  });
+
+  it('normalizes', () => {
+    expect(humanizePath('/a/b//c//', {rootPaths: []})).toBe('/a/b/c/');
+  });
+});
