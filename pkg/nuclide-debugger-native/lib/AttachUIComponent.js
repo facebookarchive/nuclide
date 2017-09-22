dec85dac17f725e57c0de9215427fa47ba52688f
Sam Pepose <sampepose@fb.com>
2017-09-22 12:17:52 -0700

    Automatically select only row in filtered native debug attach dialog
    Summary: If the user filters the process list to a single process, automatically select that row in the table.
    
    Reviewed By: ebluestein
    
    Differential Revision: D5891564
    
    fbshipit-source-id: 4a4e8c1204f00dec4800fafa6087f1e3c1d44cb5

--- pkg/nuclide-debugger-native/lib/AttachUIComponent.js
+++ pkg/nuclide-debugger-native/lib/AttachUIComponent.js
@@ -94,6 +94,25 @@ function getCompareFunction(
   return () => 0;
 }
 
+function filterTargetInfos(
+  attachTargetInfos: Array<AttachTargetInfo>,
+  filterText: string,
+) {
+  // Show all results if invalid regex
+  let filterRegex;
+  try {
+    filterRegex = new RegExp(filterText, 'i');
+  } catch (e) {
+    filterRegex = new RegExp('.*', 'i');
+  }
+  return attachTargetInfos.filter(
+    item =>
+      filterRegex.test(item.name) ||
+      filterRegex.test(item.pid.toString()) ||
+      filterRegex.test(item.commandName),
+  );
+}
+
 export class AttachUIComponent extends React.Component<PropsType, StateType> {
   props: PropsType;
   state: StateType;
@@ -185,6 +204,16 @@ export class AttachUIComponent extends React.Component<PropsType, StateType> {
       );
     }
 
+    const filteredAttachTargetInfos = filterTargetInfos(
+      this.state.attachTargetInfos,
+      filterText || this.state.filterText,
+    );
+
+    // Select only option if filtered to one result
+    if (filteredAttachTargetInfos.length === 1) {
+      newSelectedTarget = filteredAttachTargetInfos[0];
+    }
+
     if (newSelectedTarget == null) {
       newSelectedTarget =
         this.state.selectedAttachTarget == null
@@ -216,18 +245,11 @@ export class AttachUIComponent extends React.Component<PropsType, StateType> {
   };
 
   render(): React.Node {
-    const filterRegex = new RegExp(this.state.filterText, 'i');
     const {attachTargetInfos, sortedColumn, sortDescending} = this.state;
     const compareFn = getCompareFunction(sortedColumn, sortDescending);
     const {selectedAttachTarget} = this.state;
     let selectedIndex = null;
-    const rows = attachTargetInfos
-      .filter(
-        item =>
-          filterRegex.test(item.name) ||
-          filterRegex.test(item.pid.toString()) ||
-          filterRegex.test(item.commandName),
-      )
+    const rows = filterTargetInfos(attachTargetInfos, this.state.filterText)
       .sort(compareFn)
       .map((item, index) => {
         const row = {
@@ -280,8 +302,19 @@ export class AttachUIComponent extends React.Component<PropsType, StateType> {
   }
 
   _handleFilterTextChange = (text: string): void => {
+    // Check if we've filtered down to one option and select if so
+    let newSelectedTarget = this.state.selectedAttachTarget;
+    const filteredAttachTargetInfos = filterTargetInfos(
+      this.state.attachTargetInfos,
+      text,
+    );
+    if (filteredAttachTargetInfos.length === 1) {
+      newSelectedTarget = filteredAttachTargetInfos[0];
+    }
+
     this.setState({
       filterText: text,
+      selectedAttachTarget: newSelectedTarget,
     });
   };
 
