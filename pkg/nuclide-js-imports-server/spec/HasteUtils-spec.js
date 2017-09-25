071bda3ecaf266dca97bdb66ebf50856de821a28
Hanson Wang <hansonw@fb.com>
2017-09-25 10:54:07 -0700

    Implement Haste name resolution [2/n]
    Summary:
    Now that we've parsed all the configs from the previous diffs, here's the Haste name resolution algorithm:
    
    If name reducers are enabled:
    
    - Is the file contained in the name reducer whitelist and NOT the name reducer blacklist? if not, fall back to `providesModule` below.
    - Otherwise: apply each of the regexp replace patterns from `name_reducers` to the filename in order, and use that as the module name.
    
    Otherwise:
    
    - Check for `providesModule <module_name>` in a block comment. If it exists, use that as the module name.
    
    This implements that in the JS imports code.
    
    Reviewed By: wbinnssmith
    
    Differential Revision: D5886125
    
    fbshipit-source-id: 64e5720f19345ce7b1e2837b6ff8e369ba6917a0

---
+++ pkg/nuclide-js-imports-server/spec/HasteUtils-spec.js
@@ -0,0 +1,85 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ *
+ * @flow
+ * @format
+ */
+
+import nullthrows from 'nullthrows';
+import {getHasteName} from '../src/lib/HasteUtils';
+import {parseFile} from '../src/lib/AutoImportsManager';
+
+describe('getHasteName', () => {
+  it('returns null for non-haste files', () => {
+    const ast = nullthrows(parseFile('export function f() {}'));
+    expect(
+      getHasteName('/test.js', ast, {
+        isHaste: true,
+        useNameReducers: false,
+        nameReducers: [],
+        nameReducerWhitelist: [],
+        nameReducerBlacklist: [],
+      }),
+    ).toBe(null);
+  });
+
+  it('detects @providesModule', () => {
+    const ast = nullthrows(
+      parseFile('/* @providesModule test1234\nblah */ export function f() {}'),
+    );
+    expect(
+      getHasteName('/test.js', ast, {
+        isHaste: true,
+        useNameReducers: false,
+        nameReducers: [],
+        nameReducerWhitelist: [],
+        nameReducerBlacklist: [],
+      }),
+    ).toBe('test1234');
+  });
+
+  it('uses name reducers', () => {
+    const ast = nullthrows(parseFile('export function f() {}'));
+    const providesModuleAst = nullthrows(
+      parseFile('/* @providesModule test1234\nblah */ export function f() {}'),
+    );
+    const hasteSettings = {
+      isHaste: true,
+      useNameReducers: true,
+      nameReducers: [
+        // basename
+        {regexp: /^.*\/([a-zA-Z0-9$_.-]+\.js(\.flow)?)$/, replacement: '$1'},
+        // strip .js or .js.flow suffix
+        {regexp: /^(.*)\.js(\.flow)?$/, replacement: '$1'},
+      ],
+      nameReducerWhitelist: [/\/a\/.*/],
+      nameReducerBlacklist: [/\/a\/b\/.*/],
+    };
+    expect(getHasteName('/a/test.js', ast, hasteSettings)).toBe('test');
+    expect(getHasteName('/a/test2.js.flow', ast, hasteSettings)).toBe('test2');
+    expect(getHasteName('/a/b/test.js', ast, hasteSettings)).toBe(null);
+    expect(getHasteName('/b/test.js', ast, hasteSettings)).toBe(null);
+
+    // Falls back to @providesModule, even when blacklisted.
+    expect(getHasteName('/b/test.js', providesModuleAst, hasteSettings)).toBe(
+      'test1234',
+    );
+
+    // No whitelist = everything
+    expect(
+      getHasteName('/c/test.js', ast, {
+        ...hasteSettings,
+        nameReducerWhitelist: [],
+      }),
+    ).toBe('test');
+
+    // Test default reducer
+    expect(
+      getHasteName('/a/test.js', ast, {...hasteSettings, nameReducers: []}),
+    ).toBe('test');
+  });
+});
---
+++ pkg/nuclide-js-imports-server/src/lib/HasteUtils.js
@@ -0,0 +1,69 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ *
+ * @flow
+ * @format
+ */
+
+import type {NuclideUri} from 'nuclide-commons/nuclideUri';
+import type {HasteSettings} from '../getConfig';
+
+import nuclideUri from 'nuclide-commons/nuclideUri';
+
+export function hasteReduceName(
+  file: NuclideUri,
+  hasteSettings: HasteSettings,
+): ?string {
+  if (!hasteSettings.useNameReducers) {
+    return null;
+  }
+  const {
+    nameReducers,
+    nameReducerWhitelist,
+    nameReducerBlacklist,
+  } = hasteSettings;
+  if (
+    (nameReducerWhitelist.length === 0 ||
+      nameReducerWhitelist.some(r => r.test(file))) &&
+    !nameReducerBlacklist.some(r => r.test(file))
+  ) {
+    if (nameReducers.length === 0) {
+      // The default name reducer.
+      return nuclideUri.stripExtension(nuclideUri.basename(file));
+    }
+    let hasteName = file;
+    for (const reducer of nameReducers) {
+      hasteName = hasteName.replace(reducer.regexp, reducer.replacement);
+    }
+    return hasteName;
+  }
+}
+
+export function getHasteName(
+  file: NuclideUri,
+  ast: Object,
+  hasteSettings: HasteSettings,
+): ?string {
+  if (!hasteSettings.isHaste) {
+    return null;
+  }
+  // Try to use a name reducer, as long as this isn't blacklisted.
+  const nameReducerResult = hasteReduceName(file, hasteSettings);
+  if (nameReducerResult != null) {
+    return nameReducerResult;
+  }
+  // Otherwise, if there's a @providesModule comment, this is always OK.
+  for (const comment of ast.comments) {
+    if (comment.type === 'CommentBlock') {
+      const providesModule = comment.value.match(/@providesModule\s+(\S+)/);
+      if (providesModule != null) {
+        return providesModule[1];
+      }
+    }
+  }
+  return null;
+}
