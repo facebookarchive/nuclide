b98494e9fb6188a379e43197e943f9035280c343
Matthew Dapena-Tretter <matthewwithanm@fb.com>
2017-10-03 20:22:00 -0700

    Switch modal example to use showModal()
    Reviewed By: tjfryan
    
    Differential Revision: D5969220
    
    fbshipit-source-id: ccbdb46e3af4d1889534b7b841bd043fb5d88a11

---
+++ modules/nuclide-commons-ui/showModal.example.js
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @flow
+ * @format
+ */
+
+import * as React from 'react';
+import {Button} from './Button';
+import showModal from './showModal';
+
+function ModalButton() {
+  return <Button onClick={showExampleModal}>Show Modal</Button>;
+}
+
+function showExampleModal() {
+  showModal(dismiss => {
+    return (
+      <div>
+        <div>
+          I'm a modal. You can add any content you like. I have all the standard
+          behavior, like obeying the "core:cancel" command!
+        </div>
+        <Button onClick={dismiss}>Hide Modal</Button>
+      </div>
+    );
+  });
+}
+
+export const ModalExamples = {
+  sectionName: 'Modal',
+  description: 'Overlays that cover the entire screen. ',
+  examples: [
+    {
+      title: 'Click the button to toggle a modal:',
+      component: ModalButton,
+    },
+  ],
+};
