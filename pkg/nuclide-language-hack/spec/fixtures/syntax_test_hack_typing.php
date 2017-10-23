f57b15db5c4e37fdd436ce7397a28d357ade2a60
Matthew Dapena-Tretter <matthewwithanm@fb.com>
2017-10-23 11:33:35 -0700

    Improve Hack grammar parsing of types
    Summary:
    This started out with a user report that shapes weren't being tokenized correctly in type positions. It turns out we weren't doing a great job with types in general.
    
    I ended up removing some tests that enforced special-case parsing for array arguments (which seemed strange imo) and replaced them with new ones that treat arrays like anything else. I also tried to reuse my rules and cut down on repetition and special casing so things should be more consistent. For example, constants in argument positions are now parsed the same as elsewhere, and default arguments support more than just a few special-cased constants.
    
    The questionable thing about all these grammar improvements, though, is that some of our previous coloring was because of incorrect parsing. For example, the two "int"s in `function f(int $n): int {}` were parsed as classes. As a result, the code may actually end up looking a little more "samey" (but maybe only in non-real world use like the test I've been staring at). Really, this should be fixed in the syntax theme (not the parser) but…
    
    Reviewed By: hansonw
    
    Differential Revision: D6045653
    
    fbshipit-source-id: bcab8892208222b7632f10dc63893845e12ec2c5

--- pkg/nuclide-language-hack/spec/fixtures/syntax_test_hack_typing.php
+++ pkg/nuclide-language-hack/spec/fixtures/syntax_test_hack_typing.php
@@ -5,7 +5,7 @@ function example(): Awaitable<SomeType, bool> {
   //              ^ punctuation.definition.type.php
   //                     ^ support.class.php
   //                            ^ support.class.php
-  //                                     ^ support.class.php
+  //                                     ^ storage.type.php
   //                                          ^ punctuation.section.scope.begin.php
 }
 
@@ -24,17 +24,123 @@ interface ISomething {
 function exampleWithDefaultParameters(
   URI $href_uri,
   ?Something $filter = null,
+  // ^ support.class.php
   //                 ^ keyword.operator.assignment.php
   //                   ^^^^ constant.language.php
   //                       ^ meta.function.arguments.php
   bool $notfilter = false,
+  // ^ storage.type.php
   //              ^ keyword.operator.assignment.php
   //                ^^^^^ constant.language.php
   //                     ^ meta.function.arguments.php
   SomeEnum $mode = SomeEnum::NORMAL,
+  // ^ support.class.php
   //             ^ keyword.operator.assignment.php
-  //               ^^^^^^^^^^^^^^^^ constant.language.php
+  //               ^^^^^^^^  support.class.php
+  //                         ^^^^^^ constant.other.class.php
   //                               ^ meta.function.arguments.php
 ): void {
 
+}
+
+function f($x) {
+  //       ^ punctuation.definition.variable
+  //        ^ variable.other.php
+}
+]
+function f($x = 5) {
+  //          ^ keyword.operator.assignment
+  //            ^ constant.numeric.php
+}
+
+function f($x = []) {
+  //          ^ keyword.operator.assignment
+  //            ^ punctuation.section.array.begin
+  //             ^ punctuation.section.array.end
+}
+
+function f(int $x = 5) {
+  //              ^ keyword.operator.assignment
+  //                ^ constant.numeric.php
+}
+
+function f(array $x = []) {
+  //                ^ keyword.operator.assignment
+  //                  ^ punctuation.section.array.begin
+  //                   ^ punctuation.section.array.end
+}
+
+function f($x, $y) {
+  //       ^ punctuation.definition.variable
+  //        ^ variable.other.php
+  //           ^ punctuation.definition.variable
+  //            ^ variable.other.php
+}
+
+function f(array $x): void {
+  //       ^^^^^ storage.type.array.php
+}
+
+function f( array $x): void {
+  //        ^^^^^ storage.type.array.php
+}
+
+function f(array $x, array $y): void {
+  //       ^^^^^ storage.type.array.php
+}
+
+function f(int $x): void {
+  //       ^^^ storage.type.php
+}
+
+function f(int $x, shape('vc' => int) $y): shape('vc' => int) {
+  //       ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ meta.function.arguments.php
+}
+
+function f(int $x, shape('vc' => int) $y): shape('vc' => int) {
+  //               ^^^^^^^^^^^^^^^^^^ storage.type.shape.php
+}
+
+function f(int $x, shape('vc' => int) $y): shape('vc' => int) {
+  //                     ^ punctuation.definition.string.begin.php
+  //                      ^^ meta.string-contents.quoted.single.php
+  //                        ^ punctuation.definition.string.end.php
+  //                          ^^ .keyword.operator.key.php
+  //                             ^^^ .storage.type.php
+}
+
+function f(int $x, shape('vc' => int) $y): shape('vc' => int) {
+  //                                  ^ punctuation.definition.variable
+  //                                   ^ variable.other.php
+}
+
+function f(int $x, shape('vc' => int) $y): shape('vc' => int) {
+  //                                       ^^^^^^^^^^^^^^^^^^ storage.type.shape.php
+}
+
+function f(int $x, shape('vc' => int) $y): shape('vc' => int) {
+  //                                             ^ punctuation.definition.string.begin.php
+  //                                              ^^ meta.string-contents.quoted.single.php
+  //                                                ^ punctuation.definition.string.end.php
+  //                                                  ^^ .keyword.operator.key.php
+  //                                                     ^^^ .storage.type.php
+}
+
+function f(): shape('vc' => shape('vc' => int)) {
+  //                        ^^^^^^^^^^^^^^^^^^ storage.type.shape.php
+  //                              ^ punctuation.definition.string.begin.php
+  //                               ^^ meta.string-contents.quoted.single.php
+  //                                 ^ punctuation.definition.string.end.php
+  //                                   ^^ .keyword.operator.key.php
+  //                                      ^^^ .storage.type.php
+}
+
+class Something {
+  public static function f(int $x, shape('vc' => int) $y): shape('vc' => int) {
+    //                     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ meta.function.arguments.php
+  }
+
+  public static function f(int $x, shape('vc' => int) $y): shape('vc' => int) {
+    //                     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ meta.function.arguments.php
+  }
 }
