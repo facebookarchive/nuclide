0f8b06346484089b088ee86783e56381abbc70bd
Will Binns-Smith <wbinnssmith@fb.com>
2017-10-27 19:12:39 -0700

    Create iconForOutlineTree and use it in omni, breadcrumbs
    Summary:
    This creates a new module in commons-atom called `iconForOutlineTree` which, given an outline tree object, returns the icon name which should be used to render it. This is now used in breadcrumbs.
    
    It also exports a function for returning an icon name given an OutlineTreeKind. This is now used in omni2.
    
    Reviewed By: matthewwithanm
    
    Differential Revision: D6166977
    
    fbshipit-source-id: eaa6fc61d78ef8ea5792e7c52286a25bd1dac2e4

---
+++ pkg/commons-atom/OutlineTreeUtils.js
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ *
+ * @flow
+ * @format
+ */
+
+import type {IconName} from 'nuclide-commons-ui/Icon';
+import type {
+  OutlineTree,
+  OutlineTreeKind,
+} from 'atom-ide-ui/pkg/atom-ide-outline-view/lib/types';
+
+const OUTLINE_KIND_TO_ICON: {[OutlineTreeKind]: IconName} = {
+  array: 'type-array',
+  boolean: 'type-boolean',
+  class: 'type-class',
+  constant: 'type-constant',
+  constructor: 'type-constructor',
+  enum: 'type-enum',
+  field: 'type-field',
+  file: 'type-file',
+  function: 'type-function',
+  interface: 'type-interface',
+  method: 'type-method',
+  module: 'type-module',
+  namespace: 'type-namespace',
+  number: 'type-number',
+  package: 'type-package',
+  property: 'type-property',
+  string: 'type-string',
+  variable: 'type-variable',
+};
+
+export function iconForOutlineKind(kind: OutlineTreeKind): IconName {
+  return OUTLINE_KIND_TO_ICON[kind];
+}
+
+export function iconForOutlineTree(outlineTree: OutlineTree): IconName {
+  const {kind} = outlineTree;
+  // $FlowFixMe these come from the RPC and are untyped.
+  const icon: ?IconName = outlineTree.icon;
+  const iconName = icon != null ? icon : kind && iconForOutlineKind(kind);
+  if (iconName == null) {
+    return 'code';
+  }
+  return iconName;
+}
