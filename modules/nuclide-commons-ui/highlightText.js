3808680390f9b68d4c499d903607eee8b7cbd607
Will Binns-Smith <wbinnssmith@fb.com>
2017-11-02 19:54:43 -0700

    Add `highlightText` tagged template literal
    Summary:
    This tagged template literal returns a tuple of text and its
    matchingRanges, where the ranges are those of the interpolated values in
    the template.
    
    Reviewed By: matthewwithanm
    
    Differential Revision: D6205093
    
    fbshipit-source-id: e1b0402969abd270afb3d8de4c908ae23ebfbed0

---
+++ modules/nuclide-commons-ui/highlightText.js
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @flow
+ * @format
+ */
+
+type MatchRange = [/* start */ number, /* end */ number];
+type TextValueWithMatches = {
+  text: string,
+  matchRanges: Array<MatchRange>,
+};
+
+// intended to be used as a tagged template literal, where the interpolated
+// pieces are highlighted in the returned set of matches:
+//
+// e.g. highlightText`not highlighted ${'but this is'} and this is not`;
+export default function highlightText(
+  unhighlightedStrings: Array<string>,
+  ...highlightedStrings: Array<string>
+): TextValueWithMatches {
+  let concattenated = '';
+  const highlightedRanges: Array<MatchRange> = [];
+
+  for (let i = 0; i < unhighlightedStrings.length; i++) {
+    concattenated += unhighlightedStrings[i];
+
+    // the two lists of strings may be uneven by one
+    if (highlightedStrings[i] != null) {
+      const start = concattenated.length;
+      concattenated += highlightedStrings[i];
+      highlightedRanges.push([start, concattenated.length]);
+    }
+  }
+
+  return {
+    text: concattenated,
+    matchRanges: highlightedRanges,
+  };
+}
