c97302b24ed4efd84a4d97945113f15c8116131b
William Ma <williamma@fb.com>
2017-11-22 14:34:00 -0800

    Improve TabbableContainer
    Summary:
    Instead of using atom keybindings, this change will use an observable to watch for keystrokes of Tab and ShiftTab.
    This will remove the extra package nuclide-tab-focus, and put all of the logic in TabbableContainer (which I think makes it a lot cleaner).
    
    Also, observables are called before keybinding events, so this will help with being able to make the commit form tabbable (coming soon by me hopefully).
    
    Reviewed By: tjfryan
    
    Differential Revision: D6360838
    
    fbshipit-source-id: 394080eafc51786825074e4d3384fd946fe5bf6b

--- modules/nuclide-commons-ui/TabbableContainer.js
+++ modules/nuclide-commons-ui/TabbableContainer.js
@@ -10,30 +10,33 @@
  * @format
  */
 
+/* eslint-env browser */
+
 import React from 'react';
 import invariant from 'assert';
+import {Observable} from 'rxjs';
 import tabbable from 'tabbable';
 import classnames from 'classnames';
+import UniversalDisposable from 'nuclide-commons/UniversalDisposable';
 
 type DefaultProps = {
   contained: boolean,
 };
 
 type Props = {
-  children?: React$Element<any>,
+  children?: React$Node,
   contained: boolean,
   className?: string,
 };
 
-// NOTE: This constant must be kept in sync with the keybinding in
-//       ../nuclide-tab-focus/keymaps/nuclide-tab-focus.json
-export const _TABBABLE_CLASS_NAME = 'nuclide-tabbable';
+const TABBABLE_CLASS_NAME = 'nuclide-tabbable';
 
 /**
  * Enables focusing between inputs with tab and shift-tab. Can also be used to
  * trap focus within the container by using the contained property.
  */
 export default class TabbableContainer extends React.Component<Props> {
+  _disposables: UniversalDisposable;
   _rootNode: ?HTMLDivElement;
 
   static defaultProps: DefaultProps = {
@@ -54,16 +57,212 @@ export default class TabbableContainer extends React.Component<Props> {
         firstTabbableElement.focus();
       }
     }
+
+    this._disposables = new UniversalDisposable(
+      Observable.fromEvent(
+        rootNode,
+        'keydown',
+      ).subscribe((event: KeyboardEvent) => {
+        if (event.altKey || event.ctrlKey || event.metaKey) {
+          return;
+        }
+
+        if (event.key === 'Tab') {
+          if (event.shiftKey) {
+            focusPrevious();
+          } else {
+            focusNext();
+          }
+          event.preventDefault();
+          event.stopPropagation();
+        }
+      }),
+    );
+  }
+
+  componentWillUnmount() {
+    this._disposables.dispose();
   }
 
   render(): React$Node {
     return (
       <div
-        className={classnames(_TABBABLE_CLASS_NAME, this.props.className)}
+        className={classnames(TABBABLE_CLASS_NAME, this.props.className)}
         data-contained={this.props.contained}
         ref={node => (this._rootNode = node)}>
         {this.props.children}
       </div>
     );
   }
+}
+
+function focusNext(): void {
+  const currentElement = getFocusedElement();
+  if (!(currentElement instanceof HTMLElement)) {
+    return;
+  }
+  const focusedTabIndex =
+    currentElement.tabIndex >= 0 ? currentElement.tabIndex : -Infinity;
+
+  let nextElement = null;
+  let nextTabIndex = Infinity;
+  let lowestElement = null;
+  let lowestTabIndex = Infinity;
+
+  let container = findParentElement(currentElement, element => {
+    return element.classList.contains(TABBABLE_CLASS_NAME);
+  });
+  if (
+    container instanceof HTMLElement &&
+    container.dataset.contained === 'false'
+  ) {
+    container = null;
+  }
+
+  eachTabIndexedElement(
+    currentElement,
+    false /* reverse */,
+    (element, tabIndex) => {
+      if (tabIndex < lowestTabIndex) {
+        lowestTabIndex = tabIndex;
+        lowestElement = element;
+      }
+
+      if (focusedTabIndex <= tabIndex && tabIndex < nextTabIndex) {
+        nextTabIndex = tabIndex;
+        nextElement = element;
+        if (focusedTabIndex === tabIndex || focusedTabIndex + 1 === tabIndex) {
+          return true; // doneSearching
+        }
+      }
+
+      return false; // doneSearching
+    },
+    container,
+  );
+
+  if (nextElement) {
+    nextElement.focus();
+  } else if (lowestElement) {
+    lowestElement.focus();
+  }
+}
+
+function focusPrevious(): void {
+  const currentElement = getFocusedElement();
+  if (!(currentElement instanceof HTMLElement)) {
+    return;
+  }
+  const focusedTabIndex =
+    currentElement.tabIndex >= 0 ? currentElement.tabIndex : Infinity;
+
+  let previousElement = null;
+  let previousTabIndex = -Infinity;
+  let highestElement = null;
+  let highestTabIndex = -Infinity;
+
+  let container = findParentElement(currentElement, element => {
+    return element.classList.contains(TABBABLE_CLASS_NAME);
+  });
+  if (
+    container instanceof HTMLElement &&
+    container.dataset.contained === 'false'
+  ) {
+    container = null;
+  }
+
+  eachTabIndexedElement(
+    currentElement,
+    true /* reverse */,
+    (element, tabIndex) => {
+      if (tabIndex > highestTabIndex) {
+        highestTabIndex = tabIndex;
+        highestElement = element;
+      }
+
+      if (focusedTabIndex >= tabIndex && tabIndex > previousTabIndex) {
+        previousTabIndex = tabIndex;
+        previousElement = element;
+        if (focusedTabIndex === tabIndex || focusedTabIndex - 1 === tabIndex) {
+          return true; // doneSearching
+        }
+      }
+
+      return false; // doneSearching
+    },
+    container,
+  );
+
+  if (previousElement) {
+    previousElement.focus();
+  } else if (highestElement) {
+    highestElement.focus();
+  }
+}
+
+/**
+* Traverses all focusable elements for the next element to focus.
+* curentElement is where the traversal starts.
+* reverse determines whether to traverse backwards or forwards.
+* updateNextCandidate is a method that determines if the element is the best
+*                     candidate to be focused next. A boolean is returned to
+*                     stop the traversal if that element is guaranteed to be
+*                     the next candidate.
+* container is where all of the focusable elements are searched.
+*           Default value is document.
+*/
+function eachTabIndexedElement(
+  currentElement: Element,
+  reverse: boolean,
+  updateNextCandidate: (element: Element, tabIndex: number) => boolean,
+  container: ?Element,
+): void {
+  const elements = (container || document).querySelectorAll(
+    'a, input, button, [tabindex]',
+  );
+  let index = Array.from(elements).indexOf(currentElement);
+  const increment = reverse ? -1 : 1;
+  for (let i = 1; i < elements.length; ++i) {
+    index = (index + elements.length + increment) % elements.length;
+    const element = elements[index];
+    if (
+      element.disabled === true ||
+      element.tabIndex == null ||
+      element.tabIndex === -1
+    ) {
+      continue;
+    }
+    if (updateNextCandidate(element, element.tabIndex)) {
+      break;
+    }
+  }
+}
+
+function getFocusedElement(): ?Element {
+  // Some inputs have a hidden-input with tabindex = -1 that gets focused, so
+  // activeElement is actually not what we want. In these cases, we must find
+  // the parent tag that has the actual tabindex to use. An example is the
+  // atom-text-editor.
+  let currentElement = document.activeElement;
+  if (currentElement && currentElement.classList.contains('hidden-input')) {
+    currentElement = findParentElement(
+      currentElement.parentElement,
+      element => element instanceof HTMLElement && element.tabIndex >= 0,
+    );
+  }
+  return currentElement;
+}
+
+/**
+* Finds a parent of currentElement that satisfies the condition.
+*/
+function findParentElement(
+  currentElement: ?Element,
+  condition: (element: Element) => boolean,
+): ?Element {
+  let element = currentElement;
+  while (element && !condition(element)) {
+    element = element.parentElement;
+  }
+  return element;
 }
--- pkg/nuclide-tab-focus/README.md
+++
@@ -1,7 +0,0 @@
-# nuclide-tab-focus
-
-This feature manages tabbing on focusable elements.
-
-# Usage
-
-Encapsulate your tabbable area with TabbableContainer from ../nuclide-ui.
