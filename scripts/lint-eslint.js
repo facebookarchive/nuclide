b1e3ca1c7ed2b955fb681db1b64b3e781f5f7fd4
Andres Suarez <asuarez@fb.com>
2017-12-06 19:45:25 -0800

    Undo eslint script v8 bug workaround
    Reviewed By: vjeux
    
    Differential Revision: D6506663
    
    fbshipit-source-id: 6d504b8723d296b5994f215eab9156ac91c258b8

--- scripts/lint-eslint.js
+++ scripts/lint-eslint.js
@@ -43,28 +43,7 @@ while (chunks.length) {
   const ps = child_process.spawn(
     process.execPath,
     [
-      '--eval',
-      `
-      (function retry(retries) {
-        try {
-          require('./node_modules/.bin/eslint');
-        } catch (err) {
-          if (--retries && Array.isArray(err) && err[2] instanceof Error) {
-            console.error('**ESLint flow-parser mystery v8 bug... retrying**');
-            console.error(require('util').inspect(err));
-            // Reset the module cache because ESLint is stateful
-            Object.keys(require.cache)
-              .filter(x => x.includes('/node_modules/'))
-              .forEach(x => { delete require.cache[x]; });
-            retry(retries);
-          } else {
-            throw err;
-          }
-        }
-      })(3);
-      `,
-      '--',
-      '',
+      'node_modules/.bin/eslint',
       '--format', 'codeframe',
       '--max-warnings', '0',
       '--',
