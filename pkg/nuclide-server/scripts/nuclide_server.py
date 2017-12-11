2791f3d5a8925a55646c3fd77eeb875c10a9d887
Hanson Wang <hansonw@fb.com>
2017-12-11 11:42:57 -0800

    Only increase Nuclide server memory limit if >= 8GB of RAM is available
    Summary:
    This is primarily to fix development on embedded devices like the Raspberry Pi, which has ~1GB of RAM.
    The free memory detection only works on Linux but that's what we're mainly concerned about anyway.
    
    Closes https://github.com/facebook/nuclide/issues/1377
    
    Differential Revision: D6522814
    
    fbshipit-source-id: a50ba91ed5b2c2d674337163c610c864284dc5a5

--- pkg/nuclide-server/scripts/nuclide_server.py
+++ pkg/nuclide-server/scripts/nuclide_server.py
@@ -207,15 +207,19 @@ class NuclideServer(object):
                 self.logger.info('Found existing Nuclide process running on port %d.' % self.port)
                 return 1
 
-        node_flags = ' '.join([
+        node_flags = [
             # Increase stack trace limit for better debug logs.
             # For reference, Atom/Electron does not have a stack trace limit.
             '--stack-trace-limit=50',
+        ]
+
+        avail_mem_kb = self._get_avail_memory()
+        if not avail_mem_kb or avail_mem_kb >= 8192 * 1024:
             # Increase the default memory limit from ~1.76GB to 4GB.
-            '--max-old-space-size=4096',
-        ])
+            node_flags.append('--max-old-space-size=4096')
+
         # Start Nuclide server.
-        js_cmd = '%s %s --port %d' % (node_flags, NuclideServer.script_path, self.port)
+        js_cmd = '%s %s --port %d' % (' '.join(node_flags), NuclideServer.script_path, self.port)
         if cert and key and ca:
             js_cmd += ' --cert %s --key %s --ca %s' % (cert, key, ca)
         if abort_on_uncaught_exception:
@@ -263,3 +267,20 @@ class NuclideServer(object):
         with open(file_name, "r") as f:
             text = f.read()
             return text
+
+    @staticmethod
+    def _get_avail_memory():
+        # /proc/meminfo only exists on Linux.
+        if sys.platform.startswith('linux'):
+            try:
+                with open('/proc/meminfo', 'r') as f:
+                    while True:
+                        parts = f.readline().split()
+                        if len(parts) != 3:
+                            break
+                        if parts[0] == 'MemAvailable:':
+                            # Return value is in kilobytes.
+                            return int(parts[1])
+            except IOError:
+                pass
+        return None
