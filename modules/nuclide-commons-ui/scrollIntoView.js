6aa31eabd2c4a7b5e81e43bb7692224965440284
Matthew Dapena-Tretter <matthewwithanm@fb.com>
2017-12-15 17:13:51 -0800

    Improve perf of `scrollIntoView()` utils
    Summary: Rewritten to try to avoid the calls to `getComputedStyle()`—both by limiting the calls to times when the scrollTop has changed and by checking for inline styles first. When scrolling to elements in the file tree it's about twice as fast.
    
    Reviewed By: mostafaeweda
    
    Differential Revision: D6580034
    
    fbshipit-source-id: e0410651a1a0771fdf80aedafdf2653c141ec17f

--- modules/nuclide-commons-ui/scrollIntoView.js
+++ modules/nuclide-commons-ui/scrollIntoView.js
@@ -42,32 +42,40 @@
  */
 
 export function scrollIntoView(el: Element, alignToTop?: boolean): void {
-  const scrollTops = getOverflowHiddenScrollTops(el);
+  const scrollTops = getScrollTops(el);
   el.scrollIntoView(alignToTop); // eslint-disable-line rulesdir/dom-apis
-  restoreScrollTops(scrollTops);
+  restoreOverflowHiddenScrollTops(scrollTops);
 }
 
 export function scrollIntoViewIfNeeded(el: Element, center?: boolean): void {
-  const scrollTops = getOverflowHiddenScrollTops(el);
+  const scrollTops = getScrollTops(el);
   // $FlowIgnore: This should be added to the element type.
   el.scrollIntoViewIfNeeded(center); // eslint-disable-line rulesdir/dom-apis
-  restoreScrollTops(scrollTops);
+  restoreOverflowHiddenScrollTops(scrollTops);
 }
 
-function getOverflowHiddenScrollTops(el_: Element): Map<Element, number> {
+function getScrollTops(el_: Element): Map<Element, number> {
   let el = el_;
   const scrollTops = new Map();
   while (el != null) {
-    if (getComputedStyle(el).overflow === 'hidden') {
-      scrollTops.set(el, el.scrollTop);
-    }
+    scrollTops.set(el, el.scrollTop);
     el = el.parentElement;
   }
   return scrollTops;
 }
 
-function restoreScrollTops(scrollTops: Map<Element, number>): void {
+function restoreOverflowHiddenScrollTops(
+  scrollTops: Map<Element, number>,
+): void {
   scrollTops.forEach((scrollTop, el) => {
-    el.scrollTop = scrollTop;
+    if (el.scrollTop !== scrollTop && isOverflowHidden(el)) {
+      el.scrollTop = scrollTop;
+    }
   });
+}
+
+function isOverflowHidden(el: Element): boolean {
+  const overflowStyle = el.style == null ? null : (el.style: any).overflow;
+  const overflow = overflowStyle || getComputedStyle(el).overflow;
+  return overflow === 'hidden';
 }
