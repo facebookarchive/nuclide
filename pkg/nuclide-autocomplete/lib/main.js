c698bca2a47af289ea7167b8f6f44d3f33e68922
Wanderley da Silva <wander@fb.com>
2017-12-19 17:22:01 -0800

    Create nuclide-autocomplete package
    Summary: Autocomplete feature is really important for our users. This diff creates a proxy package that should be used when providing a new autocomplete functionality to Nuclide. This package will take care to track timing for autocomplete operations and in a future diff it will enable introduce a timeout for autocomplete functions.
    
    Reviewed By: hansonw
    
    Differential Revision: D6574118
    
    fbshipit-source-id: 90646a34b54cb5e2de76418a53cbff4e3fd892e7

---
+++ pkg/nuclide-autocomplete/lib/main.js
@@ -0,0 +1,119 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ *
+ * @flow
+ * @format
+ */
+
+import type {
+  AutocompleteProvider,
+  AutocompleteAnalyticEventNames,
+  AtomSuggestionInsertedRequest,
+} from './types';
+
+import invariant from 'assert';
+import UniversalDisposable from 'nuclide-commons/UniversalDisposable';
+import {track, trackTiming} from '../../nuclide-analytics';
+
+function createAutocompleteProvider<Suggestion: atom$AutocompleteSuggestion>(
+  provider: AutocompleteProvider<Suggestion>,
+): atom$AutocompleteProvider {
+  const eventNames = getAnalytics(provider);
+  // It is safe to cast it to any since AutocompleteProvider is a super type of
+  // atom$AutocompleteProvider
+  return ({
+    ...provider,
+    getSuggestions(
+      request: atom$AutocompleteRequest,
+    ): Promise<?Array<*>> | ?Array<*> {
+      const values = {};
+      return trackTiming(
+        eventNames.onGetSuggestions,
+        async () => {
+          const result = await provider.getSuggestions(request);
+          values.isEmpty = result == null || result.length === 0;
+          return result;
+        },
+        values,
+      );
+    },
+    onDidInsertSuggestion(
+      insertedSuggestionArgument: AtomSuggestionInsertedRequest<Suggestion>,
+    ): void {
+      trackOnDidInsertSuggestion(
+        eventNames.onDidInsertSuggestion,
+        provider.analytics.shouldLogInsertedSuggestion,
+        insertedSuggestionArgument,
+      );
+      const {onDidInsertSuggestion} = provider;
+      if (onDidInsertSuggestion) {
+        onDidInsertSuggestion(insertedSuggestionArgument);
+      }
+    },
+  }: any);
+}
+
+function trackOnDidInsertSuggestion<Suggestion: atom$AutocompleteSuggestion>(
+  eventName: string,
+  shouldLogInsertedSuggestion: boolean,
+  insertedSuggestionArgument: AtomSuggestionInsertedRequest<Suggestion>,
+) {
+  if (!shouldLogInsertedSuggestion) {
+    track(eventName);
+    return;
+  }
+
+  const {suggestion} = insertedSuggestionArgument;
+  const suggestionText =
+    suggestion.text != null ? suggestion.text : suggestion.snippet;
+  track(eventName, {
+    replacementPrefix: suggestion.replacementPrefix,
+    suggestionText,
+  });
+}
+
+function getAnalytics<Suggestion: atom$AutocompleteSuggestion>(
+  provider: AutocompleteProvider<Suggestion>,
+): AutocompleteAnalyticEventNames {
+  const {analytics} = provider;
+  if (analytics.eventName !== undefined) {
+    const {eventName} = analytics;
+    const onGetSuggestions = `nuclide-autocomplete:${eventName}:on-get-suggestions`;
+    const onDidInsertSuggestion = `nuclide-autocomplete:${eventName}:on-did-insert-suggestion`;
+    return {
+      onGetSuggestions,
+      onDidInsertSuggestion,
+    };
+  }
+
+  const {onGetSuggestions, onDidInsertSuggestion} = analytics;
+  invariant(
+    onGetSuggestions != null && onDidInsertSuggestion != null,
+    'Expected onGetSuggestions and onDidInsertSuggestion to be informed' +
+      'since eventName was not informed',
+  );
+  return {
+    onGetSuggestions,
+    onDidInsertSuggestion,
+  };
+}
+
+export function consumeProvider<Suggestion: atom$AutocompleteSuggestion>(
+  _provider:
+    | AutocompleteProvider<Suggestion>
+    | Array<AutocompleteProvider<Suggestion>>,
+): IDisposable {
+  const providers = Array.isArray(_provider) ? _provider : [_provider];
+  const disposables = providers.map(provider =>
+    atom.packages.serviceHub.provide(
+      'autocomplete.provider',
+      '2.0.0',
+      createAutocompleteProvider(provider),
+    ),
+  );
+  return new UniversalDisposable(...disposables);
+}
