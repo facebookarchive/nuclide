3e75ad5584245305b9f4aa0ed7f7423ab624a1e8
Matthew Dapena-Tretter <matthewwithanm@fb.com>
2018-01-05 17:35:06 -0800

    Dynamically inject nuclicons style
    Summary:
    An application of hansonw's patch from facebook-atom/atom-ide-ui#100,
    this eliminates the spurious error.
    
    Reviewed By: hansonw
    
    Differential Revision: D6669738
    
    fbshipit-source-id: 30d9e65b68c89f95da5505f9b01ddeb518479d81

--- modules/nuclide-commons-ui/index.js
+++ modules/nuclide-commons-ui/index.js
@@ -13,10 +13,29 @@
 // Requiring this module will load all stylesheets in styles/.
 // The exported value can be disposed to remove the stylesheets.
 
+import nuclideUri from 'nuclide-commons/nuclideUri';
 import UniversalDisposable from 'nuclide-commons/UniversalDisposable';
+import dedent from 'dedent';
 import fs from 'fs';
+import nullthrows from 'nullthrows';
 // eslint-disable-next-line rulesdir/prefer-nuclide-uri
 import path from 'path';
+
+const ttfUri = nuclideUri.nuclideUriToUri(
+  path.join(__dirname, 'styles', 'nuclicons.ttf'),
+);
+const newStyle = document.createElement('style');
+newStyle.appendChild(
+  document.createTextNode(dedent`
+    @font-face {
+      font-family: 'nuclicons';
+      src: url('${ttfUri}') format('truetype');
+      font-weight: normal;
+      font-style: normal;
+    }
+  `),
+);
+nullthrows(document.head).appendChild(newStyle);
 
 const styleDir = path.join(__dirname, 'styles');
 const styleDisposables = new UniversalDisposable(
@@ -24,6 +43,7 @@ const styleDisposables = new UniversalDisposable(
     .readdirSync(styleDir)
     .filter(file => ['.less', '.css'].includes(path.extname(file)))
     .map(file => atom.themes.requireStylesheet(path.join(styleDir, file))),
+  () => newStyle.remove(),
 );
 
 module.exports = styleDisposables; // eslint-disable-line rulesdir/no-commonjs
