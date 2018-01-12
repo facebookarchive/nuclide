ff01e633217ae25342c311a347a711fbb83f5ea6
Philip Weiss <philipfweiss@fb.com>
2018-01-12 14:59:04 -0800

    Add new rule to require universal disposables
    Summary:
    Add a new eslint rule which requires users to use universal disposables, rather than Disposable or Composite disposable.
    Did not enable the linting rule in this commit.
    
    Reviewed By: matthewwithanm
    
    Differential Revision: D6715477
    
    fbshipit-source-id: f20b77f25cbb57356bbc1a8e5e9250c26c154dcc

---
+++ modules/eslint-plugin-nuclide-internal/require-universal-disposable.js
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @noflow
+ */
+'use strict';
+
+/* eslint
+  comma-dangle: [1, always-multiline],
+  prefer-object-spread/prefer-object-spread: 0,
+  rulesdir/no-commonjs: 0,
+  */
+
+/**
+ * This rule prevents instantiation of the Disposable or CompositeDisposable classes.
+ * Instead, the UniversalDisposable class should be instantiated (UniversalDisposable
+ * contains a superset of the functionality of Disposable and CompositeDisposable).
+ */
+
+const DISALLOWED_DISPOSABLES = ['CompositeDisposable', 'Disposable'];
+
+const reportIncorrectDisposable = (context, node) => {
+  const message = 'Incorrect Disposable Used (must use UniversalDisposable)';
+  context.report({node, message});
+};
+
+module.exports = context => {
+  return {
+    NewExpression(node) {
+      const isDisallowedDisposable = DISALLOWED_DISPOSABLES.includes(node.callee.name);
+      if (isDisallowedDisposable) {
+        reportIncorrectDisposable(context, node);
+      }
+    },
+  };
+};
