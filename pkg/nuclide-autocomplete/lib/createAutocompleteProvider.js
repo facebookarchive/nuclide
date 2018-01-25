b15ce51a4d64a7a9ca2c90f7f7dbd4e94ce492af
Wanderley da Silva <wander@fb.com>
2018-01-25 08:24:02 -0800

    Use Proxy to wrap autocomplete provider
    Summary:
    The old approach expected providers to be immutable objects but sadly providers can change their internal data (e.g. suggestion priority) after initialization. This diff changes the wrap logic to use `Proxy` in order to trap calls to `getSuggestions` and `onDidInsertSuggestion`.
    
    {F119303041}
    
    Reviewed By: hansonw
    
    Differential Revision: D6799003
    
    fbshipit-source-id: adfd28b0d8942f55e723dd95c380e897af699fb2

--- pkg/nuclide-autocomplete/lib/createAutocompleteProvider.js
+++ pkg/nuclide-autocomplete/lib/createAutocompleteProvider.js
@@ -28,63 +28,85 @@ import {track, trackTiming} from '../../nuclide-analytics';
  */
 const AUTOCOMPLETE_TIMEOUT = 3000;
 
+/**
+ * Receives a provider and returns a proxy provider that applies time limit to
+ * `getSuggestions` calls and stop unhandled exceptions on to cascade.
+ */
 export default function createAutocompleteProvider<
   Suggestion: atom$AutocompleteSuggestion,
 >(provider: AutocompleteProvider<Suggestion>): atom$AutocompleteProvider {
+  // The `eventNames` could be computed in deep functions, but we don't want
+  // to change the logger if a provider decides to changes its name.
   const eventNames = getAnalytics(provider);
-  // It is safe to cast it to any since AutocompleteProvider is a super type of
-  // atom$AutocompleteProvider
-  return ({
-    ...provider,
-    getSuggestions(
-      request: atom$AutocompleteRequest,
-    ): Promise<?Array<*>> | ?Array<*> {
-      const logObject = {};
-
-      return trackTiming(
-        eventNames.onGetSuggestions,
-        async () => {
-          let result = null;
-          if (request.activatedManually) {
-            try {
-              result = await provider.getSuggestions(request);
-            } catch (e) {
-              track(eventNames.errorOnGetSuggestions);
-            }
-          } else {
-            try {
-              result = await timeoutPromise(
-                Promise.resolve(provider.getSuggestions(request)),
-                AUTOCOMPLETE_TIMEOUT,
-              );
-            } catch (e) {
-              if (e instanceof TimedOutError) {
-                track(eventNames.timeoutOnGetSuggestions);
-              } else {
-                track(eventNames.errorOnGetSuggestions);
-              }
-            }
-          }
-          logObject.isEmpty = result == null || result.length === 0;
-          return result;
-        },
-        logObject,
-      );
-    },
-    onDidInsertSuggestion(
-      insertedSuggestionArgument: AtomSuggestionInsertedRequest<Suggestion>,
-    ): void {
-      trackOnDidInsertSuggestion(
-        eventNames.onDidInsertSuggestion,
-        provider.analytics.shouldLogInsertedSuggestion,
-        insertedSuggestionArgument,
-      );
-      const {onDidInsertSuggestion} = provider;
-      if (onDidInsertSuggestion) {
-        onDidInsertSuggestion(insertedSuggestionArgument);
+  const proxy = new Proxy(provider, {
+    get: (target, prop, receiver) => {
+      switch (prop) {
+        case 'getSuggestions':
+          return getSuggestions.bind(null, target, eventNames);
+        case 'onDidInsertSuggestion':
+          return onDidInsertSuggestion.bind(null, target, eventNames);
+        default:
+          return Reflect.get(target, prop, receiver);
       }
     },
-  }: any);
+  });
+
+  // It is safe to cast it to any since AutocompleteProvider is a super type of
+  // atom$AutocompleteProvider
+  return (proxy: any);
+}
+
+function getSuggestions<Suggestion: atom$AutocompleteSuggestion>(
+  provider: AutocompleteProvider<Suggestion>,
+  eventNames: AutocompleteAnalyticEventNames,
+  request: atom$AutocompleteRequest,
+): Promise<?Array<*>> | ?Array<*> {
+  const logObject = {};
+
+  return trackTiming(
+    eventNames.onGetSuggestions,
+    async () => {
+      let result = null;
+      if (request.activatedManually) {
+        try {
+          result = await provider.getSuggestions(request);
+        } catch (e) {
+          track(eventNames.errorOnGetSuggestions);
+        }
+      } else {
+        try {
+          result = await timeoutPromise(
+            Promise.resolve(provider.getSuggestions(request)),
+            AUTOCOMPLETE_TIMEOUT,
+          );
+        } catch (e) {
+          if (e instanceof TimedOutError) {
+            track(eventNames.timeoutOnGetSuggestions);
+          } else {
+            track(eventNames.errorOnGetSuggestions);
+          }
+        }
+      }
+      logObject.isEmpty = result == null || result.length === 0;
+      return result;
+    },
+    logObject,
+  );
+}
+
+function onDidInsertSuggestion<Suggestion: atom$AutocompleteSuggestion>(
+  provider: AutocompleteProvider<Suggestion>,
+  eventNames: AutocompleteAnalyticEventNames,
+  insertedSuggestionArgument: AtomSuggestionInsertedRequest<Suggestion>,
+): void {
+  trackOnDidInsertSuggestion(
+    eventNames.onDidInsertSuggestion,
+    provider.analytics.shouldLogInsertedSuggestion,
+    insertedSuggestionArgument,
+  );
+  if (provider.onDidInsertSuggestion) {
+    provider.onDidInsertSuggestion(insertedSuggestionArgument);
+  }
 }
 
 function trackOnDidInsertSuggestion<Suggestion: atom$AutocompleteSuggestion>(
