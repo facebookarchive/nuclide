e2b30269f068319a9cbe79babf6751e28a0aefd1
Evan Krause <evangrayk@fb.com>
2018-01-26 13:58:46 -0800

    Add in subscribeToFileCreateAndDelete
    Summary:
    We want to be able to observe a file being created&deleted by hg easily. This will allow us to follow `.hg/wlock`, as well as other status files that indicate merge in progress, rebase in progress, etc.
    
    This change adds an abstraction that gives you a promise for a watchman subscription where you can provide a callback for each emitted value. This way it fits with other code in hgService so you can await multiple such promises together.
    
    The creation/deletion watching is done with a normal watchman subscription in which we check the `new` and `exists` fields to determine if the file was created or delete in this event. We also do some filtering to eliminate events where a file was updated but not created or destroyed, since we don't care about those events right now.
    
    Watchman does some debouncing of events, but they get coalesced in a way that we can rely on it providing us create/delete events. The added tests cover cases where there is a delay between events and cases where there are lots of events in a short time.
    
    This function actually allows you watch for multiple files at once, to prevent too many watchmanSubscriptions. The provided callback is called with a map of file name -> exists as a boolean, so that the consumer can see what has changed and keep track.
    
    Reviewed By: tjfryan
    
    Differential Revision: D6778939
    
    fbshipit-source-id: bf80f8042220a55f01b384608317d6de03ecd45a

---
+++ pkg/nuclide-hg-rpc/lib/watchFileCreationAndDeletion.js
@@ -0,0 +1,103 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ *
+ * @flow
+ * @format
+ */
+
+import type {WatchmanClient, FileChange} from 'nuclide-watchman-helpers';
+import {getLogger} from 'log4js';
+import fsPromise from 'nuclide-commons/fsPromise';
+import nuclideUri from 'nuclide-commons/nuclideUri';
+import {Observable} from 'rxjs';
+
+/**
+ * Return a map of filename => exists in response to watchman events.
+ * Note that this map does not necessarily contain the status of all watched files.
+ */
+function filesCreateOrDeleteToObserver(
+  fileChanges: Array<FileChange>,
+): ?Map<string, boolean> {
+  const state = new Map();
+  for (const fileChange of fileChanges) {
+    if (fileChange.exists) {
+      // Only emit create events if watchman says the file is new.
+      // Note: even if watchman debounces several changes, such as a create followed
+      // shortly by a modify, we will still receive new=true.
+      if (fileChange.new) {
+        state.set(fileChange.name, true);
+      }
+    } else {
+      state.set(fileChange.name, false);
+    }
+  }
+  if (state.size > 0) {
+    return state;
+  }
+  return null;
+}
+
+/**
+ * Set up a watchman subscription to watch for a file's creation and deletion.
+ * Returns a Promise so that all such subscriptions can be awaited in bulk.
+ */
+export function subscribeToFilesCreateAndDelete(
+  watchmanClient: WatchmanClient,
+  repoPath: string,
+  fileNames: Array<string>,
+  subscriptionName: string,
+): Observable<Map<string, boolean>> {
+  const filesSubscriptionPromise = watchmanClient.watchDirectoryRecursive(
+    repoPath,
+    subscriptionName,
+    {
+      expression: ['name', fileNames, 'wholename'],
+      defer_vcs: false,
+    },
+  );
+  return Observable.fromPromise(filesSubscriptionPromise).switchMap(
+    subscription => {
+      getLogger('nuclide-hg-rpc').debug(
+        `Watchman create/delete subscription ${subscriptionName}` +
+          ` established for files: ${fileNames.join(',')}`,
+      );
+      return Observable.create(observer => {
+        // Check each file being watched if it already exists. This is done
+        // individually so that no watchman event can invalidate previously
+        // checked files. We only bother updating if the file exists.
+        fileNames.map(fileName => {
+          const qualifiedFileName = nuclideUri.join(repoPath, fileName);
+          fsPromise.exists(qualifiedFileName).then(exists => {
+            if (exists) {
+              observer.next(new Map([[fileName, exists]]));
+              getLogger('nuclide-hg-rpc').info(
+                `${subscriptionName}: watched file ${fileName} already exists`,
+              );
+            }
+          });
+        });
+
+        const changeSubscription = subscription.on(
+          'change',
+          (fileChanges: Array<FileChange>) => {
+            const newState = filesCreateOrDeleteToObserver(fileChanges);
+            if (newState != null) {
+              observer.next(newState);
+            }
+          },
+        );
+        return () => {
+          getLogger('nuclide-hg-rpc').info(
+            `disposing of watchman subscription ${subscriptionName}`,
+          );
+          changeSubscription.dispose();
+          subscription.dispose();
+        };
+      });
+    },
+  );
+}
