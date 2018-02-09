66e39c602337a137739e19e3f47aeec4466d016d
Will Binns-Smith <wbinnssmith@fb.com>
2018-02-09 15:32:22 -0800

    Extract displaying keybindings for given keystrokes as <Keybinding />
    Summary:
    This extracts the behavior in the CommandResultRow which
    renders keybindings according to their keystrokes.
    
    Reviewed By: matthewwithanm
    
    Differential Revision: D6944465
    
    fbshipit-source-id: acfec01df46e397f7c19b43633b41eb0e0c87cad

---
+++ modules/nuclide-commons-ui/Keybinding.js
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @flow
+ * @format
+ */
+
+import React from 'react';
+import humanizeKeystroke from 'nuclide-commons/humanizeKeystroke';
+
+type Props = {|
+  keystrokes: string,
+|};
+
+export default function Keybinding({keystrokes}: Props) {
+  return (
+    <kbd className="key-binding">
+      {humanizeKeystroke(keystrokes, process.platform)}
+    </kbd>
+  );
+}
