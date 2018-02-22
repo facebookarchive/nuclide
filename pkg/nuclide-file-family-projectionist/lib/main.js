db634f9de13b23e9888b3807a5cece597c151643
Will Binns-Smith <wbinnssmith@fb.com>
2018-02-22 10:11:54 -0800

    Backbone of `nuclide-file-family-projections`
    Summary:
    This adds the beginnings of a package for exposing
    projectionist patterns as a file family provider.
    
    Reviewed By: aaronabramov
    
    Differential Revision: D7002837
    
    fbshipit-source-id: d7a1a694be86e29fb3fdafb9e86033ae60a75440

---
+++ pkg/nuclide-file-family-projectionist/lib/main.js
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ *
+ * @flow
+ * @format
+ */
+
+import type {CwdApi} from '../../nuclide-current-working-directory/lib/CwdApi';
+
+import nullthrows from 'nullthrows';
+import createPackage from 'nuclide-commons-atom/createPackage';
+import UniversalDisposable from 'nuclide-commons/UniversalDisposable';
+import {BehaviorSubject} from 'rxjs';
+
+class Activation {
+  _cwdApis: BehaviorSubject<?CwdApi> = new BehaviorSubject(null);
+  _subscriptions: ?UniversalDisposable;
+
+  activate() {
+    this._subscriptions = new UniversalDisposable();
+  }
+
+  dispose() {
+    nullthrows(this._subscriptions).dispose();
+  }
+
+  consumeCwdApi(cwdApi: CwdApi): void {
+    this._cwdApis.next(cwdApi);
+  }
+}
+
+createPackage(module.exports, Activation);
