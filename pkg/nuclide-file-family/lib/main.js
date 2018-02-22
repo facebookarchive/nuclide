062b953a9eda6a7c49c2eb4e6cdaf4ae4080768a
Will Binns-Smith <wbinnssmith@fb.com>
2018-02-21 19:32:04 -0800

    Add alternate file keybinding to core file-family package
    Summary:
    This adds a simple keybinding to toggle to a file's alternate
    to the core file-family package.
    
    Differential Revision: D7001851
    
    fbshipit-source-id: 6e527656d679616491030e87f994ad83c507255c

--- pkg/nuclide-file-family/lib/main.js
+++ pkg/nuclide-file-family/lib/main.js
@@ -12,18 +12,24 @@
 import type {FileFamilyProvider} from './types';
 
 import createPackage from 'nuclide-commons-atom/createPackage';
+import {goToLocation} from 'nuclide-commons-atom/go-to-location';
 import UniversalDisposable from 'nuclide-commons/UniversalDisposable';
 import {BehaviorSubject} from 'rxjs';
 import FileFamilyAggregator from './FileFamilyAggregator';
 
 class Activation {
   _disposables: UniversalDisposable;
+  _aggregator: ?FileFamilyAggregator;
   _providers: BehaviorSubject<Set<FileFamilyProvider>> = new BehaviorSubject(
     new Set(),
   );
 
-  constructor() {
-    this._disposables = new UniversalDisposable();
+  activate() {
+    this._disposables = new UniversalDisposable(
+      atom.commands.add('atom-workspace', {
+        'file:open-alternate': this._toggleAlternate,
+      }),
+    );
   }
 
   dispose() {
@@ -31,7 +37,14 @@ class Activation {
   }
 
   provideFileFamilyService(): FileFamilyProvider {
-    return new FileFamilyAggregator(this._providers.asObservable());
+    if (this._aggregator) {
+      return this._aggregator;
+    }
+
+    const aggregator = new FileFamilyAggregator(this._providers.asObservable());
+    this._aggregator = aggregator;
+    this._disposables.add(aggregator);
+    return aggregator;
   }
 
   consumeFileFamilyProvider(provider: FileFamilyProvider): IDisposable {
@@ -45,6 +58,33 @@ class Activation {
       this._providers.next(withoutProvider);
     });
   }
+
+  _toggleAlternate = async () => {
+    const provider = this._aggregator;
+    if (provider == null) {
+      return;
+    }
+
+    const activeEditor = atom.workspace.getActiveTextEditor();
+    if (activeEditor == null) {
+      return;
+    }
+
+    const activeUri = activeEditor.getURI();
+    if (activeUri == null) {
+      return;
+    }
+
+    const graph = await provider.getRelatedFiles(activeUri);
+    const testRelation = graph.relations.find(
+      r =>
+        r.from === activeUri &&
+        (r.labels.has('test') || r.labels.has('alternate')),
+    );
+    if (testRelation != null) {
+      await goToLocation(testRelation.to);
+    }
+  };
 }
 
 createPackage(module.exports, Activation);
