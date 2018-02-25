ca68b8d62861140ef308880e47e74fd77cd294b1
Will Binns-Smith <wbinnssmith@fb.com>
2018-02-24 21:20:51 -0800

    Remove nuclide-node-transpiler dependency from debugger modules
    Summary: This removes the nuclide-node-transpiler dependency from nuclide-debugger-common and nuclide-debugger-vsps, but maintains it as a `devDependency` in nuclide-debugger-vsps.
    
    Reviewed By: hansonw
    
    Differential Revision: D7077165
    
    fbshipit-source-id: 2845114d71520def5efd06e5cd52f3f690843f6a

--- modules/nuclide-debugger-vsps/vscode-ocaml/vscode-debugger-entry.js
+++ modules/nuclide-debugger-vsps/vscode-ocaml/vscode-debugger-entry.js
@@ -24,6 +24,7 @@
 // eslint-disable-next-line rulesdir/modules-dependencies
 const {__DEV__} = require('../../nuclide-node-transpiler/lib/env');
 if (__DEV__) {
+  // eslint-disable-next-line rulesdir/modules-dependencies
   require('nuclide-node-transpiler');
 }
 require('./OCamlDebugger');
