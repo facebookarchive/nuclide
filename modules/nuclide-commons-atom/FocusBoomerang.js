59dbac9ca5c8c174a9abb39b8ac2c293e6b42a64
Jordan Eldredge <jeldredge@fb.com>
2018-02-27 15:21:33 -0800

    Return focus after dismissing Breadcrumbs from the keyboard
    Summary: This also introduces a shared abstraction that various modal interfaces can use to return focus to its initial location.
    
    Reviewed By: wbinnssmith
    
    Differential Revision: D7090230
    
    fbshipit-source-id: daa20f67f8a833588a28f42bfd791fb72313482c

---
+++ modules/nuclide-commons-atom/FocusBoomerang.js
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @flow
+ * @format
+ */
+
+type Focus = {
+  node: ?HTMLElement,
+  pane: ?atom$Pane,
+};
+
+export default class FocusBoomerang {
+  _focus: ?Focus;
+
+  recordFocus(): void {
+    if (this._focus != null) {
+      return;
+    }
+
+    this._focus = {
+      node: document.activeElement,
+      pane: atom.workspace.getActivePane(),
+    };
+  }
+
+  returnFocus(): void {
+    if (this._focus == null) {
+      return;
+    }
+    const {node, pane} = this._focus;
+    if (node != null && document.body != null && document.body.contains(node)) {
+      node.focus();
+      return;
+    }
+    if (pane != null) {
+      pane.activate();
+    }
+  }
+}
