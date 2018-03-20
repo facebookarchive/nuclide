76e8bdf43aa9929cebee8d0fceebc949700cb73a
Will Binns-Smith <wbinnssmith@fb.com>
2018-03-20 12:11:16 -0700

    Update create-package to use Jest
    Reviewed By: matthewwithanm
    
    Differential Revision: D7264164
    
    fbshipit-source-id: 95d1a046998c0a2b94005289d51159abe4072461

--- scripts/create-package.py
+++ scripts/create-package.py
@@ -5,7 +5,6 @@
 #
 # This source code is licensed under the license found in the LICENSE file in
 # the root directory of this source tree.
-
 '''
 Utility to create a new feature package for Nuclide. Usage:
 
@@ -18,13 +17,19 @@ This will create the directory and all of the necessary boilerplate:
         +-- lib/main.js
 '''
 
+from __future__ import print_function
+
 import commands
 import os
 import os.path
-import subprocess
 import sys
 
 from lib.package_manager import PACKAGES_PATH
+
+try:
+    input = raw_input
+except NameError:
+    pass
 
 NPM = 'npm'
 APM = 'apm'
@@ -32,9 +37,12 @@ NODE_PACKAGE = 'node'
 ATOM_PACKAGE = 'atom'
 DEFAULT_PREFIX = 'nuclide'
 PACKAGE_PREFIXES = [DEFAULT_PREFIX, 'fb', 'sample']
-ATOM_TEST_RUNNER_FILE = os.path.join(PACKAGES_PATH, '../lib/test-runner-entry.js')
-NUCLIDE_JASMINE_BIN = os.path.join(PACKAGES_PATH,
-                                   'nuclide-jasmine/bin/jasmine-node-transpiled')
+ATOM_TEST_RUNNER_FILE = os.path.join(
+    PACKAGES_PATH, '../modules/nuclide-jest/atom-runner.js'
+)
+NUCLIDE_JEST_BIN = os.path.join(
+    PACKAGES_PATH, '../modules/nuclide-jest/bin/jest-node'
+)
 
 USERNAME = commands.getoutput('whoami')
 
@@ -124,7 +132,7 @@ NODE_NPM_PACKAGE_JSON_TEMPLATE = """\
     "testRunner": "npm"
   },
   "scripts": {
-    "test": "node %s spec"
+    "test": "node %s"
   }
 }
 """
@@ -138,6 +146,7 @@ def get_package_name(user_input):
     # If user input does not start with any known prefix, default to 'nuclide'.
     # For example, given 'foo', we shall return 'nuclide-foo'
     return DEFAULT_PREFIX + '-' + user_input
+
 
 def create_package(package_name, package_type, test_runner):
     # Create the directory for the new package.
@@ -158,48 +167,57 @@ def create_package(package_name, package_type, test_runner):
     with open(os.path.join(pkg_dir, 'package.json'), 'w') as f:
         if package_type == NODE_PACKAGE:
             if test_runner == NPM:
-                f.write(NODE_NPM_PACKAGE_JSON_TEMPLATE % \
-                    (package_name, os.path.relpath(NUCLIDE_JASMINE_BIN, pkg_dir)))
+                f.write(NODE_NPM_PACKAGE_JSON_TEMPLATE %
+                        (package_name, os.path.relpath(NUCLIDE_JEST_BIN, pkg_dir)))
             else:
-                f.write(NODE_APM_PACKAGE_JSON_TEMPLATE % \
-                    (package_name, os.path.relpath(ATOM_TEST_RUNNER_FILE, pkg_dir)))
+                f.write(NODE_APM_PACKAGE_JSON_TEMPLATE %
+                        (package_name, os.path.relpath(ATOM_TEST_RUNNER_FILE, pkg_dir)))
         else:
-            f.write(ATOM_APM_PACKAGE_JSON_TEMPLATE % \
-                (package_name, os.path.relpath(ATOM_TEST_RUNNER_FILE, pkg_dir)))
+            f.write(ATOM_APM_PACKAGE_JSON_TEMPLATE %
+                    (package_name, os.path.relpath(ATOM_TEST_RUNNER_FILE, pkg_dir)))
 
-    print 'New package created at: %s.' % pkg_dir
+    print('New package created at: %s.' % pkg_dir)
+
 
 def prompt_and_create_package(user_input):
     if user_input is None:
-        user_input = raw_input('Enter the name of your new package: ').strip()
+        user_input = input('Enter the name of your new package: ').strip()
     package_name = get_package_name(user_input)
-    print 'Using %s as package name under pkg/%s.' % (package_name, package_name)
+    print('Using %s as package name under pkg/%s.' % (
+        package_name, package_name
+    ))
 
-    answer = raw_input('Can this package be used outside of Atom ' +
-                       '(e.g., on a server)? [Y/n]: ').strip()
+    answer = input(
+        'Can this package be used outside of Atom ' +
+        '(e.g., on a server)? [Y/n]: '
+    ).strip()
     if len(answer) > 0 and (answer[0] == 'N' or answer[0] == 'n'):
         test_runner = APM
         package_type = None
     else:
         test_runner = NPM
         package_type = NODE_PACKAGE
-    print 'Using %s as test runner.' % test_runner
+    print('Using %s as test runner.' % test_runner)
 
     if package_type is None:
-        answer = raw_input('Can this package be loaded synchronously via ' +
-                           'require() in Atom? [Y/n]: ').strip()
+        answer = input(
+            'Can this package be loaded synchronously via ' +
+            'require() in Atom? [Y/n]: '
+        ).strip()
         if len(answer) > 0 and (answer[0] == 'N' or answer[0] == 'n'):
             package_type = ATOM_PACKAGE
         else:
             package_type = NODE_PACKAGE
-    print 'Using %s as package type.' % package_type
+    print('Using %s as package type.' % package_type)
 
     create_package(package_name, package_type, test_runner)
+
 
 def main():
     args = sys.argv[1:]
     user_input = args[0] if len(args) > 0 else None
     prompt_and_create_package(user_input)
+
 
 if __name__ == '__main__':
     main()
