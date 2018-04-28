ee825fe02e78738cb69ca9eeb780a5f154ac8107
Michael Bolin <mbolin@fb.com>
2018-04-27 17:49:47 -0700

    atom-notify CLI for sending notifications from machine running Nuclide server to the client.
    Summary:
    This makes it possibly to run something like:
    
    ```
    atom-notify hello world!
    ```
    
    on your server, which will then take the arguments, join them on `' '`,
    and display them as a message in an Atom notification. Although
    Atom notifications have many options, the only ones that we
    expose from the CLI for now are:
    
    * `--success`
    * `--info`
    * `--warning`
    * `--error`
    
    which affects the UI treatment of the notification in Atom.
    
    Reviewed By: hansonw
    
    Differential Revision: D7719532
    
    fbshipit-source-id: 1d95a667d7447cddc1b6c8878560ed2e6d355cec

---
+++ pkg/nuclide-remote-atom-rpc/bin/atom-notify-node.js
@@ -0,0 +1,121 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ *
+ * @flow
+ * @format
+ */
+
+import yargs from 'yargs';
+import {getCommands} from './CommandClient';
+import {
+  reportConnectionErrorAndExit,
+  setupErrorHandling,
+  setupLogging,
+  EXIT_CODE_INVALID_ARGUMENTS,
+  EXIT_CODE_SUCCESS,
+  FailedConnectionError,
+} from './errors';
+
+/**
+ * Command-line tool that allows you to send a message that gets displayed as
+ * an Atom notification in all Atom windows connected to this server.
+ *
+ * This was inspired by `zwrite` from the Zephyr notification service:
+ * https://en.wikipedia.org/wiki/Zephyr_(protocol).
+ * See the `man` page:
+ * https://www.gsp.com/cgi-bin/man.cgi?section=1&topic=zwrite.
+ */
+
+async function main(argv): Promise<number> {
+  setupLogging();
+  setupErrorHandling();
+
+  // Connect to the Nuclide server running on this host, if it exists.
+  let commands = null;
+  try {
+    commands = await getCommands(argv, /* rejectIfZeroConnections */ true);
+  } catch (error) {
+    if (error instanceof FailedConnectionError) {
+      // Note this does not throw: reportConnectionErrorAndExit()
+      // does not return. However, we use throw to convince Flow
+      // that any code after this is unreachable.
+      throw reportConnectionErrorAndExit(error);
+    } else {
+      throw error;
+    }
+  }
+
+  const message = argv._.join(' ');
+  if (message === '') {
+    // eslint-disable-next-line no-console
+    console.error('Cowardly refusing to send an empty message.'); // HT tar
+    return EXIT_CODE_INVALID_ARGUMENTS;
+  }
+
+  let type = 'info';
+  if (argv.error) {
+    type = 'error';
+  } else if (argv.warning) {
+    type = 'warning';
+  } else if (argv.success) {
+    type = 'success';
+  }
+
+  const notification = {
+    message,
+    type,
+    // Counterintuitively, by setting dismissable to true, it makes it sticky
+    // because in Atom, "dismissable" means "has a close box", which means
+    // it does not go away until the user clicks on the X.
+    dismissable: true,
+  };
+  await commands.addNotification(notification);
+
+  return EXIT_CODE_SUCCESS;
+}
+
+async function run(): Promise<void> {
+  const {argv} = yargs
+    .usage('Usage: atom-notify')
+    .help('h')
+    .alias('h', 'help')
+    .option('p', {
+      alias: 'port',
+      describe: 'Port for connecting to nuclide',
+      type: 'number',
+    })
+    .option('f', {
+      alias: 'family',
+      describe:
+        'Address family for connecting to nuclide. Either "IPv4" or "IPv6".',
+      type: 'string',
+    })
+
+    // Note that we do not support 'fatal' from the CLI because that tells
+    // the user that "This is likely a bug in Atom", which is misleading.
+    .option('success', {
+      describe: 'Display as success.',
+      type: 'boolean',
+    })
+    .option('info', {
+      describe: 'Display as info.',
+      type: 'boolean',
+    })
+    .option('warning', {
+      describe: 'Display as warning.',
+      type: 'boolean',
+    })
+    .option('error', {
+      describe: 'Display as error.',
+      type: 'boolean',
+    });
+
+  const exitCode = await main(argv);
+  process.exit(exitCode);
+}
+
+run();
