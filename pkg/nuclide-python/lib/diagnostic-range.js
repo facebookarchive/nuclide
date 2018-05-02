32025d94cf58970ee23bbcd80e52b921ac5a0dae
Hanson Wang <hansonw@fb.com>
2018-05-01 18:11:06 -0700

    Remove feature where F401 lints highlight the import's range
    Summary:
    There was this feature implemented a long time ago where F401 lint warnings (e.g. "F401 'datetime' imported but unused") would try to highlight the exact token that's imported (so in this case we'd underline 'datetime' rather than the whole line.)
    
    It turns out that this isn't working anymore with the newer Python grammars, and it also causes performance issues because it ends up scanning the entire file. This could be fixed up a little bit but I think it's easier if we just get rid of it.
    
    Reviewed By: pelmers
    
    Differential Revision: D7835614
    
    fbshipit-source-id: b2ae6c660c64c4e182ac07636ca6298a07b38666

--- pkg/nuclide-python/lib/diagnostic-range.js
+++ pkg/nuclide-python/lib/diagnostic-range.js
@@ -17,61 +17,6 @@ import {getLogger} from 'log4js';
 
 const logger = getLogger('nuclide-python');
 
-function tokenizedLineForRow(
-  editor: atom$TextEditor,
-  line: number,
-): any /* atom$TokenizedLine */ {
-  const tokenBuffer = editor.hasOwnProperty('displayBuffer')
-    ? (editor: any).displayBuffer.tokenizedBuffer
-    : (editor: any).tokenizedBuffer;
-  return tokenBuffer.tokenizedLineForRow(line);
-}
-
-// Finds the range of the module name from a pyflakes F4XX message.
-// Assumes that the module name exists.
-// Ported from https://github.com/AtomLinter/linter-flake8
-function getModuleNameRange(
-  message: string,
-  line: number,
-  editor: atom$TextEditor,
-): ?Range {
-  // Split on space or dot to get the basename or alias, i.e. retrieve <a> in
-  // "from .. import <a>" or "from .. import .. as <a>".
-  const match = /'([^']+)'/.exec(message);
-  if (match == null) {
-    return null;
-  }
-  const symbol = match[1].split(/\s|\./).pop();
-
-  let foundImport = false;
-  let lineNumber = line;
-  for (;;) {
-    let offset = 0;
-    const tokenizedLine = tokenizedLineForRow(editor, lineNumber);
-    if (!tokenizedLine) {
-      break;
-    }
-    for (let i = 0; i < tokenizedLine.tokens.length; i++) {
-      const token = tokenizedLine.tokens[i];
-      if (foundImport && token.value === symbol) {
-        return new Range(
-          [lineNumber, offset],
-          [lineNumber, offset + token.value.length],
-        );
-      }
-      if (
-        token.value === 'import' &&
-        token.scopes.indexOf('keyword.control.import.python') >= 0
-      ) {
-        foundImport = true;
-      }
-      offset += token.value.length;
-    }
-    lineNumber += 1;
-  }
-  logger.warn(`getModuleNameRange failed for message: ${message}`);
-}
-
 // Computes an appropriate underline range using the diagnostic type information.
 // Range variants include underlining the entire line, entire trimmed line,
 // or a word or whitespace range within the line.
@@ -140,13 +85,7 @@ export function getDiagnosticRange(
         break;
       // pyflakes - import related messages
       case 'F4':
-        if (code === 'F401') {
-          // 'XXX' is imported but not used
-          const range = getModuleNameRange(message, line, editor);
-          if (range != null) {
-            return range;
-          }
-        } else if (code === 'F405') {
+        if (code === 'F405') {
           // <XXX> may be undefined, or defined from import *
           const word = wordAtPosition(editor, new Point(line, column));
           if (word) {
