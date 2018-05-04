956c957af47c28e0c087ce3d860c99adb3df1a8f
Will Binns-Smith <wbinnssmith@fb.com>
2018-05-03 23:12:37 -0700

    Implement RPC side of CSS LSP
    Summary: This implements launching the css lsp server over the rpc.
    
    Reviewed By: hansonw
    
    Differential Revision: D7842020
    
    fbshipit-source-id: a75ae8986d9bebeff92884a65bcde2db38437859

---
+++ pkg/nuclide-css-lsp-client-rpc/lib/CSSLSPService.js
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ *
+ * @flow
+ * @format
+ */
+
+import type {LogLevel} from '../../nuclide-logging/lib/rpc-types';
+
+import type {NuclideUri} from 'nuclide-commons/nuclideUri';
+import type {HostServices} from '../../nuclide-language-service-rpc/lib/rpc-types';
+import type {FileNotifier} from '../../nuclide-open-files-rpc/lib/rpc-types';
+
+/* LanguageService related type imports */
+import type {LanguageService} from '../../nuclide-language-service/lib/LanguageService';
+
+import {createMultiLspLanguageService} from '../../nuclide-vscode-language-service-rpc';
+
+export async function initializeLsp(
+  projectFileNames: Array<string>,
+  fileExtensions: Array<NuclideUri>,
+  logLevel: LogLevel,
+  fileNotifier: FileNotifier,
+  host: HostServices,
+  initializationOptions: Object,
+): Promise<?LanguageService> {
+  return createMultiLspLanguageService(
+    'css',
+    process.execPath,
+    [require.resolve('vscode-css-languageserver-bin/cssServerMain'), '--stdio'],
+    {
+      fileNotifier,
+      host,
+      logCategory: 'CSS',
+      logLevel,
+      projectFileNames,
+      fileExtensions,
+      initializationOptions,
+      spawnOptions: {env: {...process.env, ELECTRON_RUN_AS_NODE: '1'}},
+    },
+  );
+}
