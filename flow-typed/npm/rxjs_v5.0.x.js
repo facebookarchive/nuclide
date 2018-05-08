2093c8560f80e9510eaf4288c8b75375072a5b3d
Matthew Dapena-Tretter <matthewwithanm@fb.com>
2018-05-08 13:04:39 -0700

    RxJS Flow types: allow filtering maybe types with Boolean
    Summary: Corresponding flow-typed PR is flowtype/flow-typed#2185
    
    Reviewed By: hansonw
    
    Differential Revision: D7912629
    
    fbshipit-source-id: ab4a03a421869705254ce7e6c755f341b229ece2

--- flow-typed/npm/rxjs_v5.0.x.js
+++ flow-typed/npm/rxjs_v5.0.x.js
@@ -446,6 +446,7 @@ declare class rxjs$Observable<+T> {
     scheduler?: rxjs$SchedulerClass
   ): rxjs$Observable<T>;
 
+  filter(predicate: typeof Boolean): rxjs$Observable<$NonMaybeType<T>>;
   filter(
     predicate: (value: T, index: number) => boolean,
     thisArg?: any
