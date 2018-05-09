92c256c8e74a4f110f16ca99688be05e97bd79ba
Wanderley da Silva <wander@fb.com>
2018-05-08 19:57:39 -0700

    Add @flow strict(-local) to Nuclide files
    Summary:
    This diff adds `flow strict` and `flow strict-local` to Nuclide's codebase. I used the following script (thanks gkz for sharing this) to create this diff:
    
    ```
    
    ag -L --ignore __snapshots__ 'flow strict|noflow|generated|partially-generated' | \
      ag '\.js$' | \
      xargs ag -l 'flow' | \
      sort -u > /tmp/flow-temp
    
    cat /tmp/flow-temp | xargs ag -L 'flow strict' | xargs sed -i '' 's/flow$/flow strict/'
    until flow; do
      flow --json | jq -r '.errors[].message[0].path' | sort -u | xargs hg revert;
    done
    
    cat /tmp/flow-temp | xargs ag -L 'flow strict' | xargs sed -i '' 's/flow$/flow strict-local/'
    until flow; do
      flow --json | jq -r '.errors[].message[0].path' | sort -u | xargs hg revert;
    done
    ```
    
    Reviewed By: hansonw
    
    Differential Revision: D7856721
    
    fbshipit-source-id: 35d6ead8ecbf021edc5a428cc7285d31e9ca7e2e

--- modules/nuclide-commons/spec/fixtures/symbol-definition-preview-sample.js
+++ modules/nuclide-commons/spec/fixtures/symbol-definition-preview-sample.js
@@ -5,7 +5,7 @@
  * This source code is licensed under the license found in the LICENSE file in
  * the root directory of this source tree.
  *
- * @flow
+ * @flow strict
  */
 // license header above without @format
 // eslint-disable-next-line
