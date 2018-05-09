c63690e648e5b4109a2645b15f5b0249f47f911d
Aaditya Sriram <asriram@fb.com>
2018-05-09 13:01:28 -0700

    Fix junk value being sent in callback on confirm
    Summary: The value being sent in the callback has nothing to do with the value itself, it is sending back details about the clicked button. This change sends back the actual value that is passed down.
    
    Reviewed By: evangrayk
    
    Differential Revision: D7916183
    
    fbshipit-source-id: 5380c33464426cb29d59080d32fcb2cb1583e5bb

--- modules/nuclide-commons-ui/SplitButtonDropdown.js
+++ modules/nuclide-commons-ui/SplitButtonDropdown.js
@@ -65,7 +65,7 @@ export class SplitButtonDropdown extends React.Component<Props> {
           size={size == null ? undefined : size}
           disabled={confirmDisabled === true}
           icon={selectedOption.icon || undefined}
-          onClick={onConfirm}>
+          onClick={onConfirm.bind(null, value)}>
           {// flowlint-next-line sketchy-null-mixed:off, sketchy-null-string:off
           selectedOption.selectedLabel || selectedOption.label || ''}
         </ButtonComponent>
