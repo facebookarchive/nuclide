d910ba62fcc66926602035cfd16b1cac0bd0edd3
Will Pitts <wipi@fb.com>
2018-05-16 11:18:12 -0700

    Fix double code lens issue
    Summary: The current code lens client-side implementation has a race condition that can lead to duplicated code lenses showing up, which causes all kinds of mess.  This change rewrites the code lens listener using RxJS to make it more reliable and less likely to race.
    
    Reviewed By: hansonw
    
    Differential Revision: D7942222
    
    fbshipit-source-id: 3109ca37d9d1b299e358c43b2f22423d89bc416e

--- pkg/nuclide-ocaml/lib/CodeLensListener.js
+++ pkg/nuclide-ocaml/lib/CodeLensListener.js
@@ -17,8 +17,11 @@ import type {
 import type {FileVersion} from '../../nuclide-open-files-rpc/lib/rpc-types';
 
 import featureConfig from 'nuclide-commons-atom/feature-config';
-import {nextTick} from 'nuclide-commons/promise';
+import {observeEditorDestroy} from 'nuclide-commons-atom/text-editor';
+import {observableFromSubscribeFunction} from 'nuclide-commons/event';
+import {microtask} from 'nuclide-commons/observable';
 import UniversalDisposable from 'nuclide-commons/UniversalDisposable';
+import {Observable} from 'rxjs';
 import {getFileVersionOfEditor} from '../../nuclide-open-files';
 import createDOMPurify from 'dompurify';
 
@@ -26,223 +29,229 @@ const domPurify = createDOMPurify();
 
 const RETRIES = 3;
 
-type ResolveInfo = {
-  fileVersion: FileVersion,
-  languageService: LanguageService,
-  lenses: Array<{
-    lens: CodeLensData,
-    element: HTMLElement,
-    resolved: boolean,
-    retries: number,
-  }>,
+type ResolvableLens = {
+  lens: CodeLensData,
+  element: HTMLElement,
+  resolved: boolean,
+  retries: number,
 };
 
-const allEditors: Map<atom$TextEditor, ResolveInfo> = new Map();
-
-async function getCodeLens(
-  languageService: LanguageService,
+type ResolveInfo = {
+  editor: atom$TextEditor,
   fileVersion: FileVersion,
-): Promise<?Array<CodeLensData>> {
-  // This method is all about retries and waits, so it needs to await inside a
-  // loop. We'd rather not do retries, but until we get some way for LSP servers
-  // to differentiate "I can't give an answer right now but will soon" vs "I
-  // have no answer full stop" vs "here's an event for when I will have an
-  // answer," this is the best we can do.
-  for (let i = 0; i < RETRIES; i++) {
-    // eslint-disable-next-line no-await-in-loop
-    const codeLens: ?Array<CodeLensData> = await languageService.getCodeLens(
-      fileVersion,
-    );
-    if (codeLens != null) {
-      // sort code lenses based on their row numbers from top to bottom, so
-      // later their resolution can start in the same order.
-      return codeLens.sort((lens1, lens2) => {
-        return lens1.range.start.row - lens2.range.start.row;
+  languageService: LanguageService,
+  lenses: Array<ResolvableLens>,
+};
+
+function makeResolvableLens(
+  editor: atom$TextEditor,
+  markerLayer: atom$DisplayMarkerLayer,
+  lens: CodeLensData,
+): ResolvableLens {
+  const marker = markerLayer.markBufferPosition(lens.range.start);
+  const element = document.createElement('span');
+
+  element.classList.add('code-lens-content');
+
+  // Put in a nonbreaking space to reserve the space in the editor. If
+  // the space is already reserved, Atom won't have to scroll the
+  // editor down as we resolve code lenses.
+  element.innerHTML = '\xa0';
+  const listener = () => {
+    if (
+      element.innerText != null &&
+      featureConfig.get('nuclide-ocaml.codeLensCopy')
+    ) {
+      atom.clipboard.write(element.innerText);
+      const tooltipDispose = atom.tooltips.add(element, {
+        title: 'Copied code lens to clipboard.',
+        placement: 'auto',
+        trigger: 'manual',
       });
+      setTimeout(() => tooltipDispose.dispose(), 3000);
     }
+  };
+  element.addEventListener('click', listener);
+  marker.onDidDestroy(() => element.removeEventListener('click', listener));
 
-    // eslint-disable-next-line no-await-in-loop
-    await new Promise((resolve, reject) => {
-      // Standard linear backoff.
-      setTimeout(resolve, (i + 1) * 1000);
-    });
-  }
+  const leadingWhitespace = document.createElement('span');
+  leadingWhitespace.innerText = ' '.repeat(lens.range.start.column);
 
-  return null;
+  // We do a span inside a div so that the tooltip and clickable area
+  // only cover the part of the code lens that has text, but the
+  // code-lens style will be applied to the entire editor row.
+  const containingElement = document.createElement('div');
+  containingElement.classList.add('code-lens');
+  containingElement.appendChild(leadingWhitespace);
+  containingElement.appendChild(element);
+
+  editor.decorateMarker(marker, {
+    type: 'block',
+    position: 'before',
+    item: containingElement,
+  });
+
+  return {lens, element, marker, resolved: false, retries: 0};
 }
 
-function resolveVisible(): void {
-  for (const [editor, resolveInfo] of allEditors.entries()) {
-    // Currently undocumented, but there's an open PR to add these to the public
-    // API: https://github.com/atom/atom/issues/15559
-    //  -wipi
-    const firstLine = (editor: any).element.getFirstVisibleScreenRow();
-    const lastLine = (editor: any).element.getLastVisibleScreenRow() + 1;
-
-    // If this begins to become a performance concern we can sort the list and
-    // then do a binary search to find the starting and ending range, but in
-    // practice I've observed that it's rare for a file to have more than a few
-    // dozen (few hundred for large files) code lenses, and the weight of
-    // going over the network and resolving the code lenses is several orders of
-    // magnitude more than looping over a small array and doing simple numerical
-    // comparisons.
-    //  -wipi
-    const resolvableLenses = resolveInfo.lenses.filter(
-      lensInfo =>
-        !lensInfo.resolved &&
-        lensInfo.lens.range.start.row >= firstLine &&
-        lensInfo.lens.range.start.row <= lastLine,
+function getCodeLensPositions(
+  atomLanguageService: AtomLanguageService<LanguageService>,
+  logger: log4js$Logger,
+  editor: atom$TextEditor,
+): Observable<?{
+  languageService: LanguageService,
+  fileVersion: FileVersion,
+  codeLens: Array<CodeLensData>,
+}> {
+  const uri = editor.getPath();
+  return Observable.defer(() =>
+    atomLanguageService.getLanguageServiceForUri(uri),
+  ).switchMap(languageService => {
+    if (languageService == null) {
+      return Observable.of(null);
+    }
+    return Observable.defer(() => getFileVersionOfEditor(editor)).switchMap(
+      fileVersion => {
+        if (fileVersion == null) {
+          return Observable.of(null);
+        }
+        return Observable.defer(async () => {
+          const codeLens = await languageService.getCodeLens(fileVersion);
+          if (codeLens == null) {
+            throw Error('Could not retrieve code lenses.');
+          }
+          return {languageService, fileVersion, codeLens};
+        })
+          .retryWhen(errs =>
+            errs.zip(Observable.range(1, RETRIES)).flatMap((_, retryCount) => {
+              return Observable.timer(retryCount * 1000);
+            }),
+          )
+          .defaultIfEmpty(null);
+      },
     );
-    resolvableLenses.forEach(async lensInfo => {
-      // Set this *before* we get the data so we don't send duplicate
-      // requests.
+  });
+}
+
+function markCodeLensPositions(
+  languageService: LanguageService,
+  editor: atom$TextEditor,
+  markerLayer: atom$DisplayMarkerLayer,
+  fileVersion: FileVersion,
+  codeLens: Array<CodeLensData>,
+): ResolveInfo {
+  // Sort code lenses based on their row numbers from top to bottom, so
+  // later their resolution can start in the same order.
+  return {
+    editor,
+    fileVersion,
+    languageService,
+    lenses: codeLens
+      .sort((lens1, lens2) => lens1.range.start.row - lens2.range.start.row)
+      .map(lens => makeResolvableLens(editor, markerLayer, lens)),
+  };
+}
+
+function resolveVisible(resolveInfo: ResolveInfo): Observable<?CodeLensData> {
+  const editor = resolveInfo.editor;
+
+  // Currently undocumented, but there's an open PR to add these to the public
+  // API: https://github.com/atom/atom/issues/15559
+  const firstLine = (editor: any).element.getFirstVisibleScreenRow();
+  const lastLine = (editor: any).element.getLastVisibleScreenRow() + 1;
+
+  // If this begins to become a performance concern we can sort the list and
+  // then do a binary search to find the starting and ending range, but in
+  // practice I've observed that it's rare for a file to have more than a few
+  // dozen (few hundred for large files) code lenses, and the weight of
+  // going over the network and resolving the code lenses is several orders of
+  // magnitude more than looping over a small array and doing simple numerical
+  // comparisons.
+  const resolvableLenses = resolveInfo.lenses.filter(
+    lensInfo =>
+      !lensInfo.resolved &&
+      lensInfo.lens.range.start.row >= firstLine &&
+      lensInfo.lens.range.start.row <= lastLine,
+  );
+
+  return Observable.from(resolvableLenses).mergeMap(lensInfo =>
+    Observable.defer(() => {
+      // Set this *before* we get the data so we don't send duplicate requests.
       lensInfo.resolved = true;
-      const lens: ?CodeLensData = await resolveInfo.languageService.resolveCodeLens(
+      return resolveInfo.languageService.resolveCodeLens(
         resolveInfo.fileVersion.filePath,
         lensInfo.lens,
       );
-
-      const currentInfo = allEditors.get(editor);
-      if (
-        currentInfo == null ||
-        currentInfo.fileVersion.version !== resolveInfo.fileVersion.version
-      ) {
-        // This request is stale.
-        return;
-      }
-
+    }).do((lens: ?CodeLensData) => {
       if (lens != null && lens.command != null) {
-        lensInfo.element.innerHTML = domPurify.sanitize(lens.command.title, {
+        const text = domPurify.sanitize(lens.command.title, {
           ALLOWED_TAGS: [],
         });
+        lensInfo.element.innerHTML = text;
       } else if (lensInfo.retries < RETRIES) {
         lensInfo.resolved = false;
         lensInfo.retries++;
       }
-    });
-  }
+    }),
+  );
 }
 
 export function observeForCodeLens(
   atomLanguageService: AtomLanguageService<LanguageService>,
   logger: log4js$Logger,
 ): IDisposable {
-  const disposable = new UniversalDisposable();
-  disposable.add(
-    atom.workspace.observeTextEditors(async editor => {
-      let isFirstUpdate = true;
-      const editorDisposable = new UniversalDisposable();
-      editorDisposable.add(
-        editor.onDidDestroy(() => {
-          logger.info(`Destroying ${JSON.stringify(editor.getPath())}`);
-          editorDisposable.dispose();
-          disposable.remove(editorDisposable);
-        }),
-      );
-
-      let elementsDisposable;
-      const markerLayer = editor.addMarkerLayer();
-      const updateCodeLens = async () => {
-        const uri = editor.getPath();
-        const languageService: ?LanguageService = await atomLanguageService.getLanguageServiceForUri(
-          uri,
+  return new UniversalDisposable(
+    observableFromSubscribeFunction(
+      atom.workspace.observeTextEditors.bind(atom.workspace),
+    )
+      // If the editor is already open when the user launches Atom, then *two*
+      // events will be fired from observeTextEditors, so group by the editor ID
+      // and then only take the first in order to avoid the second event.
+      // The cause for this is:
+      //  1. Atom loads the text editor and adds it to the registry
+      //  2. Atom loads the grammar and then loads the OCaml package (because it
+      //     only activates when an OCaml file is opened). At this point it's
+      //     able to get the list of editors, and it fires observeTextEditors.
+      //  3. did-add-text-editor is fired, causing the duplicated event.
+      // TODO(wipi): remove once https://github.com/atom/atom/pull/17299 is
+      // released.
+      .distinct(editor => editor.id)
+      .mergeMap(editor => {
+        const markerLayer = editor.addMarkerLayer();
+        return (
+          observableFromSubscribeFunction(editor.onDidSave.bind(editor))
+            // Add an additional event into the stream so that we don't need to
+            // save the file in order to get the first set of code lenses.
+            .startWith(null)
+            .switchMap(evt =>
+              getCodeLensPositions(atomLanguageService, logger, editor),
+            )
+            .do(() => markerLayer.clear())
+            .filter(Boolean)
+            .map(positions =>
+              markCodeLensPositions(
+                positions.languageService,
+                editor,
+                markerLayer,
+                positions.fileVersion,
+                positions.codeLens,
+              ),
+            )
+            .switchMap((resolveInfo, iteration) =>
+              Observable.merge(
+                iteration === 0
+                  ? microtask.do(() => {
+                      editor.scrollToCursorPosition({center: true});
+                    })
+                  : Observable.empty(),
+                Observable.timer(0, 1000).concatMap(() =>
+                  resolveVisible(resolveInfo),
+                ),
+              ),
+            )
+            .takeUntil(observeEditorDestroy(editor))
         );
-        if (languageService == null) {
-          logger.warn(
-            `Could not find language service for ${JSON.stringify(uri)}.`,
-          );
-          return null;
-        }
-
-        const fileVersion = await getFileVersionOfEditor(editor);
-        if (fileVersion == null) {
-          logger.warn(
-            `Could not find file version for ${JSON.stringify(uri)}.`,
-          );
-          return null;
-        }
-
-        const codeLens = await getCodeLens(languageService, fileVersion);
-        if (codeLens != null) {
-          markerLayer.clear();
-
-          if (elementsDisposable != null) {
-            elementsDisposable.dispose();
-            editorDisposable.remove(elementsDisposable);
-          }
-
-          elementsDisposable = new UniversalDisposable();
-          editorDisposable.add(elementsDisposable);
-
-          const lenses = codeLens.map(lens => {
-            const marker = markerLayer.markBufferPosition([
-              lens.range.start.row,
-              lens.range.start.column,
-            ]);
-
-            const element = document.createElement('span');
-            element.classList.add('code-lens-content');
-
-            // Put in a nonbreaking space to reserve the space in the editor. If
-            // the space is already reserved, Atom won't have to scroll the
-            // editor down as we resolve code lenses.
-            element.innerHTML = '\xa0';
-
-            const leadingWhitespace = document.createElement('span');
-            leadingWhitespace.innerText = ' '.repeat(lens.range.start.column);
-
-            // We do a span inside a div so that the tooltip and clickable area
-            // only cover the part of the code lens that has text, but the
-            // code-lens style will be applied to the entire editor row.
-            const containingElement = document.createElement('div');
-            containingElement.classList.add('code-lens');
-            containingElement.appendChild(leadingWhitespace);
-            containingElement.appendChild(element);
-
-            editor.decorateMarker(marker, {
-              type: 'block',
-              position: 'before',
-              item: containingElement,
-            });
-            element.addEventListener('click', () => {
-              if (
-                element.innerText != null &&
-                featureConfig.get('nuclide-ocaml.codeLensCopy')
-              ) {
-                atom.clipboard.write(element.innerText);
-                const tooltipDispose = atom.tooltips.add(element, {
-                  title: 'Copied code lens to clipboard.',
-                  placement: 'auto',
-                  trigger: 'manual',
-                });
-                setTimeout(() => tooltipDispose.dispose(), 3000);
-              }
-            });
-
-            return {lens, element, resolved: false, retries: 0};
-          });
-
-          allEditors.set(editor, {
-            fileVersion,
-            languageService,
-            lenses,
-          });
-
-          if (isFirstUpdate) {
-            await nextTick();
-            isFirstUpdate = false;
-            editor.scrollToCursorPosition({center: true});
-          }
-        }
-      };
-
-      editorDisposable.add(editor.onDidSave(updateCodeLens));
-      disposable.add(editorDisposable);
-      await updateCodeLens();
-    }),
+      })
+      .subscribe(),
   );
-
-  const resolveVisibleTimeoutID = setInterval(resolveVisible, 1000);
-  disposable.add(() => clearInterval(resolveVisibleTimeoutID));
-  return disposable;
 }
