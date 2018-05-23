9934aeb017b89b7ce9a9a6b52c974c29e0744532
Wanderley da Silva <wander@fb.com>
2018-05-22 18:52:52 -0700

    Update flow-typed libraries
    Summary:
    This diff adds type definitions for third-party libraries. It was generated in two steps. I first updated the flow typed definitions by running `flow-typed install`, then I removed the libraries that were causing flow errors to be sent in a separate diff.
    
    Reference:
    ```
    yarn global add flow-typed
    flow-typed install
    ```
    
    Reviewed By: hansonw
    
    Differential Revision: D8104731
    
    fbshipit-source-id: 98d87f748c5dfd2023724f2f14c7fa30f85af206

---
+++ flow-typed/npm/strip-json-comments_v2.x.x.js
@@ -0,0 +1,9 @@
+// flow-typed signature: 355bccb70c51fc8f5126d5130c2ff1de
+// flow-typed version: b43dff3e0e/strip-json-comments_v2.x.x/flow_>=v0.25.x
+
+declare module 'strip-json-comments' {
+  declare module.exports: (
+    input: string,
+    options?: { whitespace: boolean }
+  ) => string;
+}
