eff22201913abf5d706cd43351fde2b325e9c008
John Armstrong <drmathochist@fb.com>
2018-05-31 17:04:50 -0700

    Catch and log auto-fold range timeout
    Summary: Still not sure what causes the timeouts, but we shouldn't let this get out to throw a red box at the user when it's probably not a huge deal.
    
    Reviewed By: kcaze
    
    Differential Revision: D8228744
    
    fbshipit-source-id: 09345ea46b5fd68b153cc5a3f0c519c7ffe97e60

--- pkg/nuclide-rpc/lib/RpcConnection.js
+++ pkg/nuclide-rpc/lib/RpcConnection.js
@@ -390,7 +390,7 @@ export class RpcConnection<TransportType: Transport> {
         args,
       ),
       returnType,
-      `Calling remote method ${methodName}.`,
+      `Calling remote method ${methodName}`,
     );
   }
 
