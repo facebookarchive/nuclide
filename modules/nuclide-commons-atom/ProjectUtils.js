703f8c1e155334eb20fda3552cf3146e2662f3b7
Matthew Dapena-Tretter <matthewwithanm@fb.com>
2018-06-11 10:57:05 -0700

    Merge ProjectPathUtils into ProjectUtils
    Summary: These will be used across packages.
    
    Reviewed By: wbinnssmith
    
    Differential Revision: D8268864
    
    fbshipit-source-id: 9e010502e76303565485314a12bc9bec77f06781

--- modules/nuclide-commons-atom/ProjectUtils.js
+++ modules/nuclide-commons-atom/ProjectUtils.js
@@ -10,7 +10,11 @@
  * @format
  */
 
+import type {NuclideUri} from 'nuclide-commons/nuclideUri';
+
 import nuclideUri from 'nuclide-commons/nuclideUri';
+import featureConfig from './feature-config';
+import invariant from 'assert';
 
 export function getLabelFromPath(path: string): ?string {
   const parts = nuclideUri.basename(path).split('.');
@@ -37,4 +41,74 @@ function humanizeProjectName(name: string): string {
     .split(/[-_]+/)
     .map(hasCapitalLetters ? id : formatProjectNameWord)
     .join(' ');
+}
+
+/**
+ * Gets the array of paths which can be tried on the local machine to find
+ * the location of `repo`. For example, if repo := fbsource, then we are getting
+ * the paths to fbsource on the user's local machine.
+ */
+export function getLocalPathsForProjectRepo(repo: string): Array<string> {
+  return getPathsForProjectRepoFromLocation(repo, 'localPaths');
+}
+
+/**
+ * Gets the array of paths which can be tried on a remote machine to find
+ * the location of `repo`. For example, if repo := fbsource, then we are getting
+ * the paths to fbsource on the user's remote machine.
+ */
+export function getRemotePathsForProjectRepo(repo: string): Array<string> {
+  return getPathsForProjectRepoFromLocation(repo, 'remotePaths');
+}
+
+function getPathsForProjectRepoFromLocation(
+  repo: string,
+  featureConfigLocation: string,
+): Array<string> {
+  if (repo == null) {
+    return [];
+  }
+  const localPaths = featureConfig.get(
+    `fb-atomprojects.${featureConfigLocation}`,
+  );
+
+  invariant(Array.isArray(localPaths));
+  const repoPaths = localPaths
+    // $FlowIgnore
+    .filter(obj => obj.repo === repo)
+    // $FlowIgnore
+    .map(obj => obj.path);
+
+  if (repoPaths.length === 0) {
+    repoPaths.push(`~/${repo}`);
+  }
+  return repoPaths;
+}
+
+/**
+ * Sets an array of paths which can be tried on the local machine to find
+ * the location of <repo>. For example, if repo := fbsource, then we are setting
+ * the paths to fbsource on the user's local machine.
+ */
+export function setLocalPathsForProjectRepo(
+  paths: Array<{
+    path: NuclideUri,
+    repo: string,
+  }>,
+): void {
+  featureConfig.set('fb-atomprojects.localPaths', paths);
+}
+
+/**
+ * Sets an array of paths which can be tried on a remote machine to find
+ * the location of <repo>. For example, if repo := fbsource, then we are setting
+ * the paths to fbsource on the user's remote machine.
+ */
+export function setRemotePathsForProjectRepo(
+  paths: Array<{
+    path: NuclideUri,
+    repo: string,
+  }>,
+): void {
+  featureConfig.set('fb-atomprojects.remotePaths', paths);
 }
