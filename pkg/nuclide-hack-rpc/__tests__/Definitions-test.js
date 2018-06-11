d16032c0a5f3af98c913b8777cceb1304c2dded6
Aaron Abramov <dabramov@fb.com>
2018-06-11 15:17:20 -0700

    nuclide-hack-rpc
    Reviewed By: wanderley
    
    Differential Revision: D8337535
    
    fbshipit-source-id: be73d06ec993cc8110e65804625729dba6eb32ad

--- pkg/nuclide-hack-rpc/__tests__/Definitions-test.js
+++ pkg/nuclide-hack-rpc/__tests__/Definitions-test.js
@@ -11,14 +11,9 @@
 
 import type {HackDefinition} from '../lib/Definitions';
 
-import {addMatchers} from '../../nuclide-test-helpers';
 import {convertDefinitions} from '../lib/Definitions';
 
 describe('convertDefinitions', () => {
-  beforeEach(function() {
-    addMatchers(this);
-  });
-
   const filePath = '/tmp/file1';
   const defPath = '/tmp/file2';
   const projectRoot = '/tmp';
@@ -86,60 +81,12 @@ describe('convertDefinitions', () => {
   it('valid definition', () => {
     expect(
       convertDefinitions([filteredDef, def], filePath, projectRoot),
-    ).diffJson({
-      queryRange: [
-        {
-          start: {row: 0, column: 9},
-          end: {row: 0, column: 13},
-        },
-      ],
-      definitions: [
-        {
-          path: defPath,
-          position: {
-            row: 9,
-            column: 41,
-          },
-          range: {
-            start: {
-              row: 9,
-              column: 41,
-            },
-            end: {
-              row: 10,
-              column: 1,
-            },
-          },
-          id: 'hack-name',
-          name: 'hack-name',
-          language: 'php',
-          projectRoot,
-        },
-      ],
-    });
+    ).toMatchSnapshot();
   });
 
   it('default name definition', () => {
-    expect(convertDefinitions([defaultName], filePath, projectRoot)).diffJson({
-      queryRange: [
-        {
-          start: {row: 0, column: 9},
-          end: {row: 0, column: 13},
-        },
-      ],
-      definitions: [
-        {
-          path: filePath,
-          position: {
-            row: 9,
-            column: 41,
-          },
-          id: 'hack-name',
-          name: 'hack-name',
-          language: 'php',
-          projectRoot,
-        },
-      ],
-    });
+    expect(
+      convertDefinitions([defaultName], filePath, projectRoot),
+    ).toMatchSnapshot();
   });
 });
--- pkg/nuclide-hack-rpc/__tests__/FindReferences-test.js
+++ pkg/nuclide-hack-rpc/__tests__/FindReferences-test.js
@@ -17,10 +17,6 @@ const file1Path = '/test/file1.php';
 const file2Path = '/test/file2.php';
 
 describe('FindReferences', () => {
-  beforeEach(function() {
-    addMatchers(this);
-  });
-
   it('convertReferences', () => {
     expect(
       convertReferences(
@@ -42,28 +38,6 @@ describe('FindReferences', () => {
         ],
         projectRoot,
       ),
-    ).diffJson({
-      type: 'data',
-      baseUri: '/test/',
-      referencedSymbolName: 'TestClass::testFunction',
-      references: [
-        {
-          uri: '/test/file1.php',
-          name: null,
-          range: {
-            start: {row: 12, column: 4},
-            end: {row: 12, column: 7},
-          },
-        },
-        {
-          uri: '/test/file2.php',
-          name: null,
-          range: {
-            start: {row: 10, column: 0},
-            end: {row: 10, column: 3},
-          },
-        },
-      ],
-    });
+    ).toMatchSnapshot();
   });
 });
