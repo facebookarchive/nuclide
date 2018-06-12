2c52d17b2d4e42659f0b76c7b60fd929765be97b
Aaron Abramov <dabramov@fb.com>
2018-06-12 15:48:09 -0700

    nuclide-hack
    Reviewed By: wanderley
    
    Differential Revision: D8383946
    
    fbshipit-source-id: b4cb3bac503180a7847e93c4d859e79ff77788c3

---
+++ pkg/nuclide-hack/__atom_tests__/result-test.js
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ *
+ * @flow strict-local
+ * @format
+ */
+
+import invariant from 'assert';
+import {HackSymbolProvider} from '../lib/HackSymbolProvider';
+import TestUtils from 'react-dom/test-utils';
+import ReactDOM from 'react-dom';
+
+describe('Result rendering', () => {
+  it('should work', () => {
+    const mockResult = {
+      resultType: 'SYMBOL',
+      path: '/some/arbitrary/path',
+      name: 'IExampleSymbolInterface',
+      icon: 'puzzle',
+      hoverText: 'interface',
+      column: 1,
+      line: 3,
+      containerName: 'scope',
+    };
+    invariant(HackSymbolProvider.getComponentForItem != null);
+    const reactElement = HackSymbolProvider.getComponentForItem(mockResult);
+    expect(reactElement.props.title).toBe('interface');
+    const renderedComponent = TestUtils.renderIntoDocument(reactElement);
+    const renderedNode = ReactDOM.findDOMNode(renderedComponent);
+
+    expect(
+      // $FlowFixMe
+      renderedNode.querySelectorAll('.omnisearch-symbol-result-filename')
+        .length,
+    ).toBe(1);
+    // $FlowFixMe
+    expect(renderedNode.querySelectorAll('.icon-puzzle').length).toBe(1);
+  });
+});
--- pkg/nuclide-hack/__tests__/HackSymbolProvider-test.js
+++ pkg/nuclide-hack/__tests__/HackSymbolProvider-test.js
@@ -13,8 +13,6 @@ import type {SymbolResult} from '../../nuclide-language-service/lib/LanguageServ
 import type {NuclideUri} from 'nuclide-commons/nuclideUri';
 
 import {HackSymbolProvider} from '../lib/HackSymbolProvider';
-import ReactDOM from 'react-dom';
-import TestUtils from 'react-dom/test-utils';
 import {clearRequireCache, uncachedRequire} from 'nuclide-commons/test-helpers';
 import invariant from 'assert';
 
@@ -30,37 +28,36 @@ describe('HackSymbolProvider', () => {
   beforeEach(() => {
     getHackLanguageForUri = null;
     isFileInProject = null;
-    spyOn(require('../lib/HackLanguage'), 'getHackLanguageForUri').andCallFake(
-      (directory: NuclideUri) => {
+    jest
+      .spyOn(require('../lib/HackLanguage'), 'getHackLanguageForUri')
+      .mockImplementation((directory: NuclideUri) => {
         invariant(getHackLanguageForUri);
         return getHackLanguageForUri(directory);
-      },
-    );
-    spyOn(require('../lib/HackLanguage'), 'isFileInHackProject').andCallFake(
-      (directory: NuclideUri) => {
+      });
+    jest
+      .spyOn(require('../lib/HackLanguage'), 'isFileInHackProject')
+      .mockImplementation((directory: NuclideUri) => {
         invariant(isFileInProject);
         return isFileInProject(directory);
-      },
-    );
+      });
     uncachedRequire(require, '../lib/HackSymbolProvider');
   });
 
   afterEach(() => {
-    jasmine.unspy(require('../lib/HackLanguage'), 'isFileInHackProject');
-    jasmine.unspy(require('../lib/HackLanguage'), 'getHackLanguageForUri');
+    jest.restoreAllMocks();
     clearRequireCache(require, '../lib/HackSymbolProvider');
   });
 
   describe('executeQuery()', () => {
-    it('returns an empty array for an empty query', () => {
-      waitsForPromise(async () => {
+    it('returns an empty array for an empty query', async () => {
+      await (async () => {
         const results = await HackSymbolProvider.executeQuery('', []);
         expect(results).toEqual([]);
-      });
+      })();
     });
 
-    it('local search returns local paths when searching local directories', () => {
-      waitsForPromise(async () => {
+    it('local search returns local paths when searching local directories', async () => {
+      await (async () => {
         // Set up the HackService to return some canned results.
         const cannedResults = [
           {
@@ -71,16 +68,13 @@ describe('HackSymbolProvider', () => {
           },
         ];
         const hackService = createDummyHackService();
-        const supportsMethod = spyOn(
-          hackService,
-          'supportsSymbolSearch',
-        ).andReturn(true);
-        const searchMethod = spyOn(hackService, 'symbolSearch').andReturn(
-          cannedResults,
-        );
-        getHackLanguageForUri = jasmine
-          .createSpy('getHackLanguageForUri')
-          .andReturn(hackService);
+        const supportsMethod = jest
+          .spyOn(hackService, 'supportsSymbolSearch')
+          .mockReturnValue(true);
+        const searchMethod = jest
+          .spyOn(hackService, 'symbolSearch')
+          .mockReturnValue(cannedResults);
+        getHackLanguageForUri = jest.fn().mockReturnValue(hackService);
 
         // test that SymbolProvider.isEligibleForDirectories
         // calls into HackService.supportsSymbolSearch correctly
@@ -88,8 +82,8 @@ describe('HackSymbolProvider', () => {
           mockDirectory,
         ]);
         expect(supports).toEqual(true);
-        expect(supportsMethod.callCount).toBe(1);
-        expect(supportsMethod.argsForCall[0]).toEqual([
+        expect(supportsMethod.mock.calls.length).toBe(1);
+        expect(supportsMethod.mock.calls[0]).toEqual([
           [mockDirectory.getPath()],
         ]);
 
@@ -100,16 +94,16 @@ describe('HackSymbolProvider', () => {
           mockDirectory,
         ]);
         expect(results).toEqual(cannedResults);
-        expect(searchMethod.callCount).toBe(1);
-        expect(searchMethod.argsForCall[0]).toEqual([
+        expect(searchMethod.mock.calls.length).toBe(1);
+        expect(searchMethod.mock.calls[0]).toEqual([
           query,
           [mockDirectory.getPath()],
         ]);
-      });
+      })();
     });
 
-    it('remote search returns remote paths when searching remote directories', () => {
-      waitsForPromise(async () => {
+    it('remote search returns remote paths when searching remote directories', async () => {
+      await (async () => {
         // Set up the HackService to return some canned results.
         const cannedResults = [
           {
@@ -120,12 +114,10 @@ describe('HackSymbolProvider', () => {
           },
         ];
         const hackService = createDummyHackService();
-        const searchMethod = spyOn(hackService, 'symbolSearch').andReturn(
-          cannedResults,
-        );
-        getHackLanguageForUri = jasmine
-          .createSpy('getHackLanguageForUri')
-          .andReturn(hackService);
+        const searchMethod = jest
+          .spyOn(hackService, 'symbolSearch')
+          .mockReturnValue(cannedResults);
+        getHackLanguageForUri = jest.fn().mockReturnValue(hackService);
 
         const query = 'asdf';
         const results = await HackSymbolProvider.executeQuery(query, [
@@ -135,16 +127,16 @@ describe('HackSymbolProvider', () => {
         // Verify the expected results were returned by delegating to the HackService,
         // and that local file paths are converted to NuclideUris.
         expect(results).toEqual(cannedResults);
-        expect(searchMethod.callCount).toBe(1);
-        expect(searchMethod.argsForCall[0]).toEqual([
+        expect(searchMethod.mock.calls.length).toBe(1);
+        expect(searchMethod.mock.calls[0]).toEqual([
           query,
           [mockDirectory.getPath()],
         ]);
-      });
+      })();
     });
 
-    it('should only query once per unique service, not once per directory', () => {
-      waitsForPromise(async () => {
+    it('should only query once per unique service, not once per directory', async () => {
+      await (async () => {
         // Set up the HackService to return some canned results.
         const cannedResults = [
           {
@@ -155,12 +147,10 @@ describe('HackSymbolProvider', () => {
           },
         ];
         const hackService = createDummyHackService();
-        const searchMethod = spyOn(hackService, 'symbolSearch').andReturn(
-          cannedResults,
-        );
-        getHackLanguageForUri = jasmine
-          .createSpy('getHackLanguageForUri')
-          .andReturn(hackService);
+        const searchMethod = jest
+          .spyOn(hackService, 'symbolSearch')
+          .mockReturnValue(cannedResults);
+        getHackLanguageForUri = jest.fn().mockReturnValue(hackService);
         // both directories return the same service
 
         const query = 'asdf';
@@ -172,16 +162,16 @@ describe('HackSymbolProvider', () => {
         // Verify the expected results were returned by delegating to the HackService,
         // and that local file paths are converted to NuclideUris.
         expect(results).toEqual(cannedResults);
-        expect(searchMethod.callCount).toBe(1);
-        expect(searchMethod.argsForCall[0]).toEqual([
+        expect(searchMethod.mock.calls.length).toBe(1);
+        expect(searchMethod.mock.calls[0]).toEqual([
           query,
           [mockDirectory.getPath(), mockDirectory2.getPath()],
         ]);
-      });
+      })();
     });
 
-    it('should query once per unique service', () => {
-      waitsForPromise(async () => {
+    it('should query once per unique service', async () => {
+      await (async () => {
         // Set up the HackService to return some canned results.
         const cannedResults1 = [
           {
@@ -201,19 +191,15 @@ describe('HackSymbolProvider', () => {
         ];
         const hackService1 = createDummyHackService();
         const hackService2 = createDummyHackService();
-        const searchMethod1 = spyOn(hackService1, 'symbolSearch').andReturn(
-          cannedResults1,
-        );
-        const searchMethod2 = spyOn(hackService2, 'symbolSearch').andReturn(
-          cannedResults2,
-        );
-        getHackLanguageForUri = jasmine
-          .createSpy('getHackLanguageForUri')
-          .andCallFake(uri => {
-            return uri === mockDirectory.getPath()
-              ? hackService1
-              : hackService2;
-          });
+        const searchMethod1 = jest
+          .spyOn(hackService1, 'symbolSearch')
+          .mockReturnValue(cannedResults1);
+        const searchMethod2 = jest
+          .spyOn(hackService2, 'symbolSearch')
+          .mockReturnValue(cannedResults2);
+        getHackLanguageForUri = jest.fn().mockImplementation(uri => {
+          return uri === mockDirectory.getPath() ? hackService1 : hackService2;
+        });
 
         const query = 'asdf';
         const results = await HackSymbolProvider.executeQuery(query, [
@@ -224,45 +210,17 @@ describe('HackSymbolProvider', () => {
         // Verify the expected results were returned by delegating to the HackService,
         // and that local file paths are converted to NuclideUris.
         expect(results).toEqual(cannedResults1.concat(cannedResults2));
-        expect(searchMethod1.callCount).toBe(1);
-        expect(searchMethod1.argsForCall[0]).toEqual([
+        expect(searchMethod1.mock.calls.length).toBe(1);
+        expect(searchMethod1.mock.calls[0]).toEqual([
           query,
           [mockDirectory.getPath()],
         ]);
-        expect(searchMethod2.callCount).toBe(1);
-        expect(searchMethod2.argsForCall[0]).toEqual([
+        expect(searchMethod2.mock.calls.length).toBe(1);
+        expect(searchMethod2.mock.calls[0]).toEqual([
           query,
           [mockDirectory2.getPath()],
         ]);
-      });
-    });
-  });
-
-  describe('Result rendering', () => {
-    it('should work', () => {
-      const mockResult = {
-        resultType: 'SYMBOL',
-        path: '/some/arbitrary/path',
-        name: 'IExampleSymbolInterface',
-        icon: 'puzzle',
-        hoverText: 'interface',
-        column: 1,
-        line: 3,
-        containerName: 'scope',
-      };
-      invariant(HackSymbolProvider.getComponentForItem != null);
-      const reactElement = HackSymbolProvider.getComponentForItem(mockResult);
-      expect(reactElement.props.title).toBe('interface');
-      const renderedComponent = TestUtils.renderIntoDocument(reactElement);
-      const renderedNode = ReactDOM.findDOMNode(renderedComponent);
-
-      expect(
-        // $FlowFixMe
-        renderedNode.querySelectorAll('.omnisearch-symbol-result-filename')
-          .length,
-      ).toBe(1);
-      // $FlowFixMe
-      expect(renderedNode.querySelectorAll('.icon-puzzle').length).toBe(1);
+      })();
     });
   });
 });
