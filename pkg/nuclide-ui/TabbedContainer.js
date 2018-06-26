166b3cb86a4deeb3787b2d9f1944be383350e8f1
Evan Krause <evangrayk@fb.com>
2018-06-26 09:58:01 -0700

    Add TabbedContainer
    Summary: This is a container Component which handles multiple tabs controlling content which we want to always render. All tabs except the active one are just hidden with `display: none`. This works well for tabbed layouts that are expensive to mount or have state within them.
    
    Reviewed By: tjfryan
    
    Differential Revision: D8575646
    
    fbshipit-source-id: ed5010213f96d65f8c00d3be1ce92ee261f1979d

---
+++ pkg/nuclide-ui/TabbedContainer.js
@@ -0,0 +1,103 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ *
+ * @flow
+ * @format
+ */
+
+import Tabs from 'nuclide-commons-ui/Tabs';
+import {arrayCompact} from 'nuclide-commons/collection';
+import * as React from 'react';
+import classnames from 'classnames';
+
+type Props = {
+  // An array of possible tab names
+  tabNames: Array<string>,
+  // Any children rendered as null will have their tab name hidden
+  children: Array<React.Node>,
+
+  className?: ?string,
+};
+
+type State = {
+  activeTabName: string,
+};
+
+// Tabbed Container which renders all of its children, but applies
+// `display: none` to inactive ones. This allows tab changes to be very fast and
+// not lose state on expensive-to-mount components
+export default class TabbedContainer extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      activeTabName: props.tabNames[0],
+    };
+  }
+
+  static getDerivedStateFromProps(nextProps: Props, prevState: State): ?State {
+    const {activeTabName} = prevState;
+    const {tabNames, children} = nextProps;
+    const activeTabIndex = tabNames.findIndex(name => name === activeTabName);
+    if (children[activeTabIndex] == null) {
+      return {
+        ...prevState,
+        activeTabName: tabNames[0],
+      };
+    }
+    return null;
+  }
+
+  render(): React.Node {
+    const {children, tabNames, className} = this.props;
+
+    const visibleTabs = arrayCompact(
+      tabNames.slice(0, children.length).map((name, tabIndex) => {
+        if (children[tabIndex] == null) {
+          return null;
+        }
+        return {name, tabContent: <div>{name}</div>};
+      }),
+    );
+
+    if (visibleTabs.length === 0) {
+      return null;
+    }
+
+    const {activeTabName} = this.state;
+    const activeTabIndex = tabNames.findIndex(name => name === activeTabName);
+    const wrappedChildren = arrayCompact(
+      children.map((child, childIndex) => {
+        if (child == null) {
+          return null;
+        }
+        return (
+          <div
+            key={childIndex}
+            className={classnames({
+              hidden: childIndex !== activeTabIndex,
+            })}>
+            {child}
+          </div>
+        );
+      }),
+    );
+
+    return (
+      <div className={className}>
+        <Tabs
+          tabs={visibleTabs}
+          activeTabName={this.state.activeTabName}
+          triggeringEvent="onClick"
+          onActiveTabChange={newTabName =>
+            this.setState({activeTabName: newTabName.name})
+          }
+        />
+        {wrappedChildren}
+      </div>
+    );
+  }
+}
---
+++ pkg/nuclide-ui/__atom_tests__/TabbedContainer-test.js
@@ -0,0 +1,69 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ *
+ * @flow
+ * @format
+ */
+
+import TabbedContainer from '../TabbedContainer';
+import TestUtils from 'react-dom/test-utils';
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+
+describe('TabbedContainer', () => {
+  let containerNode: HTMLElement;
+  beforeEach(() => {
+    containerNode = document.createElement('div');
+  });
+
+  const renderTabbedContainer = (
+    tabNames: Array<string>,
+    children: Array<React.Node>,
+  ): TabbedContainer => {
+    return (ReactDOM.render(
+      <TabbedContainer tabNames={tabNames} children={children} />,
+      containerNode,
+    ): any);
+  };
+
+  afterEach(() => {
+    if (containerNode != null) {
+      containerNode.remove();
+    }
+  });
+
+  it('changes active tab when removing available children', () => {
+    class Child1 extends React.Component<*> {
+      render() {
+        return <div>hello</div>;
+      }
+    }
+    const tabbedContainer = renderTabbedContainer(
+      ['tab1', 'tab2'],
+      [<Child1 />, <Child1 />],
+    );
+    expect(tabbedContainer.state.activeTabName).toBe('tab1');
+
+    const renderedTabs = containerNode.getElementsByClassName('tab');
+    expect(renderedTabs.length).toBe(2);
+
+    // the second tab should have its contents hidden
+    expect(containerNode.getElementsByClassName('hidden').length).toBe(1);
+
+    // switch to second tab
+    TestUtils.Simulate.click(renderedTabs.item(1));
+    expect(tabbedContainer.state.activeTabName).toBe('tab2');
+
+    // now the second tab's content becomes null
+    renderTabbedContainer(['tab1', 'tab2'], [<Child1 />, null]);
+    // we should be put onto the first tab
+    expect(tabbedContainer.state.activeTabName).toBe('tab1');
+
+    // no content to hide now
+    expect(containerNode.getElementsByClassName('hidden').length).toBe(0);
+  });
+});
