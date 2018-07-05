29a4da58c7739de0839a0d2f33472302ee9f59b1
Kunal Mehta <kunalm@fb.com>
2018-07-05 16:57:09 -0700

    Syntax highlighting for is/as operators
    Summary: We're launching these operators next week; this makes them highlight the same as `instanceof`.
    
    Reviewed By: kevinastone
    
    Differential Revision: D8711663
    
    fbshipit-source-id: 87bfbdfadd89f86c564e40690ff1401005cf7d17

--- pkg/nuclide-language-hack/spec/fixtures/syntax_test_hack_operators.php
+++ pkg/nuclide-language-hack/spec/fixtures/syntax_test_hack_operators.php
@@ -13,3 +13,7 @@ $r = $a ==> $b;
 //      ^^^ keyword.operator.lambda.php
 $r = $a |> $b;
 //      ^^ keyword.operator.pipe.php
+$r = $a is int;
+//      ^^ keyword.operator.type.php
+$r = $a as int;
+//      ^^ keyword.operator.type.php
