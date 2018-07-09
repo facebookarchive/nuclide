123384d59765400c32cef18b97c090e3453823fc
Kevin Chen <kevinjchen@fb.com>
2018-07-09 10:51:04 -0700

    Using Expected to Show Spinner When Call Stack is Loading
    Summary: When fetchCallStack() is still retrieving the call frames, a Spinner is shown.
    
    Reviewed By: ebluestein
    
    Differential Revision: D8730144
    
    fbshipit-source-id: 1a2f083e2c6d0eb5d9faa01a878c551174f1dc24

--- modules/atom-ide-ui/pkg/atom-ide-debugger/lib/ui/ThreadTreeNode.js
+++ modules/atom-ide-ui/pkg/atom-ide-debugger/lib/ui/ThreadTreeNode.js
@@ -11,14 +11,33 @@
  */
 
 import type {IThread, IStackFrame, IDebugService} from '../types';
+import type {Expected} from 'nuclide-commons/expected';
 
+import {LoadingSpinner} from 'nuclide-commons-ui/LoadingSpinner';
 import {NestedTreeItem} from 'nuclide-commons-ui/Tree';
 import {observableFromSubscribeFunction} from 'nuclide-commons/event';
-import {fastDebounce} from 'nuclide-commons/observable';
 import * as React from 'react';
-import {Observable} from 'rxjs';
+import {Observable, Subject} from 'rxjs';
 import FrameTreeNode from './FrameTreeNode';
 import UniversalDisposable from 'nuclide-commons/UniversalDisposable';
+import {Expect} from 'nuclide-commons/expected';
+import classnames from 'classnames';
+
+const LOADING = (
+  <div
+    className={classnames(
+      'debugger-expression-value-row',
+      'debugger-tree-no-frames',
+    )}>
+    <span className="debugger-expression-value-content">
+      <LoadingSpinner size="SMALL" />
+    </span>
+  </div>
+);
+
+const NO_FRAMES = (
+  <span className="debugger-tree-no-frames">Call frames unavailable</span>
+);
 
 type Props = {
   thread: IThread,
@@ -28,59 +47,56 @@ type Props = {
 
 type State = {
   isCollapsed: boolean,
-  childItems: Array<IStackFrame>,
-  isFocused: boolean,
+  childItems: Expected<Array<IStackFrame>>,
 };
 
 export default class ThreadTreeNode extends React.Component<Props, State> {
   _disposables: UniversalDisposable;
+  _selectTrigger: Subject<void>;
 
   constructor(props: Props) {
     super(props);
+    this._selectTrigger = new Subject();
     this.state = this._getInitialState();
     this._disposables = new UniversalDisposable();
-    this.handleSelect = this.handleSelect.bind(this);
   }
 
   _computeIsFocused(): boolean {
     const {service, thread} = this.props;
     const focusedThread = service.viewModel.focusedThread;
-    return focusedThread == null
-      ? false
-      : thread.threadId === focusedThread.threadId;
+    return focusedThread != null && thread.threadId === focusedThread.threadId;
   }
 
   _getInitialState() {
-    const {thread} = this.props;
-    const isFocused = this._computeIsFocused();
-
-    const isCollapsed = !isFocused;
     return {
-      isCollapsed,
-      isFocused,
-      childItems: thread.getCallStack(),
+      isCollapsed: true,
+      childItems: Expect.pending(),
     };
   }
 
-  async _getState(handlingClick: boolean) {
-    const {service, thread} = this.props;
-    const {isCollapsed} = this.state;
-    const {focusedThread} = service.viewModel;
-    const isFocused =
-      focusedThread == null
-        ? false
-        : thread.threadId === focusedThread.threadId;
-    const updatedCollapsed = handlingClick
-      ? !isCollapsed
-      : !(isFocused || !isCollapsed);
-    if (!updatedCollapsed) {
-      await thread.fetchCallStack();
+  _getFrames(fetch: boolean = false): Observable<Expected<Array<IStackFrame>>> {
+    const {thread} = this.props;
+    const getValue = () => Observable.of(Expect.value(thread.getCallStack()));
+    if (
+      fetch ||
+      (!this.state.childItems.isPending &&
+        !this.state.childItems.isError &&
+        this.state.childItems.value.length === 0)
+    ) {
+      return Observable.of(Expect.pending()).concat(
+        Observable.fromPromise(
+          (async () => {
+            await thread.fetchCallStack();
+            return Expect.value(thread.getCallStack());
+          })(),
+        ),
+      );
     }
-    return {
-      isCollapsed: updatedCollapsed,
-      isFocused,
-      childItems: thread.getCallStack(),
-    };
+    return getValue();
+  }
+
+  componentWillUnmount(): void {
+    this._disposables.dispose();
   }
 
   componentDidMount(): void {
@@ -89,30 +105,71 @@ export default class ThreadTreeNode extends React.Component<Props, State> {
     const {viewModel} = service;
     this._disposables.add(
       Observable.merge(
-        observableFromSubscribeFunction(model.onDidChangeCallStack.bind(model)),
         observableFromSubscribeFunction(
           viewModel.onDidFocusStackFrame.bind(viewModel),
         ),
         observableFromSubscribeFunction(service.onDidChangeMode.bind(service)),
-      )
-        .let(fastDebounce(15))
-        .subscribe(async () => {
-          this.setState(await this._getState(false));
+      ).subscribe(() => {
+        const {isCollapsed} = this.state;
+
+        const newIsCollapsed = isCollapsed && !this._computeIsFocused();
+        this.setState({
+          isCollapsed: newIsCollapsed,
+        });
+      }),
+      this._selectTrigger
+        .asObservable()
+        .switchMap(() => this._getFrames(true))
+        .subscribe(frames => {
+          this.setState({
+            childItems: frames,
+          });
+        }),
+      observableFromSubscribeFunction(model.onDidChangeCallStack.bind(model))
+        .debounceTime(100)
+        .startWith(null)
+        .switchMap(() =>
+          this._getFrames().switchMap(frames => {
+            if (
+              !this.state.isCollapsed &&
+              !frames.isPending &&
+              !frames.isError &&
+              frames.value.length === 0
+            ) {
+              return this._getFrames(true);
+            }
+            return Observable.of(frames);
+          }),
+        )
+        .subscribe(frames => {
+          const {isCollapsed} = this.state;
+
+          this.setState({
+            childItems: frames,
+            isCollapsed: isCollapsed && !this._computeIsFocused(),
+          });
         }),
     );
   }
 
-  handleSelect = async () => {
-    if (this.state.childItems.length === 0) {
-      await this.props.thread.fetchCallStack();
+  handleSelect = () => {
+    if (!this.state.isCollapsed) {
+      this.setState({
+        isCollapsed: true,
+      });
+    } else {
+      this.setState({
+        isCollapsed: false,
+        childItems: Expect.pending(),
+      });
+      this._selectTrigger.next();
     }
-    this.setState(await this._getState(true));
   };
 
   render(): React.Node {
     const {thread, title, service} = this.props;
-    const {isFocused, childItems} = this.state;
-
+    const {childItems} = this.state;
+    const isFocused = this._computeIsFocused();
     const formattedTitle = (
       <span
         className={isFocused ? 'debugger-tree-process-thread-selected' : ''}
@@ -120,28 +177,34 @@ export default class ThreadTreeNode extends React.Component<Props, State> {
         {title}
       </span>
     );
-    const innerHTML =
-      childItems.length === 0 ? (
-        <span className="debugger-tree-no-frames">Call frames unavailable</span>
-      ) : (
-        childItems.map((frame, frameIndex) => {
-          return (
-            <FrameTreeNode
-              text={frame.name}
-              frame={frame}
-              key={frameIndex}
-              service={service}
-            />
-          );
-        })
-      );
+
+    const callFramesElements = childItems.isPending ? (
+      LOADING
+    ) : childItems.isError ? (
+      <span className="debugger-tree-no-frames">
+        Error fetching stack frames {childItems.error.toString()}
+      </span>
+    ) : childItems.value.length === 0 ? (
+      NO_FRAMES
+    ) : (
+      childItems.value.map((frame, frameIndex) => {
+        return (
+          <FrameTreeNode
+            text={frame.name}
+            frame={frame}
+            key={frameIndex}
+            service={service}
+          />
+        );
+      })
+    );
 
     return (
       <NestedTreeItem
         title={formattedTitle}
         collapsed={this.state.isCollapsed}
         onSelect={this.handleSelect}>
-        {innerHTML}
+        {callFramesElements}
       </NestedTreeItem>
     );
   }
