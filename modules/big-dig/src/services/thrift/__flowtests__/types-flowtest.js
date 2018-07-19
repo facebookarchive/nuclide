c8066adc5b7c4de18086cde6f1a608ba17cf1287
Wanderley da Silva <wander@fb.com>
2018-07-18 17:03:31 -0700

    (easy) Move flowtests to big dig folder
    Summary: As title.
    
    Differential Revision: D8886070
    
    fbshipit-source-id: 37563e9be557792f13870c5d694a8de75376b961

---
+++ modules/big-dig/src/services/thrift/__flowtests__/types-flowtest.js
@@ -0,0 +1,16 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @flow
+ * @format
+ */
+
+import type {ThriftServiceConfig, ThriftServerConfig} from '../types';
+
+// Ensure that client config is a subtype of server config
+(((null: any): ThriftServiceConfig): ThriftServerConfig);
