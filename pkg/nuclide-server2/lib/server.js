683527e150d000a4f693221964ef41e13246f8a5
Hanson Wang <hansonw@fb.com>
2018-08-03 16:55:30 -0700

    Start up the VSCode server inside the Nuclide server
    Summary:
    A potentially simpler approach. Instead of moving `big-dig-vscode-server` into `big-dig`, we can have the Nuclide launcher additionally load the `big-dig-vscode-server` launcher.
    
    (I think that moving the big-dig-vscode-server into a big-dig service is undesirable because we probably want to rethink how those individual services work anyway.)
    
    Reviewed By: semmypurewal
    
    Differential Revision: D9167919
    
    fbshipit-source-id: 599c40682bc3744204131f9766eb8992e136fa40

--- pkg/nuclide-server2/lib/server.js
+++ pkg/nuclide-server2/lib/server.js
@@ -13,6 +13,7 @@ import type {Transport} from 'big-dig/src/server/BigDigServer';
 import type {BigDigServer, LauncherType} from 'big-dig/src/server/BigDigServer';
 import type {Transport as RpcTransportType} from '../../nuclide-rpc';
 
+import vscodeLaunch from 'big-dig-vscode-server/server';
 import {initializeLogging} from '../../nuclide-logging';
 import {getServerSideMarshalers} from '../../nuclide-marshalers-common';
 import {RpcConnection, ServiceRegistry} from '../../nuclide-rpc';
@@ -46,7 +47,8 @@ function launch(server: BigDigServer): Promise<void> {
     },
   });
 
-  return Promise.resolve();
+  // Enable VSCode to connect to Nuclide servers by default.
+  return vscodeLaunch(server);
 }
 
 // eslint-disable-next-line nuclide-internal/no-commonjs
