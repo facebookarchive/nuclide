d76623e6b4fd18f70fbfad0805bc6791bd560fc4
Ryan Holinshead <rholinshead@fb.com>
2018-08-09 13:59:29 -0700

    Handle Migration of 'fb-nuclide-onboarding.showOnboarding' to 'nuclide-welcome-page.hiddenTopics'
    Summary:
    Since Onboarding will be integrated with Welcome Pages, the show/hide functionality of Welcome Pages can be leveraged. To ensure users who already chose to hide Onboarding don't see it again with the integration, the showOnboarding config value needs to be migrated so that the fb-onboarding topic is stored as a nuclide-welcome-page hiddenTopic.
    
    This diff adds a function for that migration and removes the showOnboarding config value from the fb-nuclide-onboarding package
    
    Reviewed By: wbinnssmith
    
    Differential Revision: D8944890
    
    fbshipit-source-id: 74c4b780aca047a3d3785515da568367b59e4aa8

--- pkg/nuclide-welcome-page/lib/config.js
+++ pkg/nuclide-welcome-page/lib/config.js
@@ -9,7 +9,11 @@
  * @format
  */
 
+import invariant from 'assert';
 import featureConfig from 'nuclide-commons-atom/feature-config';
+
+const NUCLIDE_ONBOARDING_CONFIG_KEY = 'fb-nuclide-onboarding';
+const NUCLIDE_ONBOARDING_TOPIC_NAME = 'fb-nuclide';
 
 export function getHiddenTopics(): Set<string> {
   const topics: ?Array<string> = (featureConfig.get(
@@ -23,4 +27,28 @@ export function setHiddenTopics(hiddenTopics: Set<string>): void {
     'nuclide-welcome-page.hiddenTopics',
     Array.from(hiddenTopics),
   );
+}
+
+// Migrate showOnboarding value to welcome-page hiddenTopics if Onboarding
+// has already been hidden (stored value === false), and remove
+// NUCLIDE_ONBOARDING_CONFIG_KEY from atom config if it exists
+export function migrateShowOnboardingConfigValue() {
+  let nuclideConfig = atom.config.get('nuclide');
+  invariant(nuclideConfig != null && typeof nuclideConfig === 'object');
+  const nuclideOnboardingConfig = nuclideConfig[NUCLIDE_ONBOARDING_CONFIG_KEY];
+  if (nuclideOnboardingConfig == null) {
+    return;
+  }
+  if (nuclideOnboardingConfig.showOnboarding === false) {
+    const hiddenTopics = getHiddenTopics();
+    if (!hiddenTopics.has(NUCLIDE_ONBOARDING_TOPIC_NAME)) {
+      hiddenTopics.add(NUCLIDE_ONBOARDING_TOPIC_NAME);
+      setHiddenTopics(hiddenTopics);
+      nuclideConfig = atom.config.get('nuclide');
+    }
+  }
+  // Remove NUCLIDE_ONBOARDING_CONFIG_KEY from atom config
+  invariant(nuclideConfig != null && typeof nuclideConfig === 'object');
+  delete nuclideConfig[NUCLIDE_ONBOARDING_CONFIG_KEY];
+  atom.config.set('nuclide', nuclideConfig);
 }
