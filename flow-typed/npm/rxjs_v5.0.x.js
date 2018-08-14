17f5a608b8d8758c1465b62f26a0831ab0b9d9c0
Varun Ramesh <varunramesh@fb.com>
2018-08-14 16:38:15 -0700

    Added Observable.race type definition
    Summary: Add missing `Observable.race` type definition. Created equivalent PR in upstream - https://github.com/flow-typed/flow-typed/pull/2625
    
    Reviewed By: hansonw
    
    Differential Revision: D9309673
    
    fbshipit-source-id: 5bc599aa6f06c5c807ba1778861f552b9c7df217

--- flow-typed/npm/rxjs_v5.0.x.js
+++ flow-typed/npm/rxjs_v5.0.x.js
@@ -350,6 +350,8 @@ declare class rxjs$Observable<+T> {
 
   static of(...values: T[]): rxjs$Observable<T>;
 
+  static race(...sources: rxjs$Observable<T>[]): rxjs$Observable<T>;
+
   static range(
     start?: number,
     count?: number,
