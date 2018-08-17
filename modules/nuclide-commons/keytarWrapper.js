0186de3063a31667176b2ac1a1aaac678dc507ac
Semmy Purewal <semmy@fb.com>
2018-08-17 15:42:14 -0700

    move keytarWrapper to nuclide-commons to remove duplication
    Summary: Better solution suggested by @[1032083261:evangrayk]. Move keytarWrapper to nuclide-commons.
    
    Reviewed By: evangrayk
    
    Differential Revision: D9386379
    
    fbshipit-source-id: e6fc623a32664286d85ef57a23011341e215fd32

--- modules/nuclide-commons/keytarWrapper.js
+++ modules/nuclide-commons/keytarWrapper.js
@@ -1,16 +1,17 @@
 /**
- * Copyright (c) 2015-present, Facebook, Inc.
+ * Copyright (c) 2017-present, Facebook, Inc.
  * All rights reserved.
  *
- * This source code is licensed under the license found in the LICENSE file in
- * the root directory of this source tree.
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
  *
  * @flow
  * @format
  */
 
-import nuclideUri from 'nuclide-commons/nuclideUri';
-import {runCommand, ProcessExitError} from 'nuclide-commons/process';
+import nuclideUri from './nuclideUri';
+import {runCommand, ProcessExitError} from './process';
 
 /**
  * If we're running outside of Atom, attempt to use the prebuilt keytar libs.
--- pkg/nuclide-remote-connection/lib/RemoteConnectionConfigurationManager.js
+++ pkg/nuclide-remote-connection/lib/RemoteConnectionConfigurationManager.js
@@ -19,7 +19,7 @@ import type {
 import crypto from 'crypto';
 import invariant from 'assert';
 import {getLogger} from 'log4js';
-import keytarWrapper from '../../commons-node/keytarWrapper';
+import keytarWrapper from 'nuclide-commons/keytarWrapper';
 import electron from 'electron';
 
 const CONFIG_DIR = 'nuclide-connections';
