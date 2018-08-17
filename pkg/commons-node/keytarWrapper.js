6216efb1a209c1fd8d30c99fcfb6b45ed8795945
Semmy Purewal <semmy@fb.com>
2018-08-17 14:51:35 -0700

    copy keytarWrapper back to fix OSS build, see T32956220
    Summary: Fix OSS build by copying keytarWrapper back to commons-node. This duplicates keytarWrapper in two places, which is wrong. I added it a task to resolve this, and I'll take a look early next week: T32956220
    
    Reviewed By: evangrayk
    
    Differential Revision: D9385169
    
    fbshipit-source-id: cda981055f7d06bd5b5e750bdb6e0355dd70312c

---
+++ pkg/commons-node/keytarWrapper.js
@@ -0,0 +1,91 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ *
+ * @flow
+ * @format
+ */
+
+import nuclideUri from 'nuclide-commons/nuclideUri';
+import {runCommand, ProcessExitError} from 'nuclide-commons/process';
+
+/**
+ * If we're running outside of Atom, attempt to use the prebuilt keytar libs.
+ * (May throw if prebuilt libs aren't available for the current platform!)
+ */
+import * as keytar from 'nuclide-prebuilt-libs/keytar';
+
+function getApmNodePath(): string {
+  const apmDir = nuclideUri.dirname(atom.packages.getApmPath());
+  return nuclideUri.normalize(nuclideUri.join(apmDir, 'node'));
+}
+
+function getApmNodeModulesPath(): string {
+  const apmDir = nuclideUri.dirname(atom.packages.getApmPath());
+  return nuclideUri.normalize(nuclideUri.join(apmDir, '..', 'node_modules'));
+}
+
+function runScriptInApmNode(
+  script: string,
+  service: string,
+  account: string,
+  password?: string,
+): Promise<string> {
+  const args = ['-e', script];
+  const options = {
+    // The newline is important so we can use readline's line event.
+    input: JSON.stringify({service, account, password}) + '\n',
+    env: {
+      ...process.env,
+      NODE_PATH: getApmNodeModulesPath(),
+    },
+  };
+  return runCommand(getApmNodePath(), args, options)
+    .toPromise()
+    .catch(err => {
+      if (err instanceof ProcessExitError) {
+        // Unwrap underlying error from stderr (as it already has a stack!)
+        throw new Error(err.stderr);
+      }
+      throw err;
+    });
+}
+
+export default {
+  /**
+   * Returns the password (or null if it doesn't exist).
+   * Rejects on keychain access failure.
+   */
+  async getPassword(service: string, account: string): Promise<?string> {
+    return keytar.getPassword(service, account);
+  },
+
+  /**
+   * Returns nothing.
+   * Rejects on keychain access failure.
+   */
+  async replacePassword(
+    service: string,
+    account: string,
+    password: string,
+  ): Promise<void> {
+    return keytar.setPassword(service, account, password);
+  },
+
+  /**
+   * Returns true if a password was deleted, or false if it didn't exist.
+   * Rejects on keychain access failure.
+   */
+  async deletePassword(service: string, account: string): Promise<boolean> {
+    return keytar.deletePassword(service, account);
+  },
+};
+
+export const __TEST__ = {
+  getApmNodeModulesPath,
+  getApmNodePath,
+  runScriptInApmNode,
+};
