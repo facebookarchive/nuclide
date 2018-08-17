018c91200c2ffad26f3fdee0cb9a90d0b0671c88
Tobi Akomolede <olutobi@fb.com>
2018-08-16 19:28:28 -0700

    fb-intl-fbt-str Rx 2: get translation info using Rx Observable rather than having asnyc code in UI
    Summary:
    Before we were using an async function call in the `FbtStringBundleRoot` UI component. This is a bit of an anti-pattern. Thus we fix this by moving the `translationInfo` into a part of the `FbtStringBundleRoot`'s properties, and then we bind this component using an `Observable`. This `Observable` emits events in the following cases:
    1) When we query the FbtBundle cache
    2) When we query the translation info cache
    
    We also modify `getOrCreateFbtBundleTranslationInfo` such that it gets translation information for the full FbtBundle (rather than the bundle passed as a parameter), so that when we update the observables for a given file path (we'll have an observable for each selection-extraction for the file), it updates for the full mapping of hashes to translation info.
    
    Reviewed By: kayhadrin
    
    Differential Revision: D9308005
    
    fbshipit-source-id: 459fb6218f35bb3093ebea1d3762d564c42a8e9c

--- flow-typed/npm/lru-cache_v4.0.x.js
+++ flow-typed/npm/lru-cache_v4.0.x.js
@@ -2,27 +2,29 @@
 // flow-typed version: b43dff3e0e/lru-cache_v4.0.x/flow_>=v0.20.0
 
 declare module 'lru-cache' {
-
   declare type LRUCache<K, V> = {
-    set: (key: K, value: V, maxAge?: number) => void;
-    get: (key: K) => V;
-    peek: (key: K) => V;
-    del: (key: K) => void;
-    reset: () => void;
-    has: (key: K) => boolean;
-    dump: () => Array<{k: K, v: V}>;
-    load: (Array<{k: K, v: V}>) => void;
-    itemCount: number;
-    forEach: (value: V, key: K, cacheInstance: LRUCache<K, V>) => void;
+    set: (key: K, value: V, maxAge?: number) => void,
+    get: (key: K) => V,
+    peek: (key: K) => V,
+    del: (key: K) => void,
+    reset: () => void,
+    has: (key: K) => boolean,
+    dump: () => Array<{k: K, v: V}>,
+    load: (Array<{k: K, v: V}>) => void,
+    itemCount: number,
+    forEach: (
+      handler: (value: V, key: K, cacheInstance: LRUCache<K, V>) => void,
+      context?: LRUCache<K, V>,
+    ) => void,
     // TODO add the rest of the things documented at https://www.npmjs.com/package/lru-cache
   };
 
   declare type Options<K, V> = {
-    max?: number;
-    maxAge?: number;
-    length?: (value: V, key: K) => number;
-    dispose?: (key: K, value: V) => void;
-    stale?: boolean;
+    max?: number,
+    maxAge?: number,
+    length?: (value: V, key: K) => number,
+    dispose?: (key: K, value: V) => void,
+    stale?: boolean,
   };
 
   // TODO You can supply just an integer (max size), or even nothing at all.
