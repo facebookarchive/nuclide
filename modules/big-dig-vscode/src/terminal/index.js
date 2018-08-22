2ea21bb8efc8f7a6303f9cdcfdeadf31a491789a
CJ Bell <siegebell@fb.com>
2018-08-21 23:39:26 -0700

    Refactor terminal/index.js
    Summary: Refactoring to prepare for generalizing of wrapping remote processes. See the next stacked diff for more context.
    
    Reviewed By: bolinfest
    
    Differential Revision: D9410747
    
    fbshipit-source-id: 30c9be7643f351fd1b713d6074ae09b55c736b58

--- modules/big-dig-vscode/src/terminal/index.js
+++ modules/big-dig-vscode/src/terminal/index.js
@@ -11,12 +11,13 @@
  */
 
 import type {RemoteChildProcess} from '../RemoteProcess';
+import type {TerminalWrapper} from '../util/TerminalWrapper';
 
 import * as vscode from 'vscode';
 import {ConnectionWrapper} from '../ConnectionWrapper';
-import {TerminalWrapper} from '../util/TerminalWrapper';
 import {getIntegratedTerminal} from '../configuration';
 import {spawnRemote} from '../RemoteProcess';
+import {createProxyExecutable} from '../util/ProxyExecutable';
 
 /**
  * Creates a `vscode.Terminal` attached to a remote process.
@@ -27,21 +28,23 @@ export async function createRemoteTerminal(
   env?: {[x: string]: string},
 ): Promise<vscode.Terminal> {
   const {shell, shellArgs} = getIntegratedTerminal();
-  const proxy = new TerminalWrapper('term: ' + conn.getAddress());
-  // $FlowFixMe(>=0.68.0) Flow suppress (T27187857)
-  const remote = await spawnRemote(conn, shell, shellArgs, {
-    cwd,
-    shell: false,
-    usePty: true,
-    env,
-    addBigDigToPath: true,
+
+  // TODO(siegbell): use the new, proposed `TerminalRenderer` API instead of a proxy process.
+  const exec = await createProxyExecutable();
+  exec.spawned.take(1).subscribe(async ({proxy}) => {
+    // $FlowFixMe(>=0.68.0) Flow suppress (T27187857)
+    const remote = await spawnRemote(conn, shell, shellArgs, {
+      cwd,
+      shell: false,
+      usePty: true,
+      env,
+      addBigDigToPath: true,
+    });
+    connectProxyToRemoteProcess(proxy, remote);
   });
-  await proxy.ready;
 
-  connectProxyToRemoteProcess(proxy, remote);
-
-  proxy.terminal.show(false);
-  return proxy.terminal;
+  exec.terminal.show(false /* take UI focus */);
+  return exec.terminal;
 }
 
 function connectProxyToRemoteProcess(
