fe5542102b91f0bc15f0f62f07d2dd72f088475b
Wanderley da Silva <wander@fb.com>
2018-08-23 15:35:09 -0700

    Create script to start FS service
    Summary: This diff creates a simple script to start FS service.  It will be used in a next diff which changes the Thrift Server Manager to create server by config.
    
    Reviewed By: hansonw
    
    Differential Revision: D8985324
    
    fbshipit-source-id: 9c870b88392a126baa921c36c27800229dfd5f2f

---
+++ modules/big-dig/src/services/fs/launchServer.js
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @flow strict-local
+ * @format
+ */
+
+import log4js from 'log4js';
+import nuclideUri from 'nuclide-commons/nuclideUri';
+import os from 'os';
+import {RemoteFileSystemServer} from './fsServer';
+
+async function main() {
+  if (process.argv.length !== 3) {
+    // eslint-disable-next-line no-console
+    console.error('Error: launchServer expected port as parameter.');
+    process.exit(1);
+  }
+
+  const port = parseInt(process.argv[2], 10);
+  log4js.getLogger().info('Initializing server on port', port);
+  const server = new RemoteFileSystemServer(port);
+  await server.initialize();
+  log4js.getLogger().info('Server listening on port', port);
+}
+
+log4js.configure({
+  appenders: [
+    {
+      type: 'file',
+      filename: nuclideUri.join(os.tmpdir(), 'big-dig-service-fs.log'),
+    },
+    {
+      type: 'stderr',
+    },
+  ],
+});
+
+process.on('unhandledRejection', error => {
+  log4js.getLogger().error('Unhandled rejection:', error);
+});
+
+process.on('uncaughtException', error => {
+  log4js.getLogger().fatal('Uncaught exception:', error);
+  log4js.shutdown(() => process.abort());
+});
+
+main();
