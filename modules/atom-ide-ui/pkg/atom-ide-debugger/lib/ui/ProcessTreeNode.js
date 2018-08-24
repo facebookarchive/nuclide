a7ecab2b8738751878a9230e7dde443a05be0fc0
Eric Bluestein <ericblue@fb.com>
2018-08-24 11:02:55 -0700

    Debugger process tree: Show warning if the focused thread is hidden by a filter
    Summary: If the user has a thread filter that hides the currently focused thread, this creates a confusing experience because if we stop at a breakpoint, the callstack is not visible. Add a warning to the UI to remind the user that he or she has filtered the visible threads, and their filter is excluding the currently focused thread.
    
    Reviewed By: velocityboy
    
    Differential Revision: D9465570
    
    fbshipit-source-id: 3968e189a032aa6121534f0660f3fa340bbc6a8b

--- modules/atom-ide-ui/pkg/atom-ide-debugger/lib/ui/ProcessTreeNode.js
+++ modules/atom-ide-ui/pkg/atom-ide-debugger/lib/ui/ProcessTreeNode.js
@@ -25,6 +25,7 @@ import {
   LoadingSpinnerSizes,
   LoadingSpinner,
 } from 'nuclide-commons-ui/LoadingSpinner';
+import {Icon} from 'nuclide-commons-ui/Icon';
 
 type Props = {
   process: IProcess,
@@ -209,6 +210,16 @@ export default class ProcessTreeNode extends React.Component<Props, State> {
     );
 
     const filteredThreads = threads.filter(t => this.filterThread(t));
+    const focusedThread = service.viewModel.focusedThread;
+    const selectedThreadFiltered =
+      threads.some(t => t === focusedThread) &&
+      !filteredThreads.some(t => t === focusedThread);
+    const focusedThreadHiddenWarning = (
+      <span className="debugger-thread-no-match-text">
+        <Icon icon="nuclicon-warning" />
+        The focused thread is hidden by your thread filter!
+      </span>
+    );
     return threads.length === 0 ? (
       <TreeItem>{formattedTitle}</TreeItem>
     ) : (
@@ -217,18 +228,24 @@ export default class ProcessTreeNode extends React.Component<Props, State> {
         collapsed={isCollapsed}
         onSelect={this.handleSelect}>
         {filteredThreads.length === 0 && threads.length > 0 ? (
-          <span className="debugger-thread-no-match-text">
-            No threads match the current filter.
-          </span>
+          selectedThreadFiltered ? (
+            focusedThreadHiddenWarning
+          ) : (
+            <span className="debugger-thread-no-match-text">
+              No threads match the current filter.
+            </span>
+          )
         ) : (
-          filteredThreads.map((thread, threadIndex) => (
-            <ThreadTreeNode
-              key={threadIndex}
-              thread={thread}
-              service={service}
-              threadTitle={this._threadTitle(thread)}
-            />
-          ))
+          filteredThreads
+            .map((thread, threadIndex) => (
+              <ThreadTreeNode
+                key={threadIndex}
+                thread={thread}
+                service={service}
+                threadTitle={this._threadTitle(thread)}
+              />
+            ))
+            .concat(selectedThreadFiltered ? focusedThreadHiddenWarning : null)
         )}
       </NestedTreeItem>
     );
