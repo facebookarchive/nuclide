110c83de5f88b8f49d50e0c740f8c0db3ee4c58a
Jerry Hamlet <jhamlet@fb.com>
2018-10-05 10:27:49 -0700

    Add Loading Spinner To Image Editor
    Summary: Add a temporary loading spinner while the image file is being loaded, and then replace it with the correct editor node.
    
    Reviewed By: matthewwithanm
    
    Differential Revision: D10207002
    
    fbshipit-source-id: 8d307597dcbab3763975cbc52746fe7195615915

--- pkg/nuclide-image-view/lib/ImageEditorView.js
+++ pkg/nuclide-image-view/lib/ImageEditorView.js
@@ -14,6 +14,7 @@ import type ImageEditor from './ImageEditor';
 import fs from 'fs';
 import {Message} from 'nuclide-commons-ui/Message';
 import {renderReactRoot} from 'nuclide-commons-ui/renderReactRoot';
+import {LoadingSpinner} from 'nuclide-commons-ui/LoadingSpinner';
 import UniversalDisposable from 'nuclide-commons/UniversalDisposable';
 import AtomImageEditorView from '../VendorLib/image-view/lib/image-editor-view';
 import nullthrows from 'nullthrows';
@@ -31,37 +32,48 @@ export default class ImageEditorView {
   constructor(editor: ImageEditor) {
     this.element = document.createElement('div');
     this.element.className = 'nuclide-image-view-wrapper';
+
+    // Add a temporary element to display a spinner until it is replaced
+    const spinner = renderReactRoot(
+      <div className="nuclide-image-view-loading-spinner">
+        <LoadingSpinner />
+      </div>,
+    );
+    this.element.appendChild(spinner);
+
     this._disposables = new UniversalDisposable(
       // We need to defer loading the real view until the local file is ready because it assumes it
       // exists.
       editor.whenReady(() => {
+        let viewElement;
         // In some weird cases (e.g. Dash cached a deleted file path?), we might have tried to open
         // a nonexistent file. In that case, just show an error. It's important that we don't create
         // an AtomImageEditorView because that will try to stat the nonexistent file and error.
         if (!fs.existsSync(nullthrows(editor.getLocalPath()))) {
-          const message = renderReactRoot(
+          viewElement = renderReactRoot(
             <Message type="error">Image doesn't exist</Message>,
           );
-          message.style.flexDirection = 'column';
-          this.element.appendChild(message);
-          return;
+          viewElement.style.flexDirection = 'column';
+        } else {
+          // AtomImageEditorView tries to do a stat using the result of `getPath()` so we give it a
+          // proxy that always returns the local path instead of the real editor. (We don't want to
+          // change the editor's `getPath()` because other things use that for display purposes and we
+          // want to show the remote path.)
+          const proxy = new Proxy(editor, {
+            get(obj, prop) {
+              if (prop === 'getPath') {
+                return editor.getLocalPath;
+              }
+              // $FlowIgnore
+              return obj[prop];
+            },
+          });
+
+          this._realView = new AtomImageEditorView(proxy);
+          viewElement = this._realView.element;
         }
 
-        // AtomImageEditorView tries to do a stat using the result of `getPath()` so we give it a
-        // proxy that always returns the local path instead of the real editor. (We don't want to
-        // change the editor's `getPath()` because other things use that for display purposes and we
-        // want to show the remote path.)
-        const proxy = new Proxy(editor, {
-          get(obj, prop) {
-            if (prop === 'getPath') {
-              return editor.getLocalPath;
-            }
-            // $FlowIgnore
-            return obj[prop];
-          },
-        });
-        this._realView = new AtomImageEditorView(proxy);
-        this.element.appendChild(this._realView.element);
+        this.element.replaceChild(viewElement, spinner);
       }),
       () => {
         if (this._realView != null) {
