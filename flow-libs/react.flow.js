d254dae45834f77880d9aa355154c85caae6c296
Will Binns-Smith <wbinnssmith@fb.com>
2018-10-17 15:18:39 -0700

    Add `react.js.flow` for extra global React typings
    Summary: This creates a file dedicated to extra flow types for React. Rather than duplicate `{current: null | HTMLElement}`, centralize this definition of what a React ref looks like. This way, if this shape changes in the future, we can update it in a single place.
    
    Reviewed By: matthewwithanm
    
    Differential Revision: D10417336
    
    fbshipit-source-id: cd20adfbe598c8510824ac33835741f2ca92b4ce

---
+++ flow-libs/react.flow.js
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ *
+ * @flow strict-local
+ * @format
+ */
+
+/* eslint-disable no-undef */
+
+// This is the type for the result of calling React.createRef where the ref is a
+// plain DOM element, like `input`, or `div`
+//
+// Usage:
+//
+//   _inputRef: ReactHTMLElementRef<HTMLInputElement> = React.createRef();
+declare type ReactHTMLElementRef<TElement: HTMLElement> = {
+  current: null | TElement,
+};
