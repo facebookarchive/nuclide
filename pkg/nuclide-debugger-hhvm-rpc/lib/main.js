cd998c89780934bb4551c44ff413cbc3265c4e7a
Eric Bluestein <ericblue@fb.com>
2018-11-07 10:00:18 -0800

    Nuclide HHVM Debugger: Always check sitevar for domain socket path
    Summary: Fixing a bug where we only checked the sitevar (and therefore only loaded the domain socket path) if no debugger TCP port was specified. We should always use the sitevar if available.
    
    Reviewed By: Goom11
    
    Differential Revision: D12951358
    
    fbshipit-source-id: a4f09ac8cd6d8c783b7c4f15ff06639a4f3b6a60

--- pkg/nuclide-debugger-hhvm-rpc/lib/main.js
+++ pkg/nuclide-debugger-hhvm-rpc/lib/main.js
@@ -208,25 +208,23 @@ async function _getAttachArgs(config: HHVMAttachConfig): Promise<Object> {
 
   let debugPort = config.debugPort;
   let domainSocketPath = null;
-  if (debugPort == null) {
-    try {
-      // $FlowFB
-      const fetch = require('fb-interngraph/sitevar').fetchSitevarOnce;
-      const siteVar = await fetch('NUCLIDE_VSP_DEBUGGER_CONFIG');
-      if (siteVar != null) {
-        if (siteVar.hhvm_attach_port != null) {
-          debugPort = siteVar.hhvm_attach_port;
-        }
-
-        if (siteVar.hhvm_domain_socket_path != null) {
-          domainSocketPath = siteVar.hhvm_domain_socket_path;
-        }
+  try {
+    // $FlowFB
+    const fetch = require('fb-interngraph/sitevar').fetchSitevarOnce;
+    const siteVar = await fetch('NUCLIDE_VSP_DEBUGGER_CONFIG');
+    if (siteVar != null) {
+      if (siteVar.hhvm_attach_port != null) {
+        debugPort = siteVar.hhvm_attach_port;
       }
-    } catch (e) {}
 
-    if (debugPort != null) {
-      config.debugPort = debugPort;
+      if (siteVar.hhvm_domain_socket_path != null) {
+        domainSocketPath = siteVar.hhvm_domain_socket_path;
+      }
     }
+  } catch (e) {}
+
+  if (debugPort != null) {
+    config.debugPort = debugPort;
   }
 
   const warnOnInterceptedFunctions = await passesGK(
