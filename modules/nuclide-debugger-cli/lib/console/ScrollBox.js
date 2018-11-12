ec2358feca794df2c9c7bcaaa3f4de452ab0cad1
Jim Geist <jimge@fb.com>
2018-11-12 10:55:27 -0800

    wrap lines
    Summary: Currently fbdbg does not wrap lines; very long output will be horizontally truncated. This diff wraps lines based on the window width.
    
    Reviewed By: ebluestein
    
    Differential Revision: D12981957
    
    fbshipit-source-id: c1d4e57629cd5222232a8e2badf039f8ca129246

--- modules/nuclide-debugger-cli/lib/console/ScrollBox.js
+++ modules/nuclide-debugger-cli/lib/console/ScrollBox.js
@@ -20,11 +20,14 @@ type ScrollBoxOptions = {
 };
 
 export default class ScrollBox extends blessed.widget.Box {
-  _maxScrollBack: number;
-  _boxTop: number;
-  _boxBottom: boolean;
-  _lines: Array<string>;
-  _more: boolean;
+  _maxScrollBack: number; // max number of non-split console lines we'll keep
+  _boxTop: number; // index of line at top of control, in _splitLines
+  _boxBottom: boolean; // true if we're at the bottom, tailing the input
+  _lines: Array<string>; // unsplit lines as they are written into the control
+  _splitLines: Array<string>; // lines, split to fit window width
+  _splitToLines: Array<number>; // map: split line back to owning unsplit line
+  _linesToSplit: Array<number>; // map: unsplit line to first split line
+  _more: boolean; // true if there's more input at the bottom that user hasn't seen
   _nextOutputSameLine: boolean; // true if the next output should be on the same line (no ending \n)
 
   constructor(options: ScrollBoxOptions) {
@@ -36,6 +39,9 @@ export default class ScrollBox extends blessed.widget.Box {
     this._boxTop = 0;
     this._boxBottom = true;
     this._lines = [];
+    this._splitLines = [];
+    this._splitToLines = [];
+    this._linesToSplit = [];
     this._more = false;
     this._nextOutputSameLine = false;
   }
@@ -52,19 +58,22 @@ export default class ScrollBox extends blessed.widget.Box {
     return this._more;
   }
 
+  // it's simpler to preset the user with screen line #'s than
+  // unsplit line #'s.
   topLine(): number {
     return this._boxTop;
   }
 
   lastLine(): number {
-    return Math.min(this._boxTop + this.height, this._lines.length);
+    return Math.min(this._boxTop + this.height, this._splitLines.length);
   }
 
   lines(): number {
-    return this._lines.length;
+    return this._splitLines.length;
   }
 
   resize(): void {
+    this._rebuildSplitLines();
     this._recomputeContents();
   }
 
@@ -80,12 +89,68 @@ export default class ScrollBox extends blessed.widget.Box {
       return;
     }
 
+    const maxWidth = this.width - 1;
+
+    const addLine = (newLastLine: string): void => {
+      const lastLineIdx = this._lines.length;
+      this._lines.push(newLastLine);
+      this._linesToSplit.push(this._splitLines.length);
+
+      // don't lose blank lines
+      if (newLastLine === '') {
+        this._splitLines.push(newLastLine);
+        this._splitToLines.push(lastLineIdx);
+        return;
+      }
+
+      // eslint-disable-next-line space-in-parens
+      for (let start = 0; newLastLine.length > start; ) {
+        const toUse = Math.min(maxWidth, newLastLine.length - start);
+        this._splitLines.push(newLastLine.substr(start, toUse));
+        this._splitToLines.push(lastLineIdx);
+        start += toUse;
+      }
+    };
+
     if (this._nextOutputSameLine && this._lines.length !== 0) {
-      this._lines[this._lines.length - 1] += lines[0];
+      const lastLineIdx = this._lines.length - 1;
+      const lastSplitIdx = this._linesToSplit[lastLineIdx];
+      const newLastLine = this._lines[lastLineIdx] + lines[0];
+
+      // remove current last line
+      this._lines.pop();
+      this._linesToSplit.pop();
+      this._splitLines = this._splitLines.slice(0, lastSplitIdx);
+      this._splitToLines = this._splitToLines.slice(0, lastSplitIdx);
+
+      addLine(newLastLine);
+
       lines.shift();
     }
 
-    this._lines = this._lines.concat(lines).slice(-this._maxScrollBack);
+    lines.forEach(line => addLine(line));
+
+    if (this.lines.length > this._maxScrollBack) {
+      const linesToDelete = this.lines.length - this._maxScrollBack;
+      const splitsToDelete = this._linesToSplit[linesToDelete];
+
+      this._lines = this._lines.slice(linesToDelete);
+      this._linesToSplit = this._linesToSplit.slice(linesToDelete);
+      this._splitLines = this._splitLines.slice(splitsToDelete);
+      this._splitToLines = this._splitToLines.splice(splitsToDelete);
+
+      // Fix up the indices to account for the deleted lines
+      this._linesToSplit = this._linesToSplit.map(
+        i => i - this._linesToSplit[0],
+      );
+      this._splitToLines = this._splitToLines.map(
+        i => i - this._splitToLines[0],
+      );
+
+      // if we're not at the bottom, adjust the top
+      this._boxTop = Math.max(0, this._boxTop - linesToDelete);
+    }
+
     this._nextOutputSameLine = !trailingNewline;
 
     if (!this._boxBottom) {
@@ -103,7 +168,7 @@ export default class ScrollBox extends blessed.widget.Box {
 
   pageDown(): void {
     this._boxTop = Math.min(
-      this._lines.length - this.height,
+      this._splitLines.length - this.height,
       this._boxTop + this.height - 1,
     );
     this._updateScrollFlags();
@@ -117,13 +182,13 @@ export default class ScrollBox extends blessed.widget.Box {
   }
 
   scrollToBottom(): void {
-    this._boxTop = Math.max(0, this._lines.length - this.height);
+    this._boxTop = Math.max(0, this._splitLines.length - this.height);
     this._updateScrollFlags();
     this._recomputeContents();
   }
 
   _updateScrollFlags(): void {
-    this._boxBottom = this._lines.length - this._boxTop <= this.height;
+    this._boxBottom = this._splitLines.length - this._boxTop <= this.height;
     if (this._boxBottom) {
       this._more = false;
     }
@@ -132,11 +197,48 @@ export default class ScrollBox extends blessed.widget.Box {
   _recomputeContents() {
     // if we're pinned to the bottom, recompute the top
     if (this._boxBottom) {
-      this._boxTop = Math.max(0, this._lines.length - this.height);
+      this._boxTop = Math.max(0, this._splitLines.length - this.height);
     }
 
     this.setContent(
-      this._lines.slice(this._boxTop, this._boxTop + this.height).join('\n'),
+      this._splitLines
+        .slice(this._boxTop, this._boxTop + this.height)
+        .join('\n'),
     );
+  }
+
+  _rebuildSplitLines(): void {
+    const top = this._splitToLines[this._boxTop];
+
+    this._splitLines = [];
+    this._splitToLines = [];
+    this._linesToSplit = [];
+
+    const maxWidth = this.width - 1;
+
+    this._lines.forEach((line, idx) => {
+      this._linesToSplit.push(this._splitLines.length);
+
+      // don't lose blank lines
+      if (line === '') {
+        this._splitLines.push(line);
+        this._splitToLines.push(idx);
+        return;
+      }
+
+      // eslint-disable-next-line space-in-parens
+      for (let start = 0; line.length > start; ) {
+        const toUse = Math.min(maxWidth, line.length - start);
+        this._splitLines.push(line.substr(start, toUse));
+        this._splitToLines.push(idx);
+        start += toUse;
+      }
+    });
+
+    // if we're tailing the bottom, recomputeContents will fix the top up.
+    // otherwise, try to stay on about the same line.
+    if (!this.atBottom()) {
+      this._boxTop = this._linesToSplit[top];
+    }
   }
 }
