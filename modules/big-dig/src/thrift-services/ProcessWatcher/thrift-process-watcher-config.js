b797e41040166e5313c4e954a3ee196b3870a67c
Chad Smith <cssmith@fb.com>
2018-11-14 09:52:22 -0800

    add service configuration definition
    Summary: add service configuration definition
    
    Reviewed By: kcaze
    
    Differential Revision: D13032246
    
    fbshipit-source-id: 76b37b4b7108d37a3c52333b7fb0b926fa5e058d

---
+++ modules/big-dig/src/thrift-services/ProcessWatcher/thrift-process-watcher-config.js
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @flow strict-local
+ * @format
+ */
+
+import type {ThriftServiceConfig} from '../../services/thrift/types';
+
+// $FlowIgnore
+import ThriftProcessWatcherService from './gen-nodejs/ThriftProcessWatcherService';
+
+export const PROCESS_WATCHER_SERVICE_CONFIG: ThriftServiceConfig = {
+  name: 'thrift-process-watcher',
+  remoteUri: '',
+  remoteCommand: 'node',
+  remoteCommandArgs: [
+    '{BIG_DIG_SERVICES_PATH}/src/thrift-services/ProcessWater/launchThriftProcessWatcherServer-entry.js',
+    '{IPC_PATH}',
+  ],
+  remoteConnection: {type: 'ipcSocket', path: ''},
+  thriftTransport: 'buffered',
+  thriftProtocol: 'binary',
+  thriftService: ThriftProcessWatcherService,
+  killOldThriftServerProcess: true,
+};
