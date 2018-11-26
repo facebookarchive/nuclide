5debdaf3446436adbfc3d668065c0af0e8ea2a8b
Aman Agarwal <goom@fb.com>
2018-11-26 15:23:57 -0800

    Sync Python Debugger: Point Old Location to New Location
    Summary: Currently all over FB codebase are snippets that point to the old version of ptvsd. We do not want those snippets to fail so this diff forwards this old location to the new location
    
    Reviewed By: velocityboy
    
    Differential Revision: D13054942
    
    fbshipit-source-id: d7f40f45f8539b1a8009484805ae8200f92b46ed

--- modules/atom-ide-debugger-python/VendorLib/vs-py-debugger/pythonFiles/PythonTools/ptvsd/__init__.py
+++ modules/atom-ide-debugger-python/VendorLib/vs-py-debugger/pythonFiles/PythonTools/ptvsd/__init__.py
@@ -14,9 +14,41 @@
 # See the Apache Version 2.0 License for specific language governing
 # permissions and limitations under the License.
 
-__author__ = "Microsoft Corporation <ptvshelp@microsoft.com>"
-__version__ = "3.0.0.0"
+__all__ = [
+    '__version__', '__author__',
+    'enable_attach', 'wait_for_attach', 'break_into_debugger', 'is_attached',
+]
+__all__ += ['enable_attach_ui', 'set_attach_ui_options', 'set_trace']
 
-__all__ = ['enable_attach', 'wait_for_attach', 'break_into_debugger', 'settrace', 'is_attached', 'AttachAlreadyEnabledError']
+import sys
+import os
+import warnings
 
-from ptvsd.attach_server import enable_attach, wait_for_attach, break_into_debugger, settrace, is_attached, AttachAlreadyEnabledError
+ptvsd_real_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'experimental', 'ptvsd'))
+sys.path.append(ptvsd_real_path)
+ptvsd_false_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.remove(ptvsd_false_path + '/')
+
+try:
+    dict_contains = dict.has_key
+except:
+    try:
+        #Py3k does not have has_key anymore, and older versions don't have __contains__
+        dict_contains = dict.__contains__
+    except:
+        try:
+            dict_contains = dict.has_key
+        except NameError:
+            def dict_contains(d, key):
+                return d.has_key(key)
+
+
+if dict_contains(sys.modules, 'ptvsd'):
+    sys.modules['$ptvsd'] = sys.modules['ptvsd']
+    __name__ = '$ptvsd'
+    del sys.modules['ptvsd']
+
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+    from ptvsd import __version__, __author__
+    from ptvsd.attach_server import enable_attach, wait_for_attach, break_into_debugger, is_attached, enable_attach_ui, set_attach_ui_options, set_trace
