ef4ad7e97336b5418aeac763562587d6d69db211
Evan Krause <evangrayk@fb.com>
2018-12-05 13:35:20 -0800

    pull operation
    Summary: Add basic hg pull operation. This doesn't support arc pull yet (because we'd need to support operations for arc...). It also doesn't show the full screen loading spinner (since we don't dispatch extra actions except to refresh). I think the fullscreen spinner makes sense for pull, so I'll probably add that in some way
    
    Reviewed By: tjfryan
    
    Differential Revision: D13248748
    
    fbshipit-source-id: c179df593135838e37b6675e4e562847bd0fb296

---
+++ pkg/nuclide-hg-repository-client/lib/operations/PullOperation.js
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ *
+ * @flow strict-local
+ * @format
+ */
+
+import type {HgOperation, TreePreviewApplierFunction} from '../HgOperation';
+import type {RevisionTree} from '../revisionTree/RevisionTree';
+
+import {Observable} from 'rxjs';
+
+export class HgPullOperation implements HgOperation {
+  name = 'pull';
+
+  getArgs() {
+    return ['pull'];
+  }
+
+  getEquivalentCommand() {
+    return this.getArgs().join(' ');
+  }
+
+  getCommandDocumentation() {
+    return null;
+  }
+
+  makeOptimisticStateApplier(
+    treeObservable: Observable<Array<RevisionTree>>,
+  ): Observable<?TreePreviewApplierFunction> {
+    // We don't want to wait for an updated revisionlist because pull won't always trigger one.
+    // Instead, consider this command complete as soon as it finishes running
+    return Observable.empty();
+  }
+}
